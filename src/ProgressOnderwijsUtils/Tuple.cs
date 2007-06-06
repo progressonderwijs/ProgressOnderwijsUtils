dcefb8f07d1b71881b8c9994ba19298a984c63d2
Eamon Nerbonne <eamon@nerbonne.org>
2007-06-06 16:41:35 +0000

    - in progress.tools een tuple toegevoegd om meerdere waardes te kunnnen retourneren.  Is mischien niet de mooiste optie, maar even spelen...  - in progress.data, de pasfoto retrieval aangepast zodat ie niet langer in progressbulk kijkt maar in de progress.net DB kijkt, een ook de mimetype retourneerd (met behulp van de tuple...)  - de "geen plaatje" logica uit de datalaag naar de weblaag verplaatst, en het plaatje ook daar opgeslagen.  - lijkt zowaar te werken  - TODO: betere resizing en cropping, code schoonmaak

---
+++ src/ProgressOnderwijsUtils/Tuple.cs
@@ -0,0 +1,176 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgressOnderwijsUtils
+{
+	public struct Tuple<A, B>
+	{
+		public A a;
+		public B b;
+		public Tuple(A a, B b)
+		{
+			this.a = a;
+			this.b = b;
+		}
+	}
+
+	public struct Tuple<A, B, C>
+	{
+		public A a;
+		public B b;
+		public C c;
+		public Tuple(A a, B b, C c)
+		{
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+	}
+	
+	public struct Tuple<A, B, C, D>
+	{
+		public A a;
+		public B b;
+		public C c;
+		public D d;
+		public Tuple(A a, B b, C c, D d)
+		{
+			this.a = a;
+			this.b = b;
+			this.c = c;
+			this.d = d;
+		}
+	}
+
+	public struct Tuple<A, B, C, D, E>
+	{
+		public A a;
+		public B b;
+		public C c;
+		public D d;
+		public E e;
+		public Tuple(A a, B b, C c, D d, E e)
+		{
+			this.a = a;
+			this.b = b;
+			this.c = c;
+			this.d = d;
+			this.e = e;
+		}
+	}
+	public struct Tuple<A, B, C, D, E, F>
+	{
+		public A a;
+		public B b;
+		public C c;
+		public D d;
+		public E e;
+		public F f;
+		public Tuple(A a, B b, C c, D d, E e, F f)
+		{
+			this.a = a;
+			this.b = b;
+			this.c = c;
+			this.d = d;
+			this.e = e;
+			this.f = f;
+		}
+	}
+
+	public struct Tuple<A, B, C, D, E, F, G>
+	{
+		public A a;
+		public B b;
+		public C c;
+		public D d;
+		public E e;
+		public F f;
+		public G g;
+		public Tuple(A a, B b, C c, D d, E e, F f, G g)
+		{
+			this.a = a;
+			this.b = b;
+			this.c = c;
+			this.d = d;
+			this.e = e;
+			this.f = f;
+			this.g = g;
+		}
+	}
+
+	public struct Tuple<A, B, C, D, E, F, G, H>
+	{
+		public A a;
+		public B b;
+		public C c;
+		public D d;
+		public E e;
+		public F f;
+		public G g;
+		public H h;
+		public Tuple(A a, B b, C c, D d, E e, F f, G g, H h)
+		{
+			this.a = a;
+			this.b = b;
+			this.c = c;
+			this.d = d;
+			this.e = e;
+			this.f = f;
+			this.g = g;
+			this.h = h;
+		}
+	}
+
+	
+	public struct Tuple<A, B, C, D, E, F, G, H, I>
+	{
+		public A a;
+		public B b;
+		public C c;
+		public D d;
+		public E e;
+		public F f;
+		public G g;
+		public H h;
+		public I i;
+		public Tuple(A a, B b, C c, D d, E e, F f, G g, H h, I i)
+		{
+			this.a = a;
+			this.b = b;
+			this.c = c;
+			this.d = d;
+			this.e = e;
+			this.f = f;
+			this.g = g;
+			this.h = h;
+			this.i = i;
+		}
+	}
+
+	public struct Tuple<A, B, C, D, E, F,G,H,I,J>
+	{
+		public A a;
+		public B b;
+		public C c;
+		public D d;
+		public E e;
+		public F f;
+		public G g;
+		public H h;
+		public I i;
+		public J j;
+		public Tuple(A a, B b, C c, D d, E e, F f,G g,H h,I i,J j) {
+			this.a = a;
+			this.b = b;
+			this.c = c;
+			this.d = d;
+			this.e = e;
+			this.f = f;
+			this.g = g;
+			this.h = h;
+			this.i = i;
+			this.j = j;
+		}
+	}
+}
