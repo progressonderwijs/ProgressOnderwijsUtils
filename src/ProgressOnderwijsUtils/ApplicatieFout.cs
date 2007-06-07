09f2ef2a69b428d19962dd170df73bf38efd90e6
Niek van Ruler <n.j.van.ruler@rug.nl>
2007-06-07 13:25:19 +0000

    bugs opgelost bij het verwerken van vchmsg05 (onderhoudenpersoonsgegevens) en vchmsg07 (onderhoudvooropleidinggegevens) fouten bij het verwerken van studielinkberichten worden nu gelogd in de database

---
+++ src/ProgressOnderwijsUtils/ApplicatieFout.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgressOnderwijsUtils
+{
+	public class ApplicatieFout
+	{
+		bool opgelost;
+		DateTime datum;
+		string email;
+		string message;
+		string versie;
+		string beschrijving;
+		string stacktrace;
+		
+		public string Versie { get { return versie; } set { versie = value; }}
+		public string Message { get { return message; } set { message = value; }}
+		public string Stacktrace { get { return stacktrace; } set { stacktrace = value; }}
+
+	}
+}
