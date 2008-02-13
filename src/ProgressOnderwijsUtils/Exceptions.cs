3f5d812d67b86c6005af42bd638887a0a18e8971
Ronald Wijnsema <r.wijnsema@rug.nl>
2008-02-13 00:30:58 +0000

    Niet ernstige Foutmeldingen kunnen nu ook als statusinformatie worden getoond. Identificatie en overlijden netjes gemaakt. Zou straks een baseclass moeten hebben. Bugjes opgelost.

---
+++ src/ProgressOnderwijsUtils/Exceptions.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgressOnderwijsUtils
+{
+	public class NoRowsFoundException : ApplicationException { }
+	public class GeenRechtException : ApplicationException { }
+}
