7f0e2bc547814a94a9f65edf11622fa8b1bc4fa1
Eamon Nerbonne <eamon@nerbonne.org>
2008-05-09 12:37:00 +0000

    Kleine verbetering exception handling
    Bugzid: 8

--- src/ProgressOnderwijsUtils/Exceptions.cs
+++ src/ProgressOnderwijsUtils/Exceptions.cs
@@ -6,4 +6,10 @@ namespace ProgressOnderwijsUtils
 {
 	public class NoRowsFoundException : ApplicationException { }
 	public class GeenRechtException : ApplicationException { }
+	public class QueryException : ApplicationException
+	{
+		public QueryException(string msg) : base(msg) { }
+		public QueryException() { }
+		public QueryException(string msg, Exception inner) : base(msg, inner) { }
+	}
 }
