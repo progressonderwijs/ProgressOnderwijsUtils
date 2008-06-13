b0cd1abacd646790c697b94f7022a25ceb6a1c30
Niek van Ruler <n.j.van.ruler@rug.nl>
2008-06-13 14:56:56 +0000

    stdole verwijderd uit web.config

--- src/ProgressOnderwijsUtils/Exceptions.cs
+++ src/ProgressOnderwijsUtils/Exceptions.cs
@@ -5,7 +5,10 @@ using System.Text;
 namespace ProgressOnderwijsUtils
 {
 	public class NoRowsFoundException : ApplicationException { }
-	public class GeenRechtException : ApplicationException { }
+	public class GeenRechtException : ApplicationException 
+	{
+		public GeenRechtException(string msg) : base(msg) { }
+	}
 	public class QueryException : ApplicationException
 	{
 		public QueryException(string msg) : base(msg) { }
