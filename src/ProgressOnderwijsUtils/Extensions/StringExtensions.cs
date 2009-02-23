2f7ae24e612b21a7f355bf74e87445d8043a01e8
Anne van der Ploeg <a.van.der.ploeg@rug.nl>
2009-02-23 16:51:15 +0000

    * string uitgebreid met extensions IsNullOrEmpty (spreekt voor zich) en GetValueOrEmptyString (retourneert de string of "" als de string null is). * Generic ReadScalar toegevoegd aan Data.Base.ReadConnectie en Data.Base.Helper. De specifieke ReadScalar methodes (ReadScalarInt, ReadScalarDateTime etc.) zijn in principe niet meer nodig, omdat de TryParse ook in de generieke versie gebeurt. * Verdere refactoring op basis van bovenstaande aanpassingen

---
+++ src/ProgressOnderwijsUtils/Extensions/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgressOnderwijsUtils
+{
+	public static class StringExtensions
+	{
+		/// <summary>
+		/// Check either string is null or contains whitespace only)
+		/// </summary>
+		/// <param name="s">String to check</param>
+		/// <returns>True if string is empty. False otherwise.</returns>
+		public static bool IsNullOrEmpty(this string s)
+		{
+			return s == null || s.Trim().Length == 0;
+		}
+
+		/// <summary>
+		/// Return string or "" if string is null
+		/// </summary>
+		/// <param name="s">string</param>
+		/// <returns>String or "" if string is null</returns>
+		public static string GetValueOrEmptyString(this string s)
+		{
+			return s == null ? "" : s;
+		}
+	}
+}
