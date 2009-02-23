fa86b33d9258040b8fbef90d22b8d531aea686e1
Anne van der Ploeg <a.van.der.ploeg@rug.nl>
2009-02-23 12:42:15 +0000

    DataRow uitgebreid met generic extension Get() voor het veilig inlezen van attribuutwaarde (geen gedonder meer met DBNull en null) in plaats van: DateTime dt = row["datum"] != DBNull.Value ? (DateTime)row["datum"] : default(DateTime); volstaat: DateTime dt = row.Get<DateTime>("datum");
    Je kunt ook een default waarde opgeven die geretourneerd wordt als het attribuut DBNull of null is. Bijvoorbeeld:
    decimal collegegeld  = row.Get<decimal>("collegegeld", 1565);
    
    Zoals het nu is geimplementeerd, komt er geen exception als het attribuut niet (meer) bestaat. Dit maskeert in feite een foutsituatie, wat z'n voor- en nadelen heeft. Ik kan er natuurlijk ook een (NietZoErge)Exception van maken.
    
    Als je de extension wilt gebruiken, moet je de namespace Progress.Extensions in je using-list opnemen
    
    De financiele Info-classes gebruiken nu deze extensions.

---
+++ src/ProgressOnderwijsUtils/Extensions/DataRowExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Progress.Extensions
+{
+	/// <summary>
+	/// Extensions for DataRow
+	/// </summary>
+	public static class DataRowExtensions
+	{
+		/// <summary>
+		/// Returns content of an attribute or default type initialization (no need to check for DBNull or null)
+		/// </summary>
+		/// <typeparam name="T">Type</typeparam>
+		/// <param name="row">DataRow</param>
+		/// <param name="fieldname">Name of attribute</param>
+		/// <returns>Content of field or default type initialization</returns>
+		public static T Get<T>(this DataRow row, string fieldname)
+		{
+			try
+			{
+				return (row[fieldname] == DBNull.Value || (T)row[fieldname] == null) ? default(T) : (T)row[fieldname];
+			}
+			catch
+			{
+				return default(T);
+			}
+		}
+
+		/// <summary>
+		/// Returns content of an attribute or default value (no need to check for DBNull or null)
+		/// </summary>
+		/// <typeparam name="T">Type</typeparam>
+		/// <param name="row">DataRow</param>
+		/// <param name="fieldname">Name of attribute</param>
+		/// <param name="defaultvalue">Value to return in case attribute is DBNull or null</param>
+		/// <returns>Content of field or default value</returns>
+		public static T Get<T>(this DataRow row, string fieldname, T defaultvalue)
+		{
+			try
+			{
+				return (row[fieldname] == DBNull.Value || (T)row[fieldname] == null) ? defaultvalue : (T)row[fieldname];
+			}
+			catch
+			{
+				return defaultvalue;
+			}
+		}
+	}
+}
