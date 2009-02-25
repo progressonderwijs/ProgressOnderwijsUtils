2d244741bd86c20f85063870c8ebcf7a67e72f86
Anne van der Ploeg <a.van.der.ploeg@rug.nl>
2009-02-25 08:36:28 +0000

    SM terugmeldingen: * Presentatie terugmelding (oude en nieuwe waarde) * Alleen verwerkingstatus 3 en 5 worden als fout gemarkeerd

--- src/ProgressOnderwijsUtils/Extensions/StringExtensions.cs
+++ src/ProgressOnderwijsUtils/Extensions/StringExtensions.cs
@@ -18,13 +18,33 @@ namespace ProgressOnderwijsUtils
 		}
 
 		/// <summary>
-		/// Return string or "" if string is null
+		/// Return s or "" if string is null
 		/// </summary>
 		/// <param name="s">string</param>
-		/// <returns>String or "" if string is null</returns>
+		/// <returns>string</returns>
 		public static string GetValueOrEmptyString(this string s)
 		{
 			return s == null ? "" : s;
+		}
+
+		/// <summary>
+		/// Replace equal characters on left side of string with other characters
+		/// </summary>
+		/// <param name="s">string</param>
+		/// <param name="src">character to replace</param>
+		/// <param name="dest">character to replace with</param>
+		/// <returns>string</returns>
+		public static string ReplaceLeftChars(this string s, char src, char dest)
+		{
+			if (s != null)
+			{
+				int length = s.Length;
+				return s.TrimStart(src).PadLeft(length, dest);
+			}
+			else
+			{
+				return s;
+			}
 		}
 	}
 }
