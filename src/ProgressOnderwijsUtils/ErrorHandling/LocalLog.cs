7772a50a3cfc4925861c771be49a20826cbbab78
Eamon Nerbonne <eamon@nerbonne.org>
2009-03-11 14:23:25 +0000

    local HDD error logging moved to progress.Tools so it can be used by both WebFramework and Global.asax

---
+++ src/ProgressOnderwijsUtils/ErrorHandling/LocalLog.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Web;
+using System.Xml.Linq;
+
+namespace ProgressOnderwijsUtils.ErrorHandling
+{
+	public static class LocalLog
+	{
+#if DEBUG
+		const string logdirectory = @"C:\xmllog\";//even IIS may make root directories!
+		const string txtlog = "currentHttpApplication.log";
+
+		static readonly string txtlogPath = Path.Combine(logdirectory, txtlog);
+
+		static LocalLog()
+		{
+			System.IO.Directory.CreateDirectory(logdirectory);
+			try
+			{
+				using (var stream = File.Open(txtlogPath, FileMode.Create, FileAccess.Write))
+					stream.Flush();//bla, no-op.
+			}
+			catch { }
+		}
+#endif
+
+		public static void SaveXmlLog(XElement document, string filename) {
+#if DEBUG
+			document.Save(Path.Combine(logdirectory, Path.GetFileName(filename)));
+#endif
+		}
+
+
+
+		public static void DoLog(string msg)
+		{
+#if DEBUG
+			StringBuilder fullmsg = new StringBuilder();
+			fullmsg.Append(DateTime.Now);
+			fullmsg.Append(", ");
+			HttpContext c = HttpContext.Current;
+			if (c != null)
+			{
+				try
+				{
+					fullmsg.Append("app[" + c.ApplicationInstance.GetHashCode() + "] ");
+				}
+				catch
+				{
+					fullmsg.Append("<noApp> ");
+				}
+
+				fullmsg.Append("context[" + c.GetHashCode() + "] ");
+				if (c.Handler != null)
+					fullmsg.Append("handler[" + c.Handler.GetType().Name + "] ");
+
+				try
+				{
+					if (c.Request != null)
+						fullmsg.Append((c.Request.RawUrl ?? "<nullURL>") + " ");
+				}
+				catch
+				{
+					fullmsg.Append("<noRequest> ");
+				}
+			}
+			fullmsg.Append(msg ?? "<noMessage>");
+			fullmsg.AppendLine();
+			try
+			{
+				//don't use Debug.Write or Trace.Write because trace listener may be Finalized before the last message is pumped.
+				lock (txtlogPath)
+					File.AppendAllText(txtlogPath, fullmsg.ToString());
+			}
+			catch { }
+#endif
+		}
+	}
+}
