250d310979ee32fb2340158271cf2bf90bacca94
Niek van Ruler <n.j.van.ruler@rug.nl>
2009-04-01 08:59:21 +0000

    -Inschrijfschermen onder organisatie hebben nu studentnaam en nummer -Meer controles ingebouwd bij invoer van data in uitschrijfscherm

---
+++ src/ProgressOnderwijsUtils/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgressOnderwijsUtils
+{
+	public static class DateTimeExtensions
+	{
+
+		public static int CollegeJaar(this DateTime datetime)
+		{
+			int collegejaar = datetime.Year;
+			//Als startdatum januari t/m augustus dan is het collegejaar een jaar eerder
+			if (datetime.Month < 9)
+				collegejaar--;
+			return collegejaar;
+		}
+	}
+}
