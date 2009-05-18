3db0ae7599017493a8cc6527dbd2e5f66a38b805
Fons Dijkstra <f.j.dijkstra@rug.nl>
2009-05-18 08:25:46 +0000

    Implemented SM-01, SM-02 and SM-03 mutations on importing 'bekostigingsfoto' from the IBG. As this is new functionality I could implement it without changing the existing sm-mutation algorithms.
    I've also created unit-tests for the mentioned mutations:
    - import photo (from IBG-stub) and check whether the correct mutations are generated;
    - export mutations (to IBG-stub) and check whether the correct file is generated;
    - import 'terugmeldingen' (from IBG-stub) and check whether they have been imported correctly.
    
    While implementing these tests, I've upgraded to NUnit 2.5 and refactored our test framework accordingly.
    
    I've also added an extension method to the IDictionary<K,V> interface, allowing to get a value with a default.

---
+++ src/ProgressOnderwijsUtils/Extensions/CollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgressOnderwijsUtils.Extensions
+{
+	/// <summary>
+	/// Extension methods for generic collections.
+	/// </summary>
+	public static class CollectionExtensions
+	{
+		/// <summary>
+		/// Utility method to retrieve a value with a default from a dictionary.
+		/// </summary>
+		/// <typeparam name="TKey"></typeparam>
+		/// <typeparam name="TValue"></typeparam>
+		/// <param name="dict"></param>
+		/// <param name="key">The key whose value to get.</param>
+		/// <param name="defaultValue">The default value of the key.</param>
+		/// <returns>The value of the key, or the default if the dictionary does not contain the key.</returns>
+		public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue defaultValue)
+		{
+			TValue result;
+			if (!dict.TryGetValue(key, out result))
+			{
+				result = defaultValue;
+			}
+			return result;
+		}
+	}
+}
---
+++ test/ProgressOnderwijsUtilsTests/CollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ProgressOnderwijsUtils.Extensions;
+
+namespace ProgressOnderwijsUtilsTests
+{
+	[TestFixture]
+	public class CollectionExtensions
+	{
+		[Test]
+		public void IDictionary_GetValueOrDefault()
+		{
+			IDictionary<int, int> sut = new Dictionary<int, int>() { { 0, 0 } };
+			Assert.That(sut.GetValueOrDefault(0, 1), Is.EqualTo(0));
+			Assert.That(sut.GetValueOrDefault(1, 2), Is.EqualTo(2));
+		}
+	}
+}
