741449975f1cc61b0f8e91d5bffa6be72c75c831
Eamon Nerbonne <eamon@nerbonne.org>
2009-07-03 15:01:23 +0000

    - added sortedlistextension to compare sorted lists  - GenericEdit cancelled niet langer op elke pageview!  - GenericEdit cancelled nu mits is niet is gerenderd, en tevens cancelled GenericContainer mits de key is veranderd.  - Manager.GetModule<StudentMenu>().BusinessStudent soms vervangen door de equivalente en meer standaarde Manager.WebSession.Student  - Manager.StudentMenu verwijdert, werd nog maar in een plek gebruik en is precies hetzelfde als Manager.GetModule<StudentMenu>()  - Manager.StudentMain maakt nu ook gebruik van de standaard GetModule<>() methode.  - PersistanceHandler in aparte file voor duidelijkheid.  - IUnload interface: het webframework gaat nu alle webmodules die IUnload implementeren aanroepen na generate node.  Wordt in de FileUploadControl gebruikt en in Generic/GenericAction.cs gebruikt om te herkennen dat GenerateNode nooit is aangeroepen.  -

---
+++ src/ProgressOnderwijsUtils/Extensions/SortedListExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ProgressOnderwijsUtils
+{
+	public static class SortedListExtension
+	{
+		public static bool EqualsKeyValue(this SortedList a, SortedList b)
+		{
+			return a == b ||
+				(a != null && b != null
+					&& a.Values.Cast<object>().SequenceEqual(b.Values.Cast<object>())
+					&& a.Keys.Cast<object>().SequenceEqual(b.Keys.Cast<object>())
+				);
+		}
+	}
+}
