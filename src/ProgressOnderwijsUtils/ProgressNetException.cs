19ed5ed29fe69c934941229d20076becb1aa926f
Eamon Nerbonne <eamon@nerbonne.org>
2009-07-06 15:40:47 +0000

    ProgressNetException.cs aangemaakt; vervanger voor ApplicationException, mischien ooit handig om hier textdefs aan te koppelen om zo Exception<-->Userinterface brug te verkleinen

---
+++ src/ProgressOnderwijsUtils/ProgressNetException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace ProgressOnderwijsUtils
+{
+	public class ProgressNetException : Exception
+	{ //TODO: this exception type might provide for TextDef integration such that throwing code can indicate the text-based error to show users.
+		public ProgressNetException() : base() { }
+		public ProgressNetException(string debugMessage) : base(debugMessage) { }
+		public ProgressNetException(string debugMessage, Exception innerException) : base(debugMessage,innerException) { }
+		protected ProgressNetException(SerializationInfo info,StreamingContext context) : base(info,context) { }
+	}
+}
