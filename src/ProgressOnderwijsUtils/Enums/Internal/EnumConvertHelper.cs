ef89a6900fede31be2bee3ac179c7a51c3023099
Eamon Nerbonne <eamon@nerbonne.org>
2009-07-06 15:46:53 +0000

    Een aantal helpers om enum conversies wat schoner te krijgen.  Nog niet in gebruik, maar de bedoeling is om uiteindelijk
    enum { SomeState=42,SomeOtherState=0 }
    
    Te kunnen vervangen met iets als
    
    enum { [DBCode(42)]SomeState,[DBCode(0)]SomeOtherState}
    
    met als winst dat wijzigingen in de enum declaratie minder snel tot fouten lijden e.g.
    
    enum { [DBCode(DBNull.Value)]UninitializedState, [DBCode(42)]SomeState, [DBCode(0)]SomeOtherState}
    
    (zonder explicite attributen zou deze code twee 0 states maken, en zou tevens de default C# state 0 in de database de betekenis van SomeOtherState hebben).

---
+++ src/ProgressOnderwijsUtils/Enums/Internal/EnumConvertHelper.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace ProgressOnderwijsUtils
+{
+	internal interface IHasLabel<TLabel> { TLabel Label { get; } }
+	internal interface IConverter<TFrom, TTo> { TTo Convert(TFrom val);	}
+	internal interface IEnumLabeller<TEnum, TLabel>
+	{
+		TLabel LabelFromEnum(TEnum val);
+		TEnum EnumFromLabel(TLabel label);
+	}
+	internal class EnumConverterImplementation<TEnum, TLabel, TAttr, TAttrToLabelConverter>
+		: IEnumLabeller<TEnum, TLabel>
+		where TEnum : struct
+		where TAttr : Attribute
+		where TAttrToLabelConverter : IConverter<TAttr, TLabel>, new()
+	{
+		static EnumConverterImplementation<TEnum, TLabel, TAttr, TAttrToLabelConverter> converter = new EnumConverterImplementation<TEnum, TLabel, TAttr, TAttrToLabelConverter>();
+
+		internal static EnumConverterImplementation<TEnum, TLabel, TAttr, TAttrToLabelConverter> Converter { get { return converter; } }
+
+		Dictionary<TEnum, TLabel> labelLookup;
+		Dictionary<TLabel, TEnum> enumValueLookup;
+		EnumConverterImplementation()
+		{
+
+			Type enumType = typeof(TEnum);
+			Type attrType = typeof(TAttr);
+			TAttrToLabelConverter labelGenerator = new TAttrToLabelConverter();
+
+			if (!enumType.IsEnum)
+				throw new EnumLabellingException(string.Format("EnumConvertHelper used on a non-enum type {0}", enumType.FullName));
+
+			var enumValues =
+				from valFieldInfo in enumType.GetFields(BindingFlags.Public | BindingFlags.Static)
+				select new
+				{
+					EnumValue = (TEnum)valFieldInfo.GetValue(null),
+					Label =
+					 labelGenerator.Convert((TAttr)
+						valFieldInfo.GetCustomAttributes(attrType, true) //gets all attrs of the type attrType
+						.Single() //will throw InvalidOperationException if not precisely one attr returned
+						) //uses AttrToLabelConverter to read the label value from the attribute.
+				};
+			try
+			{
+				enumValues = enumValues.ToArray();//evaluate query.
+			}
+			catch (InvalidOperationException ioe)
+			{
+				throw new EnumLabellingException(string.Format("The enumeration {0} has values without a unique label attribute", enumType.FullName), ioe);
+			}
+
+			try
+			{
+				labelLookup = enumValues.ToDictionary(tuple => tuple.EnumValue, tuple => tuple.Label);//lookup: map enum values to labels 
+			}
+			catch (ArgumentException ae)//ToDictionary will throw argument exception if duplicate values or duplicate labels cause the dictionaries to have a key-collision.
+			{
+				throw new EnumLabellingException(string.Format("The enumeration {0} has several members with the same value", enumType.FullName), ae);
+			}
+
+			try
+			{
+				enumValueLookup = enumValues.ToDictionary(tuple => tuple.Label, tuple => tuple.EnumValue);//lookup: map labels to enum values
+			}
+			catch (ArgumentException ae)
+			{
+				throw new EnumLabellingException(string.Format("The enumeration {0} has several members with the same label", enumType.FullName), ae);
+			}
+		}
+
+		public TLabel LabelFromEnum(TEnum val) { return labelLookup[val]; }
+		public TEnum EnumFromLabel(TLabel label) { return enumValueLookup[label]; }
+	}
+
+	internal static class EnumConverterHelper<EnumT, LabelT, AttrT>
+		where EnumT : struct
+		where AttrT : Attribute, IHasLabel<LabelT>
+	{
+		internal class ConverterImpl : IConverter<AttrT, LabelT>
+		{
+			public LabelT Convert(AttrT val) { return val.Label; }
+		}
+
+		internal static EnumConverterImplementation<EnumT, LabelT, AttrT, ConverterImpl> Converter { get { return EnumConverterImplementation<EnumT, LabelT, AttrT, ConverterImpl>.Converter; } }
+	}
+
+}
