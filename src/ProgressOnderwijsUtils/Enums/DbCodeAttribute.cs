f46e6162e76503b97c9b9623c9b047405869d852
Eamon Nerbonne <eamon@nerbonne.org>
2009-07-10 15:03:12 +0000

    Reimplemented HTMLEditMode conversion using EnumConverter and attributes.

---
+++ src/ProgressOnderwijsUtils/Enums/DbCodeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProgressOnderwijsUtils.Enums.Support;
+
+namespace ProgressOnderwijsUtils.Enums
+{
+	public class DbCodeAttribute : Attribute, IHasLabel<int>
+	{
+		public DbCodeAttribute(int code) { Label = code; }
+		public int Label { get; private set; }
+	}
+}
---
+++ src/ProgressOnderwijsUtils/Enums/XsltCodeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProgressOnderwijsUtils.Enums.Support;
+
+namespace ProgressOnderwijsUtils.Enums
+{
+	public class XsltCodeAttribute : Attribute, IHasLabel<string>
+	{
+		public XsltCodeAttribute(string code) { Label = code; }
+		public string Label { get; private set; }
+	}
+}
