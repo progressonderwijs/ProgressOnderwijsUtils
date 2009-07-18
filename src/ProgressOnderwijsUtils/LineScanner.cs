a12db8437f8f3537c85b815838008d65cdd92660
Anne van der Ploeg <a.van.der.ploeg@rug.nl>
2009-07-18 12:45:26 +0000

    ING MT940 wordt nu ingelezen (verder gebeurt er nog niets mee). Er is *geen* relatie tussen de afschriftregel in het bestand en het papieren afschrift. Er is dus ook geen controle op het begin- en eindsaldo van de afschriften mogelijk. De ER-normalisatie van bankrekening -> bankrekeningafschrift -> bankrekeningafschriftregel kan feitelijk wel weer vervallen. Een iets uitgebreidere tabel bankrekeningafschriftregel volstaat dan ook wel.

---
+++ src/ProgressOnderwijsUtils/LineScanner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ProgressOnderwijsUtils
+{
+	public class LineScanner
+	{
+		private string[] lines;
+		int position = 0;
+
+		public LineScanner(Stream stream)
+		{
+			StreamReader streamreader = new StreamReader(stream);
+			lines = Regex.Split(streamreader.ReadToEnd(), "\r\n|\n");
+		}
+
+		public LineScanner(string s)
+		{
+			lines = Regex.Split(s, "\r\n|\n");
+		}
+
+		public string GetLine()
+		{
+			return position != lines.Length ? lines[position++] : null;
+		}
+
+		public void PushBack()
+		{
+			if (position != 0)
+				--position;
+		}
+
+		public bool Eof()
+		{
+			return position == lines.Length;
+		}
+	}
+}
