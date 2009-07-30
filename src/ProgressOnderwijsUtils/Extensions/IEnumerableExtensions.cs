0d19c164a24062ca566b6e3a14a3a56766cd04f0
Eamon Nerbonne <eamon@nerbonne.org>
2009-07-30 08:47:36 +0000

    Added IndexOf Linq helper.

---
+++ src/ProgressOnderwijsUtils/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgressOnderwijsUtils
+{
+	public static class IEnumerableExtensions
+	{
+		public static int IndexOf<T>(this IEnumerable<T> list, T elem)
+		{
+			if (list == null) throw new ArgumentNullException("list");
+			if (elem == null) throw new ArgumentNullException("elem");
+			int retval=0;
+			foreach (T item in list)
+			{
+				if (elem.Equals(item))
+					return retval;
+				retval++;
+			}
+			return -1;
+		}
+	}
+}
