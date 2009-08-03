aacadc4193c0244d8ee5fc0ede95423b0685ed84
Eamon Nerbonne <eamon@nerbonne.org>
2009-08-03 09:47:34 +0000

    - AutoCollapsing+pinning herschreven, nu geen javascript en veel minder code.  Nog niet helemaal perfect, verdere details wordt zo aan geschaafd.  - selectedIndex is niet nullable maar -1 bij unselected: dit komt makkelijker overeen met .NET ingebouwde "IndexOf" functies en dergelijke (kortere code).  - Oude Tree plaatjes uit projectfile  - txtarea terug in projectfile  - Converteer: nullables systematisch aanpakken, decimal parsing code verdubbelingen verwijdert, minder code.  - TypeExtension.cs: herken nullable types.  - experimenteele reflection based converters in webframework (zodat Tag's niet strings hoeven zijn.)  Minder flexible dan Converteer, maar conversie gaat wel vanzelf. (zie StudentLijst/GenericTree Collapsed property bijv.).

---
+++ src/ProgressOnderwijsUtils/Extensions/TypeExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgressOnderwijsUtils
+{
+	public static class TypeExtension
+	{
+		/// <summary>
+		/// If type is Nullable<T>, returns typeof(T).  For non-Nullable<> types, returns null;
+		/// </summary>
+		public static Type GetNullableBaseType(this Type type)
+		{
+			return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>)
+				? type.GetGenericArguments()[0] 
+				: null;
+		}
+	}
+}
