f58d2ad9225fe1b7664dda40376f697ec56178c0
Renzo Kooi <l.kooi@rug.nl>
2009-08-15 10:27:00 +0000

    Documentatie!

--- src/ProgressOnderwijsUtils/Utils.cs
+++ src/ProgressOnderwijsUtils/Utils.cs
@@ -41,11 +41,20 @@ namespace ProgressOnderwijsUtils
 			return res != 0 && res % 11 == 0;
 		}
 
-		/*
-		 * initialiseert opties voor een reguliere expressie
-		 * zie switch-case voor mogelijke opties.
-		 * RK20090815
-		 */
+		///<summary>
+		/// initialiseert opties voor een reguliere expressie
+		/// zie switch-case voor mogelijke opties.
+		/// </summary>
+		/// <example>
+		/// <c>
+		/// RegexOptions ro = ReOpts("i,m,c");
+		/// </c>
+		/// => makes a regex operation case insensitive,
+		///    multiline and culture invariant
+		/// </example>   
+		/// <canblame name="Renzo Kooi"/>
+		/// <datelast value="2009/08/15"/>
+		/// <returns>RegexOptions enumerator</returns>
 		public static RegexOptions ReOpts(string x)
 		{
 			RegexOptions opts = new RegexOptions();
@@ -70,22 +79,49 @@ namespace ProgressOnderwijsUtils
 			}
 			return opts;
 		}
-		/*
-		 * MultiReplace: vervang 1 of meer substrings in [initialstring]
-		 * door een andere string. Zoek en vervang is een array van strings
-		 * [searchreplace], waarin 1 of meer paren van 'n reguliere expressie 
-		 * (zoek) en een vervangstring zitten.
-		 * De 3e parameter zijn de opties, in de vorm van een string met 
-		 * optie-letters gescheiden door een komma, die via ReOpts (zie hierboven)
-		 * naar een RegexOptions type worden omgezet. Mag ook een lege string
-		 * zijn (geen opties).
-		 * RK20090815
-		 */
+
+		///<summary>
+		/// Vervang 1 of meer substrings in [initialstring]
+		/// door een andere string. Zoek en vervang is een array van strings
+		/// [searchreplace], waarin 1 of meer paren van 'n reguliere expressie 
+		/// of substring (zoek) en een vervangstring zitten.
+		/// De 3e parameter zijn de opties, in de vorm van een string met 
+		/// optie-letters gescheiden door een komma, die via ReOpts (zie 
+		/// Tools.Utils.ReOpts) naar een RegexOptions type worden omgezet. 
+		/// Mag ook een lege string zijn (geen opties).
+		/// </summary>
+		/// <param name="initialstr">de string waarin substring(s) vervangen moeten worden</param>
+		/// <param name="searchreplace">paren van zoek- (substring/regex) en vervangstring</param>
+		/// <param name="opts">opties voor vervanging</param>
+		/// <example>
+		/// <c>
+		///  string jantje = "Jantje zag eens pruimen";
+		///  jantje = 
+		///   Tools.Utils.MultiReplace(
+		///		jantje,
+		///		new string[] {@"pruimen","pruimen hangen",
+		///					  @"hangen","hangen.<br />O, als eieren!"},
+		///		"m");
+		///	 </c>	
+		///	 => de string [jantje] is na deze operatie:
+		///	 => "Jantje zag eens pruimen hangen.<br />O, als eieren!"
+		/// </example>
+		/// <seealso>Tool.Utils.ReOpts</seealso>
+		/// <canblame>Renzo Kooi</canblame>
+		/// <datelast value="2009/08/15"/>
+		/// <returns>gemodificeerde string</returns>
+		/// <TODO>tweede parameter zou efficiënter kunnen</TODO>
 		public static string MultiReplace(string initialstr, string[] searchreplace, string opts)
 		{
 			string retval = initialstr;
+
 			if (searchreplace.Length % 2 != 0)
 			{
+				/*
+				 * method werkt alleen als er 1 of meer
+				 * paren regex/vervangstring in de array zitten
+				 * anders wordt de inputstring weer teruggestuurd
+				*/
 				return initialstr;
 			}
 
@@ -95,7 +131,7 @@ namespace ProgressOnderwijsUtils
 
 			if (searchreplace.Length > 2)
 			{
-				for (int i = 0; i < searchreplace.Length; i += 2)
+				for (int i = 2; i < searchreplace.Length; i += 2)
 				{
 					string[] s_ = new string[2] { searchreplace[i], searchreplace[i + 1] };
 					retval = MultiReplace(retval,s_,opts);
