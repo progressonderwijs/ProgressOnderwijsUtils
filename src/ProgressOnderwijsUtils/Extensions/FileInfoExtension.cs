0ab6624ae113ef46d46fd62ad7bf3920de706836
Fons Dijkstra <f.j.dijkstra@rug.nl>
2009-08-19 14:26:37 +0000

    Ketentest verder geimplementeerd.

--- src/ProgressOnderwijsUtils/Extensions/FileInfoExtension.cs
+++ src/ProgressOnderwijsUtils/Extensions/FileInfoExtension.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
 
 namespace ProgressOnderwijsUtils.Extensions
@@ -12,6 +9,30 @@ namespace ProgressOnderwijsUtils.Extensions
 		{
 			using (var reader = file.OpenText())
 				return reader.ReadToEnd();
-		}	
+		}
+
+		/// <summary>
+		/// Tests whether this file has the same contents as another file.
+		/// </summary>
+		/// <param name="one"></param>
+		/// <param name="other"></param>
+		/// <returns></returns>
+		public static bool SameContents(this FileInfo one, FileInfo other)
+		{
+			if (other == null) throw new ArgumentNullException("other");
+
+			bool result = true;
+			if (one.FullName == other.FullName)
+				result = true;
+			else if (one.Length != other.Length)
+				result = false;
+			else
+				using (FileStream fs1 = one.OpenRead())
+					using (FileStream fs2 = other.OpenRead())
+						while (result && (fs1.Position < fs1.Length))
+							result = fs1.ReadByte() == fs2.ReadByte();
+
+			return result;
+		}
 	}
 }
---
+++ test/ProgressOnderwijsUtilsTests/FileInfoExtensions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using ProgressOnderwijsUtils.Extensions;
+
+namespace ProgressOnderwijsUtilsTests
+{
+	[TestFixture]
+	public class FileInfoExtensions
+	{
+		private FileInfo sut;
+		private FileInfo other;
+
+		[SetUp]
+		public void SetUp()
+		{
+			sut = new FileInfo(Path.GetTempFileName());
+			other = new FileInfo(Path.GetTempFileName());
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			try
+			{
+				sut.Delete();
+			}
+			finally
+			{
+				other.Delete();
+			}
+		}
+
+		[Test, ExpectedException(typeof(ArgumentNullException))]
+		public void SameContentsNull()
+		{
+			sut.SameContents(null);
+		}
+
+		[Test]
+		public void SameContentsSame()
+		{
+			Assert.That(sut.SameContents(sut));
+			Assert.That(sut.SameContents(new FileInfo(sut.FullName)));
+		}
+
+		[Test]
+		public void SameContentsEmpty()
+		{
+			Assert.That(sut.SameContents(other));
+		}
+
+		[Test]
+		public void SameContentsFilled()
+		{
+			using (FileStream fs1 = sut.OpenWrite())
+			{
+				fs1.WriteByte(0);
+			}
+			using (FileStream fs2 = other.OpenWrite())
+			{
+				fs2.WriteByte(0);
+			}
+			Assert.That(sut.SameContents(other));
+		}
+
+		[Test]
+		public void DifferentContents()
+		{
+			using (FileStream fs1 = sut.OpenWrite())
+			{
+				fs1.WriteByte(0);
+			}
+			using (FileStream fs2 = other.OpenWrite())
+			{
+				fs2.WriteByte(1);
+			}
+			Assert.That(!sut.SameContents(other));
+		}
+	}
+}
