462d1576c51d9bb57bb3dd7c59b747f7d41b4389
Eamon Nerbonne <eamon@nerbonne.org>
2009-08-25 15:48:52 +0000

    rapport-code-verkleining v2, nu hexencode ipv base64 ivm case-sensitivity.

--- src/ProgressOnderwijsUtils/DateTimeShortAgeTag.cs
+++ src/ProgressOnderwijsUtils/DateTimeShortAgeTag.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Globalization;
 
 namespace ProgressOnderwijsUtils
 {
@@ -19,6 +20,18 @@ namespace ProgressOnderwijsUtils
 			uint granularTicks = (uint)(datetime.Ticks >> 23) & 0xffffff;
 			byte[] bytes = BitConverter.GetBytes(granularTicks).Reverse().ToArray();//reverse so most significant first.
 			return Convert.ToBase64String(bytes).Trim(trimchars); //we trim off unneded "zero" ('A') and padding chars ('=')
+		}
+		/// <summary>
+		/// The purpose of an AgeTag is to provide an unlikely-to-collide unique token based on modification time.
+		/// A tick is 10^-7 seconds; so Ticks>>23 ~ 0.84 seconds
+		/// now, we take 0xffffff of those pseudo seconds, or 2^24*2^23*10^-7 seconds, i.e. 162.9 days, or about 0.5 years.
+		/// after this time, the AgeTag will loop.  So our Tag is granular to the second, and unique within a half year.
+		/// </summary>
+		public static string ToAgeTagCaseInsensitive(DateTime datetime)
+		{
+			uint granularTicks = (uint)(datetime.Ticks >> 23) & 0xffffff; //3byte code
+			
+			return granularTicks.ToString("x2",CultureInfo.InvariantCulture);
 		}
 	}
 }
