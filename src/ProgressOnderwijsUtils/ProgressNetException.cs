1a8b801ff09c47923ee53d73f91fdd0a7ae716eb
Eamon Nerbonne <eamon@nerbonne.org>
2009-09-14 09:01:24 +0000

    RequestVars now explicitly checks against duplicate connection caching

--- src/ProgressOnderwijsUtils/ProgressNetException.cs
+++ src/ProgressOnderwijsUtils/ProgressNetException.cs
@@ -14,4 +14,18 @@ namespace ProgressOnderwijsUtils
 		public ProgressNetException(string debugMessage, Exception innerException) : base(debugMessage,innerException) { }
 		protected ProgressNetException(SerializationInfo info,StreamingContext context) : base(info,context) { }
 	}
+
+	/// <summary>
+	/// This exception is thrown when an impossible program state is encountered - i.e. when a programmer made a mistake and violated a assumption.
+	/// </summary>
+	[Serializable]
+	public class PNAssertException : ProgressNetException
+	{ 
+		public PNAssertException() : base() { }
+		public PNAssertException(string debugMessage) : base(debugMessage) { }
+		public PNAssertException(string debugMessage, Exception innerException) : base(debugMessage, innerException) { }
+		protected PNAssertException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+	}
+
+
 }
