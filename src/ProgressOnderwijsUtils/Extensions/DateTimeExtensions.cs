1b08e8ea7860948c67f1e5e31ad88abfe03db8bf
Niek van Ruler <n.j.van.ruler@rug.nl>
2009-09-18 13:10:27 +0000

    Bij niet accoord uitschrijven wordt de tekst uit opmerking administratie naar de student gestuurd. Alleen bij de eerste keer van het gebruik van uitschrijvingedit wordt een bericht naar SL gestuurd.
    Bugzid: 1471,1538

--- src/ProgressOnderwijsUtils/Extensions/DateTimeExtensions.cs
+++ src/ProgressOnderwijsUtils/Extensions/DateTimeExtensions.cs
@@ -7,7 +7,14 @@ namespace ProgressOnderwijsUtils
 {
 	public static class DateTimeExtensions
 	{
-
+		/// <summary>
+		/// Geeft het collegejaar van een bepaalde datum
+		/// voorbeelden :
+		/// 04-05-2009 -> 2008
+		/// 01-09-2008 -> 2008
+		/// </summary>
+		/// <param name="datetime"></param>
+		/// <returns></returns>
 		public static int CollegeJaar(this DateTime datetime)
 		{
 			int collegejaar = datetime.Year;
