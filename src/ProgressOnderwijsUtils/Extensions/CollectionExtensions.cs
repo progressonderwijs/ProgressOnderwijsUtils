072ec0734fd8c5aa46fc1e28284f55ef6f53f50d
Fons Dijkstra <f.j.dijkstra@rug.nl>
2009-10-05 08:14:57 +0000

    Added three utility extension methods: - System.Collections.Generic.ICollection<T>.Empty() - System.Collections.Generic.IDictionary<K, V>.GetDefault(K, V) : V - System.Collections.Generic.IDictionary<K, V>.SetDefault(K, V) : V
    De timeout op de test queries van 120 s op 300 s gezet (aangezien een query blijkbaar heel veel tijd nodig heeft?)

--- src/ProgressOnderwijsUtils/Extensions/CollectionExtensions.cs
+++ src/ProgressOnderwijsUtils/Extensions/CollectionExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace ProgressOnderwijsUtils.Extensions
 {
@@ -11,22 +12,80 @@ namespace ProgressOnderwijsUtils.Extensions
 	public static class CollectionExtensions
 	{
 		/// <summary>
+		/// Derived query to test whether a collection is empty or not.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="collection"></param>
+		/// <returns>true when Count == 0, false otherwise</returns>
+		public static bool Empty<T>(this ICollection<T> collection)
+		{
+			return collection.Count == 0;
+		}
+
+		/// <summary>
 		/// Utility method to retrieve a value with a default from a dictionary.
 		/// </summary>
 		/// <typeparam name="TKey"></typeparam>
 		/// <typeparam name="TValue"></typeparam>
 		/// <param name="dict"></param>
 		/// <param name="key">The key whose value to get.</param>
-		/// <param name="defaultValue">The default value of the key.</param>
+		/// <param name="value">The default value of the key.</param>
 		/// <returns>The value of the key, or the default if the dictionary does not contain the key.</returns>
-		public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue defaultValue)
+		public static TValue GetDefault<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue value)
 		{
 			TValue result;
 			if (!dict.TryGetValue(key, out result))
 			{
-				result = defaultValue;
+				result = value;
 			}
 			return result;
+		}
+
+		/// <summary>
+		/// Utility method to retrieve the value of a dictionary with setting it to a default if the key does not yet exist.
+		/// </summary>
+		/// <typeparam name="TKey"></typeparam>
+		/// <typeparam name="TValue"></typeparam>
+		/// <param name="dict"></param>
+		/// <param name="key">The key whose value to get.</param>
+		/// <param name="value">The default value to set if the key does not yet exists.</param>
+		/// <returns>The the key existed in the dictionary, its associated value. If not, insert key with th edefault value and return this default.</returns>
+		public static TValue SetDefault<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue value)
+		{
+			if (!dict.ContainsKey(key))
+			{
+				dict.Add(key, value);
+			}
+			return dict[key];
+		}
+	}
+
+	[TestFixture]
+	public class CollectionExtensionsTests
+	{
+		[Test]
+		public void Empty()
+		{
+			Assert.That(new List<int>().Empty());
+			Assert.That(!new Dictionary<int, int>() { { 0, 0 } }.Empty());
+		}
+
+		[Test]
+		public void GetDefault()
+		{
+			IDictionary<int, int> sut = new Dictionary<int, int>() { { 0, 0 } };
+			Assert.That(sut.GetDefault(0, 1), Is.EqualTo(0));
+			Assert.That(sut.GetDefault(1, 2), Is.EqualTo(2));
+			Assert.That(!sut.ContainsKey(1));
+		}
+
+		[Test]
+		public void SetDefault()
+		{
+			IDictionary<int, int> sut = new Dictionary<int, int>() { { 0, 0 } };
+			Assert.That(sut.SetDefault(0, 1), Is.EqualTo(0));
+			Assert.That(sut.SetDefault(1, 2), Is.EqualTo(2));
+			Assert.That(sut.ContainsKey(1));
 		}
 	}
 }
