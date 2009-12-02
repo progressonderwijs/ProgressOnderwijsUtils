c6b8c0c50473167ab13c9ec11f28481f1aa91d63
Anne van der Ploeg <a.van.der.ploeg@rug.nl>
2009-12-02 14:45:08 +0000

    Testen expressionparser uitgebreid met testen van variabelen

--- test/ProgressOnderwijsUtilsTests/ExpressionParserTest.cs
+++ test/ProgressOnderwijsUtilsTests/ExpressionParserTest.cs
@@ -16,7 +16,26 @@ namespace ProgressOnderwijsUtilsTests
 		[TestCaseSource("Expressions")]
 		public double TestExpression(string expressie)
 		{
-			return ExpressionParser.Parse(expressie, null);
+			return ExpressionParser.Parse(expressie, Variabelen);
+		}
+
+		public double Variabelen(string variabele)
+		{
+			switch (variabele)
+			{
+				default:
+					return (double)0;
+				case "A":
+					return (double)1;
+				case "B":
+					return (double)2;
+				case "C":
+					return (double)8;
+				case "D":
+					return (double)10;
+				case "E":
+					return (double)12;
+			}
 		}
 
 		public IEnumerable Expressions
@@ -30,9 +49,20 @@ namespace ProgressOnderwijsUtilsTests
 				yield return new TestCaseData("1 % 2").Returns(1);
 				yield return new TestCaseData("1 ^ 2").Returns(1);
 
+				yield return new TestCaseData("A + B").Returns(3);
+				yield return new TestCaseData("A - B").Returns(-1);
+				yield return new TestCaseData("A * B").Returns(2);
+				yield return new TestCaseData("A / B").Returns(0.5);
+				yield return new TestCaseData("A % B").Returns(1);
+				yield return new TestCaseData("A ^ B").Returns(1);
+
 				yield return new TestCaseData("2 ^ 8").Returns(256);
 				yield return new TestCaseData("((((2 ^ 8))))").Returns(256);
 				yield return new TestCaseData("((12 - 10) + (2 ^ 8))").Returns(258);
+
+				yield return new TestCaseData("B ^ C").Returns(256);
+				yield return new TestCaseData("((((B ^ C))))").Returns(256);
+				yield return new TestCaseData("((E - D) + (B ^ C))").Returns(258);
 			}
 		}
 
