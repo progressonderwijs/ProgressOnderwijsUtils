7226644359e63eee09a06f992b6c9a789990318c
Anne van der Ploeg <a.van.der.ploeg@rug.nl>
2009-12-02 14:48:28 +0000

    Testen expressionparser uitgebreid met expressies die zowel constanten als variabelen bevatten. En meer valt er volgens mij ook niet aan de expressieparser te testen. Terug naar de incassotesten...

--- test/ProgressOnderwijsUtilsTests/ExpressionParserTest.cs
+++ test/ProgressOnderwijsUtilsTests/ExpressionParserTest.cs
@@ -56,6 +56,13 @@ namespace ProgressOnderwijsUtilsTests
 				yield return new TestCaseData("A % B").Returns(1);
 				yield return new TestCaseData("A ^ B").Returns(1);
 
+				yield return new TestCaseData("1 + B").Returns(3);
+				yield return new TestCaseData("A - 2").Returns(-1);
+				yield return new TestCaseData("1 * B").Returns(2);
+				yield return new TestCaseData("A / 2").Returns(0.5);
+				yield return new TestCaseData("1 % B").Returns(1);
+				yield return new TestCaseData("A ^ 2").Returns(1);
+
 				yield return new TestCaseData("2 ^ 8").Returns(256);
 				yield return new TestCaseData("((((2 ^ 8))))").Returns(256);
 				yield return new TestCaseData("((12 - 10) + (2 ^ 8))").Returns(258);
@@ -63,6 +70,10 @@ namespace ProgressOnderwijsUtilsTests
 				yield return new TestCaseData("B ^ C").Returns(256);
 				yield return new TestCaseData("((((B ^ C))))").Returns(256);
 				yield return new TestCaseData("((E - D) + (B ^ C))").Returns(258);
+
+				yield return new TestCaseData("B ^ 8").Returns(256);
+				yield return new TestCaseData("((((2 ^ C))))").Returns(256);
+				yield return new TestCaseData("((12 - D) + (2 ^ C))").Returns(258);
 			}
 		}
 
