232dcae45b2200e88ff7fd278728ee43093f03be
Anne van der Ploeg <a.van.der.ploeg@rug.nl>
2009-12-02 14:14:01 +0000

    Clieoptesten aangepast (nu alleen nog de scenario's voor de eenmalige inning). De waardes in de scenario's zijn nu dynamisch waardoor het (gelukkig) niet nodig is scenario's voor verschillende TestFixtures te dupliceren. Om in de scenario's toch op de dynamische waarde te kunnen testen (om te bepalen of een test geslaagd is, moet de uitkomst immers bekend zijn) heb ik een generieke expressieparser gemaakt. Dit is een sterk uitgeklede versie van de expressieparser die gebruikt wordt in een bekend Nederlands ouwe-lullen-programma waarvan ik de naam niet mag noemen.
    De expressieparser kan gevoerd worden met numerieke expressies en kent de bekende operatoren voor optellen, aftrekken, vermenigvuldigen, delen, modulus en machtsverheffen. Je kunt ook variabelen gebruiken waarvan de waarde dan via een delegate opgehaald wordt. De expressieparser retourneert een double. Voorbeelden:
    double x = Tools.ExpressionParser.Parse("2 ^ 8 / ( -4 + (16 % 9))", null)
    decimal TermijnBedrag = Tools.ExpressionParser.Parse("GetGefactureerdBedrag / GetAantalTermijnen", TestDelegate)
    
    De parser kan (desgewenst) eenvoudig uitgebreid worden met strings: handig voor het maken van gepersonaliseerde mededelingen.

---
+++ src/ProgressOnderwijsUtils/ExpressionParser.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace ProgressOnderwijsUtils
+{
+	public static class ExpressionParser
+	{
+		public delegate double ParserDelegate(string variable);
+		private static event ParserDelegate parserdelegate;
+
+		static Stack<double> Variable;
+		static int current;
+		static List<Token> tokens;
+
+		public static double Parse(string expression, ParserDelegate pd)
+		{
+			parserdelegate = pd;
+			Variable = new Stack<double>();
+			tokens = new Scanner().Scan(expression);
+			current = 0;
+
+			Expression();
+
+			return Variable.Count > 0 ? Variable.Pop() : (double)0;
+		}
+
+		private static void Expression()
+		{
+			if (!Eof())
+				ExpressionAddSubtract();
+		}
+
+		private static void ExpressionAddSubtract()
+		{
+			ExpressionMultiplyDivide();
+
+			string value;
+			while (!Eof() && tokens[current].Type == TokenType.Delimiter && (tokens[current].Value == "+" || tokens[current].Value == "-"))
+			{
+				value = tokens[current].Value;
+				++current;
+				ExpressionMultiplyDivide();
+				Arithmetic(value);
+			}
+		}
+
+		private static void ExpressionMultiplyDivide()
+		{
+			ExpressionExponent();
+
+			string value;
+			while (!Eof() && tokens[current].Type == TokenType.Delimiter && (tokens[current].Value == "*" || tokens[current].Value == "/" || tokens[current].Value == "%"))
+			{
+				value = tokens[current].Value;
+				++current;
+				ExpressionExponent();
+				Arithmetic(value);
+			}
+		}
+
+		private static void ExpressionExponent()
+		{
+			ExpressionSign();
+
+			if (!Eof() && tokens[current].Type == TokenType.Delimiter && tokens[current].Value == "^")
+			{
+				++current;
+				ExpressionSign();
+				Arithmetic("^");
+			}
+		}
+
+		private static void ExpressionSign()
+		{
+			string value = null;
+			if (!Eof() && tokens[current].Type == TokenType.Delimiter && (tokens[current].Value == "+" || tokens[current].Value == "-"))
+			{
+				value = tokens[current].Value;
+				++current;
+			}
+			ExpressionParentheses();
+			if (value != null)
+				ArithmeticSign(value);
+		}
+
+		private static void ExpressionParentheses()
+		{
+			if (!Eof() && tokens[current].Type == TokenType.Delimiter && tokens[current].Value == "(")
+			{
+				++current;
+				ExpressionAddSubtract();
+				if (!Eof() && tokens[current].Type == TokenType.Delimiter && tokens[current].Value != ")")
+					throw new NietZoErgeException(") expected");
+			}
+			else
+			{
+				ExpressionPrimitive();
+			}
+			++current;
+		}
+
+		private static void ExpressionPrimitive()
+		{
+			switch (tokens[current].Type)
+			{
+				case TokenType.NumericConstant:
+					Variable.Push(Double.Parse(tokens[current].Value, CultureInfo.InvariantCulture.NumberFormat));
+					break;
+				case TokenType.Variable:
+					if (parserdelegate == null)
+						throw new NietZoErgeException("Parser needs delegate when using variables");
+					Variable.Push(parserdelegate(tokens[current].Value));
+					break;
+			}
+		}
+
+		private static void ArithmeticSign(string op)
+		{
+			if (Variable.Count == 0)
+				throw new NietZoErgeException(op + " misplaced");
+			double left = Variable.Pop();
+			Variable.Push(op == "-" ? -1 * left : left);
+		}
+
+		private static void Arithmetic(string op)
+		{
+			if (Variable.Count < 2)
+				throw new NietZoErgeException(op + " misplaced");
+
+			double right = Variable.Pop();
+			double left = Variable.Pop();
+
+			switch (op)
+			{
+				case "+":
+					Variable.Push(left + right);
+					break;
+				case "-":
+					Variable.Push(left - right);
+					break;
+				case "*":
+					Variable.Push(left * right);
+					break;
+				case "/":
+					Variable.Push(left / right);
+					break;
+				case "%":
+					Variable.Push(left % right);
+					break;
+				case "^":
+					Variable.Push(Math.Pow(left, right));
+					break;
+			}
+		}
+
+		private static bool Eof()
+		{
+			return current == tokens.Count;
+		}
+	}
+	
+	public enum TokenType
+	{
+		Delimiter,
+		NumericConstant,
+		Variable
+	}
+
+	public class Token
+	{
+		public TokenType Type { get; set; }
+		public string Value { get; set; }
+	}
+
+	public class Scanner
+	{
+		int current = 0;
+		string expression;
+
+		public List<Token> Scan(string s)
+		{
+			Token token;
+			List<Token> tokens = new List<Token>();
+			expression = s;
+
+			while (current < expression.Length)
+			{
+				while (current < expression.Length && IsWhite(expression[current]))
+					++current;
+
+				// Delimiter?
+				if (IsDelimiter(expression[current]))
+				{
+					token = new Token();
+					token.Type = TokenType.Delimiter;
+					token.Value = Convert.ToString(expression[current++]);
+					tokens.Add(token);
+					continue;
+				}
+
+				// Numeric literal?
+				if(IsNumeric(expression[current]))
+				{
+					token = new Token();
+					token.Type = TokenType.NumericConstant;
+					while (current < expression.Length && IsNumeric(expression[current]))
+					{
+						token.Value = token.Value + expression[current++];
+					}
+					tokens.Add(token);
+					continue;
+				}
+				
+				// variable
+				token = new Token();
+				token.Type = TokenType.Variable;
+				while (current < expression.Length && !IsWhite(expression[current]) && !IsDelimiter(expression[current]))
+				{
+					token.Value = token.Value + expression[current++];
+				}
+				tokens.Add(token);
+			}
+			return tokens;
+		}
+
+		private void SkipWhite()
+		{
+			while (current < expression.Length)
+			{
+				if (!IsWhite(expression[current]))
+				{
+					if (current > 0)
+						--current;
+					break;
+				}
+				++current;
+			}
+		}
+	
+		private bool IsNumeric(char c)
+		{
+			return "0123456789.".IndexOf(c) != -1;
+		}
+
+		private bool IsDelimiter(char c)
+		{
+			return "/*-+^%()".IndexOf(c) != -1;
+		}
+
+		private bool IsWhite(char c)
+		{
+			return " \t\r\n".IndexOf(c) != -1;
+		}
+	}	
+}
