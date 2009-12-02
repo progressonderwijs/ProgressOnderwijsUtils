f0058805ef5314810a941a0913086c1191dc8114
Anne van der Ploeg <a.van.der.ploeg@rug.nl>
2009-12-02 14:33:06 +0000

    Alles moet natuurlijk getest worden dus ook de expressieparser. Hiervoor een paar (eenvoudige) testjes gemaakt.

---
+++ test/ProgressOnderwijsUtilsTests/ExpressionParserTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Collections;
+using ProgressOnderwijsUtils;
+
+namespace ProgressOnderwijsUtilsTests
+{
+	[TestFixture]
+	public class ExpressionParserTest
+	{
+		[Test]
+
+		[TestCaseSource("Expressions")]
+		public double TestExpression(string expressie)
+		{
+			return ExpressionParser.Parse(expressie, null);
+		}
+
+		public IEnumerable Expressions
+		{
+			get
+			{
+				yield return new TestCaseData("1 + 2").Returns(3);
+				yield return new TestCaseData("1 - 2").Returns(-1);
+				yield return new TestCaseData("1 * 2").Returns(2);
+				yield return new TestCaseData("1 / 2").Returns(0.5);
+				yield return new TestCaseData("1 % 2").Returns(1);
+				yield return new TestCaseData("1 ^ 2").Returns(1);
+
+				yield return new TestCaseData("2 ^ 8").Returns(256);
+				yield return new TestCaseData("((((2 ^ 8))))").Returns(256);
+				yield return new TestCaseData("((12 - 10) + (2 ^ 8))").Returns(258);
+			}
+		}
+
+	}
+}
