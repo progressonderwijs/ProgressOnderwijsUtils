84d79af236659a8018a091b85754c5305916f795
Fons Dijkstra <f.j.dijkstra@rug.nl>
2010-01-14 10:23:12 +0000

    Een nieuw data-type toegevoegd: FileData welke een filenaam, mimetype en inhoud bevat. In de metadata van een tabel kan je nu een kolom toevoegen en markeren met 'bestand'. Generic-edit zal dan hiervoor een kolom toevoegen met type FileData waarbij de drie onderliggende waarden in de metarecordbestand tabel wordt weggeschreven. Verder is er een generic input control gemaakt die voor het bekijken en verwijderen van een record een link laat zien waarmee de file gedownload kan worden en voor het toeveoegen en wijzigen een file-upload control laat zien om een file te uploaden.
    Bovenstaande kan je in actie zien in de nieuwe tab 'Organisatie -> Beheer -> PDF templates' waarbij er PDF files opgeslagen kunnen worden die als een template voor het genereren van PDF documenten dienen.
    
    Het gedrag van de file-iupload control laat nog wel wat te wensen over, maar dit los ik binnenkort wel op.

---
+++ src/ProgressOnderwijsUtils/Data/FileData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgressOnderwijsUtils
+{
+	public struct FileData : IEquatable<FileData>
+	{
+		public byte[] Content { get; set; }
+		public string ContentType { get; set; }
+		public string FileName { get; set; }
+
+		public override string ToString()
+		{
+			return string.Format("{0} ({1} KB)", FileName, Content.Length / 1000m);
+		}
+
+		public override bool Equals(object other)
+		{
+			bool result = false;
+			if (other != null && other.GetType() == typeof(FileData))
+			{
+				result = Equals((FileData)other);
+			}
+			return result;
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				int result = (Content != null ? Content.GetHashCode() : 0);
+				result = (result*397) ^ (ContentType != null ? ContentType.GetHashCode() : 0);
+				result = (result*397) ^ (FileName != null ? FileName.GetHashCode() : 0);
+				return result;
+			}
+		}
+
+		public bool Equals(FileData other)
+		{
+			return ContentEqual(other) &&
+				   Equals(other.ContentType, ContentType) && 
+				   Equals(other.FileName, FileName);
+		}
+
+		private bool ContentEqual(FileData other)
+		{
+			bool result;
+			if (Content == null && other.Content == null)
+			{
+				result = true;
+			}
+			else if (Content != null && other.Content != null)
+			{
+				result = Content.SequenceEqual(other.Content);
+			}
+			else
+			{
+				result = false;
+			}
+			return result;
+		}
+
+		public static bool operator ==(FileData left, FileData right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(FileData left, FileData right)
+		{
+			return !left.Equals(right);
+		}
+	}
+}
