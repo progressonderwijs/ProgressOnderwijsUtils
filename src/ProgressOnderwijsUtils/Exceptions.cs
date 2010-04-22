d52a12ee53cd4bbc06a61d3d2a0fc0eb96d5d684
Anne van der Ploeg <a.van.der.ploeg@rug.nl>
2010-04-22 06:31:06 +0000

    TemplateException toegevoegd

--- src/ProgressOnderwijsUtils/Exceptions.cs
+++ src/ProgressOnderwijsUtils/Exceptions.cs
@@ -9,16 +9,54 @@ namespace ProgressOnderwijsUtils
 	{
 		public NoRowsFoundException() { }
 	}
+	
 	public class GeenRechtException : ProgressNetException
 	{
 		public GeenRechtException(string msg) : base(msg) { }
 	}
+	
 	public class QueryException : ProgressNetException
 	{
 		public QueryException(string msg) : base(msg) { }
 		public QueryException() { }
 		public QueryException(string msg, Exception inner) : base(msg, inner) { }
 	}
+
+	[Serializable]
+	public class TemplateException : Exception
+	{
+		public int Line { get; set; }
+		public int Start { get; set; }
+		public int Length { get; set; }
+
+		public TemplateException()
+		{ }
+
+		public TemplateException(int Line, int PositionStart, int PositionEnd, string message)
+			:
+				base(message)
+		{
+			this.Line = Line;
+			this.Start = PositionStart;
+			this.Length = PositionEnd - PositionStart;
+		}
+
+		public TemplateException(string message)
+			:
+				base(message)
+		{ }
+
+		public TemplateException(string message, Exception innerexception)
+			:
+				base(message, innerexception)
+		{ }
+
+		protected TemplateException(SerializationInfo serializationinfo, StreamingContext streamingcontext)
+			:
+			base(serializationinfo, streamingcontext)
+		{ }
+	}
+
 	[Serializable]
 	public class GenericMetaDataException : ProgressNetException
 	{ //TODO: this exception should provide for naming the table and the type of metadata where the error occured.
