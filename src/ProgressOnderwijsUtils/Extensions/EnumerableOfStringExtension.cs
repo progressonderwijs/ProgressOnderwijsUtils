de7f67a45e90c17fa77cfeab2495e430644bfd24
Renzo Kooi <l.kooi@rug.nl>
2010-05-25 07:07:01 +0000

    Paar extensies omgelinqed en testjes ervoor toegevoegd.

--- src/ProgressOnderwijsUtils/Extensions/EnumerableOfStringExtension.cs
+++ src/ProgressOnderwijsUtils/Extensions/EnumerableOfStringExtension.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace ProgressOnderwijsUtils
 {
 	public static class EnumerableOfStringExtension
 	{
+		/* >prevous code<
 		public static string Join(this IEnumerable<string> strings)
 		{
 			StringBuilder sb = new StringBuilder();
@@ -14,6 +16,7 @@ namespace ProgressOnderwijsUtils
 				sb.Append(s);
 			return sb.ToString();
 		}
+		
 		public static string JoinStrings(this IEnumerable<string> strings, string separator)
 		{
 			//string.Join(separator, strings.ToArray())
@@ -28,7 +31,50 @@ namespace ProgressOnderwijsUtils
 				sb.Append(s);
 			}
 			return sb.ToString();
+		}*/
+		/// <summary>
+		/// Joins a collection into a string (w/o joiner string)
+		/// </summary>
+		/// <param name="strings">string collection</param>
+		/// <param name="joiner">joiner string</param>
+		/// <returns>a string</returns>
+		/// <remarks>rewritten using linq RK 2010/05/25
+		/// <para/>See IenumberableExtensions (Join). This could be ditched.
+		/// </remarks>
+		public static string Join(this IEnumerable<string> strings)
+		{
+			return strings.Aggregate((a, b) => a + b.ToString());
 		}
 
+		/// <summary>
+		/// Join a collection of strings using a joiner string
+		/// </summary>
+		/// <param name="strings">string collection</param>
+		/// <param name="joiner">joiner string</param>
+		/// <returns>a string</returns>
+		/// <remarks>rewritten using linq RK 2010/05/25</remarks>
+		public static string JoinStrings(this IEnumerable<string> strings, string joiner)
+		{
+			return strings.Aggregate((a, b) => a += a.Length < 1 ? b.ToString() : joiner + b.ToString());
+		}
+
+	}
+
+	[TestFixture]
+	public class IEnumberableOFStringExtensionTest
+	{
+		
+		[Test]
+		public void testJoin()
+		{
+			List<string> lst = new List<string> { "een", "twee", "drie" };
+			Assert.That(lst.Join(), Is.EqualTo("eentweedrie"));
+		}
+
+		[Test]
+		public void testJoinStrings() {
+			List<string> lst = new List<string> { "een", "twee", "drie" };
+			Assert.That(lst.JoinStrings("!"), Is.EqualTo("een!twee!drie"));
+		}
 	}
 }
--- src/ProgressOnderwijsUtils/Extensions/IEnumerableExtensions.cs
+++ src/ProgressOnderwijsUtils/Extensions/IEnumerableExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace ProgressOnderwijsUtils
 {
@@ -19,6 +20,38 @@ namespace ProgressOnderwijsUtils
 				retval++;
 			}
 			return -1;
+		}
+		/// <summary>
+		/// Joins elements in a collection, using a joiner string
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="src">any collection</param>
+		/// <param name="joiner">string between elems</param>
+		/// <returns>a string</returns>
+		/// <codefrom value="Renzo Kooi" date="2010/05/25" dateLast="2010/05/25"/>
+		public static string Join<T>(this IEnumerable<T> src, string joiner)
+		{
+			return src.Aggregate("",(a, b) => a += a.Length < 1 ? b.ToString() : joiner + b.ToString());
+		}
+	}
+
+	[TestFixture]
+	public class IEnumberableExtensionTest
+	{
+		[Test]
+		public void testIndexOf()
+		{
+			List<string> lst = new List<string> { "een", "twee", "drie" };
+			Assert.That(lst.IndexOf("twee"), Is.EqualTo(1));
+			Assert.That(lst.IndexOf("tweeeneenhalf"), Is.EqualTo(-1));
+			Assert.That(lst.Join("!"), Is.EqualTo("een!twee!drie"));
+		}
+
+		[Test]
+		public void testJoin()
+		{
+			List<int> lst = new List<int>{1,2,3,4,5};
+			Assert.That(lst.Join("!"), Is.EqualTo("1!2!3!4!5"));
 		}
 	}
 }
