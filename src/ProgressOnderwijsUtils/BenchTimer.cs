89d40b58db44c001dc868efd14be29d2d8d1d04e
Eamon Nerbonne <eamon@nerbonne.org>
2010-06-05 17:11:15 +0000

    kleine timer util toegevoegd.

---
+++ src/ProgressOnderwijsUtils/BenchTimer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace ProgressOnderwijsUtils
+{
+	static class BenchTimer
+	{
+		public static TimeSpan MinimumTime(Action a, int numRuns=5)
+		{
+			if (numRuns < 1) throw new ArgumentException("Need to test for at least 1 run","numRuns");
+			var bestTime = TimeSpan.MaxValue;
+			Stopwatch timer = new Stopwatch();
+			for(int i=0;i<numRuns;i++) {
+				timer.Restart();
+				a();
+				timer.Stop();
+				bestTime = timer.Elapsed < bestTime ? timer.Elapsed : bestTime;
+			}
+			return bestTime;
+		}
+	}
+}
