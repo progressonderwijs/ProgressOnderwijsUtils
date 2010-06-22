963846a7b5749a17c74bfe7a9edf956fc19cf684
Fons Dijkstra <f.j.dijkstra@rug.nl>
2010-06-22 12:44:46 +0000

    GenericEdit uitgebreidt met een 'variant' metarecord: naast taalafhankelijke en bestand kolommen kunnen er nu ook via de meta data zogenaamde variant kolommen worden toegevoegd. Deze kolommen kunnen een willekeurig type bevatten en worden onder water afgebeeld met een nvarchar(32) kolom en een sql_variant kolom.
    Testen voor GenericEdit zijn erbij gemaakt, analoog aan die van de bestand metarecords. Ik heb hiervoor een generieke base class geintroduceerd waarmee het eigenlijk nog maar heel weinig tijd kost om nieuwe testen te schrijven voor eventuele nieuwe metarecord kolommen. Eventueel zouden we ook deze base class kunnen gebruiken om testen te schrijven voor de taalafhankelijke functionaliteit.
    
    Volgende stap is om op de juiste momenten de juiste input controls te genereren in de schermen.

---
+++ src/ProgressOnderwijsUtils/Data/VariantData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProgressOnderwijsUtils
+{
+	public struct VariantData : IEquatable<VariantData>
+	{
+		public string Type { get; set; }
+		public object Value { get; set; }
+
+		public override string ToString()
+		{
+			return string.Format(CultureInfo.InvariantCulture, "{0} : {1}", Value, Type);
+		}
+
+		public bool Equals(VariantData other)
+		{
+			return Equals(other.Type, Type) && Equals(other.Value, Value);
+		}
+
+		public override bool Equals(object obj)
+		{
+			bool result = false;
+			if (obj != null && obj.GetType() == typeof(VariantData))
+			{
+				result = Equals((VariantData)obj);
+			}
+			return result;
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return ((Type != null ? Type.GetHashCode() : 0) * 397) ^ (Value != null ? Value.GetHashCode() : 0);
+			}
+		}
+
+		public static bool operator ==(VariantData left, VariantData right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(VariantData left, VariantData right)
+		{
+			return !left.Equals(right);
+		}
+	}
+}
