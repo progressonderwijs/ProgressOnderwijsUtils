629c99280b07a9175aed9f21de402c5a173bcf49
Fons Dijkstra <f.j.dijkstra@rug.nl>
2010-06-30 15:00:44 +0000

    De Progress.Tools.inschrijving namespace hernoemd naar Progress.Tools.Inschrijving.
    Verder een nieuwe test categorie gemaakt: Progress.Tools.Test.NightlyOnly. Deze kan je gebruiken als je een test fixture hebt die je alleen tijdens de nachtelijke build wilt draaien en niet tijdens de 'fast'-builds. Met name nodig als ze wat lang duren of als je 1 keer testen per dag genoeg vind. Vanaf nu worden dus enkel nog maar de 'NightlyOnly' category geexclude bij de fast-builds...

---
+++ src/ProgressOnderwijsUtils/Test/TestSuite.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ProgressOnderwijsUtils.Test
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+	public class NightlyOnlyAttribute : CategoryAttribute
+	{
+	}
+}
