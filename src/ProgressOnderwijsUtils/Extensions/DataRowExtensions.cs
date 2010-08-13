474562626420c816d7f36b1f5bc740f520096501
Eamon Nerbonne <eamon@nerbonne.org>
2010-08-13 08:31:41 +0000

    Progress.Business DataRow.Get --> DataRow.Field some more...

--- src/ProgressOnderwijsUtils/Extensions/DataRowExtensions.cs
+++ src/ProgressOnderwijsUtils/Extensions/DataRowExtensions.cs
@@ -12,18 +12,6 @@ namespace ProgressOnderwijsUtils
 	public static class DataRowExtensions
 	{
 		/// <summary>
-		/// Returns content of an attribute replacing DBNull.Value with null.  Throws an exception for value types
-		/// </summary>
-		/// <typeparam name="T">Type</typeparam>
-		/// <param name="row">DataRow</param>
-		/// <param name="fieldname">Name of attribute</param>
-		/// <returns>Content of field or default type initialization</returns>
-		public static T Get<T>(this DataRow row, string fieldname)
-		{
-			return row.Field<T>(fieldname);
-		}
-
-		/// <summary>
 		/// Returns content of an attribute, replacing DBNull.Value with the specified default value.
 		/// </summary>
 		/// <typeparam name="T">Type</typeparam>
@@ -31,35 +19,20 @@ namespace ProgressOnderwijsUtils
 		/// <param name="fieldname">Name of attribute</param>
 		/// <param name="defaultvalue">Value to return in case attribute is DBNull or null</param>
 		/// <returns>Content of field or default value</returns>
-		public static T FieldDefault<T>(this DataRow row, string fieldname, T defaultvalue)
+		public static T FieldOrDefault<T>(this DataRow row, string fieldname, T defaultvalue)
 		{
 			object val = row[fieldname];
 			return val == DBNull.Value ? defaultvalue : (T)val;
 		}
-		public static T Get<T>(this DataRow row, string fieldname, T defaultvalue)
-		{
-			return row.FieldDefault(fieldname, defaultvalue);
-		}
-
 
 		public static T Field<T>(this DataRowView row, string fieldname)
 		{
 			return row.Row.Field<T>(fieldname);
 		}
 
-		public static T Get<T>(this DataRowView row, string fieldname)
+		public static T FieldOrDefault<T>(this DataRowView row, string fieldname, T defaultvalue)
 		{
-			return row.Field<T>(fieldname);
-		}
-
-
-		public static T FieldDefault<T>(this DataRowView row, string fieldname, T defaultvalue)
-		{
-			return row.Row.FieldDefault<T>(fieldname, defaultvalue);
-		}
-		public static T Get<T>(this DataRowView row, string fieldname, T defaultvalue)
-		{
-			return row.FieldDefault<T>(fieldname, defaultvalue);
+			return row.Row.FieldOrDefault<T>(fieldname, defaultvalue);
 		}
 	}
 }
