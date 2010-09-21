d9f7feb8454d4e50d93d172dda5d91bec105314a
Eamon Nerbonne <eamon@nerbonne.org>
2010-09-21 13:50:21 +0000

    Removed unused utils that we probably don't want to use...

--- src/ProgressOnderwijsUtils/Utils.cs
+++ src/ProgressOnderwijsUtils/Utils.cs
@@ -17,7 +17,7 @@ namespace ProgressOnderwijsUtils
 		/// <editinfo date="2010/05/20" editedby="RK"/>
 		/// <param name="anyObject"></param>
 		/// <returns></returns>
-		public static bool IsNull(object anyObject) { return anyObject == null || anyObject == DBNull.Value ? true : false; }
+		public static bool IsNull(object anyObject) { return anyObject == null || anyObject == DBNull.Value; }
 
 		/// <summary>
 		/// Checks object is neither null nor DBNull
@@ -59,60 +59,6 @@ namespace ProgressOnderwijsUtils
 			for (int i = 1; getal != 0; getal /= 10, ++i)
 				res += i * (getal % 10);
 			return res != 0 && res % 11 == 0;
-		}
-
-		/**
-		 * <summary>
-		 *  Utility om van een serie strings een serie
-		 *  Tuples van strings te maken (paren, om precies
-		 *  te zijn). 
-		 *  Te gebruiken bij Tools.StringExtensions.MultiReplace,
-		 *  om de tweede parameter wat gemakkelijker samen te stellen
-		 *  </summary>
-		 *  <param name="p">[stringa,stringb,stringc,stringd, ... , stringxyz]</param>
-		 *  <example>
-		 *  Om twee paren van Regex-string en vervangstring te maken bv:
-		 * 
-		 *  Tuple&lt;string,string&gt;[] tupz = 
-		 * 				Tools.Utils.ToTuples(@"\r\n" ," \n" ,
-		 * 									 @"\email","e-mail");
-		 *  of direct in de MultiReplace-extension van 'n string:
-		 * 	 [string].MultiReplace(
-		 * 						RegexOptions.Multiline | RegexOptions.IgnoreCase,
-		 * 						==&gt; Tools.Utils.ToTuples(@"\r\n", "\n") &lt;==
-		 * 					  );
-		 *  
-		 *  </example>
-		 *  <returns>Array van Tuples van twee strings</returns>
-		 *  <remarks>
-		 *  Bij oneven aantal parameter strings wordt de laatste parameter
-		 *  niet gebruikt (de facto verwijderd uit de parameter array).
-		 *  </remarks>
-		 *  <codefrom value="Renzo Kooi" date="2009/08/18"/>
-		 */
-		public static Tuple<string, string>[] ToTuples(params string[] p)
-		{
-			int i = 0, plen = p.Length;
-			plen = (plen % 2 != 0) ? plen-- : plen;
-			Tuple<string, string>[] tupz = new Tuple<string, string>[p.Length / 2];
-			do
-			{
-				tupz[i / 2] = Tuple.Create<string, string>(p[i], p[i + 1]);
-			} while ((i += 2) < plen);
-			return tupz;
-		}
-
-		/// <summary>
-		/// Swap two objects.
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="one"></param>
-		/// <param name="other"></param>
-		public static void Swap<T>(ref T one, ref T other)
-		{
-			T tmp = one;
-			one = other;
-			other = tmp;
 		}
 
 		//idee van http://www.b-virtual.be/post/Generic-Cloning-Method-in-c.aspx
@@ -183,26 +129,6 @@ namespace ProgressOnderwijsUtils
 	[TestFixture]
 	public class UtilsTest
 	{
-		[Test]
-		public void SwapValue()
-		{
-			int one = 1;
-			int other = 2;
-			Utils.Swap(ref one, ref other);
-			Assert.That(one, Is.EqualTo(2));
-			Assert.That(other, Is.EqualTo(1));
-		}
-
-		[Test]
-		public void SwapReference()
-		{
-			string one = "1";
-			string other = "2";
-			Utils.Swap(ref one, ref other);
-			Assert.That(one, Is.EqualTo("2"));
-			Assert.That(other, Is.EqualTo("1"));
-		}
-
 		private IEnumerable<TestCaseData> InClauseData()
 		{
 			yield return new TestCaseData(new List<int>()).Returns("(null)");
