eac3e5511b9fcf4c55707fd78d32cdbd7527b7d3
Eamon Nerbonne <eamon@nerbonne.org>
2010-09-24 21:30:35 +0000

    Removed obsolete helper method.

--- src/ProgressOnderwijsUtils/Extensions/DataRowExtensions.cs
+++ src/ProgressOnderwijsUtils/Extensions/DataRowExtensions.cs
@@ -11,20 +11,6 @@ namespace ProgressOnderwijsUtils
 	/// </summary>
 	public static class DataRowExtensions
 	{
-		/// <summary>
-		/// Returns content of an attribute, replacing DBNull.Value with the specified default value.
-		/// </summary>
-		/// <typeparam name="T">Type</typeparam>
-		/// <param name="row">DataRow</param>
-		/// <param name="fieldname">Name of attribute</param>
-		/// <param name="defaultvalue">Value to return in case attribute is DBNull or null</param>
-		/// <returns>Content of field or default value</returns>
-		[Obsolete("Ipv DataRow.Get kun je tegenwoordig DataRow.Field gebruiken e.g.: dr.Field<int?>(\"kolomnaam\") ?? defaultwaarde")]
-		public static T Get<T>(this DataRow row, string fieldname, T defaultvalue)
-		{
-			object val = row[fieldname];
-			return val == DBNull.Value ? defaultvalue : (T)val;
-		}
 
 		public static T Field<T>(this DataRowView row, string fieldname)
 		{
