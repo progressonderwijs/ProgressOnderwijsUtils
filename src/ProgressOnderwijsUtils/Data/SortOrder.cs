a548fe8eba2d89eb72e856ac02d3f674fcac38ec
Eamon Nerbonne <eamon@nerbonne.org>
2010-10-28 09:48:33 +0000

    SortOrder refactored to not inherit from List<SortColumn>; this avoids accidental implementation hiding and permits more verbose method names - will be simplified somewhat once AutomaticDataSource is simplified.

--- src/ProgressOnderwijsUtils/Data/SortOrder.cs
+++ src/ProgressOnderwijsUtils/Data/SortOrder.cs
@@ -10,34 +10,76 @@ namespace ProgressOnderwijsUtils
 	/// Bevat gestuctureerde orderinginformatie
 	/// </summary>
 	[Serializable]
-	public class SortOrder : List<SortColumn>
+	public class SortOrder
 	{
+		readonly List<string> sortColumns = new List<string>();
+		readonly Dictionary<string, SortDirection> sortDirections = new Dictionary<string, SortDirection>();
+
+		public SortOrder() { }
+		public SortOrder(IEnumerable<SortColumn> leastSignificantFirst) { foreach (var sc in leastSignificantFirst) AddMostSignificant(sc); }
+		public SortOrder(params SortColumn[] leastSignificantFirst) { foreach (var sc in leastSignificantFirst) AddMostSignificant(sc); }
+
+
 		public SortColumn BaseSortOrder { set; get; }
 
-		public int OrderingIndex(string column, SortDirection direction)
+
+		/// <summary>
+		/// The rank of the column in the sort order; 1 is most significant, higher numbers less so; null is not included in the sort order.
+		/// </summary>
+		public int? ColumnSortRank(string column) { return sortDirections.ContainsKey(column) ? sortColumns.Count - sortColumns.IndexOf(column) : default(int?); }
+
+		public SortDirection? ColumnSortDirection(string column)
 		{
-			return IndexOf(new SortColumn(column, direction));
+			SortDirection retval;
+			return sortDirections.TryGetValue(column, out retval) ? retval : default(SortDirection?);
 		}
 
-		public void AddReverse(string kolomnaam)
+		static SortDirection FlipDirection(SortDirection dir) { return dir == SortDirection.Asc ? SortDirection.Desc : SortDirection.Asc; }
+
+		public void AddOrReverse(string kolomnaam) //TODO:Emn:eigenlijk is dit niet logisch voor een click, want je wilt ook "ongesorteerd" weer kunnen geven, en als je alleen een kolom "als eerste" wil sorteren maar niet de richting wijzigen is dit nu lastig.
 		{
-			if (Remove(new SortColumn(kolomnaam, SortDirection.Desc)))
-				Insert(0, new SortColumn(kolomnaam, SortDirection.Asc));
-			else
-			{
-				Remove(new SortColumn(kolomnaam, SortDirection.Asc));
-				Insert(0, new SortColumn(kolomnaam, SortDirection.Desc));
-			}
+			bool containedKolom = sortColumns.Remove(kolomnaam);
+			sortDirections[kolomnaam] = containedKolom ? FlipDirection(sortDirections[kolomnaam]) : SortDirection.Desc;
+			sortColumns.Add(kolomnaam);
 		}
+
+		public void AddMostSignificant(SortColumn kolom) { AddMostSignificant(kolom.Column, kolom.Direction); }
+		public void AddMostSignificant(string kolomnaam, SortDirection dir = SortDirection.Desc)
+		{
+			sortDirections.Add(kolomnaam, dir);
+			sortColumns.Add(kolomnaam);
+		}
+
+
+		public void AddLeastSignificant(SortColumn kolom) { AddLeastSignificant(kolom.Column, kolom.Direction); }
+		public void AddLeastSignificant(string kolomnaam, SortDirection dir = SortDirection.Desc)
+		{
+			sortDirections.Add(kolomnaam, dir);
+			sortColumns.Insert(0, kolomnaam);
+		}
+
+
+
+		public bool HasNonFallbackSorting { get { return sortColumns.Any(); } }
 
 		public IEnumerable<SortColumn> CompleteSortOrder
 		{
 			get
 			{
-				foreach (var sc in this) yield return sc;
-				if (BaseSortOrder != null) yield return BaseSortOrder;
+				return BaseSortOrder == null ? ManualLeastSignificantFirstOrder.Reverse() : ManualLeastSignificantFirstOrder.Reverse().Concat(new[] { BaseSortOrder });
 			}
 		}
+
+		public IEnumerable<SortColumn> ManualLeastSignificantFirstOrder { get { return sortColumns.Select(sc => new SortColumn(sc, sortDirections[sc])); } }
+
+		public void Clear()
+		{
+			sortDirections.Clear();
+			sortColumns.Clear();
+		}
+
+		public int ManualCount { get { return sortColumns.Count; } }
+
 	}
 
 	[Serializable]
