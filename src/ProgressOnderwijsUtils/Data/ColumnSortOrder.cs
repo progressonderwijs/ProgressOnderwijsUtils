a4edbc4530735b028aef157c361ae8b6d0a30da8
Eamon Nerbonne <eamon@nerbonne.org>
2010-10-28 14:00:04 +0000

    SortOrder vervangen door ColumnSortOrder.  Deze datastructuur is immutable - helper methods maken steeds nieuwe columnsortorders aan.  Hierdoor is het makkelijk overzicht te krijgen in wie wat wijzigd in GenericLijstManagerBase en AutomaticDataSource, is tevens persistance makkelijker, en is 't tevens vaak in een oogopslag te zien wat er gebeurd.
    BaseSortOrder uit SortOrder gehaald; dit is nu een veld van een DataSource (de default order, bij wijze van).  Hierdoor kunnen wij in principe custom orders los houden van evt. gepersiste orders.
    
    Persistance van sort-orders staat nog niet aan.

---
+++ src/ProgressOnderwijsUtils/Data/ColumnSortOrder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MoreLinq;
+
+namespace ProgressOnderwijsUtils
+{
+	/// <summary>
+	/// Representeerd de volgorde van sorteren op kolommen.  
+	/// 
+	/// Deze datastructuur is READONLY!  Als je functies als "Append" "ToggleSortDirection" uitvoerd wordt 
+	/// een NIEUWE ColumnSortOrder gereturned. 
+	/// 
+	/// ColumnSortOrder is een struct; zijn default waarde representeerd "geen sorteering".
+	/// </summary>
+	[Serializable]
+	public struct ColumnSortOrder : IEquatable<ColumnSortOrder>
+	{
+		readonly static SortColumn[] EmptyOrder = new SortColumn[] { };
+
+		readonly SortColumn[] sortColumns;
+		public IEnumerable<SortColumn> SortColumns { get { if (sortColumns != null)foreach (var sc in sortColumns)yield return sc; } }
+		IEnumerable<SortColumn> DirectSortColumns { get { return sortColumns ?? EmptyOrder; } }
+
+		public static ColumnSortOrder Empty { get { return default(ColumnSortOrder); } }
+		public ColumnSortOrder(IEnumerable<SortColumn> order) { sortColumns = order.ToArray(); }
+		public ColumnSortOrder(string column, SortDirection dir) { sortColumns = new[] { new SortColumn(column, dir) }; }
+
+		public SortColumn GetSortColumn(string column) { return DirectSortColumns.FirstOrDefault(sc => sc.Column == column); }
+		public SortDirection? GetColumnSortDirection(string column)
+		{
+			var sc = GetSortColumn(column);
+			return sc == null ? default(SortDirection?) : sc.Direction;
+		}
+		public int? GetColumnSortRank(string col)
+		{
+			int index = DirectSortColumns.Select(sc => sc.Column).IndexOf(col);
+			return index == -1 ? default(int?) : index + 1;
+		}
+
+		public int ColumnCount { get { return sortColumns == null ? 0 : sortColumns.Length; } }
+
+		public ColumnSortOrder ToggleSortDirection(string kolomnaam)
+		{
+			var oldSortCol = GetSortColumn(kolomnaam);
+			return oldSortCol != null
+				? FirstSortBy(oldSortCol.WithReverseDirection)
+				: new ColumnSortOrder(DirectSortColumns.Prepend(new SortColumn(kolomnaam, SortDirection.Desc)));
+		}
+
+		static IEnumerable<SortColumn> PrependFiltered(SortColumn head, IEnumerable<SortColumn> tail) { return head.Concat(tail.Where(sc => sc.Column != head.Column)); }
+
+		public ColumnSortOrder FirstSortBy(SortColumn firstby) { return firstby == null ? this : new ColumnSortOrder(PrependFiltered(firstby, DirectSortColumns)); }
+		public ColumnSortOrder ThenSortBy(SortColumn thenby) { return thenby == null || DirectSortColumns.Any(sc => sc.Column == thenby.Column) ? this : new ColumnSortOrder(DirectSortColumns.Prepend(thenby)); }
+		public ColumnSortOrder ThenSortBy(ColumnSortOrder thenby)
+		{
+			var mySet = new HashSet<string>(DirectSortColumns.Select(sc => sc.Column));
+			return new ColumnSortOrder(DirectSortColumns.Concat(thenby.DirectSortColumns.Where(sc => !mySet.Contains(sc.Column))));
+		}
+
+		public bool Equals(ColumnSortOrder other) { return DirectSortColumns.SequenceEqual(other.DirectSortColumns); }
+		public static bool operator ==(ColumnSortOrder a, ColumnSortOrder b) { return a.Equals(b); }
+		public static bool operator !=(ColumnSortOrder a, ColumnSortOrder b) { return !a.Equals(b); }
+		public override bool Equals(object obj) { return obj is ColumnSortOrder && Equals((ColumnSortOrder)obj); }
+		public override int GetHashCode() { return 12345 + DirectSortColumns.Select((sc, i) => (2 * i + 1) * sc.GetHashCode()).Sum(); }
+	}
+}
---
+++ src/ProgressOnderwijsUtils/Data/SortColumn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProgressOnderwijsUtils
+{
+
+	[Serializable]
+	public class SortColumn : IEquatable<SortColumn>
+	{
+		readonly string column;
+		readonly SortDirection direction;
+
+		public string Column { get { return column; } }
+		public SortDirection Direction { get { return direction; } }
+		public string SqlSortString { get { return column + " " + direction; } }
+
+		public SortColumn(string column, SortDirection direction) { this.column = column; this.direction = direction; }
+
+		public SortColumn WithReverseDirection { get { return new SortColumn(column, FlipDirection(direction)); } }
+		public SortColumn WithDifferentName(string newColumn) { return new SortColumn(newColumn, direction); }
+
+
+		static SortDirection FlipDirection(SortDirection dir) { return dir == SortDirection.Asc ? SortDirection.Desc : SortDirection.Asc; }
+
+		public bool Equals(SortColumn other) { return Column == other.Column && Direction == other.Direction; }
+		public override bool Equals(object obj) { return obj is SortColumn && Equals((SortColumn)obj); }
+		public override int GetHashCode() { return column.GetHashCode() + 51 * direction.GetHashCode(); }
+	}
+}
