b950f72823057628ab72608698cfb6a96069cfc3
Eamon Nerbonne <eamon@nerbonne.org>
2010-11-01 07:02:39 +0000

    Unchanged fields marked readonly

--- src/ProgressOnderwijsUtils/Data/Criterium.cs
+++ src/ProgressOnderwijsUtils/Data/Criterium.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,76 +8,66 @@ namespace ProgressOnderwijsUtils
 	[Serializable]
 	public class Criterium
 	{
-		string kolomnaam;
-		BooleanComparer comparer;
-		object waarde;
+		readonly string _KolomNaam;
+		readonly BooleanComparer _Comparer;
+		readonly object _Waarde;
 
-		public string KolomNaam { get { return kolomnaam; } set { kolomnaam = value; } }
-		public BooleanComparer Comparer { get { return comparer; } set { comparer = value; } }
-		public object Waarde { get { return waarde; } set { waarde = value; } }
+		public string KolomNaam { get { return _KolomNaam; } }
+		public BooleanComparer Comparer { get { return _Comparer; } }
+		public object Waarde { get { return _Waarde; } }
 
-		public static BooleanComparer[] StringComparers { get { return new BooleanComparer[] { BooleanComparer.Contains, BooleanComparer.Equal, BooleanComparer.NotEqual, BooleanComparer.StartsWith, BooleanComparer.IsNull, BooleanComparer.IsNotNull, BooleanComparer.In }; } }
-		public static BooleanComparer[] NumericComparers { get { return new BooleanComparer[] { BooleanComparer.Equal, BooleanComparer.GreaterThan, BooleanComparer.GreaterThanOrEqual, BooleanComparer.LessThan, BooleanComparer.LessThanOrEqual, BooleanComparer.NotEqual, BooleanComparer.IsNull, BooleanComparer.IsNotNull, BooleanComparer.In }; } }
+		public static BooleanComparer[] StringComparers { get { return new[] { BooleanComparer.Contains, BooleanComparer.Equal, BooleanComparer.NotEqual, BooleanComparer.StartsWith, BooleanComparer.IsNull, BooleanComparer.IsNotNull, BooleanComparer.In }; } }
+		public static BooleanComparer[] NumericComparers { get { return new[] { BooleanComparer.Equal, BooleanComparer.GreaterThan, BooleanComparer.GreaterThanOrEqual, BooleanComparer.LessThan, BooleanComparer.LessThanOrEqual, BooleanComparer.NotEqual, BooleanComparer.IsNull, BooleanComparer.IsNotNull, BooleanComparer.In }; } }
 
-		public Criterium(string kolomnaam, BooleanComparer comparer, object waarde)
-		{
-			this.kolomnaam = kolomnaam;
-			this.comparer = comparer;
-			this.waarde = waarde;
-		}
+		public Criterium(string kolomnaam, BooleanComparer comparer, object waarde) { _KolomNaam = kolomnaam; _Comparer = comparer; _Waarde = waarde; }
+
 		public string ToString(List<object> pars)
 		{
-			switch (comparer)
+			switch (Comparer)
 			{
 				case BooleanComparer.LessThan:
-					pars.Add(waarde);
-					return kolomnaam + "<" + "{" + (pars.Count - 1).ToString() + "}";
+					pars.Add(Waarde);
+					return KolomNaam + "<" + "{" + (pars.Count - 1) + "}";
 				case BooleanComparer.LessThanOrEqual:
-					pars.Add(waarde);
-					return kolomnaam + "<=" + "{" + (pars.Count - 1).ToString() + "}";
+					pars.Add(Waarde);
+					return KolomNaam + "<=" + "{" + (pars.Count - 1) + "}";
 				case BooleanComparer.Equal:
-					pars.Add(waarde);
-					return kolomnaam + "=" + "{" + (pars.Count - 1).ToString() + "}";
+					pars.Add(Waarde);
+					return KolomNaam + "=" + "{" + (pars.Count - 1) + "}";
 				case BooleanComparer.GreaterThanOrEqual:
-					pars.Add(waarde);
-					return kolomnaam + ">=" + "{" + (pars.Count - 1).ToString() + "}";
+					pars.Add(Waarde);
+					return KolomNaam + ">=" + "{" + (pars.Count - 1) + "}";
 				case BooleanComparer.GreaterThan:
-					pars.Add(waarde);
-					return kolomnaam + ">" + "{" + (pars.Count - 1).ToString() + "}";
+					pars.Add(Waarde);
+					return KolomNaam + ">" + "{" + (pars.Count - 1) + "}";
 				case BooleanComparer.NotEqual:
-					pars.Add(waarde);
-					return kolomnaam + "!=" + "{" + (pars.Count - 1).ToString() + "}";
+					pars.Add(Waarde);
+					return KolomNaam + "!=" + "{" + (pars.Count - 1) + "}";
 				case BooleanComparer.In:
-					string[] nrs = waarde.ToString().Split(new string[] { " ", "\r\n", "\n", "," }, StringSplitOptions.RemoveEmptyEntries);
-					string n = kolomnaam + " in (";
-					for (int i = 0; i < nrs.Length; i++)
-					{
-						pars.Add(nrs[i]);
-						n += "{" + (pars.Count - 1).ToString() + "}, ";
-					}
-					return n.Substring(0, n.Length-2) + ")";
+					string[] nrs = Waarde.ToString().Split(new[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+					string clause = KolomNaam + " in (" + Enumerable.Range(pars.Count, nrs.Length).Select(n => "{" + n + "}").JoinStrings(", ") + ")";
+					pars.AddRange(nrs);
+					return clause;
 				case BooleanComparer.StartsWith:
-					pars.Add(waarde.ToString() + "%");
-					return kolomnaam + " like " + "{" + (pars.Count - 1).ToString() + "}";
+					pars.Add(Waarde + "%");
+					return KolomNaam + " like " + "{" + (pars.Count - 1) + "}";
 				case BooleanComparer.Contains:
-					pars.Add("%" + waarde.ToString() + "%");
-					return kolomnaam + " like " + "{" + (pars.Count - 1).ToString() + "}";
+					pars.Add("%" + Waarde + "%");
+					return KolomNaam + " like " + "{" + (pars.Count - 1) + "}";
 				case BooleanComparer.IsNull:
-					return kolomnaam + " is null";
+					return KolomNaam + " is null";
 				case BooleanComparer.IsNotNull:
-					return kolomnaam + " is not null";
+					return KolomNaam + " is not null";
 				default:
 					throw new Exception("Geen geldige operator");
 			}
 		}
+
 		public override string ToString()
 		{
 			List<object> pars = new List<object>();
 			string s = ToString(pars);
-			object[] parsarray = new object[pars.Count];
-			for (int i = 0; i < pars.Count; ++i)
-				parsarray[i] = pars[i];
-			return string.Format(s, parsarray);
+			return string.Format(s, pars.ToArray());
 		}
 	}
 }
