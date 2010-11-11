495acc82c2ad310485b267ff345d213e9d3a6998
Eamon Nerbonne <eamon@nerbonne.org>
2010-11-11 18:39:52 +0000

    Boom structuur zonder expliciet lft/rgt op te geven (werkt nog wel ermee achter de schermen).

---
+++ src/ProgressOnderwijsUtils/ITreeStructure.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgressOnderwijsUtils
+{
+	public interface ITreeStructure
+	{
+		string Naam { get; }
+		int NodeId { get; }
+		IEnumerable<ITreeStructure> Children { get; }
+	}
+	public class MinimalTreeStructure : ITreeStructure
+	{
+		readonly string naam;
+		readonly int nodeid;
+		readonly MinimalTreeStructure[] children;
+		public MinimalTreeStructure(ITreeStructure source)
+		{
+			naam = source.Naam;
+			nodeid = source.NodeId;
+			children = source.Children.Select(child => new MinimalTreeStructure(child)).ToArray();
+		}
+
+		public string Naam { get { return naam; } }
+		public int NodeId { get { return nodeid; } }
+		public IEnumerable<ITreeStructure> Children { get { return children; } }
+	}
+	public static class TreeStructureExtensions
+	{
+		public static IEnumerable<ITreeStructure> DescendantsAndSelf(this ITreeStructure tree) { return Enumerable.Repeat(tree, 1).Concat(tree.Children.SelectMany(child => child.DescendantsAndSelf())); }
+	}
+}
+
