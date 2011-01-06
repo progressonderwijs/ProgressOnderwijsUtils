b249792a9afd6f5817886226c88496dd97f54976
Eamon Nerbonne <eamon@nerbonne.org>
2011-01-06 14:53:21 +0000

    En dan de laatste paar renames...

--- src/ProgressOnderwijsUtils/ExpressionParser.cs
+++ src/ProgressOnderwijsUtils/ExpressionParser.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿#if false
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Globalization;
+using NUnit.Framework;
 
 namespace ProgressOnderwijsUtils
 {
@@ -254,5 +257,78 @@ namespace ProgressOnderwijsUtils
 		{
 			return " \t\r\n".IndexOf(c) != -1;
 		}
-	}	
+	}
+#region Tests
+	[TestFixture]
+	public class ExpressionParserTest
+	{
+		[Test]
+
+		[TestCaseSource("Expressions")]
+		public decimal TestExpression(string expressie)
+		{
+			return ExpressionParser.Parse(expressie, Variabelen);
+		}
+
+		public decimal Variabelen(string variabele)
+		{
+			switch (variabele)
+			{
+				default:
+					return (decimal)0;
+				case "A":
+					return (decimal)1;
+				case "B":
+					return (decimal)2;
+				case "C":
+					return (decimal)8;
+				case "D":
+					return (decimal)10;
+				case "E":
+					return (decimal)12;
+			}
+		}
+
+		public IEnumerable Expressions
+		{
+			get
+			{
+				yield return new TestCaseData("1 + 2").Returns(3);
+				yield return new TestCaseData("1 - 2").Returns(-1);
+				yield return new TestCaseData("1 * 2").Returns(2);
+				yield return new TestCaseData("1 / 2").Returns(0.5);
+				yield return new TestCaseData("1 % 2").Returns(1);
+				yield return new TestCaseData("1 ^ 2").Returns(1);
+
+				yield return new TestCaseData("A + B").Returns(3);
+				yield return new TestCaseData("A - B").Returns(-1);
+				yield return new TestCaseData("A * B").Returns(2);
+				yield return new TestCaseData("A / B").Returns(0.5);
+				yield return new TestCaseData("A % B").Returns(1);
+				yield return new TestCaseData("A ^ B").Returns(1);
+
+				yield return new TestCaseData("1 + B").Returns(3);
+				yield return new TestCaseData("A - 2").Returns(-1);
+				yield return new TestCaseData("1 * B").Returns(2);
+				yield return new TestCaseData("A / 2").Returns(0.5);
+				yield return new TestCaseData("1 % B").Returns(1);
+				yield return new TestCaseData("A ^ 2").Returns(1);
+
+				yield return new TestCaseData("2 ^ 8").Returns(256);
+				yield return new TestCaseData("((((2 ^ 8))))").Returns(256);
+				yield return new TestCaseData("((12 - 10) + (2 ^ 8))").Returns(258);
+
+				yield return new TestCaseData("B ^ C").Returns(256);
+				yield return new TestCaseData("((((B ^ C))))").Returns(256);
+				yield return new TestCaseData("((E - D) + (B ^ C))").Returns(258);
+
+				yield return new TestCaseData("B ^ 8").Returns(256);
+				yield return new TestCaseData("((((2 ^ C))))").Returns(256);
+				yield return new TestCaseData("((12 - D) + (2 ^ C))").Returns(258);
+			}
+		}
+	}
+
+#endregion
 }
+#endif
