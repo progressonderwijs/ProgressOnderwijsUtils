f29b8ad8a5f0ea8f973842b4a0cec2efe5f5b000
Eamon Nerbonne <eamon@nerbonne.org>
2011-01-11 15:41:34 +0000

    Fixed TestNullability; added tests for getting non-generic name and value-type inside Nullable<xyz> constructed generic type.

--- test/ProgressOnderwijsUtilsTests/TypeExtensionTest.cs
+++ test/ProgressOnderwijsUtilsTests/TypeExtensionTest.cs
@@ -24,9 +24,12 @@ namespace ProgressOnderwijsUtilsTests
 			PAssert.That(() => typeof(IEnumerable<int>).CanBeNull());
 			PAssert.That(() => typeof(ServerContext).CanBeNull());
 			PAssert.That(() => !typeof(int).CanBeNull());
-			PAssert.That(() => !typeof(Enum).CanBeNull());
+			
 			PAssert.That(() => !typeof(DocumentType).CanBeNull());
 			PAssert.That(() => !typeof(SelectItem<int?>).CanBeNull());
+
+
+			PAssert.That(() => typeof(Enum).CanBeNull());//WTF???? maar goed, dit is dan ook wel een heel gek type.
 		}
 
 		[Test]
@@ -43,5 +46,23 @@ namespace ProgressOnderwijsUtilsTests
 			PAssert.That(() => typeof(SelectItem<int?>).BaseTypes().SequenceEqual(new[] { typeof(ValueType), typeof(object) }));
 			PAssert.That(() => typeof(ClieopRegelsLijstManager).BaseTypes().SequenceEqual(new[] { typeof(GenericDualLijstManager), typeof(GenericLijstManager), typeof(object) }));
 		}
+
+		[Test]
+		public void TestNullableGetter()
+		{
+			PAssert.That(() => typeof(int?).GetNullableBaseType() == typeof(int));
+			PAssert.That(() => typeof(int).GetNullableBaseType() ==null);
+			PAssert.That(() => typeof(string).GetNullableBaseType() == null);
+		}
+
+		[Test]
+		public void TestNonGenericName()
+		{
+			PAssert.That(() => typeof(int?).GetNonGenericName() == "System.Nullable");
+			PAssert.That(() => typeof(int).GetNonGenericName() == "System.Int32");
+			PAssert.That(() => typeof(string).GetNonGenericName() == "System.String");
+			PAssert.That(() => typeof(Func<Tuple<int, string>, bool>).GetNonGenericName() == "System.Func");
+		}
+
 	}
 }
