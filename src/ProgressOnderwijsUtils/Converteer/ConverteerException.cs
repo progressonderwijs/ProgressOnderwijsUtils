1514db929c1980c729c079e81c0205008b9a9d9d
Fons Dijkstra <f.j.dijkstra@rug.nl>
2011-01-14 09:08:42 +0000

    Refactored de Progress.Businbess.Converteer class: - er is nu een generieke ToString functie die de string representatie van een object geeft, met een (optionele) formatering string en een (optionele) taal. - en er is een generieke ToText functie die gebruik maakt van de ToString fucntie om een translatable terug te geven met daarin de string representaties voor de verschillende taken.
    Zie voor verdere functionele details de documentatie in de classe zelf.
    
    Qua gedrag is deze nieuwe implementatie precies het zelfde gebleven als de oude implementatie. (Ik heb alleen een bug opgelost in het representeren van een floating-point getal.)
    
    Wele moeten we even kijken naar de engelse vertalingen van met name datums en decimale getallen: deze zijn vaak gewoon in het nederlands. Deze issues zijn aan het licht gekomen door de unit-testen die ik erbij heb geschreven zodat we deze dan ook als functionele spec kunnen gaan gebruiken. Wordt vervolgd...
    
    Ook heb ik het gebruik van tooltips wat generieker gemaakt: naast de drie DatumFormaat formateringen die al een tooltip hadden gespecificeerd, is het nu mogelijk om een tooltip te specificeren voor elke ToText aanroep.
    
    Verder was het alleen mogelijk om strings tot een bepaalde lengte af te kappen, nu is dat meer generiek geimplementeerd voor alle types.
    
    Ik heb zoveel mogelijk gezocht naar mogelijke representatie verschillen in de schermen met deze nieuwe implementatie en heb er tot nu toe maar 1 gevonden (Anne: het lijkt niet helemaal goed te gaan met het vorderings bedrag in de financiele schermen) maar als jullie hier de komende tijd ook extra op zouden willen letten kunnen we ventuele problemen nog oplossen voordat we een nieuwe productie versie uit gaan brengen...

--- src/ProgressOnderwijsUtils/Converteer/ConverteerException.cs
+++ src/ProgressOnderwijsUtils/Converteer/ConverteerException.cs
@@ -1,16 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.Serialization;
 
 namespace ProgressOnderwijsUtils
 {
 	public class ConverteerException : ProgressNetException
 	{
-		public ConverteerException() : base() { }
-		public ConverteerException(string debugMessage) : base(debugMessage) { }
-		public ConverteerException(string debugMessage, Exception innerException) : base(debugMessage, innerException) { }
-		protected ConverteerException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+		public ConverteerException() 
+			: base()
+		{
+		}
+
+		public ConverteerException(string debugMessage) 
+			: base(debugMessage)
+		{
+		}
+			
+		public ConverteerException(string debugMessage, Exception innerException) 
+			: base(debugMessage, innerException)
+		{
+		}
+			
+		protected ConverteerException(SerializationInfo info, StreamingContext context) 
+			: base(info, context)
+		{
+		}
 	}
 }
