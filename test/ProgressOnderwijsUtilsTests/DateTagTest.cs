c66333e4bfc3bd3b9ebfab47e5eeeb74d448215c
Eamon Nerbonne <eamon@nerbonne.org>
2011-01-17 13:44:51 +0000

    Added test for DateTimeShortAgeTag (using in caching logic).  It's slightly evil since it uses DateTime.Now, but it's otherwise quite slow; hence it's nightly only.

---
+++ test/ProgressOnderwijsUtilsTests/DateTagTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExpressionToCodeLib;
+using NUnit.Framework;
+using ProgressOnderwijsUtils;
+using ProgressOnderwijsUtils.Test;
+
+namespace ProgressOnderwijsUtilsTests
+{
+	[TestFixture, NightlyOnly]
+	public class DateTagTest
+	{
+		[Test]
+		public void NoCollisionsSoon()
+		{
+			DateTime start = DateTime.Now;//slightly evil in unit test;
+			int seconds = (int)TimeSpan.FromDays(162).TotalSeconds;
+			var dates=Enumerable.Range(0, seconds).Select(s => start + TimeSpan.FromSeconds(s));
+			PAssert.That(() => dates.Count() > 10000000);
+			PAssert.That(() => !dates.Select(DateTimeShortAgeTag.ToAgeTagCaseInsensitive).ContainsDuplicates());
+			PAssert.That(() => !dates.Select(DateTimeShortAgeTag.ToAgeTag).ContainsDuplicates());
+		}
+	}
+}
