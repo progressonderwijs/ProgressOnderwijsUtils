518c5ffe173b599702b42283b5706320b96010bf
Eamon Nerbonne <eamon@nerbonne.org>
2011-01-17 15:22:22 +0000

    Wrote serialization-cloner to help test exceptions.

---
+++ src/ProgressOnderwijsUtils/CloneHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace ProgressOnderwijsUtils
+{
+	public static class SerializationCloner
+	{
+		public static T Clone<T>(T obj)
+		{
+			if (!typeof(T).IsSerializable) throw new ArgumentException("Type " + typeof(T) + " is not serializable and cannot be cloned", "obj");
+			var bf = new BinaryFormatter();
+			using (var ms = new MemoryStream())
+			{
+				bf.Serialize(ms, obj);
+				ms.Seek(0, SeekOrigin.Begin);
+				return (T)bf.Deserialize(ms);
+			}
+		}
+	}
+}
+
