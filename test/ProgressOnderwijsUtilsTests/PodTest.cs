8295a0ec9be2cd4725adaef1f18a98748bf565ca
Eamon Nerbonne <eamon@nerbonne.org>
2011-01-19 10:30:53 +0000

    OlapCommon test

--- test/ProgressOnderwijsUtilsTests/PodTest.cs
+++ test/ProgressOnderwijsUtilsTests/PodTest.cs
@@ -11,25 +11,26 @@ namespace ProgressOnderwijsUtilsTests
 	[TestFixture]
 	public class PodTest
 	{
-		public static void ComparePod(object a, object b){
-			
-			Type aType= a.GetType();
-			Type bType=b.GetType();
-			PAssert.That(()=>aType.GetProperties().Select(pi=>pi.Name).OrderBy(s=>s).SequenceEqual(bType.GetProperties().Select(pi=>pi.Name).OrderBy(s=>s)));
-			
+		public static void ComparePod(object a, object b)
+		{
+
+			Type aType = a.GetType();
+			Type bType = b.GetType();
+			PAssert.That(() => aType.GetProperties().Select(pi => pi.Name).OrderBy(s => s).SequenceEqual(bType.GetProperties().Select(pi => pi.Name).OrderBy(s => s)));
+
 			object[] empty = new object[0];
 
 			PAssert.That(
-				()=>
+				() =>
 					!(from aProp in aType.GetProperties()
-					join bProp in bType.GetProperties() on aProp.Name equals bProp.Name
-					where !Equals(aProp.GetValue(a,empty),bProp.GetValue(b,empty))
-					select aProp.Name).Any());
+					  join bProp in bType.GetProperties() on aProp.Name equals bProp.Name
+					  where !Equals(aProp.GetValue(a, empty), bProp.GetValue(b, empty))
+					  select aProp.Name).Any());
 		}
 		[Test]
 		public void SanityCheck()
 		{
-			Assert.Throws<PAssertFailedException>(()=>ComparePod(new LabelCode("abc", false), new { waarde = "abc", Vrijveld = false }));//case-sensitive
+			Assert.Throws<PAssertFailedException>(() => ComparePod(new LabelCode("abc", false), new { waarde = "abc", Vrijveld = false }));//case-sensitive
 			Assert.Throws<PAssertFailedException>(() => ComparePod(new LabelCode("abc", false), new { Waarde = "Abc", Vrijveld = false }));//value-sensitive
 			Assert.Throws<PAssertFailedException>(() => ComparePod(new LabelCode(null, false), new { Waarde = "", Vrijveld = false }));//no weirdness
 		}
@@ -41,5 +42,47 @@ namespace ProgressOnderwijsUtilsTests
 			ComparePod(new LabelCode(null, true), new { Waarde = default(string), Vrijveld = true });
 			ComparePod(new LabelCode("", true), new { Waarde = "", Vrijveld = true });
 		}
+
+		[Test]
+		public void OlapCommonTest()
+		{
+			ComparePod(
+				new OlapCommon(2, 3, 4, 5, false, OlapCommon.VoltijdType.Deeltijd, OlapCommon.EoiType.Eoi, OlapCommon.NrOplType.MeerOpl,
+					OlapCommon.Per1OktType.Alle, OlapCommon.VooroplType.NonVwo, OlapCommon.HerinschrijverType.HerinschrOpl,
+					OlapCommon.RijDimensieType.Cohorten, OlapCommon.CelSomType.AbsenPercPerRij, 9,
+					OlapCommon.StudieStaakType.AlleenstudiestakersOpl)
+					{
+					EcGrenswaarde = 42	
+					},
+				new
+				{
+					ExtraRijdimensie = "",
+					Rendement = false,
+					Samenvatting = true,
+					LangeLijst = false,
+					TopZoveel = false,
+					Organisatie = 2,
+					StartJaar = 3,
+					//StopJaar = stopjaar,//niet gebruikt
+					ExamenType = 5,
+					RijDimensie = OlapCommon.RijDimensieType.Cohorten,
+					VolofDeeltijd = OlapCommon.VoltijdType.Deeltijd,
+					isEoi = OlapCommon.EoiType.Eoi,
+					eenOpl = OlapCommon.NrOplType.MeerOpl,
+					per1Okt = OlapCommon.Per1OktType.Alle,
+					Vooropleiding = OlapCommon.VooroplType.NonVwo,
+					isHerinschrijver = OlapCommon.HerinschrijverType.HerinschrOpl,
+					ToonPerCel = OlapCommon.CelSomType.AbsenPercPerRij,
+					opbasisvanCohort = false,
+					StudiejaarNr = 9,
+					isStudiestaker = OlapCommon.StudieStaakType.AlleenstudiestakersOpl,
+					EerstejrNietdef = DateTime.Now.CollegeJaar(),//blech
+					ShowTijdsverloop = false,
+					EcGrenswaarde = 42
+				}
+				);
+		}
+
+
 	}
 }
