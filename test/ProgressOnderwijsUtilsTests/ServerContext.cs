5584647f23b9ac7c3923572890c45ae23fbc8e6a
Eamon Nerbonne <eamon@nerbonne.org>
2011-02-21 16:04:07 +0000

    (minor)  - converteer default(MyEnum) to MyEnum.None om te zorgen dat resharper niet in de war raakt  - spacing and details.

--- test/ProgressOnderwijsUtilsTests/ServerContext.cs
+++ test/ProgressOnderwijsUtilsTests/ServerContext.cs
@@ -15,6 +15,7 @@ namespace ProgressOnderwijsUtilsTests
 		public void CheckBasics()
 		{
 #pragma warning disable 612,618
+			//intentionally use default(EnumType) to verify that using unintialized enums cases exceptions.
 			PAssert.That(() => ServerContext.ConstructExplicitly(ServerLocation.MachineInDMZ, DatabaseVersion.DevTestDB) != ServerContext.ConstructExplicitly(ServerLocation.MachineInDMZ, DatabaseVersion.TestDB));
 			Assert.Throws<ArgumentException>(() => ServerContext.ConstructExplicitly(default(ServerLocation), DatabaseVersion.DevTestDB));
 			Assert.Throws<ArgumentException>(() => ServerContext.ConstructExplicitly(ServerLocation.MachineInDMZ, default(DatabaseVersion)));
