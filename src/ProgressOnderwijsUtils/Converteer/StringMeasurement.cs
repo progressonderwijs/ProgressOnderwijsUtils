c8856f7ca7844205616095f58859d5f78cb2fc08
Eamon Nerbonne <eamon@nerbonne.org>
2011-02-25 12:41:51 +0000

    disable gdi-based string measurement.
    Als je dus een Converteer.ToString(...) zoekt die vroeger een maxWidth had, kun je StringMeasurement.LimitDisplayLength doen - deze kapt een string af na die een aantal characters, rekeninghoudend met font-display.

--- src/ProgressOnderwijsUtils/Converteer/StringMeasurement.cs
+++ src/ProgressOnderwijsUtils/Converteer/StringMeasurement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//#define ENABLE_GDI_MEASUREMENT
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -7,11 +8,12 @@ using System.Windows.Media;
 using NUnit.Framework;
 using ExpressionToCodeLib;
 
-namespace ProgressOnderwijsUtils.Converteer
+namespace ProgressOnderwijsUtils
 {
 	public static class StringMeasurement
 	{
-		public static string LimitTextLengthGdi(string s, int maxWidth)
+#if ENABLE_GDI_MEASUREMENT
+		public static string LimitDLengthGdi(string s, int maxWidth)
 		{
 			using (var bmp = new Bitmap(1, 1))
 			using (Graphics g = Graphics.FromImage(bmp))
@@ -29,14 +31,15 @@ namespace ProgressOnderwijsUtils.Converteer
 				return g.MeasureString(s.Replace('\n', ' ').Replace('\t', ' '), new Font("Verdana", 8)).Width / pix_per_char * ems_per_char;
 			}
 		}
-		public static double MeasureWpf(string str) { return str.Sum(c => char_to_width[c]); }
-		public static string LimitTextLengthWpf(string s, int maxWidth) { return LimitTextLength(s, maxWidth).Item1; }
+		const double pix_per_char = 7.0;
+#endif
+		public static double Measure(string str) { return str.Sum(c => char_to_width[c]); }
+		public static string LimitDisplayLength(string s, int maxWidth) { return LimitTextLength(s, maxWidth).Item1; }
 		public static Tuple<string, bool> LimitTextLength(string s, int maxWidth) { return ElideIfNecessary(s, maxWidth * ems_per_char); }
-		static Tuple<string, bool> ElideIfNecessary(string s, double ems) { return MeasureWpf(s) < ems ? Tuple.Create(s, false) : Tuple.Create(TrimToEms(s, ems - ellipsis_ems) + ellipsis, true); }
+		static Tuple<string, bool> ElideIfNecessary(string s, double ems) { return Measure(s) < ems ? Tuple.Create(s, false) : Tuple.Create(TrimToEms(s, ems - ellipsis_ems) + ellipsis, true); }
 		static string TrimToEms(string s, double ems) { return s.Substring(0, CanFitChars(s, ems)); }
 
 		const double ems_per_char = 0.638;
-		const double pix_per_char = 7.0;
 		const string ellipsis = "...";//zou ook … kunnen zijn (unicode ellipsis)
 		static readonly double ellipsis_ems;
 		static readonly double[] char_to_width;
@@ -49,7 +52,7 @@ namespace ProgressOnderwijsUtils.Converteer
 					let characterHasSymbol = gFont.CharacterToGlyphMap.ContainsKey(c)
 					select characterHasSymbol ? gFont.AdvanceWidths[gFont.CharacterToGlyphMap[c]] : 0.0
 				).ToArray();
-			ellipsis_ems = MeasureWpf(ellipsis);
+			ellipsis_ems = Measure(ellipsis);
 		}
 		static int CanFitChars(string str, double ems)
 		{
@@ -66,36 +69,38 @@ namespace ProgressOnderwijsUtils.Converteer
 	[TestFixture]
 	public class StringMeasurementTest
 	{
+#if ENABLE_GDI_MEASUREMENT
 		[Test]
 		public void TestGdiVersion()
 		{
-			PAssert.That(() => StringMeasurement.LimitTextLengthGdi("Dit is een wat langer verhaal, en zal afgebroken worden: en dat gebeurt hier!", 60) == "Dit is een wat langer verhaal, en zal afgebroken worden: en dat gebeurt ...");
+			PAssert.That(() => StringMeasurement.LimitDLengthGdi("Dit is een wat langer verhaal, en zal afgebroken worden: en dat gebeurt hier!", 60) == "Dit is een wat langer verhaal, en zal afgebroken worden: en dat gebeurt ...");
 
-			PAssert.That(() => StringMeasurement.LimitTextLengthGdi("123456789", 5) == "12...");
-			PAssert.That(() => StringMeasurement.LimitTextLengthGdi("0,123456789", 5) == "0,...");
-			PAssert.That(() => StringMeasurement.LimitTextLengthGdi("1.024", 5) == "1....");
-			PAssert.That(() => StringMeasurement.LimitTextLengthGdi("1.02", 5) == "1.02");
-			PAssert.That(() => StringMeasurement.LimitTextLengthGdi("testjes", 5) == "test...");
-			PAssert.That(() => StringMeasurement.LimitTextLengthGdi("empty.pdf", 5) == "em...");
-			PAssert.That(() => StringMeasurement.LimitTextLengthGdi("wheee!", 5) == "wh...");
-			PAssert.That(() => StringMeasurement.LimitTextLengthGdi("wheee!\ntwolines!", 5) == "wh...");
+			PAssert.That(() => StringMeasurement.LimitDLengthGdi("123456789", 5) == "12...");
+			PAssert.That(() => StringMeasurement.LimitDLengthGdi("0,123456789", 5) == "0,...");
+			PAssert.That(() => StringMeasurement.LimitDLengthGdi("1.024", 5) == "1....");
+			PAssert.That(() => StringMeasurement.LimitDLengthGdi("1.02", 5) == "1.02");
+			PAssert.That(() => StringMeasurement.LimitDLengthGdi("testjes", 5) == "test...");
+			PAssert.That(() => StringMeasurement.LimitDLengthGdi("empty.pdf", 5) == "em...");
+			PAssert.That(() => StringMeasurement.LimitDLengthGdi("wheee!", 5) == "wh...");
+			PAssert.That(() => StringMeasurement.LimitDLengthGdi("wheee!\ntwolines!", 5) == "wh...");
 
-			PAssert.That(() => StringMeasurement.LimitTextLengthGdi("wheee!\ntwolines!", 10) == "wheee!\nt...");
+			PAssert.That(() => StringMeasurement.LimitDLengthGdi("wheee!\ntwolines!", 10) == "wheee!\nt...");
 		}
+#endif
 		[Test]
 		public void TestWpfVersion()
 		{
-			PAssert.That(() => StringMeasurement.LimitTextLengthWpf("Dit is een wat langer verhaal, en zal afgebroken worden: en dat gebeurt hier!", 60) == "Dit is een wat langer verhaal, en zal afgebroken worden: en dat gebeurt ...");
+			PAssert.That(() => StringMeasurement.LimitDisplayLength("Dit is een wat langer verhaal, en zal afgebroken worden: en dat gebeurt hier!", 60) == "Dit is een wat langer verhaal, en zal afgebroken worden: en dat gebeurt ...");
 
-			PAssert.That(() => StringMeasurement.LimitTextLengthWpf("123456789", 5) == "123...");
-			PAssert.That(() => StringMeasurement.LimitTextLengthWpf("0,123456789", 5) == "0,1...");
-			PAssert.That(() => StringMeasurement.LimitTextLengthWpf("1.024", 5) == "1.024");
-			PAssert.That(() => StringMeasurement.LimitTextLengthWpf("testjes", 5) == "test...");
-			PAssert.That(() => StringMeasurement.LimitTextLengthWpf("empty.pdf", 5) == "em...");
-			PAssert.That(() => StringMeasurement.LimitTextLengthWpf("wheee!", 5) == "whe...");
-			PAssert.That(() => StringMeasurement.LimitTextLengthWpf("wheee!\ntwolines!", 5) == "whe...");
+			PAssert.That(() => StringMeasurement.LimitDisplayLength("123456789", 5) == "123...");
+			PAssert.That(() => StringMeasurement.LimitDisplayLength("0,123456789", 5) == "0,1...");
+			PAssert.That(() => StringMeasurement.LimitDisplayLength("1.024", 5) == "1.024");
+			PAssert.That(() => StringMeasurement.LimitDisplayLength("testjes", 5) == "test...");
+			PAssert.That(() => StringMeasurement.LimitDisplayLength("empty.pdf", 5) == "em...");
+			PAssert.That(() => StringMeasurement.LimitDisplayLength("wheee!", 5) == "whe...");
+			PAssert.That(() => StringMeasurement.LimitDisplayLength("wheee!\ntwolines!", 5) == "whe...");
 
-			PAssert.That(() => StringMeasurement.LimitTextLengthWpf("wheee!\ntwolines!", 10) == "wheee!\ntw...");
+			PAssert.That(() => StringMeasurement.LimitDisplayLength("wheee!\ntwolines!", 10) == "wheee!\ntw...");
 		}
 	}
 }
