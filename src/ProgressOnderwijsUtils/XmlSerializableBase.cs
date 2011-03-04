330732a2eadaec7b3861f58b09b1a6b7f13d1e81
Eamon Nerbonne <eamon@nerbonne.org>
2011-03-04 13:02:12 +0000

    Added utility helper file to aid parsing simple xml files that don't have an XSD.

---
+++ src/ProgressOnderwijsUtils/XmlSerializableBase.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace ProgressOnderwijsUtils
+{
+	public class XmlSerializableBase<T> where T : XmlSerializableBase<T>
+	{
+		readonly static XmlSerializer serializer = new XmlSerializer(typeof(T));
+		public static T Deserialize(XmlReader from) { return (T)serializer.Deserialize(from); }
+		public static T Deserialize(XDocument from) { using (var reader = from.CreateReader()) return Deserialize(reader); }
+		public static T Deserialize(XElement from) {  return Deserialize(new XDocument(from)); }
+		public void SerializeTo(Stream s) { serializer.Serialize(s, this); }
+		public void SerializeTo(TextWriter w) { serializer.Serialize(w, this); }
+		public void SerializeTo(XmlWriter xw) { serializer.Serialize(xw, this); }
+	}
+}
