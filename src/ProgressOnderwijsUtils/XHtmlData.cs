57a1db8ccff3bb68ced366f349d00b6e9a240019
Fons Dijkstra <f.j.dijkstra@rug.nl>
2011-05-24 15:15:04 +0000

    Fix the build (forgot a file)

---
+++ src/ProgressOnderwijsUtils/XHtmlData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ProgressOnderwijsUtils
+{
+	public struct XHtmlData
+	{
+		public XNode[] nodes;
+
+		public static XHtmlData Parse(string s)
+		{
+			return new XHtmlData
+			{
+				nodes = HtmlTidyWrapper.HtmlSanitizer(s).Nodes().ToArray(),
+			};
+		}
+
+		public string ToUiString()
+		{
+			using (StringWriter writer = new StringWriter())
+			using (XmlWriter inner = XmlWriter.Create(writer, new XmlWriterSettings
+				{
+					ConformanceLevel = ConformanceLevel.Fragment,
+				}))
+			{
+				foreach (var node in nodes.EmptyIfNull())
+				{
+					node.WriteTo(inner);
+				}
+				inner.Close();
+				return writer.ToString();
+			}
+		}
+	}
+}
