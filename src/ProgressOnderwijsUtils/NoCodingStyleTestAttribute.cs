f41ea59497f158389d430813a316f64edebcc82b
Eamon Nerbonne <eamon@nerbonne.org>
2011-07-07 15:56:21 +0000

    Add attribute to suppress coding style tests and instead output compile time warning.

---
+++ src/ProgressOnderwijsUtils/NoCodingStyleTestAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgressOnderwijsUtils
+{
+	[AttributeUsage(AttributeTargets.Class,Inherited=false)]
+	public class NoCodingStyleTestAttribute : Attribute
+	{
+	}
+}
