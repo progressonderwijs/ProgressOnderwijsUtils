940ea0502f022775fcf6e3362cdd46cf4dbeffb9
Fons Dijkstra <f.j.dijkstra@rug.nl>
2011-12-01 13:24:53 +0000

    Added log4net logging in a similar manner to all web application projects: - config is placed within a log4net.config file - this file is editable at runtime (!) - log files are placed by default in the c:\inetpub\logs\Progress.NET directory - depending on the assembly location, a sub directory will be created (to avoid multiple web applications on the same machine logging to the same file)

---
+++ src/ProgressOnderwijsUtils/Log4Net/RollingFileAppender.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using log4net.Layout;
+
+namespace ProgressOnderwijsUtils.Log4Net
+{
+	public class RollingFileAppender : log4net.Appender.RollingFileAppender
+	{
+		private const string PATH = @"C:\inetpub\logs\Progress.NET";
+
+		private static readonly IDictionary<DatabaseVersion, string> PATHS = new Dictionary<DatabaseVersion, string>
+		{
+			{ DatabaseVersion.Undefined, "." },
+			{ DatabaseVersion.ProductieDB, "productie" },
+			{ DatabaseVersion.TestDB, "test" },
+			{ DatabaseVersion.DevTestDB, "ontwikkel" },
+			{ DatabaseVersion.AcceptatieDB, "acceptatie" },
+			{ DatabaseVersion.BronHODB, "bronho" },
+		}; 
+
+		public RollingFileAppender()
+		{
+			Encoding = Encoding.UTF8;
+			RollingStyle = RollingMode.Size;
+			AppendToFile = false;
+			MaximumFileSize = "256MB";
+			MaxSizeRollBackups = 3;
+			Layout = new PatternLayout("%utcdate{ISO8601} [%-4thread] %-5level %-50.50logger: %message%newline");
+		}
+
+		public override string File
+		{
+			get
+			{
+				return base.File;
+			}
+			set
+			{
+				string path = PATH;
+				if (Path.IsPathRooted(value))
+				{
+					path = Path.GetDirectoryName(value);
+				}
+				base.File = Path.GetFullPath(Path.Combine(path, Context, Path.GetFileName(value)));
+			}
+		}
+
+		private string Context
+		{
+			get
+			{
+				Assembly assembly = Assembly.GetAssembly(typeof(RollingFileAppender));
+				return PATHS[ServerContext.GuessDatabaseVersionFromExecutingAssembly(assembly, DatabaseVersion.Undefined)];
+			}
+		}
+	}
+}
---
+++ test/ProgressOnderwijsUtilsTests/Log4Net.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using ProgressOnderwijsUtils.Log4Net;
+
+namespace ProgressOnderwijsUtilsTests
+{
+	[TestFixture]
+	public class RollingFileAppenderTest
+	{
+		private RollingFileAppender sut;
+
+		[SetUp]
+		public void SetUp()
+		{
+			sut = new RollingFileAppender();
+		}
+
+		[Test]
+		[TestCase("test.log", @"C:\inetpub\logs\Progress.NET\test.log")]
+		[TestCase(@"C:\test.log", @"C:\test.log")]
+		[TestCase(@"C:\log\test.log", @"C:\log\test.log")]
+		public void File(string value, string expected)
+		{
+			sut.File = value;
+			Assert.That(sut.File, Is.EqualTo(expected));
+		}
+	}
+}
