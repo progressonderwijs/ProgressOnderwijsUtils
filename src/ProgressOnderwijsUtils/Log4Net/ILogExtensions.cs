223800c7488a0e791261cb7e9d9dd3c5bc9a2224
Fons Dijkstra <f.j.dijkstra@rug.nl>
2011-12-01 15:36:47 +0000

    Added log4net.ILog extensions to avoid calling the IsLevelEnabled properties while not impacting the performance

---
+++ src/ProgressOnderwijsUtils/Log4Net/ILogExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using log4net;
+
+namespace ProgressOnderwijsUtils.Log4Net
+{
+	public static class ILogExtensions
+	{
+		public static void Debug(this ILog log, Func<object> msg)
+		{
+			if (log.IsDebugEnabled) log.Debug(msg());
+		}
+
+		public static void Debug(this ILog log, Func<object> msg, Exception exception)
+		{
+			if (log.IsDebugEnabled) log.Debug(msg(), exception);
+		}
+
+		public static void Info(this ILog log, Func<object> msg)
+		{
+			if (log.IsInfoEnabled) log.Info(msg());
+		}
+
+		public static void Info(this ILog log, Func<object> msg, Exception exception)
+		{
+			if (log.IsInfoEnabled) log.Info(msg(), exception);
+		}
+
+		public static void Warn(this ILog log, Func<object> msg)
+		{
+			if (log.IsWarnEnabled) log.Warn(msg());
+		}
+
+		public static void Warn(this ILog log, Func<object> msg, Exception exception)
+		{
+			if (log.IsWarnEnabled) log.Warn(msg(), exception);
+		}
+
+		public static void Error(this ILog log, Func<object> msg)
+		{
+			if (log.IsErrorEnabled) log.Error(msg());
+		}
+
+		public static void Error(this ILog log, Func<object> msg, Exception exception)
+		{
+			if (log.IsErrorEnabled) log.Error(msg(), exception);
+		}
+
+		public static void Fatal(this ILog log, Func<object> msg)
+		{
+			if (log.IsFatalEnabled) log.Fatal(msg());
+		}
+
+		public static void Fatal(this ILog log, Func<object> msg, Exception exception)
+		{
+			if (log.IsFatalEnabled) log.Fatal(msg(), exception);
+		}
+	}
+}
--- test/ProgressOnderwijsUtilsTests/Log4Net.cs
+++ test/ProgressOnderwijsUtilsTests/Log4Net.cs
@@ -2,6 +2,9 @@
 using System.Linq;
 using NUnit.Framework;
 using ProgressOnderwijsUtils.Log4Net;
+using log4net;
+using log4net.Core;
+using log4net.Repository.Hierarchy;
 
 namespace ProgressOnderwijsUtilsTests
 {
@@ -24,6 +27,63 @@ namespace ProgressOnderwijsUtilsTests
 		{
 			sut.File = value;
 			Assert.That(sut.File, Is.EqualTo(expected));
+		}
+	}
+
+	[TestFixture]
+	public class ILogExtensionsTest
+	{
+		private ILog sut;
+
+		[SetUp]
+		public void SetUp()
+		{
+			sut = LogManager.GetLogger(typeof(ILogExtensionsTest));
+		}
+
+		private void SetLevel(Level level)
+		{
+			((Logger)sut.Logger).Level = level;
+		}
+
+		[Test]
+		public void Debug()
+		{
+			SetLevel(Level.Info);
+			sut.Debug(() => { throw new Exception("unexpected");});	
+			sut.Debug(() => { throw new Exception("unexpected");}, new Exception());	
+		}
+
+		[Test]
+		public void Info()
+		{
+			SetLevel(Level.Warn);
+			sut.Info(() => { throw new Exception("unexpected");});	
+			sut.Info(() => { throw new Exception("unexpected");}, new Exception());	
+		}
+
+		[Test]
+		public void Warn()
+		{
+			SetLevel(Level.Error);
+			sut.Warn(() => { throw new Exception("unexpected");});	
+			sut.Warn(() => { throw new Exception("unexpected");}, new Exception());	
+		}
+
+		[Test]
+		public void Error()
+		{
+			SetLevel(Level.Fatal);
+			sut.Error(() => { throw new Exception("unexpected");});	
+			sut.Error(() => { throw new Exception("unexpected");}, new Exception());	
+		}
+
+		[Test]
+		public void Fatal()
+		{
+			SetLevel(Level.Off);
+			sut.Fatal(() => { throw new Exception("unexpected");});	
+			sut.Fatal(() => { throw new Exception("unexpected");}, new Exception());	
 		}
 	}
 }
