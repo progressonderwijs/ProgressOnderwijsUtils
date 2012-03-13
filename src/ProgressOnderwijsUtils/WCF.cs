6d07794120cd98d7e8aff1895a2eb3c457b57a88
Fons Dijkstra <f.j.dijkstra@rug.nl>
2012-03-13 10:42:35 +0000

    Moved the WCF client handle implementation from the gadgets project to the tools project so it can be reused.

---
+++ src/ProgressOnderwijsUtils/WCF.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace ProgressOnderwijsUtils
+{
+	public interface IClientHandle<out T>
+	{
+		void Call(Action<T> call);
+		TR Call<TR>(Func<T, TR> call);
+	}
+
+	public class ClientHandle<T, TB> : IClientHandle<T>, IDisposable 
+		where T : class where TB : ClientBase<T>, T
+	{
+		private readonly object monitor;
+		private readonly Func<TB> factory;
+		private TB client;
+
+		public ClientHandle(Func<TB> factory)
+		{
+			monitor = new object();
+			this.factory = factory;
+			Create();
+		}
+
+		private void Create()
+		{
+			client = factory();
+			client.InnerChannel.Faulted += Faulted;
+		}
+
+		private void Faulted(object sender, EventArgs e)
+		{
+			lock (monitor)
+			{
+				client.Abort();
+				Create();
+			}
+		}
+
+		#region Implementation of IDisposable
+
+		public void Dispose()
+		{
+			try
+			{
+				client.Close();
+			}
+			catch 
+			{
+				client.Abort();
+			}
+		}
+
+		#endregion
+
+		#region Implementation of IClientHandler<out TU>
+
+		public void Call(Action<T> call)
+		{
+			try
+			{
+				call(client);
+			}
+			catch (FaultException)
+			{
+				throw;
+			}
+			catch (CommunicationException)
+			{
+				// could be timeout on the channel, retry
+				call(client);
+			}
+		}
+
+		public TR Call<TR>(Func<T, TR> call)
+		{
+			try
+			{
+				return call(client);
+			}
+			catch (FaultException)
+			{
+				throw;
+			}
+			catch (CommunicationException)
+			{
+				// could be timeout on the channel, retry
+				return call(client);
+			}
+		}
+
+		#endregion
+	}
+}
