0deed768beec66c61db7513a92b601ac3089b947
Eamon Nerbonne <eamon@nerbonne.org>
2012-03-23 16:14:41 +0000

    Refactor MetaProperty to include accessors directly inline and also to expose strongly typed (more efficient) Accessors. also some minor renames.

--- src/ProgressOnderwijsUtils/MetaObject/MetaObject.cs
+++ src/ProgressOnderwijsUtils/MetaObject/MetaObject.cs
@@ -12,9 +12,9 @@ namespace ProgressOnderwijsUtils
 
 	public static class MetaObject
 	{
-		public static IEnumerable<MetaProperty> GetMetaProperties(this IMetaObject metaobj) { return GetCache(metaobj.GetType()).Properties; }
+		public static IEnumerable<IMetaProperty> GetMetaProperties(this IMetaObject metaobj) { return GetCache(metaobj.GetType()).Properties; }
 		public static object DynamicGet(this IMetaObject metaobj, string propertyName) { return GetCache(metaobj.GetType()).DynGet(metaobj, propertyName); }
-		public static IEnumerable<MetaProperty> GetMetaProperties<T>() where T : IMetaObject { return MetaPropCache<T>.properties; }
+		public static IEnumerable<IMetaProperty> GetMetaProperties<T>() where T : IMetaObject { return MetaPropCache<T>.properties; }
 
 		public static DataTable ToDataTable<T>(IEnumerable<T> objs, string[] primaryKey) where T : IMetaObject
 		{
@@ -23,14 +23,14 @@ namespace ProgressOnderwijsUtils
 			dt.Columns.AddRange(properties.Select(mp => new DataColumn(mp.Naam, mp.DataType.StripNullability()) { AllowDBNull = !mp.Verplicht && mp.DataType.CanBeNull() }).ToArray());
 
 			foreach (var obj in objs)
-				dt.Rows.Add(properties.Select(mp => mp.Accessors.Getter(obj) ?? DBNull.Value).ToArray());
+				dt.Rows.Add(properties.Select(mp => mp.Getter(obj) ?? DBNull.Value).ToArray());
 
 			if (primaryKey != null)
 				dt.PrimaryKey = primaryKey.Select(name => dt.Columns[name]).ToArray();
 			return dt;
 		}
 
-		public static IEnumerable<MetaProperty> GetMetaProperties(Type t)
+		public static IEnumerable<IMetaProperty> GetMetaProperties(Type t)
 		{
 			if (!typeof(IMetaObject).IsAssignableFrom(t))
 				throw new InvalidOperationException("Can't get meta-properties from type " + t + ", it's not a " + typeof(IMetaObject));
@@ -43,23 +43,23 @@ namespace ProgressOnderwijsUtils
 
 		interface IMetaPropCache
 		{
-			MetaProperty[] Properties { get; }
+			IMetaProperty[] Properties { get; }
 			object DynGet(IMetaObject obj, string propertyName);
 			void DynSet(IMetaObject obj, string propertyName, object val);
 		}
 
 		sealed class MetaPropCache<T> : IMetaPropCache
 		{
-			public readonly static MetaProperty[] properties;
+			public readonly static IMetaProperty[] properties;
 			static MetaPropCache() { properties = GetMetaPropertiesImpl(typeof(T)).ToArray(); }
-			public MetaProperty[] Properties { get { return properties; } }
-			public object DynGet(IMetaObject obj, string propertyName) { return properties.Single(prop => prop.Naam == propertyName).Accessors.Getter(obj); }
-			public void DynSet(IMetaObject obj, string propertyName, object val) { properties.Single(prop => prop.Naam == propertyName).Accessors.Setter(obj, val); }
+			public IMetaProperty[] Properties { get { return properties; } }
+			public object DynGet(IMetaObject obj, string propertyName) { return properties.Single(prop => prop.Naam == propertyName).Getter(obj); }
+			public void DynSet(IMetaObject obj, string propertyName, object val) { properties.Single(prop => prop.Naam == propertyName).Setter(obj, val); }
 		}
 		#endregion
 
 		#region private Helpers
-		static IEnumerable<MetaProperty> GetMetaPropertiesImpl(Type t) { return t.GetProperties().OrderBy(pi=>pi.MetadataToken).Select(MetaProperty.LoadIfMetaProperty).Where(mp => mp != null); }
+		static IEnumerable<IMetaProperty> GetMetaPropertiesImpl(Type t) { return t.GetProperties().OrderBy(pi=>pi.MetadataToken).Select(MetaProperty.LoadIfMetaProperty).Where(mp => mp != null); }
 		#endregion
 	}
 }
--- src/ProgressOnderwijsUtils/MetaObject/MetaProperty.cs
+++ src/ProgressOnderwijsUtils/MetaObject/MetaProperty.cs
@@ -1,79 +1,141 @@
 ﻿using System;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using ExpressionToCodeLib;
 using ProgressOnderwijsUtils;
 
 namespace ProgressOnderwijsUtils
 {
-	public sealed class MetaProperty
+	public interface IMetaProperty
 	{
-		public readonly string Naam;
-		public readonly IPropertyAccessors Accessors;
-		public readonly int Volgorde;
-		public readonly bool Verplicht;
-		public readonly bool AllowNull;
-		public readonly int Lengte;
-		public readonly string Regex;
-		public readonly ITranslatable Label;
-		public readonly string KoppelTabelNaam;
-		public readonly bool IsReadonly;
-		public readonly bool ShowDefaultOnNew;
-		public Type DataType { get { return Accessors.PropertyType; } }
+		string Naam { get; }
+		Func<object, object> Getter { get; }
+		Action<object, object> Setter { get; }
+		int Volgorde { get; }
+		bool Verplicht { get; }
+		bool AllowNull { get; }
+		int Lengte { get; }
+		string Regex { get; }
+		ITranslatable Label { get; }
+		string KoppelTabelNaam { get; }
+		bool IsReadonly { get; }
+		bool ShowDefaultOnNew { get; }
+		bool CanRead { get; }
+		Type DataType { get; }
+	}
 
-
-		public MetaProperty(IPropertyAccessors accessors, string naam, int volgorde, ITranslatable label, string koppelTabelNaam, bool verplicht, bool allowNull, bool isReadonly, int lengte, string regex, bool showDefaultOnNew)
+	public static class MetaProperty
+	{
+		public sealed class Impl<TOwner> : IMetaProperty
 		{
-			Accessors = accessors;
-			Verplicht = verplicht;
-			AllowNull = allowNull;
-			Naam = naam;
-			Volgorde = volgorde;
-			Lengte = lengte;
-			Regex = regex;
-			Label = label;
-			KoppelTabelNaam = koppelTabelNaam;
-			IsReadonly = isReadonly || accessors.Setter == null;
-			ShowDefaultOnNew = showDefaultOnNew;
-		}
+			readonly string naam;
+			public string Naam { get { return naam; } }
 
-		MetaProperty(PropertyInfo pi, int implicitOrder)
-		{
-			Accessors = GetAccessors(pi);
-			Naam = pi.Name;
-			var mpVolgordeAttribute = Attr<MpVolgordeAttribute>(pi);
-			Volgorde = mpVolgordeAttribute == null ? implicitOrder * 10 : mpVolgordeAttribute.Volgorde;
-			Label = new[] {
+			readonly int volgorde;
+			public int Volgorde { get { return volgorde; } }
+
+			readonly bool verplicht;
+			public bool Verplicht { get { return verplicht; } }
+
+			readonly bool allowNull;
+			public bool AllowNull { get { return allowNull; } }
+
+			readonly int lengte;
+			public int Lengte { get { return lengte; } }
+
+			readonly string regex;
+			public string Regex { get { return regex; } }
+
+			readonly ITranslatable label;
+			public ITranslatable Label { get { return label; } }
+
+			readonly string koppelTabelNaam;
+			public string KoppelTabelNaam { get { return koppelTabelNaam; } }
+
+			readonly bool isReadonly;
+			public bool IsReadonly { get { return isReadonly; } }
+
+			readonly bool showDefaultOnNew;
+			public bool ShowDefaultOnNew { get { return showDefaultOnNew; } }
+
+			public Type DataType { get { return propertyInfo.PropertyType; } }
+
+			readonly PropertyInfo propertyInfo;
+
+			public Impl(PropertyInfo pi, int implicitOrder)
+			{
+				propertyInfo = pi;
+
+				ParameterExpression objParamExpr = Expression.Parameter(typeof(object), "propertyOwner");
+				MemberExpression propExpr = Expression.Property(Expression.Convert(objParamExpr, typeof(TOwner)), pi);
+				getter = Expression.Lambda<Func<object, object>>(Expression.Convert(propExpr, typeof(object)), objParamExpr).Compile();
+
+
+				ParameterExpression typedParamExpr = Expression.Parameter(typeof(TOwner), "propertyOwner");
+				MemberExpression typedPropExpr = Expression.Property(typedParamExpr, pi);
+				TypedGetter = Expression.Lambda<Func<TOwner, object>>(Expression.Convert(typedPropExpr, typeof(object)), typedParamExpr).Compile();
+
+
+				var valParamExpr = Expression.Parameter(typeof(object), "newValue");
+
+				bool canWrite = !pi.CanWrite || pi.GetSetMethod() == null;
+				setter = canWrite ? default(Action<object, object>) :
+						Expression.Lambda<Action<object, object>>(
+							Expression.Assign(propExpr,
+								Expression.Convert(valParamExpr, pi.PropertyType)
+							), objParamExpr, valParamExpr
+						).Compile();
+
+				TypedSetter = canWrite ? default(Action<TOwner, object>) :
+						Expression.Lambda<Action<TOwner, object>>(
+							Expression.Assign(typedPropExpr, Expression.Convert(valParamExpr, pi.PropertyType)
+							), typedParamExpr, valParamExpr
+						).Compile();
+
+
+				naam = pi.Name;
+				var mpVolgordeAttribute = Attr<MpVolgordeAttribute>(pi);
+				volgorde = mpVolgordeAttribute == null ? implicitOrder * 10 : mpVolgordeAttribute.Volgorde;
+				label = new[] {
 					OrDefault(Attr<MpSimpleLabelAttribute>(pi), mkAttr => mkAttr.Label),
 					OrDefault(Attr<MpTextDefKeyAttribute>(pi), mkAttr => mkAttr.Label) }
-				.SingleOrDefault(label => label != null);
-			if (Label == null && Attr<MpLabelsRequiredAttribute>(pi.DeclaringType) != null)
-				throw new ArgumentException("You must specify an MpSimpleLabel or MpTextDefKey on " + Naam + ", since the class " + ObjectToCode.GetCSharpFriendlyTypeName(pi.DeclaringType) + " is marked MpLabelsRequired");
-			KoppelTabelNaam = OrDefault(Attr<MpKoppelTabelAttribute>(pi), mkAttr => mkAttr.KoppelTabelNaam ?? pi.Name);
-			Verplicht = OrDefault(Attr<MpVerplichtAttribute>(pi), mkAttr => true);
-			AllowNull = OrDefault(Attr<MpAllowNullAttribute>(pi), mkAttr => true);
-			ShowDefaultOnNew = OrDefault(Attr<MpShowDefaultOnNewAttribute>(pi), mkAttr => true);
-			IsReadonly = Accessors.Setter == null || OrDefault(Attr<MpReadonlyAttribute>(pi), mkAttr => true);
-			Lengte = OrDefault(Attr<MpLengteAttribute>(pi), mkAttr => mkAttr.Lengte);
-			Regex = OrDefault(Attr<MpRegexAttribute>(pi), mkAttr => mkAttr.Regex);
+					.SingleOrDefault(text => text != null);
+				if (Label == null && Attr<MpLabelsRequiredAttribute>(pi.DeclaringType) != null)
+					throw new ArgumentException("You must specify an MpSimpleLabel or MpTextDefKey on " + Naam + ", since the class " + ObjectToCode.GetCSharpFriendlyTypeName(pi.DeclaringType) + " is marked MpLabelsRequired");
+				koppelTabelNaam = OrDefault(Attr<MpKoppelTabelAttribute>(pi), mkAttr => mkAttr.KoppelTabelNaam ?? pi.Name);
+				verplicht = OrDefault(Attr<MpVerplichtAttribute>(pi), mkAttr => true);
+				allowNull = OrDefault(Attr<MpAllowNullAttribute>(pi), mkAttr => true);
+				showDefaultOnNew = OrDefault(Attr<MpShowDefaultOnNewAttribute>(pi), mkAttr => true);
+				isReadonly = Setter == null || OrDefault(Attr<MpReadonlyAttribute>(pi), mkAttr => true);
+				lengte = OrDefault(Attr<MpLengteAttribute>(pi), mkAttr => mkAttr.Lengte);
+				regex = OrDefault(Attr<MpRegexAttribute>(pi), mkAttr => mkAttr.Regex);
 
-			if (KoppelTabelNaam != null && DataType != typeof(int) && DataType != typeof(int?))
-				throw new ProgressNetException(Accessors.OwnerType + " heeft Kolom " + Naam + " heeft koppeltabel " + KoppelTabelNaam + " maar is van type " + DataType + "!");
+				if (KoppelTabelNaam != null && DataType != typeof(int) && DataType != typeof(int?))
+					throw new ProgressNetException(typeof(TOwner) + " heeft Kolom " + Naam + " heeft koppeltabel " + KoppelTabelNaam + " maar is van type " + DataType + "!");
+
+			}
+
+
+			public bool CanRead { get { return getter != null; } }
+
+			public readonly Func<object, object> getter;
+			public readonly Func<TOwner, object> TypedGetter;
+			public Func<object, object> Getter { get { return getter; } }
+
+			public readonly Action<object, object> setter;
+			public readonly Action<TOwner, object> TypedSetter;
+			public Action<object, object> Setter { get { return setter; } }
 		}
 
-		public static MetaProperty LoadIfMetaProperty(PropertyInfo pi, int implicitOrder)
-		{
-			return Attr<MpNotMappedAttribute>(pi) != null ? null : new MetaProperty(pi, implicitOrder);
-		}
 
 		static T Attr<T>(MemberInfo mi) where T : Attribute { return mi.GetCustomAttributes(typeof(T), true).Cast<T>().SingleOrDefault(); }
 		static TR OrDefault<T, TR>(T val, Func<T, TR> project, TR defaultVal = default(TR)) { return Equals(val, default(T)) ? defaultVal : project(val); }
-
-		static IPropertyAccessors GetAccessors(PropertyInfo pi)
+		public static IMetaProperty LoadIfMetaProperty(PropertyInfo pi, int implicitOrder)
 		{
-			return (IPropertyAccessors)Activator.CreateInstance(typeof(MetaPropertyAccessors<,>).MakeGenericType(pi.DeclaringType, pi.PropertyType), pi);
+			return Attr<MpNotMappedAttribute>(pi) != null ? null :
+				(IMetaProperty)Activator.CreateInstance(typeof(Impl<>).MakeGenericType(pi.DeclaringType), pi, implicitOrder);
 		}
 
-		public bool CanRead { get { return Accessors.Getter != null; } }
 	}
 }
