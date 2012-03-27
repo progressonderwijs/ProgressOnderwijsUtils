2bdba780c554013fdc6f5a10d80c88f7cf9f74ab
Eamon Nerbonne <eamon@nerbonne.org>
2012-03-27 14:24:34 +0000

    eenvoudige refactoring: geef de voorkeur aan de enum Taal over een int taal parameter voor de robuustheid (vooral met overload-resolution)

--- src/ProgressOnderwijsUtils/Converteer/ITranslatable.cs
+++ src/ProgressOnderwijsUtils/Converteer/ITranslatable.cs
@@ -5,8 +5,14 @@ namespace ProgressOnderwijsUtils
 	/// <summary>
 	/// A Marker interface to indicate that this class can be translated.
 	/// </summary>
-	public interface ITranslatable { 
+	public interface ITranslatable
+	{
 		string GenerateUid();
 		TextVal Translate(ITranslationKeyLookup connectionOrContext, int langId);
+	}
+
+	public static class TranslatorHelper
+	{
+		public static TextVal Translate(this ITranslatable textdef, ITranslationKeyLookup connectionOrContext, Taal lang) { return textdef.Translate(connectionOrContext, (int)lang); }
 	}
 }
