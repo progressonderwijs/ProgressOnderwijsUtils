8b747c9bfb2a135029adbc4ec05db466cb6f121d
Fons Dijkstra <f.j.dijkstra@rug.nl>
2012-05-28 09:31:59 +0000

    Correctly (de)init the log4net service.

--- src/ProgressOnderwijsUtils/Log4Net/RollingFileAppender.cs
+++ src/ProgressOnderwijsUtils/Log4Net/RollingFileAppender.cs
@@ -26,9 +26,9 @@ namespace ProgressOnderwijsUtils.Log4Net
 		{
 			Encoding = Encoding.UTF8;
 			RollingStyle = RollingMode.Size;
-			AppendToFile = false;
+			AppendToFile = true;
 			MaximumFileSize = "256MB";
-			MaxSizeRollBackups = 5;
+			MaxSizeRollBackups = 9;
 			Layout = new PatternLayout("%utcdate{ISO8601} [%-4thread] %-5level %-50.50logger: %message%newline");
 		}
 
