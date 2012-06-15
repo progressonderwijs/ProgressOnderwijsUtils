656f847cc20da9c67ca078ddc5ab4a87492bb12c
Eamon Nerbonne <eamon@nerbonne.org>
2012-06-15 15:04:26 +0000

    Add unit test for SList - efficient, immutable, threadsafe singly-linked list.

--- src/ProgressOnderwijsUtils/Collections/SList.cs
+++ src/ProgressOnderwijsUtils/Collections/SList.cs
@@ -1,8 +1,6 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace ProgressOnderwijsUtils.Collections
 {
@@ -10,13 +8,16 @@ namespace ProgressOnderwijsUtils.Collections
 	/// <summary>
 	/// Immutable, thread-safe, singly-linked list
 	/// </summary>
-	public struct SList<T> : IEquatable<SList<T>>, IEnumerable<T>
+	public struct SList<T> : IEquatable<SList<T>>, IEnumerable<T> where T : IEquatable<T>
 	{
+		static readonly int typeHash = typeof(T).GetHashCode();
+		static readonly IEqualityComparer<T> elemEquality = EqualityComparer<T>.Default;
+
 		sealed class Impl
 		{
 			public readonly T Head;
 			public readonly Impl Tail;
-			public Impl(T head, Impl tail) { this.Head = head; this.Tail = tail; }
+			public Impl(T head, Impl tail) { Head = head; Tail = tail; }
 		}
 
 		SList(Impl list) { this.list = list; }
@@ -25,12 +26,57 @@ namespace ProgressOnderwijsUtils.Collections
 
 		public bool IsEmpty { get { return list == null; } }
 		public SList<T> Prepend(T head) { return new SList<T>(new Impl(head, list)); }
-		public SList<T> Reverse() { return this.Aggregate(Empty, (current, item) => current.Prepend(item)); }
+		public SList<T> Reverse()
+		{
+			var retval = Empty;
+			for (var cur = this; !cur.IsEmpty; cur = cur.Tail)
+				retval = retval.Prepend(cur.Head);
+			return retval;
+		}
+
+		public SList<T> Reverse2()
+		{
+			var retval = default(Impl);
+			for (var cur = list; cur != null; cur = cur.Tail)
+				retval = new Impl(cur.Head, retval);
+			return new SList<T>(retval);
+		}
 
 		public T Head { get { return list.Head; } }
 		public SList<T> Tail { get { return new SList<T>(list.Tail); } }
 
-		public bool Equals(SList<T> other) { return this.SequenceEqual(other); }
+		public bool Equals(SList<T> other)
+		{
+			var alist = list;
+			var blist = other.list;
+			while (alist != null && blist != null)
+			{
+				if (ReferenceEquals(alist, blist)) return true;
+				if (!elemEquality.Equals(alist.Head, blist.Head)) return false;
+				alist = alist.Tail;
+				blist = blist.Tail;
+			}
+			return alist == null && blist == null;
+		}
+		public override bool Equals(object obj) { return obj is SList<T> && Equals((SList<T>)obj); }
+		public override int GetHashCode()
+		{
+			ulong hash = (ulong)(typeHash + 1);
+			for (var current = list; current != null; current = current.Tail)
+				hash = hash * 137ul + (ulong)elemEquality.GetHashCode(current.Head);
+			return (int)hash ^ (int)(hash >> 32);
+		}
+		public static bool operator ==(SList<T> a, SList<T> b) { return a.Equals(b); }
+		public static bool operator !=(SList<T> a, SList<T> b) { return !a.Equals(b); }
+
+		IEnumerable<SList<T>> NonEmpySuffixes
+		{
+			get
+			{
+				for (var current = list; current != null; current = current.Tail)
+					yield return new SList<T>(current);
+			}
+		}
 
 		IEnumerator<T> IEnumerable<T>.GetEnumerator()
 		{
@@ -42,6 +88,25 @@ namespace ProgressOnderwijsUtils.Collections
 
 	public static class SList
 	{
-		public static SList<T> Create<T>(IEnumerable<T> mutable) { return mutable.Aggregate(SList<T>.Empty, (current, item) => current.Prepend(item)).Reverse(); }
+		public static SList<T> Create<T>(IEnumerable<T> list) where T : IEquatable<T>
+		{
+			if (list is IList<T>) return Create((IList<T>)list);//use IList interface for reverse iterability
+			else return Create(list.ToArray());//can't help but iterate forwards, so at least stick to it with the fastest possible path.
+		}
+		public static SList<T> Create<T>(IList<T> list) where T : IEquatable<T>
+		{
+			if (list is T[]) return Create((T[])list);
+			var retval = SList<T>.Empty;
+			for (int i = list.Count - 1; i >= 0; i--)
+				retval = retval.Prepend(list[i]);
+			return retval;
+		}
+		public static SList<T> Create<T>(T[] list) where T : IEquatable<T>
+		{
+			var retval = SList<T>.Empty;
+			for (int i = list.Length - 1; i >= 0; i--)
+				retval = retval.Prepend(list[i]);
+			return retval;
+		}
 	}
 }
---
+++ test/ProgressOnderwijsUtilsTests/SListTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ExpressionToCodeLib;
+using NUnit.Framework;
+using ProgressOnderwijsUtils.Collections;
+
+namespace ProgressOnderwijsUtilsTests
+{
+	[TestFixture]
+	public sealed class SListTest
+	{
+		[Test]
+		public void CanContainValues()
+		{
+			var list = SList.Create(new[] { 1, 3, 4, 1 });
+			PAssert.That(() => list.SequenceEqual(new[] { 1, 3, 4, 1 }));
+		}
+
+		[Test]
+		public void EmptyIsEmpty()
+		{
+			var list = SList.Create(new int[] { });
+			PAssert.That(() => list.IsEmpty);
+		}
+
+		[Test]
+		public void EmptyIsEmpty2()
+		{
+			var list = SList<int>.Empty;
+			PAssert.That(() => list.IsEmpty);
+		}
+
+		[Test]
+		public void PrependTest()
+		{
+			var list = SList<int>.Empty.Prepend(3).Prepend(4);
+			PAssert.That(() => list.SequenceEqual(new[] { 4, 3 }));
+		}
+		[Test]
+		public void EqualsTest()
+		{
+			var a = SList.Create(new[] { 1, 2, 3 });
+			var b = SList<int>.Empty.Prepend(3).Prepend(2).Prepend(1);
+			var c = SList.Create(new[] { 1, 2, 3, 4 });
+
+			PAssert.That(() => a == b);
+			PAssert.That(() => a.Equals(b));
+			PAssert.That(() => a.Equals((object)b));
+			PAssert.That(() => !(a != b));
+			PAssert.That(() => a != c);
+			PAssert.That(() => !(a == c));
+			PAssert.That(() => !(a.Equals(c)));
+			PAssert.That(() => !(a.Equals((object)c)));
+			PAssert.That(() => !a.Equals(SList<int>.Empty));
+
+		}
+		[Test]
+		public void ReverseTest()
+		{
+			var a = SList.Create(new[] { 1, 2, 3 });
+			var b = SList<int>.Empty.Prepend(1).Prepend(2).Prepend(3).Reverse();
+
+			PAssert.That(() => a == b);
+		}
+		[Test]
+		public void EnumeratorTest()
+		{
+			var b = SList<int>.Empty.Prepend(1).Prepend(2).Prepend(3).Reverse();
+
+			PAssert.That(() => StructuralComparisons.StructuralEqualityComparer.Equals(b.ToArray(), new[] { 1, 2, 3 }));
+		}
+		[Test]
+		public void TailTest()
+		{
+			var list = SList.Create(new[] { 1, 2, 3 });
+
+			PAssert.That(() => list.Tail.Tail.Tail.IsEmpty);
+			PAssert.That(() => list.Tail.Prepend(1) == list);
+			Assert.Throws<NullReferenceException>(() => { var x = list.Tail.Tail.Tail.Tail; });
+		}
+		[Test]
+		public void HeadTest()
+		{
+			var list = SList.Create(new[] { 1, 2, 3 });
+
+			PAssert.That(() => list.Head == 1);
+			PAssert.That(() => list.Tail.Head == 2);
+			PAssert.That(() => list.Tail.Tail.Head == 3);
+			Assert.Throws<NullReferenceException>(() => { var x = list.Tail.Tail.Tail.Head; });
+		}
+
+
+		[Test]
+		public void HashReasonableness()
+		{
+			var list = SList.Create( Enumerable.Range(0, 100).Select(i=> i*14678355468^i) );
+			List<int> hashcodes = new List<int>();
+			for (var suffix = list; !suffix.IsEmpty; suffix = suffix.Tail)
+				hashcodes.Add(suffix.GetHashCode());
+
+			PAssert.That(()=> hashcodes.Count == hashcodes.Distinct().Count());
+		}
+
+		[Test]
+		public void HashDiffersPerType()
+		{
+			PAssert.That(() => SList<int>.Empty.GetHashCode() != SList<double>.Empty.GetHashCode());
+		}
+	}
+}
