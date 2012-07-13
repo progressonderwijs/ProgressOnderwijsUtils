9c2bae5262bb37ee99c09c68a7577b3485c7f693
Eamon Nerbonne <eamon@nerbonne.org>
2012-07-13 09:15:44 +0000

    Opzettelijke fout in test geintroduceerd om jenkins te checken.

---
+++ test/ProgressOnderwijsUtilsTests/TemporaryIntentionallyFailingTestFixtureToTestJenkins.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ProgressOnderwijsUtilsTests
+{
+	[TestFixture]
+	public class TemporaryIntentionallyFailingTestFixtureToTestJenkins1
+	{
+		[TestFixtureSetUp]
+		public void TFSU() {
+			throw new Exception("Intentional failure to check jenkins.");
+		}
+		[Test]
+		public void ATest() { 
+		
+		}
+	}
+
+	[TestFixture]
+	public class TemporaryIntentionallyFailingTestFixtureToTestJenkins2
+	{
+		[TestFixtureTearDown]
+		public void TFTD()
+		{
+			throw new Exception("Intentional failure to check jenkins.");
+		}
+		[Test]
+		public void ATest()
+		{
+
+		}
+	}
+
+	[TestFixture]
+	public class TemporaryIntentionallyFailingTestFixtureToTestJenkins3
+	{
+		[SetUp]
+		public void SetUp()
+		{
+			throw new Exception("Intentional failure to check jenkins.");
+		}
+		[Test]
+		public void ATest()
+		{
+
+		}
+	}
+
+	[TestFixture]
+	public class TemporaryIntentionallyFailingTestFixtureToTestJenkins4
+	{
+		[TearDown]
+		public void TearDown()
+		{
+			throw new Exception("Intentional failure to check jenkins.");
+		}
+		[Test]
+		public void ATest()
+		{
+
+		}
+	}
+}
