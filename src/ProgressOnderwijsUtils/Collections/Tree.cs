8ee7efc24473a2042ccac5bbe23582a9999ceb38
Eamon Nerbonne <eamon@nerbonne.org>
2012-07-17 10:02:36 +0000

    Added Unit tests for the new immutable tree structure.

--- src/ProgressOnderwijsUtils/Collections/Tree.cs
+++ src/ProgressOnderwijsUtils/Collections/Tree.cs
@@ -7,33 +7,106 @@ using System.Text;
 
 namespace ProgressOnderwijsUtils.Collections
 {
-	public sealed class Tree<T>
+	public sealed class Tree<T> : IEquatable<Tree<T>>
 	{
-		static readonly Tree<T>[] EmptyArray = new Tree<T>[0];
+		static readonly ReadOnlyCollection<Tree<T>> EmptyArray = new Tree<T>[0].AsReadOnlyView(); // cache this since it will be used very commonly.
 
 		readonly T nodeValue;
-		readonly Tree<T>[] kidArray;//children is either null (leaf node) or length>0.  This optimization matters since there are many leaf nodes.
+		readonly ReadOnlyCollection<Tree<T>> kidArray;
 		public T NodeValue { get { return nodeValue; } }
-		public ReadOnlyCollection<Tree<T>> Children { get { return (kidArray ?? EmptyArray).AsReadOnlyView(); } }
-		public Tree(T value, IEnumerable<Tree<T>> children)
+		public ReadOnlyCollection<Tree<T>> Children { get { return kidArray; } }
+
+		/// <summary>
+		/// Creates a Tree with specified child nodes.  The child node enumeration is materialized using ToArray() before usage.
+		/// </summary>
+		/// <param name="value">The value of this node.</param>
+		/// <param name="children">The children of this node, (null is allowed and means none).</param>
+		public Tree(T value, IEnumerable<Tree<T>> children) : this(value, children == null ? null : children.ToArray()) { }
+
+		/// <summary>
+		/// Creates a Tree with specified child nodes.  The child node array is used directly. Do not mutate the array after passing it into the tree; doing so
+		/// results in undefined behavior.
+		/// </summary>
+		/// <param name="value">The value of this node.</param>
+		/// <param name="children">The children of this node, (null is allowed and means none).</param>
+		public Tree(T value, Tree<T>[] children)
 		{
 			nodeValue = value;
+			kidArray = children == null || children.Length == 0 ? EmptyArray : children.AsReadOnlyView();
+		}
+		public static readonly Comparer DefaultComparer = new Comparer(EqualityComparer<T>.Default);
 
-			if (children == null)
-				kidArray = null;
-			else
+		public sealed class Comparer : IEqualityComparer<Tree<T>>
+		{
+			static readonly int typeHash = typeof(Tree<T>).GetHashCode();
+			readonly IEqualityComparer<T> ValueComparer;
+			public Comparer(IEqualityComparer<T> valueComparer) { ValueComparer = valueComparer; }
+
+			public bool Equals(Tree<T> x, Tree<T> y)
 			{
-				var array = children.ToArray();
-				kidArray = array.Length == 0 ? null : array;
+				return
+					ReferenceEquals(x, y) ||
+					!ReferenceEquals(x, null) && !ReferenceEquals(y, null)
+					&& x.Children.Count == y.Children.Count
+					&& ValueComparer.Equals(x.NodeValue, y.NodeValue)
+					&& x.Children.SequenceEqual(y.Children, this);
+			}
+
+			ulong InternalHash(Tree<T> obj)
+			{
+				ulong hash = (uint)ValueComparer.GetHashCode(obj.NodeValue);
+				ulong offset = 1;
+				foreach (var kid in obj.Children)
+				{
+					hash += offset * InternalHash(kid);
+					offset += 2;
+				}
+				return hash;
+			}
+
+			public int GetHashCode(Tree<T> obj)
+			{
+				if (obj == null)
+					return typeHash;
+				var hash = InternalHash(obj);
+				return (int)((uint)(hash >> 32) + (uint)hash);
 			}
 		}
+		public override bool Equals(object obj) { return DefaultComparer.Equals(this, obj as Tree<T>); }
+		public bool Equals(Tree<T> other) { return DefaultComparer.Equals(this, other); }
+		public override int GetHashCode() { return DefaultComparer.GetHashCode(this); }
 	}
 
 	public static class Tree
 	{
-		public static Tree<T> CreateNode<T>(T value, IEnumerable<Tree<T>> children)
+		public static Tree<T> Node<T>(T value, IEnumerable<Tree<T>> children)
 		{
 			return new Tree<T>(value, children);
+		}
+		public static Tree<T> Node<T>(T value, Tree<T> a)
+		{
+			return new Tree<T>(value, new[] { a, });
+		}
+		public static Tree<T> Node<T>(T value, Tree<T> a, Tree<T> b)
+		{
+			return new Tree<T>(value, new[] { a, b });
+		}
+		public static Tree<T> Node<T>(T value, Tree<T> a, Tree<T> b, Tree<T> c)
+		{
+			return new Tree<T>(value, new[] { a, b, c });
+		}
+		public static Tree<T> Node<T>(T value, params  Tree<T>[] kids)
+		{
+			return new Tree<T>(value, kids);
+		}
+		public static Tree<T> Node<T>(T value)
+		{
+			return new Tree<T>(value, null);
+		}
+
+		public static IEqualityComparer<Tree<T>> EqualityComparer<T>(IEqualityComparer<T> valueComparer)
+		{
+			return new Tree<T>.Comparer(valueComparer);
 		}
 	}
 
@@ -61,68 +134,13 @@ namespace ProgressOnderwijsUtils.Collections
 			}
 		}
 
-		public class TreePathEnumerator<T> : IEnumerator<SList<TreePathSegment<T>>>
-		{
-			Tree<T> current;
-			SList<TreePathSegment<T>> path;
-
-			public SList<TreePathSegment<T>> Current { get { return path; } }
-			object IEnumerator.Current { get { return Current; } }
-
-			void IDisposable.Dispose() { current = null; path = default(SList<TreePathSegment<T>>); }
-
-			public bool MoveNext()
-			{
-				if (current.Children.Count > 0)
-				{ //iterate children.
-					var node = current.Children[0];
-					path = path.Prepend(new TreePathSegment<T>(0, node));
-					current = node;
-					return true;
-				}
-				else
-				{ // no children; go to element past current
-					return MovePastCurrent();
-				}
-			}
-
-			bool MovePastCurrent()
-			{
-				//one of three scenarios:
-				// scenario (1):  I have no parent and hence no siblings; cannot move to next node therefore: DONE!
-				// scenario (2):  I have a parent and that parent has a child after me: go to that child.
-				// scenario (3):  I have a parent but no subsequent siblings: move past parent (i.e. loop to parent.)
-
-				int nextIndex = path.Head.Index + 1;
-				path = path.Tail;
-				while (!path.IsEmpty) // scenario (1) or (2)
-				{
-					var container = path.Head.SubTree;
-
-					if (container.Children.Count > nextIndex)
-					{ //scenario (2)
-						path = path.Prepend(new TreePathSegment<T>(nextIndex, container.Children[nextIndex]));
-						return true;
-					}
-					else
-					{ //scenario (3)
-						nextIndex = path.Head.Index + 1;
-						path = path.Tail;
-					}
-				}
-				return false;//scenario (3)
-			}
-
-			public void Reset() { throw new NotSupportedException(); }
-		}
-
-
 		public static IEnumerable<SList<TreePathSegment<T>>> PreorderTraversal<T>(this Tree<T> tree)
 		{
 			var path = SList<TreePathSegment<T>>.Empty.Prepend(new TreePathSegment<T>(0, tree));
 			yield return path;
 
-			while (!path.IsEmpty) {
+			while (!path.IsEmpty)
+			{
 				var current = path.Head.SubTree;
 				if (current.Children.Count > 0)
 				{ //iterate children.
@@ -130,7 +148,13 @@ namespace ProgressOnderwijsUtils.Collections
 					path = path.Prepend(new TreePathSegment<T>(0, node));
 					yield return path;
 				}
-				else {
+				else
+				{
+					//one of three scenarios:
+					// scenario (1):  I have no parent and hence no siblings; cannot move to next node therefore: DONE!
+					// scenario (2):  I have a parent and that parent has a child after me: go to that child.
+					// scenario (3):  I have a parent but no subsequent siblings: move past parent (i.e. loop to parent.)
+
 					int nextIndex = path.Head.Index + 1;
 					path = path.Tail;
 					while (!path.IsEmpty) // scenario (1) or (2)
@@ -148,7 +172,7 @@ namespace ProgressOnderwijsUtils.Collections
 							nextIndex = path.Head.Index + 1;
 							path = path.Tail;
 							if (path.IsEmpty)
-								yield break;//scenario (3)
+								yield break;//scenario (1)
 						}
 					}
 				}
---
+++ test/ProgressOnderwijsUtilsTests/TreeTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExpressionToCodeLib;
+using NUnit.Framework;
+using ProgressOnderwijsUtils.Collections;
+
+namespace ProgressOnderwijsUtilsTests
+{
+	[TestFixture]
+	public sealed class TreeTest
+	{
+		[Test]
+		public void TreeStoresInfo()
+		{
+			var tree = Tree.Node("abc", Tree.Node("def"), Tree.Node("xyz"));
+			PAssert.That(() => tree.NodeValue == "abc");
+			PAssert.That(() => tree.Children.Count == 2);
+			PAssert.That(() => tree.Children.All(kid => kid.Children.Count == 0));
+			PAssert.That(() => tree.Children[0].NodeValue == "def");
+			PAssert.That(() => tree.Children[1].NodeValue == "xyz");
+		}
+
+
+		[Test]
+		public void TreeWithManyLeaves()
+		{
+			var tree = Tree.Node(100, Tree.Node(1), Tree.Node(2), Tree.Node(3), Tree.Node(4));
+			PAssert.That(() => tree.Children.Count == 4);
+			PAssert.That(() => tree.Children.Select((kid, index) => kid.NodeValue == index + 1).All(b => b));
+		}
+
+
+		[Test]
+		public void SaneDefaultComparer()
+		{
+			var leaf2 = Tree.Node(2);
+			var tree1 = Tree.Node(100, Tree.Node(1), leaf2, Tree.Node(3), Tree.Node(4));
+			var tree2 = Tree.Node(100, Tree.Node(1), leaf2, Tree.Node(3), Tree.Node(4));
+			
+			PAssert.That(() => !ReferenceEquals(tree1, tree2));
+			PAssert.That(() => Equals(tree1, tree2));
+			PAssert.That(() => Equals(tree2, tree1));
+			PAssert.That(() => tree2.Equals(tree1));
+			PAssert.That(() => tree2.Equals((object)tree1));
+			PAssert.That(() => tree1.Equals(tree1));
+			PAssert.That(() => !tree1.Equals(null));
+			PAssert.That(() => !Equals(tree2, null));
+			PAssert.That(() => !Equals(null, tree2));
+			PAssert.That(() => !Equals(tree2, leaf2));
+		}
+		[Test]
+		public void CustomizableComparerWorks()
+		{
+			var tree1 = Tree.Node("a", Tree.Node("x"), Tree.Node("b"), Tree.Node(default(string)), Tree.Node(""));
+			var tree2 = Tree.Node("a", Tree.Node("x"), Tree.Node("B"), Tree.Node(default(string)), Tree.Node(""));
+			var tree3 = Tree.Node("a", Tree.Node("x"), Tree.Node("b"), Tree.Node(default(string)), Tree.Node(""));
+			var tree4 = Tree.Node("a", Tree.Node("y"), Tree.Node("b"), Tree.Node(default(string)), Tree.Node(""));
+
+			PAssert.That(() => !tree1.Equals(tree2));
+			PAssert.That(() => tree1.Equals(tree3));
+			PAssert.That(() => !tree2.Equals(tree3));
+
+			PAssert.That(() => Tree.EqualityComparer(StringComparer.OrdinalIgnoreCase).Equals(tree1,tree2));
+			PAssert.That(() => !Tree.EqualityComparer(StringComparer.OrdinalIgnoreCase).Equals(tree1, tree4));
+		}
+
+	}
+}
