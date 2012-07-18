516cb4b9419d7ce8eb96f04a22b92b95f2a6b2a4
Eamon Nerbonne <eamon@nerbonne.org>
2012-07-18 13:47:21 +0000

    Debug tools: track main thread CPU time too.

---
+++ src/ProgressOnderwijsUtils/ThreadCpuTimer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ProgressOnderwijsUtils
+{
+	public sealed class ThreadCpuTimer
+	{
+		readonly TimeSpan start;
+
+
+		readonly ProcessThread thread;
+
+		ThreadCpuTimer()
+		{
+			var id = AppDomain.GetCurrentThreadId();
+			thread = Process.GetCurrentProcess().Threads.Cast<ProcessThread>().Single(pt => pt.Id == id);
+			start = thread.TotalProcessorTime;
+		}
+
+		/// <summary>
+		/// May return null when no longer on the same OS thread.
+		/// </summary>
+		public double? ElapsedMilliseconds()
+		{
+			var currentId = AppDomain.GetCurrentThreadId();
+			return thread.Id != currentId ? default(double?) : (thread.TotalProcessorTime - start).TotalMilliseconds;
+		}
+
+		public static ThreadCpuTimer StartNew() { return new ThreadCpuTimer(); }
+	}
+}
