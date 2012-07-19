5e0f23857bf2fee406a575014a894d36d9eca6fd
Eamon Nerbonne <eamon@nerbonne.org>
2012-07-19 09:50:44 +0000

    (minor) add comments, #region

--- src/ProgressOnderwijsUtils/Text/ConverteerHelper.cs
+++ src/ProgressOnderwijsUtils/Text/ConverteerHelper.cs
@@ -9,6 +9,10 @@ namespace ProgressOnderwijsUtils.Text
 	public delegate string TranslateFunction(Taal taal = Taal.NL);
 	public static class ConverteerHelper
 	{
+		#region ToString implementations for use from dynamic code
+		// ReSharper disable MemberCanBePrivate.Global
+		// ReSharper disable UnusedParameter.Global
+		//used in dynamic code:
 		public static TranslateFunction ToString(long l, string format) { return language => l.ToString(format ?? "D", language.GetCulture()); }
 		public static TranslateFunction ToString(double d, string format) { return language => d.ToString(format ?? "0.##", language.GetCulture()); }
 		public static TranslateFunction ToString(decimal d, string format) { return language => d.ToString(format ?? GELD_EURO, language.GetCulture()); }
@@ -19,16 +23,19 @@ namespace ProgressOnderwijsUtils.Text
 		public static TranslateFunction ToString(string str, string format) { return language => str; }
 		public static TranslateFunction ToString(char c, string format) { return language => new string(c, 1); }
 		public static TranslateFunction ToString(XHtmlData obj, string format) { return language => obj.ToUiString(); }
-		public static TranslateFunction ToString<T>(T[] arr, string format) {
-			var subtrans=arr.Select(obj => (TranslateFunction)ConverteerHelper.ToString((dynamic)obj, format)).ToArray();
+		public static TranslateFunction ToString<T>(T[] arr, string format)
+		{
+			var subtrans = arr.Select(obj => (TranslateFunction)ConverteerHelper.ToString((dynamic)obj, format)).ToArray();
 
-			return language => subtrans.Select(f => f(language)).JoinStrings("\n"); 
+			return language => subtrans.Select(f => f(language)).JoinStrings("\n");
 		}
 
 
-		public static TranslateFunction ToString(bool b, string format) { 
+		public static TranslateFunction ToString(bool b, string format)
+		{
 			return language => {
-				switch (language) {
+				switch (language)
+				{
 					case Taal.NL: return b ? "Ja" : "Nee";
 					case Taal.EN: return b ? "Yes" : "No";
 					case Taal.DU: return b ? "Ja" : "Nein";
@@ -36,7 +43,9 @@ namespace ProgressOnderwijsUtils.Text
 				}
 			};
 		}
-
+		// ReSharper restore UnusedParameter.Global
+		// ReSharper restore MemberCanBePrivate.Global
+		#endregion
 		public const string GELD_EURO = "N02";
 		public const string ALLEEN_DATUM = "dd-MM-yyyy";
 		public const string DATUM_EN_TIJD_IN_MINUTEN = "dd-MM-yyyy HH:mm";
