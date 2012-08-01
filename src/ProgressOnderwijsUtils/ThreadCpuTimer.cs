54ea3fb65802554ed16454b9340104e78e47ecdc
Eamon Nerbonne <eamon@nerbonne.org>
2012-08-01 12:05:35 +0000

    Revamp perf tracing: no longer trace Entity Framework Queries.  If you want to do this; use the SQL profiler.

--- src/ProgressOnderwijsUtils/ThreadCpuTimer.cs
+++ src/ProgressOnderwijsUtils/ThreadCpuTimer.cs
@@ -10,7 +10,7 @@ namespace ProgressOnderwijsUtils
 	{
 		readonly TimeSpan start;
 
-
+		readonly Stopwatch wallclock;
 		readonly ProcessThread thread;
 
 		ThreadCpuTimer()
@@ -20,12 +20,13 @@ namespace ProgressOnderwijsUtils
 #pragma warning restore 612,618
 			thread = Process.GetCurrentProcess().Threads.Cast<ProcessThread>().Single(pt => pt.Id == id);
 			start = thread.TotalProcessorTime;
+			wallclock = Stopwatch.StartNew();
 		}
 
 		/// <summary>
 		/// May return null when no longer on the same OS thread.
 		/// </summary>
-		public double? ElapsedMilliseconds()
+		public double? CpuMilliseconds()
 		{
 #pragma warning disable 612,618
 			var currentId = AppDomain.GetCurrentThreadId();
@@ -33,6 +34,7 @@ namespace ProgressOnderwijsUtils
 			return thread.Id != currentId ? default(double?) : (thread.TotalProcessorTime - start).TotalMilliseconds;
 		}
 
+		public double WallClockMilliseconds() { return wallclock.Elapsed.TotalMilliseconds; }
 		public static ThreadCpuTimer StartNew() { return new ThreadCpuTimer(); }
 	}
 }
