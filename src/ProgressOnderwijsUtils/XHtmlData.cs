7680c1e75a21e9090f103901f7ce3bc0f0a9ce49
Eamon Nerbonne <eamon@nerbonne.org>
2012-08-02 16:59:25 +0000

    GenericEditLogging tooltips verhipt; en oorspronkelijk aanmaken tijdstip ook laten zien.  Tests nog niet hierop aangepast.

--- src/ProgressOnderwijsUtils/XHtmlData.cs
+++ src/ProgressOnderwijsUtils/XHtmlData.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml;
@@ -6,9 +8,19 @@ using System.Xml.Linq;
 
 namespace ProgressOnderwijsUtils
 {
-	public struct XHtmlData
+	public struct ToolTipSettings
+	{
+		public string Header;
+		public int? Width;
+	}
+
+	public struct XHtmlData : IEnumerable<XNode>
 	{
 		readonly XNode[] nodes;
+		public IEnumerable<XNode> Nodes { get { return nodes.EmptyIfNull(); } }
+
+		public static XHtmlData Empty { get { return default(XHtmlData); } }
+
 
 		public static XHtmlData Create(params object[] contents)
 		{
@@ -20,6 +32,24 @@ namespace ProgressOnderwijsUtils
 			return new XHtmlData(XhtmlCleaner.HtmlSanitizer(s).Nodes().ToArray());
 		}
 
+		public static XHtmlData GenerateToolTipHtmlFragment(IEnumerable<XNode> text, IEnumerable<XNode> tooltip, ToolTipSettings settings)
+		{
+			return
+				Create(
+				tooltip != null && tooltip.Any() ?
+														new XNode[]{ 	
+					                                        new XElement("span", new XAttribute("class","hastooltip"),
+																settings.Width.HasValue? new XAttribute("data-tipwidth",settings.Width) : null,
+						                                        new XElement("div", new XAttribute("class","tipContent"),
+																	string.IsNullOrWhiteSpace(settings.Header) ? null : new XElement("div", new XAttribute("class", "header"), settings.Header),
+							                                        new XElement("div", new XAttribute("class", "tipbody"),tooltip)
+																),
+						                                        text
+															)
+				                                        }
+				: text);
+		}
+
 		XHtmlData(XNode[] nodes) { this.nodes = nodes; }
 
 		public string ToUiString()
@@ -29,15 +59,21 @@ namespace ProgressOnderwijsUtils
 				ConformanceLevel = ConformanceLevel.Fragment,
 			}))
 			{
-				foreach (var node in nodes.EmptyIfNull())
+				foreach (var node in Nodes)
 					node.WriteTo(inner);
 				inner.Close();
 				return writer.ToString();
 			}
 		}
 
-		public XElement ToXHtmlDataElement() {
+		public XElement ToXHtmlDataElement()
+		{
+			// ReSharper disable CoVariantArrayConversion
 			return new XElement("XHtmlData", nodes);
+			// ReSharper restore CoVariantArrayConversion
 		}
+
+		public IEnumerator<XNode> GetEnumerator() { return Nodes.GetEnumerator(); }
+		IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
 	}
 }
