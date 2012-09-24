07827757b62628f514c7cb3f77d8afe64dfe3f98
Eamon Nerbonne <eamon@nerbonne.org>
2012-09-24 17:03:01 +0000

    User .NET 4.5 feature to verify everything's working as expected.

---
+++ src/ProgressOnderwijsUtils/Test/Net45Works.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ProgressOnderwijsUtils.Test
+{
+	[TestFixture]
+	class Net45Works
+	{
+		public void UseAsyncKeywork()
+		{
+			Assert.AreEqual(42, Utils.F(async (int x) => await Task.FromResult(123 + x))(-81).Result);
+		}
+	}
+}
