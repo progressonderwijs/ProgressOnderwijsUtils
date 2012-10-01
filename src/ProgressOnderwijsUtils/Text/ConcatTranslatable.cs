7f0bf4e6417d9efd7a0785b4bfd279c0b08bd69e
Eamon Nerbonne <eamon@nerbonne.org>
2012-10-01 12:10:26 +0000

    ConcatTranslatable, e.g. Translatable.Append: check for nulls

--- src/ProgressOnderwijsUtils/Text/ConcatTranslatable.cs
+++ src/ProgressOnderwijsUtils/Text/ConcatTranslatable.cs
@@ -11,13 +11,18 @@ namespace ProgressOnderwijsUtils.Text
 		readonly ITranslatable[] parts;
 		//public ITranslatable[] Parts { get { return parts; } }
 
-		public ConcatTranslatable(params ITranslatable[] parts) { this.parts = parts;}
-		public string GenerateUid() { return parts.Select(it=> it.GenerateUid()).JoinStrings(); }
+		public ConcatTranslatable(params ITranslatable[] parts)
+		{
+			this.parts = parts;
+			if (parts.Any(p => p == null))
+				throw new ArgumentNullException("parts", "element of parts is null");
+		}
+		public string GenerateUid() { return parts.Select(it => it.GenerateUid()).JoinStrings(); }
 
 		public TextVal Translate(Taal taal)
 		{
 			var translation = parts.Select(it => it.Translate(taal)).ToArray();
-			return new TextVal(translation.Select(tv => tv.Text).JoinStrings(), translation.Select(tv => tv.ExtraText??"").JoinStrings());
+			return new TextVal(translation.Select(tv => tv.Text).JoinStrings(), translation.Select(tv => tv.ExtraText ?? "").JoinStrings());
 		}
 	}
 }
