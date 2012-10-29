c0f5dc03fb53078ab74524d7e17c708a7088a695
Eamon Nerbonne <eamon@nerbonne.org>
2012-10-29 17:25:59 +0000

    Add some comments to Maybe, explaining what its for and how to use it.

--- src/ProgressOnderwijsUtils/Maybe.cs
+++ src/ProgressOnderwijsUtils/Maybe.cs
@@ -9,18 +9,42 @@ namespace ProgressOnderwijsUtils
 	/// </summary>
 	public struct Unit { public static Unit Value { get { return default(Unit); } } }
 
+	/// <summary>
+	/// A value or an error message describing why no value is present.
+	/// 
+	/// "Maybe" can be used to pass around possibly missing or erroneous values that aren't exceptional (e.g. that should not cause a crash).
+	/// See the utility functions  "WhenOk", "WhenOkTry" and "ExtractToValue" for ways to handily combine values of type Maybe. 
+	/// 
+	/// "Maybe" is immutable and hence thread safe (assuming the wrapped value is thread safe).
+	/// </summary>
 	public abstract class Maybe<T>
 	{
+		/// <summary>
+		/// Use this sparingly!  Usually "WhenOk",  "WhenOkTry" and "ExtracToValue" result in clearer code.
+		/// 
+		/// Value: whether this Maybe is in the OK state.
+		/// </summary>
 		public abstract bool IsOk { get; }
-		public abstract T GetValue();
-		public abstract ITranslatable GetError();
-		public abstract TOut ExtractToValue<TOut>(Func<T, TOut> ifOk, Func<ITranslatable, TOut> ifError);
-		public static implicit operator Maybe<T>(Maybe.ErrorValue err) { return Maybe.Error<T>(err.ErrorMessage); }
 
-		public static bool operator true(Maybe<T> state) { return state.IsOk; }
-		public static bool operator false(Maybe<T> state) { return !state.IsOk; }
-		public static Maybe<T> operator &(Maybe<Unit> maybe1, Maybe<T> maybe2) { return maybe1.ExtractToValue(_ => maybe2, Maybe.Error<T>); }
-		//public static MaybeError<T> operator |(MaybeError<T> a, MaybeError<T> b) { return a.IsOk ? a : b; }
+		/// <summary>
+		/// Gets the value of this Maybe if it is OK; throws an Exception if called when this Maybe is not OK.
+		/// </summary>
+		public abstract T GetValue();
+
+		/// <summary>
+		/// Gets the error message of this Maybe if present; throws an Exception if called when this Maybe is OK.
+		/// </summary>
+		public abstract ITranslatable GetError();
+
+		/// <summary>
+		/// Extracts a value from the Maybe by calling either the ifOk function or the ifError function, depending on the state of the Maybe.
+		/// </summary>
+		public abstract TOut ExtractToValue<TOut>(Func<T, TOut> ifOk, Func<ITranslatable, TOut> ifError);
+
+		/// <summary>
+		/// Converts an untyped error message into a specific type of failed Maybe.  This operator is a  workaround to make it easy to create an error message without redundant type info.
+		/// </summary>
+		public static implicit operator Maybe<T>(Maybe.ErrorValue err) { return Maybe.Error<T>(err.ErrorMessage); }
 
 		private Maybe() { }
 
@@ -57,23 +81,61 @@ namespace ProgressOnderwijsUtils
 			public readonly ITranslatable ErrorMessage;
 			public ErrorValue(ITranslatable errorMessage)
 			{
-				if (errorMessage == null) throw new ArgumentNullException("errorMessage"); ErrorMessage = errorMessage;
+				if (errorMessage == null)
+					throw new ArgumentNullException("errorMessage"); ErrorMessage = errorMessage;
 			}
 		}
 
+		/// <summary>
+		/// Creates a succesful Maybe that stores the provided value.
+		/// </summary>
 		public static Maybe<T> Ok<T>(T val) { return new Maybe<T>.OkValue(val); }
+		
+		/// <summary>
+		/// Creates a succesful Maybe value without a value.
+		/// </summary>
 		public static Maybe<Unit> Ok() { return new Maybe<Unit>.OkValue(Unit.Value); }
+
+		/// <summary>
+		/// Creates a failed Maybe value with the specified error message.
+		/// </summary>
 		public static Maybe<T> Error<T>(ITranslatable val) { return new Maybe<T>.ErrorValue(val); }
+
+		/// <summary>
+		/// Create an error message to show to the user describing a failed operation.
+		/// </summary>
+		/// <param name="val">The text of the error message</param>
+		/// <returns>An ErrorValue (this is implicitly cast to whatever type of Maybe&lt;T&gt; you use.</returns>
 		public static ErrorValue Error(ITranslatable val) { return new ErrorValue(val); }
 
-
+		/// <summary>
+		/// Converts an ITranslatable to a Maybe&lt;Unit&gt;.  A null translatable represents success, any other value the error message to display.
+		/// </summary>
 		public static Maybe<Unit> ErrorWhenNotNull(this ITranslatable val)
 		{
 			return val == null ? Ok() : new ErrorValue(val);
 		}
 
+		/// <summary>
+		/// Maps a possibly failed value to a new value.
+		/// When the input state is failed, the output state is also failed (with the same message).  If the input is OK, the output is OK and is mapped
+		/// using the provided function.  The function is eagerly evaluated, i.e. not like Enumerable.Select, but like Enumerable.ToArray.
+		/// </summary>
 		public static Maybe<TOut> WhenOk<T, TOut>(this Maybe<T> state, Func<T, TOut> map) { return state.ExtractToValue(v => Ok(map(v)), Error<TOut>); }
+
+		/// <summary>
+		/// Processes a possibly failed value.  
+		/// When the input state is failed, the output state is also failed (with the same message).  If the input is OK, the output is OK, but the output
+		/// has no value (value of type Unit).  The function is eagerly evaluated, i.e. not like Enumerable.Select, but like Enumerable.ToArray.
+		/// </summary>
 		public static Maybe<Unit> WhenOk<T>(this Maybe<T> state, Action<T> map) { return state.ExtractToValue(v => { map(v); return Ok(); }, Error<Unit>); }
+
+		/// <summary>
+		/// Maps a possibly failed value to a new value using a mapping function that itself can fail.
+		/// When the input state is failed, the output state is also failed (with the same message).  If the input is OK, it is transformed using the
+		/// provided "map" function (which may itself report an error).   The function is eagerly evaluated, i.e. not like Enumerable.Select, but like
+		/// Enumerable.ToArray.
+		/// </summary>
 		public static Maybe<TOut> WhenOkTry<T, TOut>(this Maybe<T> state, Func<T, Maybe<TOut>> map) { return state.ExtractToValue(map, Error<TOut>); }
 	}
 }
