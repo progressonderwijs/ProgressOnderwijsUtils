8baea6c34377cad87ce2673e067c5bee0ca97108
Renzo Kooi <l.kooi@rug.nl>
2012-11-08 14:24:30 +0000

    * Een aantal studielinkberichttests zodanig aangepast dat er wordt uitgegaan van een standaard setje tabelnamen (TablesThatShouldBeAffected) voor gebruik in TestHelper.AssertStudentForeignKeys, wat in het parameterblok van de aanroep van die method zonodig kan worden aangevuld via extension methods voor HashSet

--- src/ProgressOnderwijsUtils/Extensions/SetExtensions.cs
+++ src/ProgressOnderwijsUtils/Extensions/SetExtensions.cs
@@ -11,5 +11,35 @@ namespace ProgressOnderwijsUtils.Extensions
 		{
 			return new ReadOnlySet<T>(set);
 		}
+
+		public static HashSet<T> CopyAndAddItems<T>(this IEnumerable<T> H, IEnumerable<T> toAdd)
+		{
+			HashSet<T> newHashSet = new HashSet<T>(H);
+			foreach (T s in toAdd) 
+				newHashSet.Add(s);
+			return newHashSet;
+		}
+
+		public static HashSet<T> CopyAndRemoveItems<T>(this IEnumerable<T> H, IEnumerable<T> toRemove)
+		{
+			HashSet<T> newHashSet = new HashSet<T>(H);
+			foreach (T s in toRemove)
+				newHashSet.Remove(s);
+			return newHashSet;
+		}
+
+		public static HashSet<T> CopyAndRemoveItem<T>(this IEnumerable<T> H, T toRemove)
+		{
+			HashSet<T> newHashSet = new HashSet<T>(H);
+			newHashSet.Remove(toRemove);
+			return newHashSet;
+		}
+
+		public static HashSet<T> CopyAndAddItem<T>(this IEnumerable<T> H, T toAdd)
+		{
+			HashSet<T> newHashSet = new HashSet<T>(H) { toAdd };
+			return newHashSet;
+		}
+
 	}
 }
