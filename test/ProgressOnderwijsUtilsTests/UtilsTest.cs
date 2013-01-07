cdd86c0123401749bd8811e0c59ac2cb2e0af67c
Eamon Nerbonne <eamon@nerbonne.org>
2013-01-07 17:00:33 +0100

    Intentionally cause test failure to check jenkins' configuration (will fix next commit).

--- test/ProgressOnderwijsUtilsTests/UtilsTest.cs
+++ test/ProgressOnderwijsUtilsTests/UtilsTest.cs
@@ -7,7 +7,10 @@ namespace ProgressOnderwijsUtilsTests
 {
 	[TestFixture] public class UtilsTest
 	{
-		//[Test]
-		//public void 
+		[Test]
+		public void IntentionallyBrokenTest()
+		{
+			throw new Exception("Eamon broke this to test jenkin's email functionality 2012-01-07 (will be disabled soon)");
+		}
 	}
 }
