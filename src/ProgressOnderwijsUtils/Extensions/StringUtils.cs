d2b7777a85c2fa24308a63f531db88d766dfde5e
Eamon Nerbonne <eamon@nerbonne.org>
2013-01-22 16:27:08 +0100

    OnderwijsZoek: filter logischer en veel eenvoudiger  - niet meer een foreach-return jungle  - filter op contains in titel.

--- src/ProgressOnderwijsUtils/Extensions/StringUtils.cs
+++ src/ProgressOnderwijsUtils/Extensions/StringUtils.cs
@@ -78,6 +78,17 @@ namespace ProgressOnderwijsUtils
 			return LevenshteinDistance(s, t) / (double)Math.Max(1, Math.Max(s.Length, t.Length));
 		}
 
+		public static long? ParseInt64(string s)
+		{
+			long val;
+			return long.TryParse(s, out val) ? val : default(long?);
+		}
+		public static int? ParseInt32(string s)
+		{
+			int val;
+			return int.TryParse(s, out val) ? val : default(int?);
+		}
+
 		/// <summary>
 		/// Vervang in een [naam]string beginletters door hoofdletters,
 		/// rekening houdend met tussenvoegsels en interpunctie
