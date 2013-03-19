d70735d77e9e8a322ed70c382779da93fd0aa96d
Eamon Nerbonne <eamon@nerbonne.org>
2013-03-19 11:28:27 +0100

    (minor) extra commentaar; test in continuous.

--- src/ProgressOnderwijsUtils/HexHelper.cs
+++ src/ProgressOnderwijsUtils/HexHelper.cs
@@ -7,22 +7,48 @@ namespace ProgressOnderwijsUtils
 {
 	public static class HexHelper
 	{
+
+		/// <summary>
+		/// Very fast hex-to-bytearray conversion.  Verifies whether all characters are legal [0-9a-fA-F] and throws if this is not so.
+		/// </summary>
+		/// <param name="hex">a hexadecimal string without spaces, dashes, and without a '0x' prefix.</param>
+		/// <returns>the byte array it represents.</returns>
 		public static byte[] HexToBytes(string hex)
 		{
 			if (hex.Length % 2 != 0) throw new ArgumentException("Cannot convert odd number of hex digits to byte array");
 			var arr = new byte[hex.Length / 2];
 			for (var i = 0; i < arr.Length; i++)
 			{
-				var b = (byteLookup[hex[2 * i]] << 4) | byteLookup[hex[2 * i + 1]];
+
+				int b;
+				try
+				{
+					b = (byteLookup[hex[2 * i]] << 4) | byteLookup[hex[2 * i + 1]];
+				}
+				catch (IndexOutOfRangeException)
+				{
+					if (hex[2 * i] >= byteLookup.Length)
+						throw badCharExn(2 * i, hex[2 * i]);
+					else
+						throw badCharExn(2 * i + 1, hex[2 * i + 1]);
+				}
+
 				if (b < 0)
 				{
-					var bad = byteLookup[hex[2 * i]] < 0 ? hex[2 * i] : hex[2 * i + 1];
-					throw new InvalidOperationException("Invalid character '" + bad + "' (" + ((int)bad).ToString("x") + ")");
+					if (byteLookup[hex[2 * i]] < 0)
+						throw badCharExn(2 * i, hex[2 * i]);
+					else
+						throw badCharExn(2 * i + 1, hex[2 * i + 1]);
 				}
 				arr[i] = (byte)b;
 			}
 			return arr;
 		}
+		static Exception badCharExn(int index, char c)
+		{
+			throw new InvalidOperationException("Invalid character '" + c + "' (" + ((int)c).ToString("x") + ") at index " + index);
+		}
+
 
 		public static string BytesToHex(byte[] arr)
 		{
