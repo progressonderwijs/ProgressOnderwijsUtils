025b52aba2ed454f90b420d6b21dbd8564ade696
Eamon Nerbonne <eamon@nerbonne.org>
2013-04-15 13:58:49 +0200

    C clean up ReferenceEqualityComparer.

--- src/ProgressOnderwijsUtils/ReferenceEqualityComparer.cs
+++ src/ProgressOnderwijsUtils/ReferenceEqualityComparer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -9,9 +10,12 @@ namespace ProgressOnderwijsUtils
 	/// Equality comparer that will compare on reference equality.
 	/// </summary>
 	/// <remarks>This might be handy to have collections on reference equality while the elements are value comparable.</remarks>
-	public class ReferenceEqualityComparer<T> : IEqualityComparer<T>
+	public sealed class ReferenceEqualityComparer<T> : IEqualityComparer<T>, IEqualityComparer
 	{
+		public static readonly ReferenceEqualityComparer<T> Default = new ReferenceEqualityComparer<T>();
 		public bool Equals(T one, T other) { return object.ReferenceEquals(one, other); }
 		public int GetHashCode(T obj) { return RuntimeHelpers.GetHashCode(obj); }
+		bool IEqualityComparer.Equals(object x, object y) { return object.ReferenceEquals(x, y); }
+		int IEqualityComparer.GetHashCode(object obj) { return RuntimeHelpers.GetHashCode(obj); }
 	}
 }
