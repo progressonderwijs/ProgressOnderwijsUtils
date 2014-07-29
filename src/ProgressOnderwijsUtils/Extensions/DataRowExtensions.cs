39d55263c61c14990060ebc1951911e89d84853a
Tjibbe Chris Kuipers <t.c.kuipers@rug.nl>
2014-07-29 16:42:07 +0200

    Naar aanleiding van commit 36ba047088bc: Gebruik liever geen datarow, dus ook deze nieuwe toevoeging ongedaan maken.

--- src/ProgressOnderwijsUtils/Extensions/DataRowExtensions.cs
+++ src/ProgressOnderwijsUtils/Extensions/DataRowExtensions.cs
@@ -14,23 +14,5 @@ namespace ProgressOnderwijsUtils
 		{
 			return row.Row.Field<T>(fieldname);
 		}
-
-		public static T GetId<T>(this DataRow row) where T : IIdentifier, new()
-		{
-			var id = new T();
-			string columnName;
-			if (row.Table.Columns.Contains(id.DbPrimaryKeyName))
-				columnName = id.DbPrimaryKeyName;
-			else if (row.Table.Columns.Contains(id.DbForeignKeyName))
-				columnName = id.DbForeignKeyName;
-			else
-				throw new NietZoErgeException(string.Format("Geen waarde voor {0} of {1}", id.DbPrimaryKeyName, id.DbForeignKeyName));
-
-			if (!row.IsNull(columnName))
-				id.Value = (int)row.Field<int>(columnName);
-
-			return id;
-		}
-
 	}
 }
