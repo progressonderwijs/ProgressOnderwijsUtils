f7d4b218ff97ed65b28b1189f74cbc3561e5c948
Eamon Nerbonne <eamon@nerbonne.org>
2014-08-07 17:54:53 +0200

    (translation) Nicer formatting/syntax for case selection; also re-add missing bool translation.

--- src/ProgressOnderwijsUtils/Text/ConverteerHelper.cs
+++ src/ProgressOnderwijsUtils/Text/ConverteerHelper.cs
@@ -20,52 +20,59 @@ namespace ProgressOnderwijsUtils
 			return obj is T ? translator((T)obj) : null;
 		}
 
-		static TranslateFunction OrToString<T>(this TranslateFunction f, object obj, Func<T, TranslateFunction> translator)
+		static IEnumerable<TranslateFunction> ResolveTranslator(object obj, string format)
 		{
-			return f ?? TryToString(obj, translator);
+			yield return TryToString<ITranslatable>(obj, o => language =>
+				o.Translate(language).Text);
+			yield return TryToString<string>(obj, o => language =>
+				o);
+			yield return TryToString<int>(obj, o => language =>
+				((long)o).ToString(format ?? "D", language.GetCulture()));
+			yield return TryToString<Enum>(obj, o => language =>
+				Converteer.TranslateEnum(o).Translate(language).Text);
+			yield return TryToString<decimal>(obj, o => language =>
+				o.ToString(format ?? GELD_EURO, language.GetCulture()));
+			yield return TryToString<DateTime>(obj, o => language =>
+				o.ToString(format ?? (o == o.Date ? ALLEEN_DATUM : DATUM_EN_TIJD_IN_MINUTEN), language.GetCulture()));
+			yield return TryToString<TimeSpan>(obj, o => language =>
+				o.ToString(format ?? "g", language.GetCulture()));
+			yield return TryToString<char>(obj, o => language =>
+				new string(o, 1));
+			yield return TryToString<XhtmlData>(obj, o => language =>
+				o.ToUiString());
+			yield return TryToString<VariantData>(obj, o => language =>
+				o.ToUiString());
+			yield return TryToString<FileData>(obj, o => language =>
+				o.ContainsFile ? string.Format("{0} ({1} KB)", o.FileName, o.Content.Length / 1000m) : "");
+			yield return TryToString<double>(obj, o => language =>
+				o.ToString(format ?? "0.##", language.GetCulture()));
+			yield return TryToString<long>(obj, o => language =>
+				o.ToString(format ?? "D", language.GetCulture()));
+			yield return TryToString<bool>(obj, o => language => {
+				switch (language)
+				{
+					case Taal.NL: return o ? "Ja" : "Nee";
+					case Taal.EN: return o ? "Yes" : "No";
+					case Taal.DU: return o ? "Ja" : "Nein";
+					default: throw new ArgumentOutOfRangeException("language", "Taal niet bekend: " + language);
+				}
+			});
+			yield return TryToString<IIdentifier>(obj, o => language =>
+				o.Value.ToString(format ?? "D", language.GetCulture()));
+			yield return TryToString<IEnumerable>(obj, o =>
+				ArrayToStringHelper(o, format));
 		}
 
 		public static TranslateFunction ToStringDynamic(object obj, string format = null)
 		{
 			if (obj == null || obj == DBNull.Value)
 				return language => "";
-			//perf critical: We avoid the overhead of dynamic in common cases by adding
-			//these redundant if statements.  Removing them means (in particular) that
-			//CriteriumFilter.ToString relies on dynamic code, which slows down test
-			//discovery.
 
-			var retval = TryToString<ITranslatable>(obj, o => language => o.Translate(language).Text)
-				.OrToString<string>(obj, o => language =>
-					o)
-				.OrToString<int>(obj, o => language =>
-					((long)o).ToString(format ?? "D", language.GetCulture()))
-				.OrToString<Enum>(obj, o => language => Converteer.TranslateEnum(o).Translate(language).Text)
-				.OrToString<decimal>(obj, o => language =>
-					o.ToString(format ?? GELD_EURO, language.GetCulture()))
-				.OrToString<DateTime>(obj, o => language =>
-					o.ToString(format ?? (o == o.Date ? ALLEEN_DATUM : DATUM_EN_TIJD_IN_MINUTEN), language.GetCulture()))
-				.OrToString<TimeSpan>(obj, o => language =>
-					o.ToString(format ?? "g", language.GetCulture()))
-				.OrToString<char>(obj, o => language =>
-					new string(o, 1))
-				.OrToString<XhtmlData>(obj, o => language =>
-					o.ToUiString())
-				.OrToString<VariantData>(obj, o => language =>
-					o.ToUiString())
-				.OrToString<FileData>(obj, o => language =>
-					o.ContainsFile ? string.Format("{0} ({1} KB)", o.FileName, o.Content.Length / 1000m) : "")
-				.OrToString<double>(obj, o => language =>
-					o.ToString(format ?? "0.##", language.GetCulture()))
-				.OrToString<long>(obj, o => language =>
-					o.ToString(format ?? "D", language.GetCulture()))
-				.OrToString<IIdentifier>(obj, o => language =>
-					o.Value.ToString(format ?? "D", language.GetCulture()))
-				.OrToString<IEnumerable>(obj, o => ArrayToStringHelper(o, format))
-				;
-			if (retval != null)
-				return retval;
-			else
-				throw new ConverteerException("unknown type " + obj.GetType() + " to stringify");
+			foreach (var func in ResolveTranslator(obj, format))
+				if (func != null)
+					return func;
+
+			throw new ConverteerException("unknown type " + obj.GetType() + " to stringify");
 		}
 
 		static TranslateFunction ArrayToStringHelper(IEnumerable o, string format)
