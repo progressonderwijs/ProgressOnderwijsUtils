9abbc269bba7551fb15d0f7f0be15d9490ebc2ca
Eamon Nerbonne <eamon@nerbonne.org>
2014-08-07 17:42:42 +0200

    Remove dynamic code dependency from Converteer

--- src/ProgressOnderwijsUtils/Text/ConverteerHelper.cs
+++ src/ProgressOnderwijsUtils/Text/ConverteerHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using MoreLinq;
@@ -6,98 +7,73 @@ using MoreLinq;
 namespace ProgressOnderwijsUtils
 {
 	public delegate string TranslateFunction(Taal taal = Taal.NL);
+
+
 	public static class ConverteerHelper
 	{
-		#region ToString implementations for use from dynamic code
-		// ReSharper disable MemberCanBePrivate.Global
-		// ReSharper disable UnusedParameter.Global
-		//used in dynamic code:
-		public static TranslateFunction ToString(IIdentifier i, string format) { return language => i.Value.ToString(format ?? "D", language.GetCulture()); }
-		public static TranslateFunction ToString(long l, string format) { return language => l.ToString(format ?? "D", language.GetCulture()); }
-		public static TranslateFunction ToString(double d, string format) { return language => d.ToString(format ?? "0.##", language.GetCulture()); }
-		public static TranslateFunction ToString(decimal d, string format) { return language => d.ToString(format ?? GELD_EURO, language.GetCulture()); }
-		public static TranslateFunction ToString(TimeSpan ts, string format) { return language => ts.ToString(format ?? "g", language.GetCulture()); }
-		public static TranslateFunction ToString(DateTime dt, string format) { return language => dt.ToString(format ?? (dt == dt.Date ? ALLEEN_DATUM : DATUM_EN_TIJD_IN_MINUTEN), language.GetCulture()); }
-		public static TranslateFunction ToString(FileData obj, string format) { return language => obj.ContainsFile ? string.Format("{0} ({1} KB)", obj.FileName, obj.Content.Length / 1000m) : ""; }
-		public static TranslateFunction ToString(VariantData obj, string format) { return language => obj.ToUiString(); }
-		public static TranslateFunction ToString(string str, string format) { return language => str; }
-		public static TranslateFunction ToString(char c, string format) { return language => new string(c, 1); }
-		public static TranslateFunction ToString(XhtmlData obj, string format) { return language => obj.ToUiString(); }
-		public static TranslateFunction ToString(ITranslatable obj, string format) { return language => obj.Translate(language).Text; }
-		public static TranslateFunction ToString(Enum obj, string format) { return ToString(Converteer.TranslateEnum(obj), format); }
-		public static TranslateFunction ToString<T>(T[] arr, string format)
-		{
-			var subtrans = arr.Select(obj => (TranslateFunction)ConverteerHelper.ToString((dynamic)obj, format)).ToArray();
-
-			return language => subtrans.Select(f => f(language)).JoinStrings("\n");
-		}
-
-
-		public static TranslateFunction ToString(bool b, string format)
-		{
-			return language => {
-				switch (language)
-				{
-					case Taal.NL: return b ? "Ja" : "Nee";
-					case Taal.EN: return b ? "Yes" : "No";
-					case Taal.DU: return b ? "Ja" : "Nein";
-					default: throw new ArgumentOutOfRangeException("language", "Taal niet bekend: " + language);
-				}
-			};
-		}
-		// ReSharper restore UnusedParameter.Global
-		// ReSharper restore MemberCanBePrivate.Global
-		#endregion
 		public const string GELD_EURO = "N02";
 		public const string ALLEEN_DATUM = "dd-MM-yyyy";
 		public const string DATUM_EN_TIJD_IN_MINUTEN = "dd-MM-yyyy HH:mm";
 
+		static TranslateFunction TryToString<T>(object obj, Func<T, TranslateFunction> translator)
+		{
+			return obj is T ? translator((T)obj) : null;
+		}
+
+		static TranslateFunction OrToString<T>(this TranslateFunction f, object obj, Func<T, TranslateFunction> translator)
+		{
+			return f ?? TryToString(obj, translator);
+		}
+
 		public static TranslateFunction ToStringDynamic(object obj, string format = null)
 		{
-			if (obj == null || obj == DBNull.Value) return language => "";
+			if (obj == null || obj == DBNull.Value)
+				return language => "";
 			//perf critical: We avoid the overhead of dynamic in common cases by adding
 			//these redundant if statements.  Removing them means (in particular) that
 			//CriteriumFilter.ToString relies on dynamic code, which slows down test
 			//discovery.
 
-			if (obj is ITranslatable)
-				return ToString((ITranslatable)obj, format);
-			if (obj is string)
-				return ToString((string)obj, format);
-			if (obj is int)
-				return ToString((int)obj, format);
-			if (obj is Enum)
-				return ToString((Enum)obj, format);
-			if (obj is decimal)
-				return ToString((decimal)obj, format);
-			if (obj is DateTime)
-				return ToString((DateTime)obj, format);
-			if (obj is TimeSpan)
-				return ToString((TimeSpan)obj, format);
-			if (obj is char)
-				return ToString((char)obj, format);
-			if (obj is XhtmlData)
-				return ToString((XhtmlData)obj, format);
-			if (obj is VariantData)
-				return ToString((VariantData)obj, format);
-			if (obj is FileData)
-				return ToString((FileData)obj, format);
-			if (obj is double)
-				return ToString((double)obj, format);
-			if (obj is long)
-				return ToString((long)obj, format);
-			if (obj is IIdentifier)
-				return ToString((IIdentifier)obj, format);
-			try
-			{
-				return ConverteerHelper.ToString((dynamic)obj, format);
-			}
-			catch (Exception e)
-			{
-				throw new ConverteerException("unknown type " + obj.GetType() + " to stringify", e);
-			}
+			var retval = TryToString<ITranslatable>(obj, o => language => o.Translate(language).Text)
+				.OrToString<string>(obj, o => language =>
+					o)
+				.OrToString<int>(obj, o => language =>
+					((long)o).ToString(format ?? "D", language.GetCulture()))
+				.OrToString<Enum>(obj, o => language => Converteer.TranslateEnum(o).Translate(language).Text)
+				.OrToString<decimal>(obj, o => language =>
+					o.ToString(format ?? GELD_EURO, language.GetCulture()))
+				.OrToString<DateTime>(obj, o => language =>
+					o.ToString(format ?? (o == o.Date ? ALLEEN_DATUM : DATUM_EN_TIJD_IN_MINUTEN), language.GetCulture()))
+				.OrToString<TimeSpan>(obj, o => language =>
+					o.ToString(format ?? "g", language.GetCulture()))
+				.OrToString<char>(obj, o => language =>
+					new string(o, 1))
+				.OrToString<XhtmlData>(obj, o => language =>
+					o.ToUiString())
+				.OrToString<VariantData>(obj, o => language =>
+					o.ToUiString())
+				.OrToString<FileData>(obj, o => language =>
+					o.ContainsFile ? string.Format("{0} ({1} KB)", o.FileName, o.Content.Length / 1000m) : "")
+				.OrToString<double>(obj, o => language =>
+					o.ToString(format ?? "0.##", language.GetCulture()))
+				.OrToString<long>(obj, o => language =>
+					o.ToString(format ?? "D", language.GetCulture()))
+				.OrToString<IIdentifier>(obj, o => language =>
+					o.Value.ToString(format ?? "D", language.GetCulture()))
+				.OrToString<IEnumerable>(obj, o => ArrayToStringHelper(o, format))
+				;
+			if (retval != null)
+				return retval;
+			else
+				throw new ConverteerException("unknown type " + obj.GetType() + " to stringify");
 		}
 
+		static TranslateFunction ArrayToStringHelper(IEnumerable o, string format)
+		{
+			var subtrans = o.Cast<object>().Select(elem =>
+				ToStringDynamic(elem, format)).ToArray();
 
+			return language => subtrans.Select(f => f(language)).JoinStrings("\n");
+		}
 	}
 }
