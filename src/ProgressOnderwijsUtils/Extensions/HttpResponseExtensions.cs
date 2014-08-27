4db8b63f58fb3efb731b594f8820e1f4e2c7f0bc
Christiaan Arnoldus <c.arnoldus@rug.nl>
2014-08-27 13:40:49 +0200

    Extension method AddContentDispositionHeader gemaakt die automatisch bestandsnaam omringd met "". Als dit niet gebeurt gaat het fout in o.a. Chrome als de bestandnaam een komma bevat.

---
+++ src/ProgressOnderwijsUtils/Extensions/HttpResponseExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProgressOnderwijsUtils
+{
+	public static class HttpResponseExtensions
+	{
+		public static void AddContentDispositionHeader(this HttpResponse response, string fileName)
+		{
+			response.AddHeader("Content-Disposition", string.Format("attachment;filename=\"{0}\";", fileName));
+		}
+	}
+}
