8b818532d6544b60d035c1f01e72cf8219469023
Fons Dijkstra <f.j.dijkstra@rug.nl>
2014-09-09 15:59:05 +0200

    Fix de lang falende test cases door apart om te gaan met de timeouts.
    --HG--
    branch : Test

--- src/ProgressOnderwijsUtils/Utils.cs
+++ src/ProgressOnderwijsUtils/Utils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Data.Entity.Core;
 using System.Collections.Generic;
 using System.Data;
@@ -199,6 +200,11 @@ namespace ProgressOnderwijsUtils
 
 		static bool IsTimeoutException(Exception e)
 		{
+			if (e is AggregateException)
+			{
+				return (e as AggregateException).InnerExceptions.All(IsTimeoutException);
+			}
+
 			for (var current = e; current != null; current = current.InnerException)
 			{
 				var sqlE = current as SqlException;
