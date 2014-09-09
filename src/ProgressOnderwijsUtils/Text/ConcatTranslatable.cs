30c2945d1a71cc87afcbd8216d77b14d633aa661
Eamon Nerbonne <eamon@nerbonne.org>
2014-09-09 09:50:13 +0200

    Optimize Translatable concatenation.

--- src/ProgressOnderwijsUtils/Text/ConcatTranslatable.cs
+++ src/ProgressOnderwijsUtils/Text/ConcatTranslatable.cs
@@ -9,14 +9,15 @@ namespace ProgressOnderwijsUtils.Text
 	public sealed class ConcatTranslatable : ITranslatable
 	{
 		readonly ITranslatable[] parts;
-		//public ITranslatable[] Parts { get { return parts; } }
 
 		public ConcatTranslatable(params ITranslatable[] parts)
 		{
 			this.parts = parts;
-			if (parts.Any(p => p == null))
-				throw new ArgumentNullException("parts", "element of parts is null");
+			foreach (ITranslatable p in parts)
+				if (p == null) //Perf: no LINQ
+					throw new ArgumentNullException("parts", "element of parts is null");
 		}
+
 		public string GenerateUid() { return parts.Select(it => it.GenerateUid()).JoinStrings(); }
 
 		public TextVal Translate(Taal taal)
