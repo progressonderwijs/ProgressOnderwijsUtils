e189f540f5c7ea9486de383668b147dbe23a4b86
Eamon Nerbonne <eamon@nerbonne.org>
2014-09-16 16:19:04 +0200

    Simplify enumhelpers, minor optimizations

--- src/ProgressOnderwijsUtils/Enums/EnumHelpers.cs
+++ src/ProgressOnderwijsUtils/Enums/EnumHelpers.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using ExpressionToCodeLib;
-using ProgressOnderwijsUtils.Collections;
 
 namespace ProgressOnderwijsUtils
 {
@@ -35,17 +34,9 @@ namespace ProgressOnderwijsUtils
 			{
 				return (a & b) != 0;
 			}
-			public static bool Equals(int a, int b)
-			{
-				return (a == b);
-			}
-			public static int GetHashcode(int a)
+			public static long ToInt64(int a)
 			{
 				return a;
-			}
-			public static int Compare(int a, int b)
-			{
-				return a < b ? -1 : a == b ? 0 : 1;
 			}
 		}
 
@@ -66,63 +57,32 @@ namespace ProgressOnderwijsUtils
 			{
 				return (a & b) != 0L;
 			}
-			public static bool Equals(long a, long b)
+			public static long ToInt64(long a)
 			{
-				return (a == b);
-			}
-			public static int GetHashcode(long a)
-			{
-				return (int)(a >> 32) ^ (int)a;
-			}
-			public static int Compare(long a, long b)
-			{
-				return a < b ? -1 : a == b ? 0 : 1;
+				return a;
 			}
 		}
 
 
 		struct FlagOperationMethods
 		{
-			public static FlagOperationMethods Get<T>(Func<T, T, T> or, Func<T, T, bool> hasFlag, Func<T, T, bool> overlaps, Func<T, T, bool> equals, Func<T, int> getHashcode, Func<T, T, int> compare)
+			public static FlagOperationMethods Get<T>(Func<T, T, T> or, Func<T, T, bool> hasFlag, Func<T, T, bool> overlaps, Func<T, long> toInt64)
 			{
 				return new FlagOperationMethods
 				{
 					Or = or.Method,
 					HasFlag = hasFlag.Method,
 					HasFlagOverlap = overlaps.Method,
-					EqualsMethod = equals.Method,
-					GetHashcode = getHashcode.Method,
-					Compare = compare.Method,
+					ToInt64 = toInt64.Method,
 				};
 			}
 
-			public MethodInfo Or, HasFlag, HasFlagOverlap, EqualsMethod, GetHashcode, Compare;
+			public MethodInfo Or, HasFlag, HasFlagOverlap,   ToInt64;
 		}
 
-		static FlagOperationMethods forInt = FlagOperationMethods.Get<int>(Int32Helpers.Or, Int32Helpers.HasFlag, Int32Helpers.HasFlagOverlap, Int32Helpers.Equals, Int32Helpers.GetHashcode, Int32Helpers.Compare)
+		static readonly FlagOperationMethods forInt = FlagOperationMethods.Get<int>(Int32Helpers.Or, Int32Helpers.HasFlag, Int32Helpers.HasFlagOverlap, Int32Helpers.ToInt64)
 
-			, forLong = FlagOperationMethods.Get<long>(Int64Helpers.Or, Int64Helpers.HasFlag, Int64Helpers.HasFlagOverlap, Int64Helpers.Equals, Int64Helpers.GetHashcode, Int64Helpers.Compare);
-
-		class LambdaEqualityComparer<T> : IEqualityComparer<T>
-		{
-			readonly Func<T, T, bool> equals;
-			readonly Func<T, int> hash;
-			public LambdaEqualityComparer(Func<T, T, bool> equals, Func<T, int> hash)
-			{
-				this.equals = equals;
-				this.hash = hash;
-			}
-
-			public bool Equals(T x, T y)
-			{
-				return equals(x, y);
-			}
-
-			public int GetHashCode(T obj)
-			{
-				return hash(obj);
-			}
-		}
+			, forLong = FlagOperationMethods.Get<long>(Int64Helpers.Or, Int64Helpers.HasFlag, Int64Helpers.HasFlagOverlap, Int64Helpers.ToInt64);
 
 		static readonly ITranslatable translatableComma = Translatable.Raw(", ");
 		static readonly ConcurrentDictionary<Type, IEnumMetaCache> enumMetaCache = new ConcurrentDictionary<Type, IEnumMetaCache>();
@@ -303,17 +263,17 @@ namespace ProgressOnderwijsUtils
 
 			struct AttrEntry
 			{
-				public TEnum Value;
+				public long Value;
 				public object[] Attrs;
 				public ITranslatable Label;
 			}
 
 
 			static AttrEntry[] sortedAttrs;
-			static Comparison<TEnum> comp;
+			static Func<TEnum, long> toInt64;
 
 
-			static int IdxAfterLastLtNode(TEnum needle)
+			static int IdxAfterLastLtNode(long needle)
 			{
 				//based on https://github.com/EamonNerbonne/a-vs-an/blob/master/A-vs-An/A-vs-An-DotNet/Internals/Node.cs
 				int start = 0, end = sortedAttrs.Length;
@@ -323,7 +283,7 @@ namespace ProgressOnderwijsUtils
 				{
 					int midpoint = end + start >> 1;
 					// start <= midpoint < end
-					if (comp(sortedAttrs[midpoint].Value, needle) < 0)
+					if (sortedAttrs[midpoint].Value < needle)
 					{
 						start = midpoint + 1;//i.e. midpoint < start1 so start0 < start1
 					}
@@ -339,8 +299,9 @@ namespace ProgressOnderwijsUtils
 			{
 				if (sortedAttrs == null)
 					InitAttrCache();
-				int idx = IdxAfterLastLtNode(value);
-				if (idx < sortedAttrs.Length && comp(sortedAttrs[idx].Value, value) == 0)
+				long key = toInt64(value);
+				int idx = IdxAfterLastLtNode(key);
+				if (idx < sortedAttrs.Length && sortedAttrs[idx].Value == key)
 					return sortedAttrs[idx].Attrs;
 				else return ArrayExtensions.Empty<object>();
 			}
@@ -349,11 +310,11 @@ namespace ProgressOnderwijsUtils
 			{
 				if (typeof(int) == underlying)
 				{
-					CreateDelegate(out comp, forInt.Compare);
+					CreateDelegate(out toInt64, forInt.ToInt64);
 				}
 				else
 				{
-					comp = (a, b) => a.CompareTo(b);
+					toInt64 = a => a.ToInt64(null);
 				}
 
 				var entries = new AttrEntry[EnumValues.Length];
@@ -364,18 +325,25 @@ namespace ProgressOnderwijsUtils
 					if (customAttributes.Length == 0)
 						continue;
 					var value = (TEnum)field.GetValue(null);
+					long key = toInt64(value);
 					int insertIdx = nextIdx - 1;
 					while (true)
 					{
-
-						var comparison = insertIdx == -1 ? 1 : comp(value, entries[insertIdx].Value);
-						if (comparison < 0)
+						if (insertIdx == -1 || key > entries[insertIdx].Value)
 						{
-							insertIdx--;
+							insertIdx++;
+							for (int j = nextIdx - 1; j >= insertIdx; j--)
+								entries[j + 1] = entries[j];
+							entries[insertIdx] = new AttrEntry
+							{
+								Value = key,
+								Attrs = customAttributes
+							};
+							nextIdx++;
+							break;
 						}
-						else if (comparison == 0)
+						else if (key == entries[insertIdx].Value)
 						{
-
 							var oldLength = entries[insertIdx].Attrs.Length;
 							Array.Resize(ref entries[insertIdx].Attrs, oldLength + customAttributes.Length);
 							int i = oldLength;
@@ -385,16 +353,7 @@ namespace ProgressOnderwijsUtils
 						}
 						else
 						{
-							insertIdx++;
-							for (int j = nextIdx - 1; j >= insertIdx; j--)
-								entries[j + 1] = entries[j];
-							entries[insertIdx] = new AttrEntry
-							{
-								Value = value,
-								Attrs = customAttributes
-							};
-							nextIdx++;
-							break;
+							insertIdx--;
 						}
 					}
 				}
@@ -461,8 +420,9 @@ namespace ProgressOnderwijsUtils
 			{
 				if (sortedAttrs == null)
 					InitAttrCache();
-				var idx = IdxAfterLastLtNode(f);
-				bool validIdx = idx < sortedAttrs.Length && comp(sortedAttrs[idx].Value, f) == 0;
+				var key = toInt64(f);
+				var idx = IdxAfterLastLtNode(key);
+				bool validIdx = idx < sortedAttrs.Length && sortedAttrs[idx].Value == key;
 
 
 				if (validIdx && sortedAttrs[idx].Label != null)
