7aa1c7aff7641121045cdb4555c0efc2e396cd0f
Fons Dijkstra <f.j.dijkstra@rug.nl>
2014-10-23 15:06:09 +0200

    Fix de lazy log4net logger met lazy formatting

--- src/ProgressOnderwijsUtils/Log4Net/ILogExtensions.cs
+++ src/ProgressOnderwijsUtils/Log4Net/ILogExtensions.cs
@@ -11,40 +11,44 @@ namespace ProgressOnderwijsUtils.Log4Net
 			if (log.IsDebugEnabled) log.Debug(msg());
 		}
 
-		public static void Debug(this Lazy<ILog> log, Func<object> msg)
+        public static void Debug(this ILog log, Func<object> msg, Exception exception)
+        {
+            if (log.IsDebugEnabled) log.Debug(msg(), exception);
+        }
+
+        public static void Debug(this Lazy<ILog> log, Func<object> msg)
 		{
-			log.Value.Debug(msg);
+			if (log.Value.IsDebugEnabled) log.Value.Debug(msg());
 		}
+
 		public static void Debug(this Lazy<ILog> log, object msg)
 		{
 			log.Value.Debug(msg);
 		}
+
 		public static void DebugFormat(this Lazy<ILog> log, string format, params object[] args)
 		{
 			log.Value.DebugFormat(format, args);
-		}
-
-		public static void Debug(this ILog log, Func<object> msg, Exception exception)
-		{
-			if (log.IsDebugEnabled) log.Debug(msg(), exception);
 		}
 
 		public static void Info(this ILog log, Func<object> msg)
 		{
 			if (log.IsInfoEnabled) log.Info(msg());
 		}
-		public static void Info(this Lazy<ILog> log, object msg)
-		{
-			log.Value.Info(msg);
-		}
-		public static void Info(this Lazy<ILog> log, Func<object> msg)
-		{
-			log.Value.Info(msg);
-		}
 
-		public static void Info(this ILog log, Func<object> msg, Exception exception)
+        public static void Info(this ILog log, Func<object> msg, Exception exception)
+        {
+            if (log.IsInfoEnabled) log.Info(msg(), exception);
+        }
+
+        public static void Info(this Lazy<ILog> log, Func<object> msg)
+        {
+            if (log.Value.IsInfoEnabled) log.Value.Info(msg());
+        }
+
+        public static void Info(this Lazy<ILog> log, object msg)
 		{
-			if (log.IsInfoEnabled) log.Info(msg(), exception);
+			log.Value.Info(msg);
 		}
 
 		public static void Warn(this ILog log, Func<object> msg)
@@ -56,19 +60,21 @@ namespace ProgressOnderwijsUtils.Log4Net
 		{
 			if (log.IsWarnEnabled) log.Warn(msg(), exception);
 		}
+
 		public static void Warn(this Lazy<ILog> log, Func<object> msg)
 		{
-			log.Value.Warn(msg);
+			if (log.Value.IsWarnEnabled) log.Value.Warn(msg());
 		}
+
 		public static void Warn(this Lazy<ILog> log, object msg)
 		{
 			log.Value.Warn(msg);
 		}
+
 		public static void WarnFormat(this Lazy<ILog> log, string format, params object[] args)
 		{
 			log.Value.WarnFormat(format, args);
 		}
-
 
 		public static void Error(this ILog log, Func<object> msg)
 		{
@@ -79,19 +85,21 @@ namespace ProgressOnderwijsUtils.Log4Net
 		{
 			if (log.IsErrorEnabled) log.Error(msg(), exception);
 		}
+
 		public static void Error(this Lazy<ILog> log, Func<object> msg)
 		{
-			log.Value.Error(msg);
+			if (log.Value.IsErrorEnabled) log.Value.Error(msg());
 		}
+
 		public static void Error(this Lazy<ILog> log, object msg)
 		{
 			log.Value.Error(msg);
 		}
+
 		public static void ErrorFormat(this Lazy<ILog> log, string format, params object[] args)
 		{
 			log.Value.ErrorFormat(format, args);
 		}
-
 
 		public static void Fatal(this ILog log, Func<object> msg)
 		{
