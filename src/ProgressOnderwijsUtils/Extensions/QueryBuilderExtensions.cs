5e4637ba88fa988b7e9979bf5e15941d96ecf071
Tjibbe Chris Kuipers <t.c.kuipers@rug.nl>
2014-11-20 08:33:16 +0100

    Querybuilder Extensions pure gemaakt en methode AppendIf toegevoegd
    --HG--
    branch : tc-querybuilder-extension-appendif

--- src/ProgressOnderwijsUtils/Extensions/QueryBuilderExtensions.cs
+++ src/ProgressOnderwijsUtils/Extensions/QueryBuilderExtensions.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 
 namespace ProgressOnderwijsUtils
 {
 	public static class QueryBuilderExtensions
 	{
+		[Pure]
 		public static QueryBuilder Append(this QueryBuilder source, string str, params object[] parms)
 		{
 			return source + QueryBuilder.Create(Environment.NewLine + str + " ", parms);
+		}
+
+		[Pure]
+		public static QueryBuilder AppendIf(this QueryBuilder source, Boolean condition, string str, params object[] parms)
+		{
+			return condition ? source.Append(str, parms) : source;
 		}
 	}
 }
