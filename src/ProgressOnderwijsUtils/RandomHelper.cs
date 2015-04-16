1d140492e5ff0a35559bb774e075ae3e4402a188
Eamon Nerbonne <eamon@nerbonne.org>
2015-04-16 10:53:09 +0200

    Implement random helper for uri printable chars.
    --HG--
    branch : eamon/AlternateRndMethod

--- src/ProgressOnderwijsUtils/RandomHelper.cs
+++ src/ProgressOnderwijsUtils/RandomHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using ExpressionToCodeLib;
 using MoreLinq;
 using NUnit.Framework;
 using ProgressOnderwijsUtils.Test;
@@ -90,6 +91,13 @@ namespace ProgressOnderwijsUtils
             return sb.ToString();
         }
 
+        static readonly char[] UriPrintableCharacters =
+            Enumerable.Range('A', 26).Concat(Enumerable.Range('a', 26)).Concat(Enumerable.Range('0', 10)).Select(i => (char)i).Concat("_-~").ToArray();
+
+        public static string GetStringOfUriPrintableCharacters(int length)
+        {
+            return new string(Enumerable.Range(0, length).Select(_ => UriPrintableCharacters[GetUInt32((uint)UriPrintableCharacters.Length)]).ToArray());
+        }
     }
 
     [Continuous]
@@ -120,6 +128,16 @@ namespace ProgressOnderwijsUtils
         }
 
         [Test]
+        public void CheckUriPrintable()
+        {
+            for (var i = 0; i < 50; i++) {
+                var str = RandomHelper.GetStringOfUriPrintableCharacters(i);
+                var escaped = Uri.EscapeDataString(str);
+                PAssert.That(() => str == escaped);
+            }
+        }
+
+        [Test]
         public void CheckStrings()
         {
             Assert.That(RandomHelper.GetStringOfNumbers(10), Is.StringMatching("[0-9]{10}"));
@@ -127,6 +145,5 @@ namespace ProgressOnderwijsUtils
             Assert.That(RandomHelper.GetStringOfLatinLower(7), Is.StringMatching("[a-z]{7}"));
             Assert.That(RandomHelper.GetStringOfLatinUpperOrLower(10), Is.StringMatching("[a-zA-Z]{10}"));
         }
-
     }
 }
