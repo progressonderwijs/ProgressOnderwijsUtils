0e280ed6cb1ef397a4888b6c5df4dabbb02a957e
Mark IJbema <markijbema@gmail.com>
2015-07-07 09:19:08 +0200

    minor cleanup of textval

--- src/ProgressOnderwijsUtils/Text/TextVal.cs
+++ src/ProgressOnderwijsUtils/Text/TextVal.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using ProgressOnderwijsUtils;
-
-namespace ProgressOnderwijsUtils
+﻿namespace ProgressOnderwijsUtils
 {
     public struct TextVal
     {
-        readonly string m_text, m_helptext;
-        public string Text => m_text;
-        public string ExtraText => m_helptext;
+        public string Text { get; }
+        public string ExtraText { get; }
         public static string UndefinedExtraText => "Sorry, this text is not available in your language";
         public bool IsDefined => !(UndefinedExtraText == ExtraText || Text != null && Text.StartsWith("~") && ExtraText != null && ExtraText.StartsWith("~"));
         public bool IsEmpty => string.IsNullOrEmpty(Text) && string.IsNullOrEmpty(ExtraText);
@@ -21,18 +15,17 @@ namespace ProgressOnderwijsUtils
         /// <param name="helptext">Any additional text to potentially be shown in a tooltip.</param>
         public TextVal(string text, string helptext)
         {
-            m_text = text;
-            m_helptext = helptext;
+            Text = text;
+            ExtraText = helptext;
         }
 
         public static TextVal CreateUndefined(string hint) => new TextVal("ONVERTAALD: " + hint, UndefinedExtraText);
-        public static bool operator ==(TextVal a, TextVal b) { return a.m_text == b.m_text && a.m_helptext == b.m_helptext; }
+        public static bool operator ==(TextVal a, TextVal b) { return a.Text == b.Text && a.ExtraText == b.ExtraText; }
         public static bool operator !=(TextVal a, TextVal b) { return !(a == b); }
-        public override bool Equals(object obj) => obj != null && obj is TextVal && this == (TextVal)obj;
-        public override int GetHashCode() => (m_text == null ? 0 : m_text.GetHashCode()) + (m_helptext == null ? 0 : 137 * m_helptext.GetHashCode());
+        public override bool Equals(object obj) => obj is TextVal && this == (TextVal)obj;
+        public override int GetHashCode() => (Text?.GetHashCode() ?? 0) + (137 * ExtraText?.GetHashCode() ?? 0);
         public override string ToString() => "\"" + Text + "\"/\"" + ExtraText + "\"";
         public static TextVal Create(string text, string helptext) => new TextVal(text, helptext);
         public static TextVal Create(string text) => new TextVal(text, "");
-        public static TextVal EmptyText => new TextVal("", "");
     }
 }
