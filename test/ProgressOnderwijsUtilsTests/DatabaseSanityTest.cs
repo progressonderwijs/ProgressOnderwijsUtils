f8d31ea3f8581887663c6f4b139695d0f631af89
Fons Dijkstra <f.j.dijkstra@rug.nl>
2015-08-05 12:32:12 +0200

    De index op log.actietype werd nooit gebruikt en was daarom verwijderd. Daarom hier als exceptie aangemerkt.

--- test/ProgressOnderwijsUtilsTests/DatabaseSanityTest.cs
+++ test/ProgressOnderwijsUtilsTests/DatabaseSanityTest.cs
@@ -1,15 +1,22 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 using Progress.Business;
 using Progress.Business.Test;
 using ProgressOnderwijsUtils;
+using ProgressOnderwijsUtils.Test;
 
 namespace ProgressOnderwijsUtilsTests
 {
+    [Continuous]
     public sealed class DatabaseSanityTest : TestSuiteBase
     {
         [Test]
         public void OntbrekendeForeignKeyIndexes()
         {
+            IEnumerable<string> exceptions = new[] {
+                "FK_log_logactietype", // dit was een niet gebruikte index die verwijderd is
+            };
+
             const string q = @"
                 select
                     'Geen index op kolom ['  + c.name + '] in tabel [' + s.name + '.' + pt.name + '], gebruikt in [' + k.name + ']'
@@ -20,9 +27,10 @@ namespace ProgressOnderwijsUtilsTests
                 join sys.schemas s on s.schema_id = pt.schema_id
                 left join sys.index_columns ic on ic.object_id = kc.parent_object_id and ic.column_id = kc.parent_column_id
                 where ic.object_id is null
+                    and k.name not in (select val from {0})
                 ";
 
-            Assert.That(QueryBuilder.Create(q).ReadPlain<string>(conn), Is.Empty, "Geen index op FK-kolom.");
+            Assert.That(QueryBuilder.Create(q, exceptions).ReadPlain<string>(conn), Is.Empty, "Geen index op FK-kolom.");
         }
     }
 }
