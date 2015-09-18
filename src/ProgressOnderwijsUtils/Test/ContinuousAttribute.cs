38feb56991c698206bda9a2620a2aa6f885f7866
Tjibbe Chris Kuipers <t.c.kuipers@rug.nl>
2015-09-18 12:32:03 +0200

    Revert "Extra testattribuut om DataControletests te markeren"
    This reverts commit 519e001f2872933533c95a3a05dbad9ff135c24d.

--- src/ProgressOnderwijsUtils/Test/ContinuousAttribute.cs
+++ src/ProgressOnderwijsUtils/Test/ContinuousAttribute.cs
@@ -5,7 +5,4 @@ namespace ProgressOnderwijsUtils.Test
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
     public sealed class ContinuousAttribute : CategoryAttribute { }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
-    public sealed class DataControle : CategoryAttribute { }
 }
