28e3f58c48e6618d11f8674ae5464bb3abbf455b
Tjibbe Chris Kuipers <t.c.kuipers@rug.nl>
2015-09-18 12:20:34 +0200

    Extra testattribuut om DataControletests te markeren

--- src/ProgressOnderwijsUtils/Test/ContinuousAttribute.cs
+++ src/ProgressOnderwijsUtils/Test/ContinuousAttribute.cs
@@ -5,4 +5,7 @@ namespace ProgressOnderwijsUtils.Test
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
     public sealed class ContinuousAttribute : CategoryAttribute { }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class DataControle : CategoryAttribute { }
 }
