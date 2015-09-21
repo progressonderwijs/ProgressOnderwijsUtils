181ce63d84d9da020c679e5289a6bf3f3d6b105d
Mark IJbema <markijbema@gmail.com>
2015-09-21 14:40:22 +0200

    removed unused method

--- src/ProgressOnderwijsUtils/Extensions/StringFormatter.cs
+++ src/ProgressOnderwijsUtils/Extensions/StringFormatter.cs
@@ -5,11 +5,6 @@ namespace ProgressOnderwijsUtils
 {
     public static class StringFormatter
     {
-        public static string FormatWithInvariantCulture(FormattableString interpolatedString)
-        {
-            return CultureInfo.InvariantCulture.FormatString(interpolatedString);
-        }
-
         public static string FormatString(this CultureInfo culture, FormattableString interpolatedString)
         {
             return interpolatedString.ToString(culture);
