d13b21204db350660639a2b1fc70a79b0a64a939
Mark IJbema <markijbema@gmail.com>
2015-09-22 09:55:58 +0200

    marked dead code because this is preparation

--- src/ProgressOnderwijsUtils/Test/ContinuousAttribute.cs
+++ src/ProgressOnderwijsUtils/Test/ContinuousAttribute.cs
@@ -6,6 +6,7 @@ namespace ProgressOnderwijsUtils.Test
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
     public sealed class ContinuousAttribute : CategoryAttribute { }
 
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false),
+        UsefulToKeep("We gaan continuous hiermee vervangen")]
     public sealed class ExcludeFromContinuousAttribute : CategoryAttribute { }
 }
