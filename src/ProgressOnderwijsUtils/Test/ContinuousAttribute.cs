108b7bc5d7e82751f3c0325e927e16bd70ee27a8
Fons Dijkstra <f.j.dijkstra@rug.nl>
2015-09-24 09:58:42 +0200

    Test categorie attribuut gemaakt om de missende betaalplan test cases mee uit te zetten.

--- src/ProgressOnderwijsUtils/Test/ContinuousAttribute.cs
+++ src/ProgressOnderwijsUtils/Test/ContinuousAttribute.cs
@@ -9,4 +9,13 @@ namespace ProgressOnderwijsUtils.Test
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false),
         UsefulToKeep("We gaan continuous hiermee vervangen")]
     public sealed class ExcludeFromContinuousAttribute : CategoryAttribute { }
+
+    /// <summary>
+    /// Bij het 'uitzetten' van de betaalplannen in de code zijn er een 100-tal test cases die nog wel uitgaan van een betaalplan in het opzetten van hun scenario.
+    /// Deze test cases zetten we nu even tijdelijk uit, maar moeten zo snel mogelijk of gefixed worden of verwijderd worden indien niet meer van toepassing.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class MissendeBetaalplanAttribute : CategoryAttribute
+    {
+    }
 }
