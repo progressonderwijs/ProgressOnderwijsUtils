5b5c8b14a1157144078f541c7bb64064857b4540
Christiaan Arnoldus <c.arnoldus@rug.nl>
2015-10-01 16:21:23 +0200

    HtmlHelper-class gemaakt

---
+++ src/ProgressOnderwijsUtils/HtmlHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Linq;
+
+namespace ProgressOnderwijsUtils
+{
+    public static class HtmlHelper
+    {
+        public static XhtmlData RenderPercentageGrafisch(decimal percentage)
+        {
+            var divOuter = new XElement(
+                "div",
+                new object[] {
+                    new XAttribute("class", "staafdiagramprocentcontainer"),
+                    new XElement(
+                        "div",
+                        new object[] {
+                            new XAttribute("class", "staafdiagramprocentdata"),
+                            (percentage / 100).ToString("P1")
+                        }),
+                    new XElement(
+                        "div",
+                        new object[] {
+                            new XAttribute("class", "staafdiagramprocentbalkachtergrond"),
+                            new XElement(
+                                "div",
+                                new object[] {
+                                    new XAttribute("class", "staafdiagramprocentbalk"),
+                                    new XAttribute("style", $"width: {percentage:N0}px;"),
+                                })
+                        })
+                });
+            return XhtmlData.Create(divOuter);
+        }
+    }
+}
