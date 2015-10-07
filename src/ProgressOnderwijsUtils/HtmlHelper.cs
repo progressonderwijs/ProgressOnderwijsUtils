924f73e94802282cd8436fbb9987be290f5760d4
Christiaan Arnoldus <c.arnoldus@rug.nl>
2015-10-07 10:35:21 +0200

    AbstractInschrijvingMetBronHoLijst weggegooid.

--- src/ProgressOnderwijsUtils/HtmlHelper.cs
+++ src/ProgressOnderwijsUtils/HtmlHelper.cs
@@ -4,22 +4,28 @@ namespace ProgressOnderwijsUtils
 {
     public static class HtmlHelper
     {
-        public static XhtmlData CheckmarkGreenNoBorderSymbol() => XhtmlData.Create(new XElement("span", new XAttribute("class", "CheckmarkGreenNoBorderSymbol")));
-
-        public static XhtmlData QuestionRedSymbol(string toolTip) => XhtmlData.Create(
+        static XhtmlData Symbol(string cssClass) => XhtmlData.Create(
             new XElement("span",
-                new XAttribute("class", "QuestionRedSymbol hastooltip"),
+                new XAttribute("class", cssClass)));
+
+        static XhtmlData Symbol(string cssClass, string toolTip) => XhtmlData.Create(
+            new XElement("span",
+                new XAttribute("class", cssClass + " hastooltip"),
                 new XAttribute("data-tiptext", toolTip)));
 
-        public static XhtmlData CrossRedSymbol(string toolTip) => XhtmlData.Create(
-            new XElement("span",
-                new XAttribute("class", "CrossRedSymbol hastooltip"),
-                new XAttribute("data-tiptext", toolTip)));
+        public static XhtmlData CheckmarkGreenNoBorderSymbol() => Symbol("CheckmarkGreenNoBorderSymbol");
 
-        public static XhtmlData ExclamationGreenSymbol(string toolTip) => XhtmlData.Create(
-            new XElement("span",
-                new XAttribute("class", "ExclamationGreenSymbol hastooltip"),
-                new XAttribute("data-tiptext", toolTip)));
+        public static XhtmlData QuestionRedSymbol(string toolTip) => Symbol("QuestionRedSymbol", toolTip);
+
+        public static XhtmlData CrossRedSymbol(string toolTip) => Symbol("CrossRedSymbol", toolTip);
+
+        public static XhtmlData CrossGreenSymbol(string toolTip) => Symbol("CrossGreenSymbol", toolTip);
+
+        public static XhtmlData ExclamationGreenSymbol(string toolTip) => Symbol("ExclamationGreenSymbol", toolTip);
+
+        public static XhtmlData LedGreenSymbol(string toolTip) => Symbol("LedGreenSymbol", toolTip);
+
+        public static XhtmlData LedRedSymbol(string toolTip) => Symbol("LedRedSymbol", toolTip);
 
         public static XhtmlData RenderPercentageGrafisch(decimal percentage)
         {
