aa28f1d3656616085268c8b6789bab12957ff5eb
Fons Dijkstra <f.j.dijkstra@rug.nl>
2015-10-14 16:42:27 +0200

    Definieer 'fake' assembly types zodat we hiermee de assemblies kunnen opvragen ipv daadwerkelijk functionele types die we wellicht willen gaan refactoren in de toekomst.

---
+++ src/ProgressOnderwijsUtils/ToolsAssembly.cs
@@ -0,0 +1,7 @@
+﻿namespace ProgressOnderwijsUtils
+{
+    /// <summary>
+    /// Fake type to retrieve the containing assembly from.
+    /// </summary>
+    public static class ToolsAssembly { }
+}
