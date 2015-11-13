6517beae7315ade22a0a4c0ccaf7b28d0a9cb487
Christiaan Arnoldus <c.arnoldus@rug.nl>
2015-11-13 11:24:40 +0100

    Test voor lezen SmartEnum met QueryBuilder.

---
+++ test/ProgressOnderwijsUtilsTests/SmartEnumTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using ExpressionToCodeLib;
+using NUnit.Framework;
+using Progress.Business;
+using Progress.Business.Test;
+using ProgressOnderwijsUtils;
+
+namespace ProgressOnderwijsUtilsTests
+{
+    public sealed class SmartEnumTest : TestsWithBusinessConnection
+    {
+        struct Inschrijving : IMetaObject
+        {
+            public SmartPeriodeStudiejaar PeriodeStudiejaar { get; set; }
+        }
+
+        [Test]
+        public void SmartEnum_can_be_read_using_QueryBuilder()
+        {
+            var inschrijving = SafeSql.SQL($@"
+                    select top 1
+                        si.periodestudiejaar
+                    from studentinschrijving si
+                    where 1=1
+                        and si.periodestudiejaar = {SmartPeriodeStudiejaar.C2015}
+                ").ReadMetaObjects<Inschrijving>(conn).Single();
+
+            PAssert.That(() => inschrijving.PeriodeStudiejaar == SmartPeriodeStudiejaar.C2015);
+        }
+    }
+}
