67bf01212c1d56a900864fc7da33eb69bf0575ec
Eamon Nerbonne <eamon@nerbonne.org>
2015-11-21 20:33:45 +0100

    Define interface IBuildableQuery
    This is a step towards a less fancy QueryBuilder.  Rather than
    specifying that each query must be roughly analogous to a singly linked
    list of query parts and then specify that query parts are sql or
    parameters, instead specify the "bare minimum", that a query needs to be
    able to append itself to an sql accumulator that is little more than an
    append-only dictionary of params and a stringbuilder.

---
+++ src/ProgressOnderwijsUtils/Data/QueryBuilder2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressOnderwijsUtils.Data
+{
+    interface IBuildableQuery
+    {
+        void AppendTo(CommandFactory factory);
+    }
+}
