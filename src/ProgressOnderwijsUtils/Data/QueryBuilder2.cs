6782cf724aea87917baa1e85966a6af872734283
Eamon Nerbonne <eamon@nerbonne.org>
2015-11-21 20:47:23 +0100

    Wrap IBuildableQuery in QueryBuilder2
    This lets me have a simple api on the inside (the interface) with a more
    QueryBuilder-esque api on the outside fo simplify conversion.

--- src/ProgressOnderwijsUtils/Data/QueryBuilder2.cs
+++ src/ProgressOnderwijsUtils/Data/QueryBuilder2.cs
@@ -8,8 +8,31 @@ using System.Threading.Tasks;
 
 namespace ProgressOnderwijsUtils.Data
 {
+    public struct QueryBuilder2
+    {
+        readonly IBuildableQuery impl;
+        internal QueryBuilder2(IBuildableQuery impl)
+        {
+            this.impl = impl;
+        }
+
+        internal void AppendTo(CommandFactory factory) => impl.AppendTo(factory);
+
+        public SqlCommand CreateSqlCommand(SqlCommandCreationContext conn)
+        {
+            var factory = new CommandFactory();
+            impl.AppendTo(factory);
+            return factory.CreateCommand(conn.Connection, conn.CommandTimeoutInS);
+        }
+    }
+
     interface IBuildableQuery
     {
         void AppendTo(CommandFactory factory);
+    }
+
+    public static class SqlFactory
+    {
+        internal static QueryBuilder2 ToQuery(this IBuildableQuery q) => new QueryBuilder2(q);
     }
 }
