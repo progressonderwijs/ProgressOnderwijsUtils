db588bef57394fe97b6dcf3ad1d3cfcc7713025f
Christiaan Arnoldus <c.arnoldus@rug.nl>
2015-11-23 14:19:54 +0100

    Maak een IDisposable wrapper rond een action.

---
+++ src/ProgressOnderwijsUtils/Disposer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProgressOnderwijsUtils
+{
+    public sealed class Disposer : IDisposable
+    {
+        readonly Action action;
+
+        public Disposer(Action action)
+        {
+            if (action == null) {
+                throw new ArgumentNullException(nameof(action));
+            }
+            this.action = action;
+        }
+
+        public void Dispose()
+        {
+            action();
+        }
+    }
+}
