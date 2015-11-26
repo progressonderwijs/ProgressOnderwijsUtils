59c015082ae74bd7d3375d9a1b8249b7adef859e
Eamon Nerbonne <eamon@nerbonne.org>
2015-11-26 16:30:07 +0100

    Split file again

--- src/ProgressOnderwijsUtils/Enums/EnumMetaCache.cs
+++ src/ProgressOnderwijsUtils/Enums/EnumMetaCache.cs
@@ -355,21 +355,4 @@ namespace ProgressOnderwijsUtils
         }
 
     }
-
-    class EnumMetaData<TEnum> : IEnumMetaData<TEnum>
-        where TEnum : struct, IConvertible, IComparable
-    {
-        public EnumMetaData(TEnum value)
-        {
-            EnumValue = value;
-        }
-
-        public TEnum EnumValue { get; }
-        public Enum UntypedEnumValue => (Enum)(object)EnumValue;
-        public ITranslatable Label => EnumMetaDataCache<TEnum>.Instance.GetLabel(EnumValue);
-
-        public IEnumerable<TAttr> Attributes<TAttr>()
-            where TAttr : Attribute
-            => EnumMetaDataCache<TEnum>.Instance.AllAttributes(EnumValue).OfType<TAttr>();
-    }
 }
---
+++ src/ProgressOnderwijsUtils/Enums/EnumMetaData.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgressOnderwijsUtils
+{
+    class EnumMetaData<TEnum> : IEnumMetaData<TEnum>
+        where TEnum : struct, IConvertible, IComparable
+    {
+        public EnumMetaData(TEnum value)
+        {
+            EnumValue = value;
+        }
+
+        public TEnum EnumValue { get; }
+        public Enum UntypedEnumValue => (Enum)(object)EnumValue;
+        public ITranslatable Label => EnumMetaDataCache<TEnum>.Instance.GetLabel(EnumValue);
+
+        public IEnumerable<TAttr> Attributes<TAttr>()
+            where TAttr : Attribute
+            => EnumMetaDataCache<TEnum>.Instance.AllAttributes(EnumValue).OfType<TAttr>();
+    }
+}
