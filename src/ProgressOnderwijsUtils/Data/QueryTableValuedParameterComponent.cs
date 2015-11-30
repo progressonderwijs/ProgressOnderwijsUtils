723d4ad4debad6df0a6fc10c98641b625c61fe6e
Eamon Nerbonne <eamon@nerbonne.org>
2015-11-30 12:10:03 +0100

    Simplify and shorten some more

--- src/ProgressOnderwijsUtils/Data/QueryTableValuedParameterComponent.cs
+++ src/ProgressOnderwijsUtils/Data/QueryTableValuedParameterComponent.cs
@@ -9,17 +9,17 @@ namespace ProgressOnderwijsUtils
     sealed class QueryTableValuedParameterComponent<T> : IQueryParameter
         where T : IMetaObject
     {
-        const string subselect_part1 = "(select ";
-        const string subselect_part3 = " from ";
-        const string subselect_part5 = " TVP)";
-
         static readonly string columnListClause =
             MetaObject.GetMetaProperties<T>()
                 .Select(mp => "TVP." + mp.Name)
                 .JoinStrings(", ");
 
+        const string subselect_part1 = "(select ";
+        const string subselect_part3 = " from ";
+        const string subselect_part5 = " TVP)";
         readonly IEnumerable<T> objs;
         readonly string DbTypeName;
+        public object EquatableValue => Tuple.Create(objs, DbTypeName);
 
         internal QueryTableValuedParameterComponent(string dbTypeName, IEnumerable<T> list)
         {
@@ -27,29 +27,23 @@ namespace ProgressOnderwijsUtils
             DbTypeName = dbTypeName;
         }
 
-        public object EquatableValue => Tuple.Create(objs, DbTypeName);
-
         public void AppendTo<TCommandFactory>(ref TCommandFactory factory)
             where TCommandFactory : struct, ICommandFactory
         {
-            var name = factory.RegisterParameterAndGetName(this);
-
             SqlFactory.AppendSql(ref factory, subselect_part1);
             SqlFactory.AppendSql(ref factory, columnListClause);
             SqlFactory.AppendSql(ref factory, subselect_part3);
-            SqlFactory.AppendSql(ref factory, name);
+            SqlFactory.AppendSql(ref factory, factory.RegisterParameterAndGetName(this));
             SqlFactory.AppendSql(ref factory, subselect_part5);
         }
 
         public SqlParameter ToSqlParameter(string paramName)
-        {
-            return new SqlParameter {
+            => new SqlParameter {
                 IsNullable = false,
                 ParameterName = paramName,
                 Value = MetaObject.CreateDataReader(objs),
                 SqlDbType = SqlDbType.Structured,
                 TypeName = DbTypeName,
             };
-        }
     }
 }
