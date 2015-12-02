a7e2a5a6e449bc419dbfb761b6a81661df05346a
Eamon Nerbonne <eamon@nerbonne.org>
2015-12-02 09:29:43 +0100

    Split static QueryBuilder methods into own class
    These methods likely won't change in future optimization, so this
    reduces that diff size.

--- src/ProgressOnderwijsUtils/Data/QueryBuilder.cs
+++ src/ProgressOnderwijsUtils/Data/QueryBuilder.cs
@@ -4,8 +4,6 @@ using System.Data.SqlClient;
 using JetBrains.Annotations;
 using System.Globalization;
 using System.Linq;
-using System.Text.RegularExpressions;
-using static ProgressOnderwijsUtils.SafeSql;
 
 namespace ProgressOnderwijsUtils
 {
@@ -332,92 +330,5 @@ namespace ProgressOnderwijsUtils
 
         [Pure]
         public override string ToString() => DebugText();
-
-        static readonly QueryBuilder[] AllColumns = { SQL($"*") };
-        static readonly QueryBuilder Comma_ColumnSeperator = SQL($", ");
-
-        static QueryBuilder SubQueryHelper(
-            QueryBuilder subquery,
-            IEnumerable<QueryBuilder> projectedColumns,
-            QueryBuilder filterClause,
-            OrderByColumns sortOrder,
-            QueryBuilder topRowsOrNull)
-        {
-            projectedColumns = projectedColumns ?? AllColumns;
-
-            var topClause = topRowsOrNull != null ? SQL($"top ({topRowsOrNull}) ") : Empty;
-            var projectedColumnsClause = CreateProjectedColumnsClause(projectedColumns);
-            return
-                SQL($"select {topClause}{projectedColumnsClause} from (\r\n{subquery}\r\n) as _g1 where {filterClause}\r\n")
-                    + CreateFromSortOrder(sortOrder);
-        }
-
-        static QueryBuilder CreateProjectedColumnsClause(IEnumerable<QueryBuilder> projectedColumns)
-            => projectedColumns.Aggregate((a, b) => a.Append(Comma_ColumnSeperator).Append(b));
-
-        [Pure]
-        static QueryBuilder CreateFromSortOrder(OrderByColumns sortOrder)
-        {
-            return !sortOrder.Columns.Any()
-                ? Empty
-                : CreateDynamic("order by " + sortOrder.Columns.Select(sc => sc.SqlSortString()).JoinStrings(", "));
-        }
-
-        [Pure]
-        public static QueryBuilder CreatePagedSubQuery(
-            QueryBuilder subQuery,
-            IEnumerable<QueryBuilder> projectedColumns,
-            QueryBuilder filterClause,
-            OrderByColumns sortOrder,
-            int skipNrows,
-            int takeNrows)
-        {
-            projectedColumns = projectedColumns ?? AllColumns;
-            if (!projectedColumns.Any()) {
-                throw new InvalidOperationException(
-                    "Cannot create subquery without any projected columns: at least one column must be projected (are your columns all virtual?)\nQuery:\n"
-                        + subQuery.DebugText());
-            }
-
-            var takeRowsParam = Param((long)takeNrows);
-            var skipNrowsParam = Param((long)skipNrows);
-
-            var sortorder = sortOrder;
-            var orderClause = sortorder == OrderByColumns.Empty ? SQL($"order by (select 1)") : CreateFromSortOrder(sortorder);
-            var projectedColumnsClause = CreateProjectedColumnsClause(projectedColumns);
-
-            var topNSubQuery = SubQueryHelper(subQuery, projectedColumns, filterClause, sortOrder, takeRowsParam + SQL($"+") + skipNrowsParam);
-            return SQL($@"
-select top ({takeRowsParam}) {projectedColumnsClause}
-from (select _row=row_number() over ({orderClause}),
-      _g2.*
-from (
-
-{topNSubQuery}
-
-) as _g2) t
-where _row > {skipNrowsParam}
-order by _row");
-        }
-
-        [Pure]
-        public static QueryBuilder CreateSubQuery(QueryBuilder subQuery, IEnumerable<QueryBuilder> projectedColumns, QueryBuilder filterClause, OrderByColumns sortOrder)
-            => SubQueryHelper(subQuery, projectedColumns, filterClause, sortOrder, null);
-
-        //TODO: dit aanzetten voor datasource tests
-        // ReSharper disable once UnusedMember.Global
-        public void AssertNoVariableColumns()
-        {
-            var commandText = CommandText();
-            var commandTextWithoutComments = Regex.Replace(
-                commandText,
-                @"/\*.*?\*/|--.*?$",
-                "",
-                RegexOptions.CultureInvariant | RegexOptions.Compiled | RegexOptions.Multiline);
-            if (Regex.IsMatch(commandTextWithoutComments, @"(?<!count\()\*")) {
-                throw new InvalidOperationException(
-                    GetType().FullName + ": Query may not use * as that might cause runtime exceptions in productie when DB changes:\n" + commandText);
-            }
-        }
     }
 }
