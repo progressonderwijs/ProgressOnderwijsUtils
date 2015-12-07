efb0423f01a6ea75258fbf883c8fc7497948df40
Christiaan Arnoldus <c.arnoldus@rug.nl>
2015-12-07 11:09:05 +0100

    Added more tests for the SmartEnum.

--- test/ProgressOnderwijsUtilsTests/SmartEnumTest.cs
+++ test/ProgressOnderwijsUtilsTests/SmartEnumTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using ExpressionToCodeLib;
 using NUnit.Framework;
 using Progress.Business;
@@ -27,6 +28,44 @@ namespace ProgressOnderwijsUtilsTests
                 ").ReadMetaObjects<Inschrijving>(conn).Single();
 
             PAssert.That(() => inschrijving.PeriodeStudiejaar == SmartPeriodeStudiejaar.C2015);
+        }
+
+        sealed class TestEnum : SmartEnum
+        {
+            TestEnum(int id, ITranslatable text)
+                : base(id, text) { }
+
+            public static readonly TestEnum A = new TestEnum(0, Translatable.Raw("A"));
+            public static readonly TestEnum B = new TestEnum(1, Translatable.Raw("B"));
+        }
+
+        [Test]
+        public void SmartEnum_GetValues_returns_all_values()
+        {
+            PAssert.That(() => SmartEnum.GetValues<TestEnum>().SetEqual(new[] { TestEnum.A, TestEnum.B, }));
+        }
+
+        [Test]
+        public void SmartEnum_GetId_returns_correct_value()
+        {
+            PAssert.That(() => SmartEnum.GetById<TestEnum>(1) == TestEnum.B);
+        }
+
+        sealed class BrokenEnum : SmartEnum
+        {
+            BrokenEnum(int id, ITranslatable text)
+                : base(id, text) { }
+
+            // ReSharper disable UnusedMember.Local
+            public static readonly BrokenEnum A = new BrokenEnum(0, Translatable.Raw("A"));
+            public static readonly BrokenEnum B = new BrokenEnum(0, Translatable.Raw("B"));
+            // ReSharper restore UnusedMember.Local
+        }
+
+        [Test]
+        public void SmartEnums_cannot_have_duplicate_ids()
+        {
+            Assert.Throws<TypeInitializationException>(() => SmartEnum.GetValues<BrokenEnum>());
         }
     }
 }
