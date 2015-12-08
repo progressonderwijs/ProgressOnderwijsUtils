152efcd4acc2ba933c569145a290f582b6a66016
Christiaan Arnoldus <c.arnoldus@rug.nl>
2015-12-08 09:23:08 +0100

    More SmartEnum tests

--- test/ProgressOnderwijsUtilsTests/SmartEnumTest.cs
+++ test/ProgressOnderwijsUtilsTests/SmartEnumTest.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
 using ExpressionToCodeLib;
+using JetBrains.Annotations;
 using NUnit.Framework;
 using Progress.Business;
 using Progress.Business.Inschrijvingen;
 using Progress.Business.Test;
 using ProgressOnderwijsUtils;
 using ProgressOnderwijsUtils.Test;
+using ValueUtils;
 
 namespace ProgressOnderwijsUtilsTests
 {
@@ -91,6 +96,75 @@ namespace ProgressOnderwijsUtilsTests
         public void SmartEnums_cannot_have_duplicate_ids()
         {
             Assert.Throws<TypeInitializationException>(() => SmartEnum.GetValues<BrokenEnum>());
+        }
+    }
+
+    public sealed class AllSmartEnumsTest
+    {
+        static readonly Type[] allSmartEnumTypes = typeof(BusinessAssembly).Assembly
+            .GetTypes()
+            .Where(typeof(ISmartEnum).IsAssignableFrom)
+            .ToArray();
+
+        static bool InvokeGenericCheck(string name, Type type)
+        {
+            var method = typeof(AllSmartEnumsTest)
+                .GetMethods(BindingFlags.Static | BindingFlags.NonPublic)
+                .Single(m => m.Name == name && m.IsGenericMethod)
+                .MakeGenericMethod(type);
+
+            return (bool)method.Invoke(null, null);
+        }
+
+        [Test]
+        public void All_SmartEnums_implement_equals_correctly()
+        {
+            var smartEnumTypesThatImplementEqualsIncorrectly = allSmartEnumTypes
+                .Where(t => !InvokeGenericCheck(nameof(AllSmartEnumMembersAreOnlyEqualToThemselves), t));
+
+            PAssert.That(() => smartEnumTypesThatImplementEqualsIncorrectly.None());
+        }
+
+        [UsedImplicitly]
+        static bool AllSmartEnumMembersAreOnlyEqualToThemselves<T>()
+            where T : ISmartEnum
+        {
+            // ReSharper disable once EqualExpressionComparison
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+            return SmartEnum
+                .GetValues<T>()
+                .All(v1 => Equals(v1, v1) && SmartEnum.GetValues<T>().Count(v2 => Equals(v1, v2)) == 1);
+        }
+
+        [Test]
+        public void All_serializable_SmartEnums_deserialize_correctly()
+        {
+            var smartEnumsTypesThatDeserializeIncorrectly = allSmartEnumTypes
+                .Where(t => t.GetCustomAttribute<SerializableAttribute>() != null)
+                .Where(t => !InvokeGenericCheck(nameof(AllSmartEnumMembersRemainEqualAfterDeserialization), t));
+
+            PAssert.That(() => smartEnumsTypesThatDeserializeIncorrectly.None());
+        }
+
+        [UsedImplicitly]
+        static bool AllSmartEnumMembersRemainEqualAfterDeserialization<T>()
+            where T : ISmartEnum
+        {
+            return SmartEnum
+                .GetValues<T>()
+                .Select(v => new { Original = v, Deserialized = (T)SerializeAndDeserialize(v) })
+                .All(vals => Equals(vals.Original, vals.Deserialized) &&
+                    FieldwiseEquality.AreEqual(vals.Original, vals.Deserialized) /*Test fieldwise in case Equals only checks id*/);
+        }
+
+        static object SerializeAndDeserialize(object obj)
+        {
+            using (var stream = new MemoryStream()) {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, obj);
+                stream.Position = 0;
+                return formatter.Deserialize(stream);
+            }
         }
     }
 }
