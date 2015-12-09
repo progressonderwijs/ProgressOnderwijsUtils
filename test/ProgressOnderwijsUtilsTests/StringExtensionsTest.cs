570e998eefc462ac3efea56823434ebd7814b0d1
Fons Dijkstra <f.j.dijkstra@rug.nl>
2015-12-09 16:56:42 +0100

    Zet de plaatsnamen van de lesplaatsen in de documenten altijd om naar Camel-Casing.

---
+++ test/ProgressOnderwijsUtilsTests/StringExtensionsTest.cs
@@ -0,0 +1,39 @@
+﻿using ExpressionToCodeLib;
+using NUnit.Framework;
+using ProgressOnderwijsUtils;
+
+namespace ProgressOnderwijsUtilsTests
+{
+    public sealed class StringExtensionsTest
+    {
+        [Test]
+        public void ToCamelCase_is_robuust_bij_null()
+        {
+            PAssert.That(() => ((string)null).ToCamelCase() == null);
+        }
+
+        [Test]
+        public void ToCamelCase_werkt_ook_bij_lege_string()
+        {
+            PAssert.That(() => string.Empty.ToCamelCase() == string.Empty);
+        }
+
+        [Test]
+        public void ToCamelCase_ADIS_ABEBA_wordt_Adis_Abeba()
+        {
+            PAssert.That(() => "ADIS ABEBA".ToCamelCase() == "Adis Abeba");
+        }
+
+        [Test]
+        public void ToCamelCase_JAN_BENJAMIN_wordt_Jan_Benjamin()
+        {
+            PAssert.That(() => "JAN-BENJAMIN".ToCamelCase() == "Jan-Benjamin");
+        }
+
+        [Test]
+        public void ToCamelCase_S_GRAVENHAGE_wordt_s_Gravenhage()
+        {
+            PAssert.That(() => "'S-GRAVENHAGE".ToCamelCase() == "'s-Gravenhage");
+        }
+    }
+}
