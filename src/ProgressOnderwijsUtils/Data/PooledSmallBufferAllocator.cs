068df392a5eaeeee112274daff39945271595335
Eamon Nerbonne <eamon@nerbonne.org>
2015-12-10 13:31:58 +0100

    Simplify and optimize

--- src/ProgressOnderwijsUtils/Data/PooledSmallBufferAllocator.cs
+++ src/ProgressOnderwijsUtils/Data/PooledSmallBufferAllocator.cs
@@ -8,33 +8,30 @@ namespace ProgressOnderwijsUtils
     {
         static readonly int IndexCount = 129;
         static readonly int MaxArrayLength = IndexCount - 1;
+        static readonly ConcurrentQueue<T[]>[] bagsByIndex = InitBags();
 
-        static readonly ConcurrentQueue<T[]>[] bagsByIndex = new ConcurrentQueue<T[]>[IndexCount];
-
-        static ConcurrentQueue<T[]> GetBag(int index)
+        static ConcurrentQueue<T[]>[] InitBags()
         {
-            var bag = bagsByIndex[index];
-            if (bag != null)
-                return bag;
-            var otherBag = Interlocked.CompareExchange(ref bagsByIndex[index], bag = new ConcurrentQueue<T[]>(), null);
-            //CompareExchange returns previous value
-            //if that wasn't null, this was a no-op and that value shold be returned
-            //if that *was* null, we set that location, and return our bag
-            return otherBag ?? bag;
+            var allBags = new ConcurrentQueue<T[]>[IndexCount];
+            for (int i = 0; i < IndexCount; i++) {
+                allBags[i] = new ConcurrentQueue<T[]>();
+            }
+            return allBags;
         }
-
 
         /// <summary>
         /// Provides a new or reused array of the given length.
         /// </summary>
         public static T[] GetByLength(int length)
         {
-            if (length > MaxArrayLength)
+            if (length > MaxArrayLength) {
                 return new T[length];
-            var bag = GetBag(length);
+            }
+            var bag = bagsByIndex[length];
             T[] result;
-            if (bag.TryDequeue(out result))
+            if (bag.TryDequeue(out result)) {
                 return result;
+            }
             return new T[length];
         }
 
@@ -44,9 +41,10 @@ namespace ProgressOnderwijsUtils
         /// </summary>
         public static void ReturnToPool(T[] arr)
         {
-            if (arr.Length > MaxArrayLength)
+            if (arr.Length > MaxArrayLength) {
                 return;
-            var bag = GetBag(arr.Length);
+            }
+            var bag = bagsByIndex[arr.Length];
             Array.Clear(arr, 0, arr.Length);
             bag.Enqueue(arr);
         }
