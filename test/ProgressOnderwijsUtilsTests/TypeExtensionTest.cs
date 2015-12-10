0a6e00448b890422ff660b9fd639d5cb3b08a3ed
Christiaan Arnoldus <c.arnoldus@rug.nl>
2015-12-10 08:25:04 +0100

    DocumentType naar business

--- test/ProgressOnderwijsUtilsTests/TypeExtensionTest.cs
+++ test/ProgressOnderwijsUtilsTests/TypeExtensionTest.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using ExpressionToCodeLib;
 using NUnit.Framework;
+using Progress.Business.DomainUnits;
 using ProgressOnderwijsUtils;
 
 namespace ProgressOnderwijsUtilsTests
