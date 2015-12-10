dece5e6e523057b0aa10e9860684469635bcab6c
Eamon Nerbonne <eamon@nerbonne.org>
2015-12-10 14:16:36 +0100

    Split file

--- src/ProgressOnderwijsUtils/Data/CommandFactory.cs
+++ src/ProgressOnderwijsUtils/Data/CommandFactory.cs
@@ -150,46 +150,4 @@ namespace ProgressOnderwijsUtils
         public string SqlTextKey { get; set; }
         public ComparableArray<object> Params { get; set; }
     }
-
-    static class PooledExponentialBufferAllocator<T>
-    {
-        static readonly int IndexCount = 15;
-        static readonly int MaxIndex = IndexCount - 1;
-        static readonly int MaxArrayLength = 1 << MaxIndex;
-        static readonly ConcurrentQueue<T[]>[] bagsByIndex = InitBags();
-
-        static ConcurrentQueue<T[]>[] InitBags()
-        {
-            var allBags = new ConcurrentQueue<T[]>[IndexCount];
-            for (int i = 0; i < IndexCount; i++) {
-                allBags[i] = new ConcurrentQueue<T[]>();
-            }
-            return allBags;
-        }
-
-        public static T[] GetByLength(uint length)
-        {
-            if (length > MaxArrayLength) {
-                return new T[length];
-            }
-            var i = Utils.LogBase2RoundedUp(length);
-            var bag = bagsByIndex[i];
-            T[] result;
-            if (bag.TryDequeue(out result)) {
-                return result;
-            }
-            return new T[1 << i];
-        }
-
-        public static void ReturnToPool(T[] arr)
-        {
-            if (arr.Length > MaxArrayLength) {
-                return;
-            }
-            var i = Utils.LogBase2RoundedUp((uint)arr.Length);
-            var bag = bagsByIndex[i];
-            //Array.Clear(arr,0,arr.Length);
-            bag.Enqueue(arr);
-        }
-    }
 }
