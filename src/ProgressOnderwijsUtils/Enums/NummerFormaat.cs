3223b34bd1a4bce3c29724642906762c7764f0b4
Eamon Nerbonne <eamon@nerbonne.org>
2015-12-16 16:38:20 +0100

    Split up GeneralEnums.cs

---
+++ src/ProgressOnderwijsUtils/Enums/NummerFormaat.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgressOnderwijsUtils
+{
+    public enum NummerFormaat
+    {
+        Afgerond = 0,
+        AfgerondOp1Decimaal = 1,
+        AfgerondOp2Decimaal = 2,
+        GeldEuro = 3,
+        GeldEuroGrootboek = 4
+    }
+}
