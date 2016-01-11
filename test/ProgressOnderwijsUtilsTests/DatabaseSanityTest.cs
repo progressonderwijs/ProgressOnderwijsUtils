cfc0314a8e4c355b01fdc60a1b8db143b0be4528
Fons Dijkstra <f.j.dijkstra@rug.nl>
2016-01-11 11:15:24 +0100

    Gebruik geen string match, maar de expliciete system waarden

--- test/ProgressOnderwijsUtilsTests/DatabaseSanityTest.cs
+++ test/ProgressOnderwijsUtilsTests/DatabaseSanityTest.cs
@@ -23,6 +23,7 @@ namespace ProgressOnderwijsUtilsTests
                     'Geen index op kolom ['  + c.name + '] in tabel [' + s.name + '.' + pt.name + '], gebruikt in [' + k.name + ']'
                 from sys.foreign_keys k
                 join sys.foreign_key_columns kc on kc.constraint_object_id = k.object_id
+                join sys.tables ct on ct.object_id = kc.referenced_object_id
                 join sys.tables pt on pt.object_id = kc.parent_object_id
                 join sys.columns c on c.object_id = kc.parent_object_id and c.column_id = kc.parent_column_id
                 join sys.schemas s on s.schema_id = pt.schema_id
@@ -30,7 +31,7 @@ namespace ProgressOnderwijsUtilsTests
                 where 1=1
                     and ic.object_id is null
                     and k.name not in {exceptions}
-                    and k.name not like 'fk_pnetlog_%%_Log_ActieTypeId' -- exclude al die actietype indexes in de log tabellen; deze zitten meer in de weg dan dat ze iets opleveren
+                    and not (ct.name = 'logactietype' and c.name = 'ActieTypeId') -- exclude al die actietype indexes in de log tabellen; deze zitten meer in de weg dan dat ze iets opleveren
                 ").ReadPlain<string>(conn), Is.Empty, "Geen index op FK-kolom.");
         }
     }
