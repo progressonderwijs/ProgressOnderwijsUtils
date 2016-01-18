e0feca92c810bf4f3ece76e6da4afe898097c4bd
Christiaan Arnoldus <c.arnoldus@rug.nl>
2016-01-18 15:14:50 +0100

    Remove unused usings

--- test/ProgressOnderwijsUtilsTests/EnumerableExtensionsTests.cs
+++ test/ProgressOnderwijsUtilsTests/EnumerableExtensionsTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using ExpressionToCodeLib;
 using NUnit.Framework;
-using Progress.Business.DomainUnits;
 using ProgressOnderwijsUtils;
 using ProgressOnderwijsUtils.Test;
 
