84c73eb5e1421315f3227d76ef48cca2e9a4d406
Eamon Nerbonne <eamon@nerbonne.org>
2016-02-05 11:46:45 +0100

    Clean up HtmlTemplateBusinessEdit
    Mostly resharper standard stuff with one exception:
    
    added "ThrowPreconditionViolation" extension method to exception as
    alternative to throw which quietens unused parameter warnings for used
    parameters
    
    @markijbema @fdij

---
+++ src/ProgressOnderwijsUtils/WorkAroundResharperSuckiness.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProgressOnderwijsUtils
+{
+    public static class WorkAroundResharperSuckiness
+    {
+        //NOT [Pure] so resharper shuts up; the aim of this method is to make resharper 
+        //shut up about "Parameter 'Foobaar' is used only for precondition checks" 
+        [DebuggerHidden]
+        public static void ThrowPreconditionViolation(this Exception e)
+        {
+            throw e;
+        }
+    }
+}
