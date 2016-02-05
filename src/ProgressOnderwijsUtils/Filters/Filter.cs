d168061bb2ae3eae8e6150b8187721809647778a
Christiaan Arnoldus <c.arnoldus@rug.nl>
2016-02-05 10:28:05 +0100

    Added support for In/NotIn

--- src/ProgressOnderwijsUtils/Filters/Filter.cs
+++ src/ProgressOnderwijsUtils/Filters/Filter.cs
@@ -141,6 +141,11 @@ namespace ProgressOnderwijsUtils
             BooleanComparer.NotIn,
         };
 
+        public static readonly IReadOnlyCollection<BooleanComparer> ComparersThatTakeAnArrayArgument = new[] {
+            BooleanComparer.In,
+            BooleanComparer.NotIn,
+        };
+
         public static readonly IReadOnlyCollection<BooleanComparer> ComparersThatTakeNoArguments = new[] {
             BooleanComparer.IsNull,
             BooleanComparer.IsNotNull,
