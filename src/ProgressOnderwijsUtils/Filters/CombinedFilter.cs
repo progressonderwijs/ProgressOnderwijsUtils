cbc66f8d6d73c68762fbc52c905468bbfc70c4b4
Eamon Nerbonne <eamon@nerbonne.org>
2016-02-05 21:46:24 +0100

    Add fewer spaces in various places now that this happens automatically

--- src/ProgressOnderwijsUtils/Filters/CombinedFilter.cs
+++ src/ProgressOnderwijsUtils/Filters/CombinedFilter.cs
@@ -34,8 +34,8 @@ namespace ProgressOnderwijsUtils
                     .Aggregate(andor == BooleanOperator.And ? Expression.AndAlso : (Func<Expression, Expression, Expression>)Expression.OrElse);
         }
 
-        static readonly ParameterizedSql sql_and_keyword = SQL($" and ");
-        static readonly ParameterizedSql sql_or_keyword = SQL($" or ");
+        static readonly ParameterizedSql sql_and_keyword = SQL($"and");
+        static readonly ParameterizedSql sql_or_keyword = SQL($"or");
         ParameterizedSql OperatorToSql() => andor == BooleanOperator.Or ? sql_or_keyword : sql_and_keyword;
 
         protected internal override ParameterizedSql ToParameterizedSqlImpl()
