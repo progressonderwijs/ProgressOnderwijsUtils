694ef86ed9efff9f397d200596679e19d711da4f
Christiaan Arnoldus <c.arnoldus@rug.nl>
2016-02-09 10:37:42 +0100

    Added failing test to show that the string representation of a FlowStep as used in the export is not equal to the integer representation.

---
+++ test/ProgressOnderwijsUtilsTests/FlowStepTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using ExpressionToCodeLib;
+using NUnit.Framework;
+using ProgressOnderwijsUtils;
+using ProgressOnderwijsUtils.Test;
+
+namespace ProgressOnderwijsUtilsTests
+{
+    public sealed class FlowStepTest
+    {
+        [Test, Continuous]
+        public void String_representation_corresponds_to_integer_representation()
+        {
+            var flowStepsWhoseStringIsNotTheInteger = EnumHelpers
+                .GetValues<FlowStep>()
+                .Where(flowStep => Converteer.ToString(flowStep, Taal.NL) != Converteer.ToString((int)flowStep, Taal.NL));
+            PAssert.That(() => flowStepsWhoseStringIsNotTheInteger.None());
+        }
+    }
+}
