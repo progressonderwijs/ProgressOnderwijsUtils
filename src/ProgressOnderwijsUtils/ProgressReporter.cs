725b05cefa48d7b70af2c0b15da844b19d61e21f
Eamon Nerbonne <eamon@nerbonne.org>
2016-02-12 15:29:55 +0100

    Add time-based progress reporting

--- src/ProgressOnderwijsUtils/ProgressReporter.cs
+++ src/ProgressOnderwijsUtils/ProgressReporter.cs
@@ -26,42 +26,49 @@ namespace ProgressOnderwijsUtils
         readonly Stopwatch sw;
         readonly DateTime start;
         readonly int TotalSteps;
+        readonly long ReportEveryFractionOfTotal;
         readonly Action<ProgressReport> report;
-        int stepsDone;
+        readonly long reportEveryTicks;
 
-        public ProgressReporter(int TotalSteps, Action<ProgressReport> report)
+        int stepsDone;
+        long nextReportDueAt;
+
+        public ProgressReporter(int TotalSteps, Action<ProgressReport> report, int? reportEveryFractionOfTotal = null, TimeSpan? reportEveryTimeSpan = null)
         {
             this.TotalSteps = TotalSteps;
             this.report = report;
+            ReportEveryFractionOfTotal = reportEveryFractionOfTotal ?? 20; //default 1/20 i.e. every five percent
             start = DateTime.Now;
             sw = Stopwatch.StartNew();
+            nextReportDueAt = reportEveryTicks =  (reportEveryTimeSpan?? TimeSpan.FromMinutes(1.0)).Ticks;
         }
 
-        public ProgressReporter(int TotalSteps, string actionName)
+        public ProgressReporter(int TotalSteps, string reportNameForConsole, int? reportEveryFractionOfTotal = null, TimeSpan? reportEveryTimeSpan = null)
             : this(TotalSteps,
                 report =>
                     Console.WriteLine(
-                        actionName + ": " + report.PercentDone + "%, " + report.TimeLeft.TotalSeconds.ToString("f1") + "s left (" + report.Start + " - " + report.Eta + ")")) { }
+                        reportNameForConsole + ": " + report.PercentDone + "%, " + report.TimeLeft.TotalSeconds.ToString("f1") + "s left (" + report.Start + " - " + report.Eta + ")"), reportEveryFractionOfTotal, reportEveryTimeSpan) { }
 
-        public void Step(int steps=1)
+        public void Step(int steps = 1)
         {
-            int newProgressVal = Interlocked.Add(ref stepsDone,steps); //stepsDone++; is thread unsafe.
+            var newProgressVal = Interlocked.Add(ref stepsDone, steps); //stepsDone++; is thread unsafe.
             var oldProgressVal = newProgressVal - steps;
-            var percentProgress = PercentProgress(newProgressVal);
-            if (percentProgress / 5 > PercentProgress(oldProgressVal) / 5) {
-                var elapsed = sw.Elapsed;
+            var elapsed = sw.Elapsed;
+            if (ReportEveryFractionOfTotal * newProgressVal / TotalSteps > ReportEveryFractionOfTotal * oldProgressVal / TotalSteps
+                || elapsed.Ticks > nextReportDueAt) {
+                var percentProgress = PercentProgress(newProgressVal);
                 var elapsedMS = elapsed.TotalMilliseconds;
                 var totalMS = elapsedMS * TotalSteps / newProgressVal;
                 var scaled = TimeSpan.FromMilliseconds(totalMS);
                 var leftOver = scaled - elapsed;
                 var now = start + elapsed;
                 var eta = start + scaled;
+                nextReportDueAt = elapsed.Ticks + reportEveryTicks;
                 report(new ProgressReport(start, now, eta, newProgressVal, percentProgress, leftOver));
             }
         }
 
         public int StepsDone => Volatile.Read(ref stepsDone);
-
         int PercentProgress(int newProgressVal) => 100 * newProgressVal / TotalSteps;
     }
 }
