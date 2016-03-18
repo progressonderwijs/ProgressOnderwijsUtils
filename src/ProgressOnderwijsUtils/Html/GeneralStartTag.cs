df7370484a85cc18f014c4f251dba1b60b262fc4
Eamon Nerbonne <eamon@nerbonne.org>
2016-03-18 10:36:30 +0100

    Remove IHtmlStartTag and GeneralStartTag
    those may have some value at some point, but not now

---
+++ src/ProgressOnderwijsUtils/Html/GeneralStartTag.cs
@@ -0,0 +1,36 @@
+#if false
+
+//probably want this after the html conversion.
+using JetBrains.Annotations;
+
+namespace ProgressOnderwijsUtils.Html
+{
+    public struct GeneralStartTag
+        : IFluentHtmlTagExpression<GeneralStartTag>
+    {
+        public string TagName { get; }
+        public HtmlAttribute[] Attributes { get; }
+
+        public GeneralStartTag(string tagName)
+            : this(tagName, HtmlAttributeHelpers.EmptyAttributes) { }
+
+        GeneralStartTag(string tagName, HtmlAttribute[] attributes)
+        {
+            TagName = tagName;
+            Attributes = attributes ?? HtmlAttributeHelpers.EmptyAttributes;
+        }
+
+        [Pure]
+        public GeneralStartTag Attribute(string attrName, string attrValue)
+            => new GeneralStartTag(TagName, Attributes.appendAttr(attrName, attrValue));
+
+        [Pure]
+        public HtmlElement Content() => Content(null);
+
+        [Pure]
+        public HtmlElement Content(params HtmlFragment[] content) => new HtmlElement(TagName, Attributes, content);
+
+        public static implicit operator HtmlFragment(GeneralStartTag startTag) => HtmlFragment.HtmlElement(startTag.TagName, startTag.Attributes, null);
+    }
+}
+#endif
