01a6b7aba8c1dd6a33be6cf66a4c7117f4fbf127
Eamon Nerbonne <eamon@nerbonne.org>
2016-06-20 15:25:22 +0200

    Implement stage 1 xml compression
    Allow removing junk namespaces and normalizing xsi and xsd namespaces

---
+++ src/ProgressOnderwijsUtils/XmlCompression.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ProgressOnderwijsUtils
+{
+    public static class XmlCompression
+    {
+        /// <summary>
+        /// Removes unused namespaces and makes sure the XMLSchema and XMLSchema-instance namespaces are mapped to the conventional prefix.
+        /// </summary>
+        public static void CleanupNamespaces(XDocument doc)
+        {
+            var usedNamespaces = doc.Descendants().Select(el => el.Name.Namespace).ToSet();
+            usedNamespaces.UnionWith(doc.Descendants().Attributes().Select(attr => attr.Name.Namespace));
+
+            var unusedRootNamespaceDeclarations = doc.Descendants().Attributes()
+                .Where(attr => attr.IsNamespaceDeclaration && !usedNamespaces.Contains(attr.Value))
+                .ToArray();
+
+            foreach (var unusedNamespaceDeclaration in unusedRootNamespaceDeclarations)
+                unusedNamespaceDeclaration.Remove();
+
+            //*
+            if (usedNamespaces.Contains(XNamespace.Get("http://www.w3.org/2001/XMLSchema"))) {
+                foreach (var attr in doc.Root.Attributes().Where(attr => attr.IsNamespaceDeclaration && attr.Value == "http://www.w3.org/2001/XMLSchema"))
+                    attr.Remove();
+                doc.Root.SetAttributeValue(XNamespace.Xmlns + "xsd", "http://www.w3.org/2001/XMLSchema");
+            }
+            if (usedNamespaces.Contains(XNamespace.Get("http://www.w3.org/2001/XMLSchema-instance"))) {
+                foreach (var attr in doc.Root.Attributes().Where(attr => attr.IsNamespaceDeclaration && attr.Value == "http://www.w3.org/2001/XMLSchema-instance"))
+                    attr.Remove();
+                doc.Root.SetAttributeValue(XNamespace.Xmlns + "xsi", "http://www.w3.org/2001/XMLSchema-instance");
+            }
+        }
+    }
+}
---
+++ test/ProgressOnderwijsUtilsTests/XmlCompressionTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using ExpressionToCodeLib;
+using NUnit.Framework;
+using Progress.Business.Test;
+using ProgressOnderwijsUtils;
+
+namespace ProgressOnderwijsUtilsTests
+{
+    [Continuous]
+    class XmlCompressionTest
+    {
+        [Test]
+        public void TrivialDocIsNotChanged()
+        {
+            var doc = XDocument.Parse("<test/>");
+            XmlCompression.CleanupNamespaces(doc);
+            var output = doc.ToString(SaveOptions.DisableFormatting);
+            PAssert.That(() => output == "<test />");
+        }
+
+        [Test]
+        public void DocumentWithUnusedRootNamespaceHasNamespaceRemoved()
+        {
+            var doc = XDocument.Parse("<test xmlns:bla='bla'/>");
+            XmlCompression.CleanupNamespaces(doc);
+            var output = doc.ToString(SaveOptions.DisableFormatting);
+            PAssert.That(() => output == "<test />");
+        }
+
+        [Test]
+        public void DocumentWithDefaultNsIsUnchanged()
+        {
+            var doc = XDocument.Parse("<test xmlns='bla'/>");
+            XmlCompression.CleanupNamespaces(doc);
+            var output = doc.ToString(SaveOptions.DisableFormatting);
+            PAssert.That(() => output == "<test xmlns=\"bla\" />");
+        }
+
+        [Test]
+        public void DocumentWithChildNamespaceIsUnchanged()
+        {
+            var doc = XDocument.Parse("<test xmlns='bla'><hmm:this xmlns:hmm='foo'/></test>");
+            XmlCompression.CleanupNamespaces(doc);
+            var output = doc.ToString(SaveOptions.DisableFormatting);
+            PAssert.That(() => output == "<test xmlns=\"bla\"><hmm:this xmlns:hmm=\"foo\" /></test>");
+        }
+
+        [Test]
+        public void DocumentWithNamespaceUsedInDescendantIsUnchanged()
+        {
+            var doc = XDocument.Parse("<test xmlns:hmm='bla'><hmm:this /></test>");
+            XmlCompression.CleanupNamespaces(doc);
+            var output = doc.ToString(SaveOptions.DisableFormatting);
+            PAssert.That(() => output == "<test xmlns:hmm=\"bla\"><hmm:this /></test>");
+        }
+
+        [Test]
+        public void DocumentWithNamespaceUsedInDescendantAttributeIsUnchanged()
+        {
+            var doc = XDocument.Parse("<test xmlns:hmm='bla'><this><that hmm:id='' /></this></test>");
+            XmlCompression.CleanupNamespaces(doc);
+            var output = doc.ToString(SaveOptions.DisableFormatting);
+            PAssert.That(() => output == "<test xmlns:hmm=\"bla\"><this><that hmm:id=\"\" /></this></test>");
+        }
+
+        [Test]
+        public void UnusualAliasesAreNormalized()
+        {
+            var doc = XDocument.Parse("<test xmlns:notxsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:notxsi=\"http://www.w3.org/2001/XMLSchema-instance\"><this notxsd:foo='' notxsi:nil='true' /></test>");
+            XmlCompression.CleanupNamespaces(doc);
+            var output = doc.ToString(SaveOptions.DisableFormatting);
+            PAssert.That(() => output == "<test xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><this xsd:foo=\"\" xsi:nil=\"true\" /></test>");
+        }
+
+        [Test]
+        public void UnusedAreRemoved()
+        {
+            var doc = XDocument.Parse("<test xmlns:notxsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:notxsi=\"http://www.w3.org/2001/XMLSchema-instance\"><this notxsi:nil='true' /></test>");
+            XmlCompression.CleanupNamespaces(doc);
+            var output = doc.ToString(SaveOptions.DisableFormatting);
+            PAssert.That(() => output == "<test xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><this xsi:nil=\"true\" /></test>");
+        }
+    }
+}
