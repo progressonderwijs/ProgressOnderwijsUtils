9037a3c8d89bf66330c73e76fbfc38c4d35a2cc8
Eamon Nerbonne <eamon@nerbonne.org>
2016-07-05 15:44:19 +0200

    Allow marking an enum as non-literal

---
+++ src/ProgressOnderwijsUtils/Data/IEnumShouldBeParameterizedInSqlAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProgressOnderwijsUtils
+{
+    /// <summary>
+    /// If an enum is annotated with an attribute [MyCustomAttr("example")], 
+    /// and the attributes implements this marker interface (IEnumShouldBeParameterizedInSqlAttribute),
+    /// then when sql is generated enum values are not represented as literals but parameterized (the way an int or long might be)
+    /// </summary>
+    public interface IEnumShouldBeParameterizedInSqlAttribute { }
+}
