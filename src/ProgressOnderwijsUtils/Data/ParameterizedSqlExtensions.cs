2f575d6ab9251b0a6501dbc95f62bd3e66a36794
Eamon Nerbonne <eamon@nerbonne.org>
2016-09-28 14:04:23 +0200

    Implement an sql-concatenation helper
    ...so I don't need to call Aggregate all the time.
    
    aim: for the client code to be more readable due to this extension.
    It's modeled off JoinStrings

--- src/ProgressOnderwijsUtils/Data/ParameterizedSqlExtensions.cs
+++ src/ProgressOnderwijsUtils/Data/ParameterizedSqlExtensions.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExpressionToCodeLib;
 using JetBrains.Annotations;
+using NUnit.Framework;
+using static ProgressOnderwijsUtils.SafeSql;
 
 namespace ProgressOnderwijsUtils
 {
@@ -21,6 +26,50 @@ namespace ProgressOnderwijsUtils
         public static ParameterizedSql AppendIf(this ParameterizedSql source, bool condition, Func<ParameterizedSql> extra)
         {
             return condition ? source.Append(extra()) : source;
+        }
+
+        /// <summary>
+        /// Concatenate a sequence of sql expressions with a space separator.
+        /// e.g.  concatenating 'a' and 'b' results in 'a b'
+        /// </summary>
+        public static ParameterizedSql ConcatenateSql(this IEnumerable<ParameterizedSql> sqlExpressions) => ConcatenateSql(sqlExpressions, ParameterizedSql.Empty);
+
+        /// <summary>
+        /// Concatenate a sequence of sql expressions with a separator (surrounded by space).  A sequence of N items includes the separator N-1 times.
+        /// e.g.  concatenating 'a' and 'b' with separator 'X' results in 'a X b'
+        /// </summary>
+        public static ParameterizedSql ConcatenateSql(this IEnumerable<ParameterizedSql> sqlExpressions, ParameterizedSql separator) => sqlExpressions.Aggregate((a, b) => a.Append(separator).Append(b));
+    }
+
+    public sealed class ConcatenateSqlTest
+    {
+        [Test]
+        public void ConcatenateWithEmptySeparatorIsStillSpaced()
+        {
+            PAssert.That(() => new[] { SQL($"een"), SQL($"twee"), SQL($"drie") }.ConcatenateSql() == SQL($"een twee drie"));
+        }
+
+        [Test]
+        public void EmptyConcatenateFails()
+        {
+            Assert.Catch<Exception>(() => new ParameterizedSql[] { }.ConcatenateSql(SQL($"bla")));
+        }
+
+        [Test]
+        public void ConcatenateIsNonQuadratic()
+        {
+            var someSqls = Enumerable.Range(0, 20000).Select(i => ParameterizedSql.CreateDynamic(i.ToStringInvariant())).ToArray();
+            var time = BenchTimer.BestTime(() => {
+                // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                someSqls.ConcatenateSql().CommandText();
+            }, 5);
+            PAssert.That(() => time.TotalMilliseconds < 5.0);
+        }
+
+        [Test]
+        public void ConcatenateWithSeparatorUsesSeparatorSpaced()
+        {
+            PAssert.That(() => new[] { SQL($"een"), SQL($"twee"), SQL($"drie") }.ConcatenateSql(SQL($"!")) == SQL($"een ! twee ! drie"));
         }
     }
 }
