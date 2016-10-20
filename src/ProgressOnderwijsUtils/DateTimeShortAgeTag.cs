051ca0b16f9e6bd07c0575ca00331f2384439869
Eamon Nerbonne <eamon@nerbonne.org>
2016-10-20 11:20:16 +0200

    (minor) add spacing to comments
    Purpose: to make it easier for me to regex search for webframework-syle
    starred-paths.

--- src/ProgressOnderwijsUtils/DateTimeShortAgeTag.cs
+++ src/ProgressOnderwijsUtils/DateTimeShortAgeTag.cs
@@ -12,7 +12,7 @@ namespace ProgressOnderwijsUtils
         /// <summary>
         /// The purpose of an AgeTag is to provide an unlikely-to-collide unique token based on modification time.
         /// A tick is 10^-7 seconds; so Ticks>>23 ~ 0.84 seconds
-        /// now, we take 0xffffff of those pseudo seconds, or 2^24*2^23*10^-7 seconds, i.e. 162.9 days, or about 0.5 years.
+        /// now, we take 0xffffff of those pseudo seconds, or 2^24 * 2^23 * 10^-7 seconds, i.e. 162.9 days, or about 0.5 years.
         /// after this time, the AgeTag will loop.  So our Tag is granular to the second, and unique within a half year.
         /// </summary>
         public static string ToAgeTag(DateTime datetime)
@@ -25,7 +25,7 @@ namespace ProgressOnderwijsUtils
         /// <summary>
         /// The purpose of an AgeTag is to provide an unlikely-to-collide unique token based on modification time.
         /// A tick is 10^-7 seconds; so Ticks>>23 ~ 0.84 seconds
-        /// now, we take 0xffffff of those pseudo seconds, or 2^24*2^23*10^-7 seconds, i.e. 162.9 days, or about 0.5 years.
+        /// now, we take 0xffffff of those pseudo seconds, or 2^24 * 2^23 * 10^-7 seconds, i.e. 162.9 days, or about 0.5 years.
         /// after this time, the AgeTag will loop.  So our Tag is granular to the second, and unique within a half year.
         /// </summary>
         public static string ToAgeTagCaseInsensitive(DateTime datetime)
