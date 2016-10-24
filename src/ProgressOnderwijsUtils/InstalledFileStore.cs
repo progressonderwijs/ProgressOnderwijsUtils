d2255f16972d5a0f4537076ff33731272c518f50
Fons Dijkstra <f.j.dijkstra@rug.nl>
2016-10-24 11:36:58 +0200

    Maak installed-file tooling om een bestand te kunnen vinden. Dit wil ik straks gaan gebruiken voor het laden van een door de packager geinstalleerde certificaat. Zoiets gebeurde ook al bij het laden van de templates assembly dus die dacht ik maar te gaan hergebruiken.

---
+++ src/ProgressOnderwijsUtils/InstalledFileStore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ProgressOnderwijsUtils
+{
+    public static class InstalledFileStore
+    {
+#if DEBUG
+        const string PrivateBinPath = @"bin;bin\Debug";
+#else
+        const string PrivateBinPath = @"bin;bin\Release";
+#endif
+
+        public static string FileLocation(string name)
+        {
+            var searchPaths = new[] { string.Empty }
+                .Concat((AppDomain.CurrentDomain.RelativeSearchPath ?? PrivateBinPath).Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
+                .ToArray();
+            foreach (var path in searchPaths) {
+                var result = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path, name);
+                if (File.Exists(result)) {
+                    return result;
+                }
+            }
+
+            throw new FileNotFoundException("file location not found", name);
+        }
+    }
+}
