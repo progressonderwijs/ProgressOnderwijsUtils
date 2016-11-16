3539b65cca5b4c72eae53575eb1bad01a58e2be0
Christiaan Arnoldus <c.arnoldus@rug.nl>
2016-11-16 16:18:30 +0100

    Test All_UserLabelDefinition_TableNames_exists

--- test/ProgressOnderwijsUtilsTests/UserLabelsTest.cs
+++ test/ProgressOnderwijsUtilsTests/UserLabelsTest.cs
@@ -7,6 +7,7 @@ using Progress.Business.Test;
 using Progress.Business.Text;
 using Progress.Business.Tools;
 using ProgressOnderwijsUtils;
+using static ProgressOnderwijsUtils.SafeSql;
 
 namespace ProgressOnderwijsUtilsTests
 {
@@ -58,11 +59,26 @@ namespace ProgressOnderwijsUtilsTests
         [Continuous]
         public void Members_UserLabels_always_have_at_most_one_bit_set()
         {
-            var membersUserLabelsWithMoreThanOneBitSet = EnumHelpers.GetValues<UserLabels>()
+            var membersWithMoreThanOneBitSet = EnumHelpers.GetValues<UserLabels>()
                 .Where(ul => ((int)ul & (int)ul - 1) == 0)
                 .ToArray();
 
-            PAssert.That(() => membersUserLabelsWithMoreThanOneBitSet.None());
+            PAssert.That(() => membersWithMoreThanOneBitSet.None());
+        }
+
+        [Test]
+        [DataControle]
+        public void All_UserLabelDefinition_TableNames_exists()
+        {
+            var nonExistent = SQL($@"
+                    select distinct
+                        uld.TableName
+                    from UserLabelDefinition uld
+                    where 1=1
+                        and object_id(uld.TableName) is null
+                ").ReadPlain<string>(conn);
+
+            PAssert.That(() => nonExistent.None());
         }
     }
 }
