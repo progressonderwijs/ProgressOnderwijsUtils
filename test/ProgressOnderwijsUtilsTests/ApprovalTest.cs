d25fba6e48f1a5c76b26b876ef34f045e0db2c03
Eamon Nerbonne <eamon@nerbonne.org>
2017-01-31 14:51:10 +0100

    Simplify

--- test/ProgressOnderwijsUtilsTests/ApprovalTest.cs
+++ test/ProgressOnderwijsUtilsTests/ApprovalTest.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using ApprovalTests;
 using ApprovalTests.Approvers;
 using ApprovalTests.Core;
