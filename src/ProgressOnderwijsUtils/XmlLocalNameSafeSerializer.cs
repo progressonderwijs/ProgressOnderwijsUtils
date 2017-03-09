516f4801f65b5feae808c27ee7de2f49affd19e6
Christiaan Arnoldus <c.arnoldus@rug.nl>
2017-03-09 13:36:03 +0100

    Serialize objects as xml name

---
+++ src/ProgressOnderwijsUtils/XmlLocalNameSafeSerializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace ProgressOnderwijsUtils
+{
+    public static class XmlLocalNameSafeSerializer
+    {
+        // using base64 generally results in shorter strings, because less character have to be escaped and XmlConvert escaping is inefficient
+        public static string Encode(object obj)
+            => XmlConvert.EncodeLocalName(Convert.ToBase64String(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj))));
+
+        public static T Decode<T>(string str)
+            => JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(Convert.FromBase64String(XmlConvert.DecodeName(str))));
+    }
+}
---
+++ test/ProgressOnderwijsUtilsTests/XmlLocalNameSafeSerializerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Xml;
+using ExpressionToCodeLib;
+using ProgressOnderwijsUtils;
+using Xunit;
+
+namespace ProgressOnderwijsUtilsTests
+{
+    public sealed class XmlLocalNameSafeSerializerTest
+    {
+        struct Data
+        {
+            public string Url;
+            public DateTime Time;
+            public string Token;
+        }
+
+        readonly Data data = new Data {
+            Url = "https://en.wikipedia.org/wiki/Main_Page",
+            Time = new DateTime(2017, 3, 9, 12, 33, 32, DateTimeKind.Utc),
+            Token = "BQac8Qd2aQaJWrOnL-8LEh~VDRpnM1yWDOLyAmwUTeeLOWxGb3",
+        };
+
+        [Fact]
+        public void Encode_is_reversible_using_Decode()
+        {
+            var encoded = XmlLocalNameSafeSerializer.Encode(data);
+            var decoded = XmlLocalNameSafeSerializer.Decode<Data>(encoded);
+            PAssert.That(() => Equals(decoded, data));
+        }
+
+        [Fact]
+        public void Encode_produces_a_valid_NCName()
+        {
+            var encoded = XmlLocalNameSafeSerializer.Encode(data);
+            XmlConvert.VerifyNCName(encoded);
+        }
+    }
+}
