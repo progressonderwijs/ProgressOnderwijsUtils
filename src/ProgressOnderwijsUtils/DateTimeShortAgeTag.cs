cb3f35fdcefc31c35ed4103baabd6d78e981a2dc
Eamon Nerbonne <eamon@nerbonne.org>
2017-04-26 15:00:00 +0200

    Fix minor tests

--- src/ProgressOnderwijsUtils/DateTimeShortAgeTag.cs
+++ src/ProgressOnderwijsUtils/DateTimeShortAgeTag.cs
@@ -6,9 +6,6 @@ namespace ProgressOnderwijsUtils
 {
     public static class DateTimeShortAgeTag
     {
-        static readonly char[] trimStart = { 'A' };
-        static readonly char[] trimEnd = { '=' };
-
         /// <summary>
         /// The purpose of an AgeTag is to provide an unlikely-to-collide unique token based on modification time.
         /// A tick is 10^-7 seconds; so Ticks>>23 ~ 0.84 seconds
@@ -17,22 +14,8 @@ namespace ProgressOnderwijsUtils
         /// </summary>
         public static string ToAgeTag(DateTime datetime)
         {
-            var granularTicks = (uint)(datetime.Ticks >> 23) & 0xffffff;
-            var bytes = BitConverter.GetBytes(granularTicks).Reverse().ToArray(); //reverse so most significant first.
-            return Convert.ToBase64String(bytes).TrimStart(trimStart).TrimEnd(trimEnd); //we trim off unneeded preceding "zero" ('A') and trailing padding chars ('=')
-        }
-
-        /// <summary>
-        /// The purpose of an AgeTag is to provide an unlikely-to-collide unique token based on modification time.
-        /// A tick is 10^-7 seconds; so Ticks>>23 ~ 0.84 seconds
-        /// now, we take 0xffffff of those pseudo seconds, or 2^24 * 2^23 * 10^-7 seconds, i.e. 162.9 days, or about 0.5 years.
-        /// after this time, the AgeTag will loop.  So our Tag is granular to the second, and unique within a half year.
-        /// </summary>
-        public static string ToAgeTagCaseInsensitive(DateTime datetime)
-        {
             var granularTicks = (uint)(datetime.Ticks >> 23) & 0xffffff; //3byte code
-
-            return granularTicks.ToString("x2", CultureInfo.InvariantCulture);
+            return Utils.ToSortableShortString(granularTicks);
         }
     }
 }
