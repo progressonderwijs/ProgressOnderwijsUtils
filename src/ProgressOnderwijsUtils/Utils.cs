af20764c35139d4467fb93ccbc0a7aed1e5f345b
Eamon Nerbonne <eamon@nerbonne.org>
2017-05-31 13:49:07 +0200

    introduce constants

--- src/ProgressOnderwijsUtils/Utils.cs
+++ src/ProgressOnderwijsUtils/Utils.cs
@@ -139,11 +139,14 @@ namespace ProgressOnderwijsUtils
             } else if (e is SqlException sqlE) {
                 //sqlE.Number docs at https://msdn.microsoft.com/en-us/library/cc645611.aspx
                 //see also system error codes: https://msdn.microsoft.com/en-us/library/windows/desktop/ms681382
-                return sqlE.Number == -2 //timeout expired 
-                    || sqlE.Number == 53 //failed to establish connection
-                    || sqlE.Number == 1205 //deadlock victim
-                    || e.Message.StartsWith("A transport-level error has occurred when receiving results from the server.")//number 121, but unclear if that's sufficient, so check string too.
-                    || e.Message.StartsWith("A transport-level error has occurred when sending the request to the server.");//number 121, but unclear if that's sufficient, so check string too.
+                const int timeoutExpired = -2;
+                const int failedToEstablishConnection = 53;
+                const int deadlockVictim = 1205;
+                return sqlE.Number == timeoutExpired
+                    || sqlE.Number == failedToEstablishConnection
+                    || sqlE.Number == deadlockVictim
+                    || e.Message.StartsWith("A transport-level error has occurred when receiving results from the server.") //number 121, but unclear if that's sufficient, so check string too.
+                    || e.Message.StartsWith("A transport-level error has occurred when sending the request to the server."); //number 121, but unclear if that's sufficient, so check string too.
             } else if (e is DBConcurrencyException) {
                 return e.Message.StartsWith("Concurrency violation:");
             } else if (e is DataException) {
