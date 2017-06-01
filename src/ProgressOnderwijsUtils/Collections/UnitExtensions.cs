12e746b7ee03d7779b36fa59eab7c7a9e614b3ea
Eamon Nerbonne <eamon@nerbonne.org>
2017-06-01 12:59:37 +0200

    Add Maybe+Unit

---
+++ src/ProgressOnderwijsUtils/Collections/UnitExtensions.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ProgressOnderwijsUtils.Collections
+{
+    public static class UnitExtensions
+    {
+        public static Func<Unit> ToUnitReturningFunc(this Action action)
+            => () => {
+                action();
+                return Unit.Value;
+            };
+
+        public static Func<T, Unit> ToUnitReturningFunc<T>(this Action<T> action)
+            => x => {
+                action(x);
+                return Unit.Value;
+            };
+    }
+}
