505e33068bb085060628e9fdeec9c821f489357b
Eamon Nerbonne <eamon@nerbonne.org>
2017-06-01 13:00:11 +0200

    Add maybe tests.

---
+++ test/ProgressOnderwijsUtilsTests/UnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using ExpressionToCodeLib;
+using ProgressOnderwijsUtils.Collections;
+using Xunit;
+
+namespace ProgressOnderwijsUtilsTests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void TwoInstancesAreEqual()
+        {
+            PAssert.That(() => Unit.Value.Equals(Unit.Value));
+            PAssert.That(() => Unit.Value.Equals(default(Unit)));
+            PAssert.That(() => Unit.Value.Equals(new Unit()));
+
+            Action action = () => { };
+            PAssert.That(() => Unit.Value.Equals(Unit.SideEffect(action)));
+        }
+
+        [Fact]
+        public void ReturnIsTheIdentityFunction()
+        {
+            PAssert.That(() => Unit.Value.Return(3) == 3);
+        }
+    }
+}
