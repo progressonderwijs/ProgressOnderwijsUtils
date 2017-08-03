c9995bda869ea34f43e2c0c644e31ad349f4c190
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2017-08-03 10:26:03 +0200

    Enable tests in comments

--- test/ProgressOnderwijsUtilsTests/ImageToolsTests.cs
+++ test/ProgressOnderwijsUtilsTests/ImageToolsTests.cs
@@ -1,13 +1,10 @@
-﻿#if false
-using System;
+﻿using System;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using ExpressionToCodeLib;
-using Xunit;
-using Progress.Business.Test;
-using Progress.Test.Resources;
 using ProgressOnderwijsUtils;
+using Xunit;
 
 namespace ProgressOnderwijsUtilsTests
 {
@@ -19,46 +16,12 @@ namespace ProgressOnderwijsUtilsTests
     
     public class ImageToolsTests
     {
-        [Fact]
-        public void CanResaveImage()
+        static Bitmap GetRainbow()
         {
-            var resImage = TestResources.rainbow;
-            using (var ms = new MemoryStream()) {
-                ImageTools.SaveImageAsJpeg(resImage, ms, 100);
-                using (var loadedImage = (Bitmap)ImageTools.ToImage(ms.ToArray())) {
-                    PAssert.That(() => loadedImage.Width == resImage.Width && loadedImage.Height == resImage.Height);
-                    PAssert.That(
-                        () =>
-                            !(from y in Enumerable.Range(0, loadedImage.Height)
-                                from x in Enumerable.Range(0, loadedImage.Width)
-                                select new { x, y }).Where(p => loadedImage.GetPixel(p.x, p.y).Distance(resImage.GetPixel(p.x, p.y)) > 0.05).Any());
-                }
-            }
-        }
-
-        [Fact]
-        public void CanResizeImage()
-        {
-            var resImage = TestResources.rainbow;
-            using (var down_W = ImageTools.Resize(resImage, 100, resImage.Height))
-            using (var down_H = ImageTools.Resize(resImage, resImage.Width, 100))
-            using (var down_W_H = ImageTools.Resize(down_W, 50, 50))
-            using (var down_H_W = ImageTools.Resize(down_H, 50, 50))
-            using (var down_WH = ImageTools.Resize(resImage, 50, 50)) {
-                AssertImagesSimilar(down_W_H, down_WH);
-                AssertImagesSimilar(down_H_W, down_WH);
-                AssertImagesSimilar(down_H_W, down_W_H);
-
-                //als het test-plaatje geen contrast heeft, dan is deze hele test zinloos: verifieer dat het test plaatje contrast heeft:
-                PAssert.That(() => down_W_H.GetPixel(45, 25) != down_W_H.GetPixel(10, 30));
-            }
-        }
-
-        [Fact]
-        public void CannotResizeToZero()
-        {
-            var resImage = TestResources.rainbow;
-            Assert.Throws<ArgumentException>(() => ImageTools.Resize(resImage, 100, 0));
+            return (Bitmap)Image.FromStream(
+                typeof(ImageToolsTests).Assembly.GetManifestResourceStream(typeof(ImageToolsTests), "rainbow.jpg")
+                    ?? throw new Exception("rainbow.jpg is not embedded")
+                );
         }
 
         static void AssertImagesSimilar(Bitmap img1, Bitmap img2)
@@ -81,7 +44,7 @@ namespace ProgressOnderwijsUtilsTests
         [Fact]
         public void CanDownscaleImage()
         {
-            var resImage = TestResources.rainbow;
+            using (var resImage = GetRainbow())
             using (var down_W = ImageTools.DownscaleAndClip(resImage, 100, 50))
             using (var down_H = ImageTools.DownscaleAndClip(resImage, 2000, 1000))
             using (var down_W_H = ImageTools.DownscaleAndClip(down_W, 50, 50))
@@ -92,6 +55,48 @@ namespace ProgressOnderwijsUtilsTests
                 PAssert.That(() => down_W_H.GetPixel(25, 25) != down_W_H.GetPixel(20, 20));
             }
         }
+
+        [Fact]
+        public void CannotResizeToZero()
+        {
+            using (var resImage = GetRainbow()) {
+                Assert.Throws<ArgumentException>(() => ImageTools.Resize(resImage, 100, 0));
+            }
+        }
+
+        [Fact]
+        public void CanResaveImage()
+        {
+            using (var resImage = GetRainbow())
+            using (var ms = new MemoryStream()) {
+                ImageTools.SaveImageAsJpeg(resImage, ms, 100);
+                using (var loadedImage = (Bitmap)ImageTools.ToImage(ms.ToArray())) {
+                    PAssert.That(() => loadedImage.Width == resImage.Width && loadedImage.Height == resImage.Height);
+                    PAssert.That(
+                        () =>
+                            !(from y in Enumerable.Range(0, loadedImage.Height)
+                                from x in Enumerable.Range(0, loadedImage.Width)
+                                select new { x, y }).Where(p => loadedImage.GetPixel(p.x, p.y).Distance(resImage.GetPixel(p.x, p.y)) > 0.05).Any());
+                }
+            }
+        }
+
+        [Fact]
+        public void CanResizeImage()
+        {
+            using (var resImage = GetRainbow())
+            using (var down_W = ImageTools.Resize(resImage, 100, resImage.Height))
+            using (var down_H = ImageTools.Resize(resImage, resImage.Width, 100))
+            using (var down_W_H = ImageTools.Resize(down_W, 50, 50))
+            using (var down_H_W = ImageTools.Resize(down_H, 50, 50))
+            using (var down_WH = ImageTools.Resize(resImage, 50, 50)) {
+                AssertImagesSimilar(down_W_H, down_WH);
+                AssertImagesSimilar(down_H_W, down_WH);
+                AssertImagesSimilar(down_H_W, down_W_H);
+
+                //als het test-plaatje geen contrast heeft, dan is deze hele test zinloos: verifieer dat het test plaatje contrast heeft:
+                PAssert.That(() => down_W_H.GetPixel(45, 25) != down_W_H.GetPixel(10, 30));
+            }
+        }
     }
 }
-#endif
