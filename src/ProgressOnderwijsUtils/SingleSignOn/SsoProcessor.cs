5766861e3ffd4d47d1121a60340b22621b3ec74a
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2017-10-17 17:22:08 +0200

    Move AuthnRequest to outer scope

--- src/ProgressOnderwijsUtils/SingleSignOn/SsoProcessor.cs
+++ src/ProgressOnderwijsUtils/SingleSignOn/SsoProcessor.cs
@@ -17,40 +17,6 @@ namespace ProgressOnderwijsUtils.SingleSignOn
 {
     public static class SsoProcessor
     {
-        struct AuthnRequest
-        {
-            public string ID { get; set; }
-            public string Destination { get; set; }
-            public string Issuer { private get; set; }
-
-            public string Encode()
-            {
-                var xml = Encoding.UTF8.GetBytes(ToXml().ToString());
-                using (var stream = new MemoryStream()) {
-                    using (var deflate = new DeflateStream(stream, CompressionMode.Compress))
-                        deflate.Write(xml, 0, xml.Length);
-                    return Convert.ToBase64String(stream.ToArray());
-                }
-            }
-
-            XElement ToXml()
-            {
-                return new XElement(
-                    SamlNamespaces.SAMLP_NS + "AuthnRequest",
-                    new XAttribute(XNamespace.Xmlns + "saml", SamlNamespaces.SAML_NS.NamespaceName),
-                    new XAttribute(XNamespace.Xmlns + "sampl", SamlNamespaces.SAMLP_NS.NamespaceName),
-                    new XAttribute("ID", XmlConvert.EncodeLocalName(ID)),
-                    new XAttribute("Version", "2.0"),
-                    new XAttribute("IssueInstant", DateTime.UtcNow),
-                    new XAttribute("Destination", Destination),
-                    new XAttribute("ForceAuthn", "true"),
-                    new XAttribute("IsPassive", "false"),
-                    new XAttribute("ProtocolBinding", "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"),
-                    new XElement(SamlNamespaces.SAML_NS + "Issuer", Issuer)
-                    );
-            }
-        }
-
         const string UID = "urn:mace:dir:attribute-def:uid";
         const string MAIL = "urn:mace:dir:attribute-def:mail";
         const string DOMAIN = "urn:mace:terena.org:attribute-def:schacHomeOrganization";
@@ -260,6 +226,40 @@ namespace ProgressOnderwijsUtils.SingleSignOn
         public static void ValidateSchema(XElement assertion)
         {
             new XDocument(assertion).Validate(schemaSet, null, false);
+        }
+    }
+
+    struct AuthnRequest
+    {
+        public string ID { get; set; }
+        public string Destination { get; set; }
+        public string Issuer { private get; set; }
+
+        public string Encode()
+        {
+            var xml = Encoding.UTF8.GetBytes(ToXml().ToString());
+            using (var stream = new MemoryStream()) {
+                using (var deflate = new DeflateStream(stream, CompressionMode.Compress))
+                    deflate.Write(xml, 0, xml.Length);
+                return Convert.ToBase64String(stream.ToArray());
+            }
+        }
+
+        XElement ToXml()
+        {
+            return new XElement(
+                SamlNamespaces.SAMLP_NS + "AuthnRequest",
+                new XAttribute(XNamespace.Xmlns + "saml", SamlNamespaces.SAML_NS.NamespaceName),
+                new XAttribute(XNamespace.Xmlns + "sampl", SamlNamespaces.SAMLP_NS.NamespaceName),
+                new XAttribute("ID", XmlConvert.EncodeLocalName(ID)),
+                new XAttribute("Version", "2.0"),
+                new XAttribute("IssueInstant", DateTime.UtcNow),
+                new XAttribute("Destination", Destination),
+                new XAttribute("ForceAuthn", "true"),
+                new XAttribute("IsPassive", "false"),
+                new XAttribute("ProtocolBinding", "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"),
+                new XElement(SamlNamespaces.SAML_NS + "Issuer", Issuer)
+                );
         }
     }
 }
