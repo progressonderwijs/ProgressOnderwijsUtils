f045645f671b8b51ff2e71e49fe2b1b507081c88
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2017-12-04 22:44:02 +0100

    Rename test projects to follow new naming convention

---
+++ test/ProgressOnderwijsUtils.Tests/HashCodeTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ExpressionToCodeLib;
+using Xunit;
+using ProgressOnderwijsUtils;
+
+namespace ProgressOnderwijsUtils.Tests
+{
+    
+    public class HashCodeTest
+    {
+        [Fact]
+        public void IsConsistent()
+        {
+            PAssert.That(() => HashCodeHelper.ComputeHash("test", 3, null, DateTime.MinValue) != HashCodeHelper.ComputeHash("test", 3, null, DateTime.MinValue, null));
+            //extra null matters
+            PAssert.That(() => HashCodeHelper.ComputeHash("test", 3, null, DateTime.MinValue) != HashCodeHelper.ComputeHash("test", 3, DateTime.MinValue, null));
+            //order matters
+        }
+    }
+}
