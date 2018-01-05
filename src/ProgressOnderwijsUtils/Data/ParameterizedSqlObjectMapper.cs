cee0f8535ef752c3243d191ac6cbbd16fef57161
Tjibbe Chris Kuipers <t.c.kuipers@rug.nl>
2018-01-05 14:41:56 +0100

    Allow nullable structs as custom objects for IMetaObject

--- src/ProgressOnderwijsUtils/Data/ParameterizedSqlObjectMapper.cs
+++ src/ProgressOnderwijsUtils/Data/ParameterizedSqlObjectMapper.cs
@@ -330,8 +330,8 @@ namespace ProgressOnderwijsUtils
             {
                 methodInfo = null;
                 var underlyingType = type.GetNonNullableUnderlyingType();
-                if (!underlyingType.IsValueType) {
-                    var method = underlyingType.GetMethods(BindingFlags.Static | BindingFlags.Public).SingleOrDefault(m => m.GetCustomAttributes<MetaObjectPropertyLoaderAttribute>().Any());
+                var method = underlyingType.GetMethods(BindingFlags.Static | BindingFlags.Public).SingleOrDefault(m => m.GetCustomAttributes<MetaObjectPropertyLoaderAttribute>().Any());
+                if (!underlyingType.IsValueType || method != null) {
                     if (method == null) {
                         return false;
                     }
@@ -385,15 +385,15 @@ namespace ProgressOnderwijsUtils
                     callExpr = Expression.Call(readerParamExpr, GetterForType(underlyingType), iConstant);
                 }
                 Expression colValueExpr;
-                if (!canBeNull) {
+                var isTypeWithCreateMethod = CreateMethodOfTypeWithCreateMethod(underlyingType, out var methodInfo);
+                if (!canBeNull && !isTypeWithCreateMethod) {
                     colValueExpr = GetCastExpression(callExpr, type);
                 }
-                else if (CreateMethodOfTypeWithCreateMethod(underlyingType, out var methodInfo)) {
+                else if (isTypeWithCreateMethod) {
                     var test = Expression.Call(readerParamExpr, IsDBNullMethod, iConstant);
-                    var paramType = methodInfo.GetParameters()[0].ParameterType;
-                    var ifDbNull = Expression.Default(paramType);
-                    var ifNotDbNull = Expression.Convert(GetCastExpression(callExpr, paramType), paramType);
-                    colValueExpr = Expression.Call(methodInfo, Expression.Condition(test, ifDbNull, ifNotDbNull));
+                    var ifDbNull = Expression.Default(type);
+                    var ifNotDbNull = Expression.Convert(Expression.Call(methodInfo, callExpr), type);
+                    colValueExpr = Expression.Condition(test, ifDbNull, ifNotDbNull);
                 } else {
                     var test = Expression.Call(readerParamExpr, IsDBNullMethod, iConstant);
                     var ifDbNull = Expression.Default(type);
