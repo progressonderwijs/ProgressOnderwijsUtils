44b6711cf721d2eb3e56886156eb57c3f98621a1
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2018-01-15 10:17:59 +0100

    Signer

---
+++ test/ProgressOnderwijsUtils.Tests/SignerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using ExpressionToCodeLib;
+using Xunit;
+
+namespace ProgressOnderwijsUtils.Tests
+{
+    public sealed class SignerTest
+    {
+        [Fact]
+        public void Signature_verifies()
+        {
+            var certificate = new X509Certificate2("testCert.pfx", "testPassword");
+            var signed = Signer.SignJson(certificate, "https://example.com");
+
+            PAssert.That(() => Signer.VerifySignedJson<string>(certificate, signed) == "https://example.com");
+        }
+
+        [Fact]
+        public void Signature_can_fail()
+        {
+            var certificate = new X509Certificate2("testCert.pfx", "testPassword");
+            var signed = Signer.SignJson(certificate, "https://example.com").Replace("example", "evilexample");
+
+            PAssert.That(() => Signer.VerifySignedString(certificate, signed) == null);
+        }
+    }
+}
