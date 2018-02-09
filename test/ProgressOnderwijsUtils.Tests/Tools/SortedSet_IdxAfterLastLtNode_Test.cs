677e79c7864305ec2ac7350f78e79c499bc6f7c2
Eamon Nerbonne <eamon@nerbonne.org>
2018-02-09 09:50:39 +0100

    A few binary search tests

---
+++ test/ProgressOnderwijsUtils.Tests/Tools/SortedSet_IdxAfterLastLtNode_Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExpressionToCodeLib;
+using NUnit.Framework;
+using Progress.Business.Tools;
+
+namespace Progress.Business.Tests.Tools
+{
+    public sealed class SortedSet_IdxAfterLastLtNode_Test
+    {
+        static int IdxAfterLastLtNode(int[] sortedArray, int needle) //Just to make this test read a little nicer.
+            => SortedSet<int, IntOrdering>.Algorithms.IdxAfterLastLtNode(sortedArray, needle);
+
+        [Test]
+        public void IndexIs0ForEmptyArray()
+        {
+            PAssert.That(() => IdxAfterLastLtNode(Array.Empty<int>(), 37) == 0);
+        }
+
+        [Test]
+        public void IndexIs0IfNeedleIsSmallerThanSingleElement()
+        {
+            PAssert.That(() => IdxAfterLastLtNode(new[] { 42 }, 37) == 0);
+        }
+
+        [Test]
+        public void IndexIsOneIfNeedleIsLargerThanSingleElement()
+        {
+            PAssert.That(() => IdxAfterLastLtNode(new[] { 37 }, 42) == 1);
+        }
+
+        [Test]
+        public void IndexIs0IfNeedleIsEqualToAllValues()
+        {
+            PAssert.That(() => IdxAfterLastLtNode(new[] { 3, 3, 3, 3 }, 3) == 0);
+        }
+
+        [Test]
+        public void IndexIs10IfNeedleIsGreaterThanAll10Values()
+        {
+            PAssert.That(() => IdxAfterLastLtNode(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 100) == 10);
+        }
+
+        [Test]
+        public void IndexIs5IfNeedleEqualToNode5Value()
+        {
+            PAssert.That(() => IdxAfterLastLtNode(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 5) == 5);
+        }
+
+        [Test]
+        public void IndexIs2IfNeedleBetweenValueOfNode1AndNode2()
+        {
+            PAssert.That(() => IdxAfterLastLtNode(new[] { 0, 2, 4, 6, 8, 10, 12, 14, 16 }, 3) == 2);
+        }
+    }
+}
