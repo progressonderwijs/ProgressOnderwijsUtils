3f9994188416dcd04f04f6ef53e7368c467a25fb
Eamon Nerbonne <eamon@nerbonne.org>
2018-04-25 14:17:28 +0200

    Update comment.

--- src/ProgressOnderwijsUtils/RetryDelayChooser.cs
+++ src/ProgressOnderwijsUtils/RetryDelayChooser.cs
@@ -3,8 +3,15 @@
 namespace ProgressOnderwijsUtils
 {
     /// <summary>
-    /// This class pick a retry delay based on the current error rate (see ErrorsPerDayToRetryDelay for the formula)
-    /// The error rate is estimated using exponential decay, so
+    /// This class picks a retry delay appropriate for a possibly intermittently failing service or similar process.
+    /// The aim is to have long retry delays during a real outage, yet very low retry delays during normal operations even when the occasional call fails.
+    /// By delaying for a long time during an outage, you can avoid log spam, and avoid excessive load on a service that's possibly already overloaded.
+    /// Simultaneously, the retry delay is bounded such that when the outage stops, the system automatically resumes normal operations without requiring human intervention.
+    /// The algorithm is very simple; and not intended to find a truly optimal delay (which would depend on the circumstances); the intent
+    /// is to a pick a delays that works reasonably even when the underlying error rate varies wildly.
+    ///
+    /// The algorithm accepts one parameter, the retry delay that the algorithm should converge to during a 100% outages;
+    /// i.e. an infinite loop of Thread.Sleep(chooser.RegisterErrorAndGetDelay(DateTime.UtcNow)) will eventually converge to the value provided.
     /// </summary>
     public sealed class RetryDelayChooser
     {
