001b4042f907c91bcf09bea17703b19825f32bfa
Fons Dijkstra <f.j.dijkstra@rug.nl>
2018-11-28 14:09:36 +0100

    Addfed test that computed columns also use correct indexes.

--- test/ProgressOnderwijsUtils.Tests/MetaObjectBulkCopyTest.cs
+++ test/ProgressOnderwijsUtils.Tests/MetaObjectBulkCopyTest.cs
@@ -9,12 +9,12 @@ namespace ProgressOnderwijsUtils.Tests
 {
     public sealed class MetaObjectBulkCopyTest : TransactedLocalConnection
     {
-        static readonly BlaOk[] SampleObjects = new[] {
+        static readonly BlaOk[] SampleObjects = {
             new BlaOk { Bla = "bl34ga", Bla2 = "blaasdfgasfg2", Id = -1 },
             new BlaOk { Bla = "bla", Bla2 = "bla2", Id = 0 },
             new BlaOk { Bla = "dfg", Bla2 = "bla342", Id = 1 },
             new BlaOk { Bla = "blfgjha", Bla2 = "  bla2  ", Id = 2 },
-            new BlaOk { Bla2 = "", Id = 3 },
+            new BlaOk { Bla2 = "", Id = 3 }
         };
 
         public sealed class BlaOk : ValueBase<BlaOk>, IMetaObject, IPropertiesAreUsedImplicitly
@@ -80,6 +80,29 @@ namespace ProgressOnderwijsUtils.Tests
             return (tableName.CommandText(), DbColumnMetaData.ColumnMetaDatas(Context.Connection, tableName));
         }
 
+        sealed class ComputedColumnExample : ValueBase<ComputedColumnExample>, IMetaObject, IPropertiesAreUsedImplicitly
+        {
+            public int Id { get; set; }
+            public bool Computed { internal get; set; }
+            public string Bla { get; set; }
+        }
+
+        [Fact]
+        public void BulkCopyAllowsExactMatch()
+        {
+            var (table, columns) = CreateTempTable();
+            SampleObjects.BulkCopyToSqlServer(Context.Connection, table, columns);
+            var fromDb = SQL($"select * from #MyTable order by Id").ReadMetaObjects<BlaOk>(Context);
+            PAssert.That(() => SampleObjects.SequenceEqual(fromDb));
+        }
+
+        [Fact]
+        public void BulkCopyAllowsExtraDestinationColumns()
+        {
+            var (table, columns) = CreateTempTable();
+            new BlaWithMissingClrFields[0].BulkCopyToSqlServer(Context.Connection, table, columns);
+        }
+
         [Fact]
         public void BulkCopyChecksNames()
         {
@@ -102,35 +125,45 @@ namespace ProgressOnderwijsUtils.Tests
         }
 
         [Fact]
-        public void BulkCopyVerifiesExistanceOfDestinationColumns()
-        {
-            var (table, columns) = CreateTempTable();
-            Assert.ThrowsAny<Exception>(() => new BlaWithExtraClrFields[0].BulkCopyToSqlServer(Context.Connection, table, columns));
-        }
-
-        [Fact]
-        public void BulkCopyAllowsExtraDestinationColumns()
-        {
-            var (table, columns) = CreateTempTable();
-            new BlaWithMissingClrFields[0].BulkCopyToSqlServer(Context.Connection, table, columns);
-        }
-
-        [Fact]
-        public void BulkCopyAllowsExactMatch()
-        {
-            var (table, columns) = CreateTempTable();
-            SampleObjects.BulkCopyToSqlServer(Context.Connection, table, columns);
-            var fromDb = SQL($"select * from #MyTable order by Id").ReadMetaObjects<BlaOk>(Context);
-            PAssert.That(() => SampleObjects.SequenceEqual(fromDb));
-        }
-
-        [Fact]
         public void BulkCopySupportsColumnReordering()
         {
             var (table, columns) = CreateTempTable();
             SampleObjects.BulkCopyToSqlServer(Context.Connection, table, columns);
             var fromDb = SQL($"select * from #MyTable order by Id").ReadMetaObjects<BlaOk2>(Context);
             PAssert.That(() => SampleObjects.SequenceEqual(fromDb.Select(x => new BlaOk { Id = x.Id, Bla = x.Bla, Bla2 = x.Bla2 })));
+        }
+
+        [Fact]
+        public void BulkCopySupportsCumputedColumn()
+        {
+            var tableName = SQL($"#MyTable");
+            SQL($@"
+                create table {tableName} (
+                    Id int not null primary key
+                    , Computed as convert(bit, 1) -- deliberately not placed at the end
+                    , Bla nvarchar(max) not null
+                )
+            ").ExecuteNonQuery(Context);
+
+            new[] {
+                new ComputedColumnExample {
+                    Id = 11,
+                    Bla = "Something"
+                }
+            }.BulkCopyToSqlServer(Context.Connection, tableName.CommandText(), DbColumnMetaData.ColumnMetaDatas(Context.Connection, tableName));
+
+            var fromDb = SQL($@"
+                select *
+                from {tableName}
+            ").ReadMetaObjects<ComputedColumnExample>(Context).Single();
+            PAssert.That(() => fromDb.Computed);
+        }
+
+        [Fact]
+        public void BulkCopyVerifiesExistanceOfDestinationColumns()
+        {
+            var (table, columns) = CreateTempTable();
+            Assert.ThrowsAny<Exception>(() => new BlaWithExtraClrFields[0].BulkCopyToSqlServer(Context.Connection, table, columns));
         }
     }
 }
