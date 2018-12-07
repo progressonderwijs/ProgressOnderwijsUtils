9d91ee573438218f26d1a0a0f999fb2d525ccb9b
Eamon Nerbonne <eamon@nerbonne.org>
2018-12-07 13:56:26 +0100

    Move actual implementation into helper options object

--- src/ProgressOnderwijsUtils/MetaObject/MetaObjectBulkCopy.cs
+++ src/ProgressOnderwijsUtils/MetaObject/MetaObjectBulkCopy.cs
@@ -20,6 +20,109 @@ namespace ProgressOnderwijsUtils
         AllowExtraDatabaseColumns,
     }
 
+    struct MetaObjectBulkInsertOperation<T>
+        where T : IMetaObject, IPropertiesAreUsedImplicitly
+    {
+        public SqlBulkCopy bulkCopy;
+        public SqlCommandCreationContext context;
+        public IEnumerable<T> metaObjects;
+        public string tableName;
+        public ColumnDefinition[] tableColumnDefinitions; //var tableColumnDefinitions = ColumnDefinition.GetFromCompleteSetOfColumns(columns.Where(column => !column.Is_Computed && !column.Is_RowVersion).ToArray());
+        public BulkCopyFieldMappingMode mode;
+        public CancellationToken cancellationToken;
+
+        public void Execute()
+        {
+            var sqlconn = context.Connection;
+            if (metaObjects == null) {
+                throw new ArgumentNullException(nameof(metaObjects));
+            }
+            if (sqlconn == null) {
+                throw new ArgumentNullException(nameof(sqlconn));
+            }
+            if (sqlconn.State != ConnectionState.Open) {
+                throw new InvalidOperationException($"Cannot bulk copy into {tableName}: connection isn't open but {sqlconn.State}.");
+            }
+            bulkCopy.DestinationTableName = tableName;
+
+            using (var objectReader = new MetaObjectDataReader<T>(metaObjects, cancellationToken)) {
+                var mapping = ApplyMetaObjectColumnMapping(bulkCopy, objectReader, tableName, mode, tableColumnDefinitions);
+                var sw = Stopwatch.StartNew();
+                try {
+                    bulkCopy.WriteToServer(objectReader);
+                    //so why no async?
+                    //WriteToServerAsync "supports" cancellation, but causes deadlocks when buggy code uses the connection while enumerating metaObjects, and that's hard to detect and very nasty on production servers, so we stick to sync instead - that throws exceptions instead, and hey, it's slightly faster too.
+                } catch (SqlException ex) when (ParseDestinationColumnIndexFromMessage(ex.Message).HasValue) {
+                    var destinationColumnIndex = ParseDestinationColumnIndexFromMessage(ex.Message).Value;
+                    throw HelpfulException(bulkCopy, destinationColumnIndex, ex) ?? MetaObjectBasedException(mapping, destinationColumnIndex, ex);
+                } finally {
+                    TraceBulkInsertDuration(context.Tracer, tableName, sw, objectReader.RowsProcessed);
+                }
+            }
+        }
+
+        [NotNull]
+        static Exception MetaObjectBasedException([NotNull] FieldMapping[] mapping, int destinationColumnIndex, SqlException ex)
+        {
+            var sourceColumnName = "??unknown??";
+            foreach (var m in mapping) {
+                if (m.Dst.Index == destinationColumnIndex) {
+                    sourceColumnName = m.Src.Name;
+                }
+            }
+
+            var metaPropName = typeof(T).ToCSharpFriendlyTypeName() + "." + sourceColumnName;
+            return new Exception($"Received an invalid column length from the bcp client for metaobject property ${metaPropName}.", ex);
+        }
+
+        [CanBeNull]
+        static Exception HelpfulException(SqlBulkCopy bulkCopy, int destinationColumnIndex, SqlException ex)
+        {
+            var fi = typeof(SqlBulkCopy).GetField("_sortedColumnMappings", BindingFlags.NonPublic | BindingFlags.Instance);
+            var sortedColumns = fi.GetValue(bulkCopy);
+            var items = (object[])sortedColumns.GetType().GetField("_items", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(sortedColumns);
+
+            var itemdata = items?[destinationColumnIndex].GetType().GetField("_metadata", BindingFlags.NonPublic | BindingFlags.Instance);
+            var metadata = itemdata?.GetValue(items[destinationColumnIndex]);
+
+            var column = metadata?.GetType().GetField("column", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(metadata);
+            var length = metadata?.GetType().GetField("length", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(metadata);
+            return column == null || length == null ? null : new Exception($"Column: {column} contains data with a length greater than: {length}", ex);
+        }
+
+        static void TraceBulkInsertDuration([CanBeNull] ISqlCommandTracer tracerOrNull, string tableName, Stopwatch sw, int rowsInserted)
+        {
+            if (tracerOrNull?.IsTracing ?? false) {
+                tracerOrNull.RegisterEvent($"Bulk inserted {rowsInserted} rows into {tableName}.", sw.Elapsed);
+            }
+        }
+
+        static readonly Regex colidMessageRegex = new Regex(@"Received an invalid column length from the bcp client for colid ([0-9]+).", RegexOptions.Compiled);
+
+        static int? ParseDestinationColumnIndexFromMessage([NotNull] string message)
+        {
+            //note: sql colid is 1-based!
+            var match = colidMessageRegex.Match(message);
+            return !match.Success ? default(int?) : int.Parse(match.Groups[1].Value) - 1;
+        }
+
+        [NotNull]
+        static FieldMapping[] ApplyMetaObjectColumnMapping([NotNull] SqlBulkCopy bulkCopy, [NotNull] MetaObjectDataReader<T> objectReader, string tableName, BulkCopyFieldMappingMode mode, [NotNull] ColumnDefinition[] tableColumns)
+        {
+            var clrColumns = ColumnDefinition.GetFromReader(objectReader);
+            var mapping = FieldMapping.VerifyAndCreate(
+                clrColumns,
+                typeof(T).ToCSharpFriendlyTypeName(),
+                mode == BulkCopyFieldMappingMode.AllowExtraMetaObjectProperties,
+                tableColumns,
+                "table " + tableName,
+                mode == BulkCopyFieldMappingMode.AllowExtraDatabaseColumns);
+
+            FieldMapping.ApplyFieldMappingsToBulkCopy(mapping, bulkCopy);
+            return mapping;
+        }
+    }
+
     /// <summary>
     /// Contains extension methods to insert metaobjects (POCOs) into database tables using SqlBulkCopy.
     /// </summary>
@@ -123,96 +226,15 @@ namespace ProgressOnderwijsUtils
             CancellationToken cancellationToken)
             where T : IMetaObject, IPropertiesAreUsedImplicitly
         {
-            var sqlconn = context.Connection;
-            if (metaObjects == null) {
-                throw new ArgumentNullException(nameof(metaObjects));
-            }
-            if (sqlconn == null) {
-                throw new ArgumentNullException(nameof(sqlconn));
-            }
-            if (sqlconn.State != ConnectionState.Open) {
-                throw new InvalidOperationException($"Cannot bulk copy into {tableName}: connection isn't open but {sqlconn.State}.");
-            }
-            bulkCopy.DestinationTableName = tableName;
-
-            using (var objectReader = new MetaObjectDataReader<T>(metaObjects, cancellationToken)) {
-                var tableColumnDefinitions = ColumnDefinition.GetFromCompleteSetOfColumns(columns.Where(column => !column.Is_Computed && !column.Is_RowVersion).ToArray());
-                var mapping = ApplyMetaObjectColumnMapping(bulkCopy, objectReader, tableName, mode, tableColumnDefinitions);
-                var sw = Stopwatch.StartNew();
-                try {
-                    bulkCopy.WriteToServer(objectReader);
-                    //so why no async?
-                    //WriteToServerAsync "supports" cancellation, but causes deadlocks when buggy code uses the connection while enumerating metaObjects, and that's hard to detect and very nasty on production servers, so we stick to sync instead - that throws exceptions instead, and hey, it's slightly faster too.
-                } catch (SqlException ex) when (ParseDestinationColumnIndexFromMessage(ex.Message).HasValue) {
-                    var destinationColumnIndex = ParseDestinationColumnIndexFromMessage(ex.Message).Value;
-                    throw HelpfulException(bulkCopy, destinationColumnIndex, ex) ?? MetaObjectBasedException<T>(mapping, destinationColumnIndex, ex);
-                } finally {
-                    TraceBulkInsertDuration(context.Tracer, tableName, sw, objectReader.RowsProcessed);
-                }
-            }
-        }
-
-        [NotNull]
-        static Exception MetaObjectBasedException<T>([NotNull] FieldMapping[] mapping, int destinationColumnIndex, SqlException ex)
-            where T : IMetaObject, IPropertiesAreUsedImplicitly
-        {
-            var sourceColumnName = "??unknown??";
-            foreach (var m in mapping) {
-                if (m.Dst.Index == destinationColumnIndex) {
-                    sourceColumnName = m.Src.Name;
-                }
-            }
-
-            var metaPropName = typeof(T).ToCSharpFriendlyTypeName() + "." + sourceColumnName;
-            return new Exception($"Received an invalid column length from the bcp client for metaobject property ${metaPropName}.", ex);
-        }
-
-        [CanBeNull]
-        static Exception HelpfulException(SqlBulkCopy bulkCopy, int destinationColumnIndex, SqlException ex)
-        {
-            var fi = typeof(SqlBulkCopy).GetField("_sortedColumnMappings", BindingFlags.NonPublic | BindingFlags.Instance);
-            var sortedColumns = fi.GetValue(bulkCopy);
-            var items = (object[])sortedColumns.GetType().GetField("_items", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(sortedColumns);
-
-            var itemdata = items?[destinationColumnIndex].GetType().GetField("_metadata", BindingFlags.NonPublic | BindingFlags.Instance);
-            var metadata = itemdata?.GetValue(items[destinationColumnIndex]);
-
-            var column = metadata?.GetType().GetField("column", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(metadata);
-            var length = metadata?.GetType().GetField("length", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(metadata);
-            return column == null || length == null ? null : new Exception($"Column: {column} contains data with a length greater than: {length}", ex);
-        }
-
-        static void TraceBulkInsertDuration([CanBeNull] ISqlCommandTracer tracerOrNull, string tableName, Stopwatch sw, int rowsInserted)
-        {
-            if (tracerOrNull?.IsTracing ?? false) {
-                tracerOrNull.RegisterEvent($"Bulk inserted {rowsInserted} rows into {tableName}.", sw.Elapsed);
-            }
-        }
-
-        static readonly Regex colidMessageRegex = new Regex(@"Received an invalid column length from the bcp client for colid ([0-9]+).", RegexOptions.Compiled);
-
-        static int? ParseDestinationColumnIndexFromMessage([NotNull] string message)
-        {
-            //note: sql colid is 1-based!
-            var match = colidMessageRegex.Match(message);
-            return !match.Success ? default(int?) : int.Parse(match.Groups[1].Value) - 1;
-        }
-
-        [NotNull]
-        static FieldMapping[] ApplyMetaObjectColumnMapping<T>([NotNull] SqlBulkCopy bulkCopy, [NotNull] MetaObjectDataReader<T> objectReader, string tableName, BulkCopyFieldMappingMode mode, [NotNull] ColumnDefinition[] tableColumns)
-            where T : IMetaObject
-        {
-            var clrColumns = ColumnDefinition.GetFromReader(objectReader);
-            var mapping = FieldMapping.VerifyAndCreate(
-                clrColumns,
-                typeof(T).ToCSharpFriendlyTypeName(),
-                mode == BulkCopyFieldMappingMode.AllowExtraMetaObjectProperties,
-                tableColumns,
-                "table " + tableName,
-                mode == BulkCopyFieldMappingMode.AllowExtraDatabaseColumns);
-
-            FieldMapping.ApplyFieldMappingsToBulkCopy(mapping, bulkCopy);
-            return mapping;
+            new MetaObjectBulkInsertOperation<T> {
+                bulkCopy = bulkCopy,
+                cancellationToken = cancellationToken,
+                context = context,
+                metaObjects = metaObjects,
+                mode = mode,
+                tableColumnDefinitions = ColumnDefinition.GetFromCompleteSetOfColumns(columns.Where(column => !column.Is_Computed && !column.Is_RowVersion).ToArray()),
+                tableName = tableName,
+            }.Execute();
         }
     }
 }
