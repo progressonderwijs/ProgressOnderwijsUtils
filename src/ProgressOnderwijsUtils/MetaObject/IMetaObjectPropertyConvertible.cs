7aaa860d42498c9bc24f4e5f74c088b297a14f3d
Tjibbe Chris Kuipers <t.c.kuipers@progressonderwijs.nl>
2019-04-26 16:12:51 +0200

    To own class

---
+++ src/ProgressOnderwijsUtils/MetaObject/IMetaObjectPropertyConvertible.cs
@@ -0,0 +1,8 @@
+﻿using JetBrains.Annotations;
+
+namespace ProgressOnderwijsUtils {
+    public interface IMetaObjectPropertyConvertible<TModel, TProvider, [UsedImplicitly] TConverterSource>
+        where TConverterSource : struct, IConverterSource<TModel, TProvider>
+        where TModel : struct, IMetaObjectPropertyConvertible<TModel, TProvider, TConverterSource>
+    { }
+}
--- src/ProgressOnderwijsUtils/MetaObject/MetaObjectPropertyLoaderAttribute.cs
+++ src/ProgressOnderwijsUtils/MetaObject/MetaObjectPropertyLoaderAttribute.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 // ReSharper disable once CheckNamespace
@@ -11,9 +10,4 @@ namespace ProgressOnderwijsUtils
     {
         ValueConverter<TModel, TProvider> GetValueConverter();
     }
-
-    public interface IMetaObjectPropertyConvertible<TModel, TProvider, [UsedImplicitly] TConverterSource>
-        where TConverterSource : struct, IConverterSource<TModel, TProvider>
-        where TModel : struct, IMetaObjectPropertyConvertible<TModel, TProvider, TConverterSource>
-    { }
 }
