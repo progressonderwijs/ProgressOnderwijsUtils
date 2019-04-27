dbd2ca15f24efb62f54ebafddabf508310ea292e
Eamon Nerbonne <eamon@nerbonne.org>
2019-04-27 11:32:54 +0200

    Move sanitization into utils

---
+++ src/ProgressOnderwijsUtils/Html/HtmlFragmentSanitizeExtension.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using ProgressOnderwijsUtils;
+
+namespace ProgressOnderwijsUtils.Html
+{
+    public enum TagSafety
+    {
+        /// <summary>
+        /// This tag is black-listed; the entire tag with all its descendants should be excluded from the output.
+        /// </summary>
+        ShouldRemoveCompletely,
+
+        /// <summary>
+        /// This tag is not white-listed; it's contents should be included in the output but the tag itself (and its attributes) should be removed.
+        /// </summary>
+        ShouldRemoveButKeepContent,
+
+        /// <summary>
+        /// This tag is safe even when a hostile hacker controls its contents and white-listed attributes; it can be included in output.
+        /// </summary>
+        SafeToKeep
+    }
+
+    public interface IHtmlFilter
+    {
+        /// <summary>
+        /// Returns whether the tag should be allowed.
+        /// </summary>
+        /// <param name="elem">The element being checked for security</param>
+        /// <returns>The status of the tag.</returns>
+        TagSafety AllowTag(IHtmlElement elem);
+
+        /// <summary>
+        /// Whether the attribute is safe and should be allowed.
+        /// </summary>
+        bool AllowAttribute(HtmlAttribute attr);
+    }
+
+    public static class HtmlFilters
+    {
+        public sealed class StripUnsafeStyleTagsFilter : IHtmlFilter
+        {
+            StripUnsafeStyleTagsFilter() { }
+            public static readonly IHtmlFilter Instance = new StripUnsafeStyleTagsFilter();
+
+            public TagSafety AllowTag(IHtmlElement elem)
+                => TagSafety.SafeToKeep;
+
+            public bool AllowAttribute(HtmlAttribute attr)
+                => !attr.Name.EqualsOrdinalCaseInsensitive("style") || SafeStyleRegex.IsMatch(attr.Value);
+
+            static readonly Regex
+                SafeStyleRegex = new Regex(@"^
+                    \s*margin(-(left|right|top|bottom))?\s*:\s*
+                    \d+(px|em|cm|mm|)\s*;?\s*
+                    $", RegexOptions.CultureInvariant | RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture);
+        }
+
+        public sealed class StripElementsWithInlineJavascriptFilter : IHtmlFilter
+        {
+            StripElementsWithInlineJavascriptFilter() { }
+            public static readonly IHtmlFilter Instance = new StripElementsWithInlineJavascriptFilter();
+
+            public TagSafety AllowTag(IHtmlElement elem)
+            {
+                foreach (var attr in elem.Attributes) {
+                    if (attr.Name.StartsWith("on", StringComparison.OrdinalIgnoreCase)) {
+                        return TagSafety.ShouldRemoveButKeepContent;
+                    }
+                    if (attr.Name.EqualsOrdinalCaseInsensitive("href")
+                        && !attr.Value.StartsWith("http:", StringComparison.InvariantCultureIgnoreCase)
+                        && !attr.Value.StartsWith("https:", StringComparison.InvariantCultureIgnoreCase)) {
+                        return TagSafety.ShouldRemoveButKeepContent;
+                    }
+                }
+                return TagSafety.SafeToKeep;
+            }
+
+            public bool AllowAttribute(HtmlAttribute attr)
+                => true;
+        }
+
+        public sealed class PickMostRestrictiveFilter : IHtmlFilter
+        {
+            readonly IHtmlFilter[] filters;
+
+            public PickMostRestrictiveFilter(params IHtmlFilter[] filters)
+                => this.filters = filters;
+
+            public TagSafety AllowTag(IHtmlElement elem)
+            {
+                var retval = TagSafety.SafeToKeep;
+                foreach (var filter in filters) {
+                    retval = MostRestrictiveFilter(retval, filter.AllowTag(elem));
+                }
+                return retval;
+            }
+
+            static TagSafety MostRestrictiveFilter(TagSafety retval, TagSafety tagSafety)
+                => tagSafety < retval ? tagSafety : retval;
+
+            public bool AllowAttribute(HtmlAttribute attr)
+            {
+                var retval = true;
+                foreach (var filter in filters) {
+                    retval = retval && filter.AllowAttribute(attr);
+                }
+                return retval;
+            }
+        }
+
+        public sealed class SetBasedHtmlFilter : IHtmlFilter
+        {
+            readonly HashSet<string> bannedElements, safeElements, safeAttributes;
+
+            public SetBasedHtmlFilter(IEnumerable<string> bannedElements, IEnumerable<string> safeElements, IEnumerable<string> safeAttributes)
+            {
+                this.bannedElements = MkSet(bannedElements);
+                this.safeElements = MkSet(safeElements);
+                this.safeAttributes = MkSet(safeAttributes);
+            }
+
+            static HashSet<string> MkSet(IEnumerable<string> elems)
+                => new HashSet<string>(elems ?? new string[0], StringComparer.OrdinalIgnoreCase);
+
+            public TagSafety AllowTag(IHtmlElement elem)
+                => bannedElements.Contains(elem.TagName)
+                    ? TagSafety.ShouldRemoveCompletely
+                    : safeElements.Contains(elem.TagName)
+                        ? TagSafety.SafeToKeep
+                        : TagSafety.ShouldRemoveButKeepContent;
+
+            public bool AllowAttribute(HtmlAttribute attr)
+                => safeAttributes.Contains(attr.Name);
+        }
+
+        static readonly string[]
+            banned = "script style".Split(' '),
+            safe = Regex.Split(@"a abbr acronym b big blockquote br caption center
+                cite code col colgroup dd del dfn div dl dt em fieldset font h1 h2
+                h3 h4 h5 h6 hr i img ins legend li ol p pre q samp small span strong
+                sub sup table tbody td tfoot th thead tr tt u ul var", @"\s+"),
+            safeAttr = Regex.Split(@"align alt bgcolor border cellpadding cellspacing
+                char charoff cite color colspan dir face height href lang rowspan size
+                style title width", @"\s+");
+
+        //om tracer elements te vermijden zijn is img wel maar attribuut src niet toegestaan Bovendien kan src="javascript:..." dus src mag echt niet! Om geen form-problemen te hebben mogen form elementen niet.
+        public static readonly IHtmlFilter Default = new PickMostRestrictiveFilter(new SetBasedHtmlFilter(banned, safe, safeAttr), StripUnsafeStyleTagsFilter.Instance, StripElementsWithInlineJavascriptFilter.Instance);
+    }
+
+    public static class HtmlFragmentSanitizeExtension
+    {
+        static HtmlFragment FilterElem(HtmlFragment node, IHtmlFilter filter)
+        {
+            if (node.Implementation is string) {
+                return node;
+            }
+            if (node.Implementation is HtmlFragment[] fragments) {
+                return fragments.ArraySelect(child => FilterElem(child, filter)).AsFragment(); //don't copy comments, processing instructions, etc.
+            }
+            if (node.Implementation is IHtmlElement elem) {
+                var safety = filter.AllowTag(elem);
+                var elemChildren = elem.Contents().NodesOfFragment();
+                var safeChildren = elemChildren.Select(childnode => FilterElem(childnode, filter)).Where(frag => !frag.IsEmpty).ToArray();
+                if (safety == TagSafety.ShouldRemoveButKeepContent) {
+                    return safeChildren.AsFragment();
+                }
+                if (safety == TagSafety.SafeToKeep) {
+                    return HtmlElementAlterations.ReplaceAttributesAndContents(
+                        elem,
+                        HtmlAttributes.FromArray(elem.Attributes.Where(filter.AllowAttribute).ToArray()),
+                        safeChildren
+                    ).AsFragment();
+                }
+            }
+
+            return HtmlFragment.Empty; //unsafe or empty
+        }
+
+        /// <summary>This function sanitizes an html tree.
+        ///  - Any html that isn't recognized as html is considered content (e.g. a lone ampersand)
+        ///  - Any html that can't be parsed (say, an unclosed element) is stripped.
+        ///  - Any html that can be parsed but is explicitly considered "safe" list removed.
+        ///  - Html that can be parsed and is explicitly on the "safe" list is retained.
+        ///  
+        ///  Be aware that the filters may receive a mixture of upper and/or lowercase tags, depending on the source!
+        /// 
+        /// For practical purposes, there is a HashSet based version that simply filters on element name.
+        /// </summary>
+        /// <param name="sourceHtml">The xhtml to sanitize.  The root element is ignored.</param>
+        /// <returns>The parsed xhtml fragments without non-validating or unsafe tags.</returns>
+        public static HtmlFragment Sanitize(this HtmlFragment sourceHtml)
+            => sourceHtml.Sanitize(HtmlFilters.Default);
+
+        /// <summary>This function sanitizes an html tree.
+        ///  - Any html that isn't recognized as html is considered content (e.g. a lone ampersand)
+        ///  - Any html that can't be parsed (say, an unclosed element) is stripped.
+        ///  - Any html that can be parsed but is explicitly considered "safe" list removed.
+        ///  - Html that can be parsed and is explicitly on the "safe" list is retained.
+        ///  
+        ///  Be aware that the filters may receive a mixture of upper and/or lowercase tags, depending on the source!
+        /// 
+        /// For practical purposes, there is a HashSet based version that simply filters on element name.
+        /// </summary>
+        /// <param name="sourceHtml">The xhtml to sanitize.  The root element is ignored.</param>
+        /// <param name="filter">The filter to apply to elements (called for each element in the source).</param>
+        /// <returns>The parsed xhtml fragments without non-validating or unsafe tags.</returns>
+        public static HtmlFragment Sanitize(this HtmlFragment sourceHtml, IHtmlFilter filter)
+            => FilterElem(sourceHtml, filter ?? HtmlFilters.Default);
+    }
+}
---
+++ test/ProgressOnderwijsUtils.Tests/HtmlSanitizerTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Xml.Linq;
+using ExpressionToCodeLib;
+using ProgressOnderwijsUtils.Html;
+using Xunit;
+
+namespace ProgressOnderwijsUtils.Tests
+{
+    public sealed class HtmlSanitizerTest
+    {
+        const string sample =
+            @"<p><b>HTML sanitization</b> is the process of examining an HTML document and producing a new HTML document that preserves only whatever tags are designated ""safe"". HTML sanitization can be used to protect against <a href=""http://yadayada.com/wiki/Cross-site_scripting"" title=""Cross-site scripting"">cross-site scripting</a> attacks by sanitizing any HTML code submitted by a user.</p> 
+<p><br /></p> ";
+
+        [Fact]
+        public void TextLimitWorks()
+        {
+            var sampleParsed = HtmlFragment.Parse(sample);
+            var tidiedSample = sampleParsed.SerializeToStringWithoutDoctype();
+            PAssert.That(() => StringUtils.LevenshteinDistanceScaled(sample, tidiedSample) < 0.05);
+            var lastLength = tidiedSample.Length + 15; //15 chars fudge-factor due to xhtml vs. html serialization differences. not ideal.
+            for (var i = tidiedSample.Length + 14; i >= 0; i--) {
+                var limitedVer = sampleParsed.LimitLength(i).SerializeToStringWithoutDoctype();
+                PAssert.That(() => limitedVer.Length <= i);
+                if (limitedVer.Length < i) { // if more than "needed" trimmed then:
+                    PAssert.That(() => lastLength == i + 1 || lastLength == limitedVer.Length);
+                    //either the string was already this short or it was just trimmed due to real need - but it wasn't unnecessarily trimmed!
+                }
+                lastLength = limitedVer.Length;
+            }
+        }
+
+        [Fact]
+        public void LimitIsIgnoredIfNull()
+        {
+            var limitedVer = HtmlFragment.Parse(sample).LimitLength(null).SerializeToStringWithoutDoctype();
+            var tidiedSample = HtmlFragment.Parse(sample).SerializeToStringWithoutDoctype();
+            PAssert.That(() => limitedVer == tidiedSample);
+        }
+
+        const string sample2 =
+            @"<p><script>lalala<b>innerlala</b></script>test this! <!--lalala<p>--> <div class=""lalala"" style=""lalala"">hmm</div> <unknown>include this though</unknown></p>";
+
+        [Fact]
+        public void CleanerWorks()
+        {
+            var tidiedSample = HtmlFragment.Parse(sample2).Sanitize().SerializeToStringWithoutDoctype();
+            PAssert.That(() => !tidiedSample.Contains("lalala") && !tidiedSample.Contains("script") && !tidiedSample.Contains("innerlala"));
+            PAssert.That(() => !tidiedSample.Contains("class") && !tidiedSample.Contains("style") && !tidiedSample.Contains("unknown"));
+            PAssert.That(() => tidiedSample.Contains("include this") && tidiedSample.Contains("test this")
+                && tidiedSample.Contains("<div>") && tidiedSample.StartsWith("<p>", StringComparison.Ordinal));
+        }
+
+        const string sample3 = "<p>&nbsp;whee</p>";
+        const string sample4 = "<p>1 < 2 && 3 > 0</p>";
+
+        [Fact]
+        public void FixerWorks()
+        {
+            PAssert.That(() => HtmlFragment.Parse(sample3).Sanitize().SerializeToStringWithoutDoctype() == "<p>\u00A0whee</p>");
+            PAssert.That(() => HtmlFragment.Parse(sample4).Sanitize().SerializeToStringWithoutDoctype() == "<p>1 &lt; 2 &amp;&amp; 3 &gt; 0</p>");
+        }
+
+        [Fact]
+        public void SupportsUnicode()
+            => PAssert.That(() => HtmlFragment.Parse("ÂÅÉéé").Sanitize().SerializeToStringWithoutDoctype() == "ÂÅÉéé");
+
+        [Fact]
+        public void DontMisinterpretEncodedXml()
+        {
+            var xEl = new XElement("b", sample3);
+            var xElEncoded = xEl.ToString(SaveOptions.None);
+
+            PAssert.That(() => HtmlFragment.Parse("<p>" + xElEncoded + "</p>").Sanitize().SerializeToStringWithoutDoctype() == "<p>" + xElEncoded + "</p>");
+        }
+
+        [Fact]
+        public void DecodeUnnecessaryEntities()
+            => PAssert.That(() => HtmlFragment.Parse("<p>&Acirc;&Eacute;&eacute;&amp;<br /></p>").Sanitize().SerializeToStringWithoutDoctype() == "<p>ÂÉé&amp;<br></p>");
+
+        [Fact]
+        public void RemovesUnknownElements()
+            => PAssert.That(() => HtmlFragment.Parse("This <p>paragraph contains <unknown>nonsense</unknown></p>.").Sanitize().SerializeToStringWithoutDoctype() == "This <p>paragraph contains nonsense</p>.");
+
+        [Fact]
+        public void RemoveStyleWithDisallowedValues()
+            => PAssert.That(() => HtmlFragment.Parse(@"This is not <span style=""display: none"">invisible</span>.").Sanitize().SerializeToStringWithoutDoctype() == "This is not <span>invisible</span>.");
+
+        [Fact]
+        public void KeepStyleWithAllowedValues()
+            => PAssert.That(() => HtmlFragment.Parse(@"<div style=""margin-left: 20px"">this style is kept</div>.").Sanitize().SerializeToStringWithoutDoctype() == @"<div style=""margin-left: 20px"">this style is kept</div>.");
+
+        [Fact]
+        public void RemoveNonHttpHrefs()
+            => PAssert.That(() => HtmlFragment.Parse(@"This link is <a href=""javascript:alert('noxss')"">not a link</a>.").Sanitize().SerializeToStringWithoutDoctype() == "This link is not a link.");
+
+        [Fact]
+        public void HttpHrefsAreAllowed()
+            => PAssert.That(() => HtmlFragment.Parse(@"This is <a href=""https://somelink.com/?saynomore=1"">a link</a>.").Sanitize().SerializeToStringWithoutDoctype() == @"This is <a href=""https://somelink.com/?saynomore=1"">a link</a>.");
+
+        [Fact]
+        public void AllowsMarginStyleTags()
+            => PAssert.That(
+                () => HtmlFragment.Parse(@"This <p style=""margin-left: 40px;"">is indented</p>!").Sanitize().SerializeToStringWithoutDoctype() == @"This <p style=""margin-left: 40px;"">is indented</p>!");
+    }
+}
