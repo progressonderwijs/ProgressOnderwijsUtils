ed7cacd26e384367ebb3edb518a646093c06253f
Eamon Nerbonne <eamon@nerbonne.org>
2019-05-10 11:36:40 +0200

    Convert to existing recursive matcher

--- src/ProgressOnderwijsUtils/Extensions/ExceptionExtensions.cs
+++ src/ProgressOnderwijsUtils/Extensions/ExceptionExtensions.cs
@@ -26,29 +26,27 @@ namespace ProgressOnderwijsUtils
             => e.AnyNestingLevelMatches(ex => ex is SqlException sqlE && sqlE.Number == -2);
 
         public static bool IsRetriableConnectionFailure([CanBeNull] this Exception e)
-        {
-            if (e == null) {
-                return false;
-            } else if (e is SqlException sqlE) {
-                //sqlE.Number docs at https://msdn.microsoft.com/en-us/library/cc645611.aspx
-                //see also system error codes: https://msdn.microsoft.com/en-us/library/windows/desktop/ms681382
-                const int timeoutExpired = -2;
-                const int failedToEstablishConnection = 53;
-                const int deadlockVictim = 1205;
-                return sqlE.Number == timeoutExpired
-                    || sqlE.Number == failedToEstablishConnection
-                    || sqlE.Number == deadlockVictim
-                    || e.Message.StartsWith("A transport-level error has occurred when receiving results from the server.", StringComparison.Ordinal) //number 121 and possibly others
-                    || e.Message.StartsWith("A transport-level error has occurred when sending the request to the server.", StringComparison.Ordinal); //number 121 and possibly others
-            } else if (e is DBConcurrencyException) {
-                return e.Message.StartsWith("Concurrency violation:", StringComparison.Ordinal);
-            } else if (e is DataException) {
-                return e.Message == "The underlying provider failed on Open.";
-            } else if (e is AggregateException aggEx) {
-                return aggEx.Flatten().InnerExceptions.DefaultIfEmpty().All(IsRetriableConnectionFailure);
-            } else {
-                return e.InnerException.IsRetriableConnectionFailure();
-            }
-        }
+            => e.AnyNestingLevelMatches(ex => {
+                if (e == null) {
+                    return false;
+                } else if (e is SqlException sqlE) {
+                    //sqlE.Number docs at https://msdn.microsoft.com/en-us/library/cc645611.aspx
+                    //see also system error codes: https://msdn.microsoft.com/en-us/library/windows/desktop/ms681382
+                    const int timeoutExpired = -2;
+                    const int failedToEstablishConnection = 53;
+                    const int deadlockVictim = 1205;
+                    return sqlE.Number == timeoutExpired
+                        || sqlE.Number == failedToEstablishConnection
+                        || sqlE.Number == deadlockVictim
+                        || e.Message.StartsWith("A transport-level error has occurred when receiving results from the server.", StringComparison.Ordinal) //number 121 and possibly others
+                        || e.Message.StartsWith("A transport-level error has occurred when sending the request to the server.", StringComparison.Ordinal); //number 121 and possibly others
+                } else if (e is DBConcurrencyException) {
+                    return e.Message.StartsWith("Concurrency violation:", StringComparison.Ordinal);
+                } else if (e is DataException) {
+                    return e.Message == "The underlying provider failed on Open.";
+                } else {
+                    return false;
+                }
+            });
     }
 }
