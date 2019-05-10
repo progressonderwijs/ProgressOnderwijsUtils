61ea0ff86b39c2586fb4275077bc5f182b08fe43
Eamon Nerbonne <eamon@nerbonne.org>
2019-05-10 11:26:40 +0200

    Import ExceptionExtensions from progress

---
+++ src/ProgressOnderwijsUtils/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgressOnderwijsUtils.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static bool AnyNestingLevelMatches(this Exception exception, Func<Exception, bool> predicate)
+        {
+            if (predicate(exception)) {
+                return true;
+            } else if (exception is AggregateException aggEx) {
+                return aggEx.InnerExceptions.Count > 0 && aggEx.InnerExceptions.All(child => AnyNestingLevelMatches(child, predicate));
+            } else {
+                return exception.InnerException is Exception child && AnyNestingLevelMatches(child, predicate);
+            }
+        }
+    }
+}
