5ff4a1ae59f258f95ab42e77c14e67a5e187269b
Eamon Nerbonne <eamon@nerbonne.org>
2019-05-10 11:44:31 +0200

    extract helper method function

--- src/ProgressOnderwijsUtils/Extensions/ExceptionExtensions.cs
+++ src/ProgressOnderwijsUtils/Extensions/ExceptionExtensions.cs
@@ -28,18 +28,10 @@ namespace ProgressOnderwijsUtils
             => e.AnyNestingLevelMatches(ex => ex is SqlException sqlE && sqlE.Number == -2);
 
         public static bool IsRetriableConnectionFailure([CanBeNull] this Exception e)
-            => e.AnyNestingLevelMatches(ex => {
+        {
+            return e.AnyNestingLevelMatches(ex => {
                 if (e is SqlException sqlE) {
-                    //sqlE.Number docs at https://msdn.microsoft.com/en-us/library/cc645611.aspx
-                    //see also system error codes: https://msdn.microsoft.com/en-us/library/windows/desktop/ms681382
-                    const int timeoutExpired = -2;
-                    const int failedToEstablishConnection = 53;
-                    const int deadlockVictim = 1205;
-                    return sqlE.Number == timeoutExpired
-                        || sqlE.Number == failedToEstablishConnection
-                        || sqlE.Number == deadlockVictim
-                        || e.Message.StartsWith("A transport-level error has occurred when receiving results from the server.", StringComparison.Ordinal) //number 121 and possibly others
-                        || e.Message.StartsWith("A transport-level error has occurred when sending the request to the server.", StringComparison.Ordinal); //number 121 and possibly others
+                    return IsRetriableSqlException(sqlE);
                 } else if (e is DBConcurrencyException) {
                     return e.Message.StartsWith("Concurrency violation:", StringComparison.Ordinal);
                 } else if (e is DataException) {
@@ -48,5 +40,19 @@ namespace ProgressOnderwijsUtils
                     return false;
                 }
             });
+        }
+
+        static bool IsRetriableSqlException(SqlException sqlException)
+        { //sqlE.Number docs at https://msdn.microsoft.com/en-us/library/cc645611.aspx
+            //see also system error codes: https://msdn.microsoft.com/en-us/library/windows/desktop/ms681382
+            const int timeoutExpired = -2;
+            const int failedToEstablishConnection = 53;
+            const int deadlockVictim = 1205;
+            return sqlException.Number == timeoutExpired
+                || sqlException.Number == failedToEstablishConnection
+                || sqlException.Number == deadlockVictim
+                || sqlException.Message.StartsWith("A transport-level error has occurred when receiving results from the server.", StringComparison.Ordinal) //number 121 and possibly others
+                || sqlException.Message.StartsWith("A transport-level error has occurred when sending the request to the server.", StringComparison.Ordinal); //number 121 and possibly others
+        }
     }
 }
