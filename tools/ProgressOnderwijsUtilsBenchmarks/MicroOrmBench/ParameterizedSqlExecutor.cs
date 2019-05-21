167ee89cd60fad4f908aecaec0bd2ccb872fd8b7
Eamon Nerbonne <eamon@nerbonne.org>
2019-05-21 14:04:03 +0200

    Fix body style in file

--- tools/ProgressOnderwijsUtilsBenchmarks/MicroOrmBench/ParameterizedSqlExecutor.cs
+++ tools/ProgressOnderwijsUtilsBenchmarks/MicroOrmBench/ParameterizedSqlExecutor.cs
@@ -8,40 +8,29 @@ namespace ProgressOnderwijsUtilsBenchmarks.MicroOrmBench
     static class ParameterizedSqlExecutor
     {
         public static void RunQuery([NotNull] Benchmarker benchmarker)
-        {
-            benchmarker.BenchSqlServer("ParameterizedSql",
-                    (ctx, rows) => ExampleObject.ParameterizedSqlForRows(rows)
-                        .ReadMetaObjects<ExampleObject>(ctx)
-                        .Length)
-                ;
-        }
+            => benchmarker.BenchSqlServer("ParameterizedSql",
+                (ctx, rows) => ExampleObject.ParameterizedSqlForRows(rows)
+                    .ReadMetaObjects<ExampleObject>(ctx)
+                    .Length);
 
         public static void RunTvpQuery([NotNull] Benchmarker benchmarker)
-        {
-            benchmarker.BenchSqlServer("ParameterizedSql-TVP",
-                    (ctx, rows) =>
-                        SQL($"select QueryTableValue from ({Enumerable.Range(0, rows).ToArray()}) x")
-                            .ReadPlain<int>(ctx)
-                            .Length)
-                ;
-        }
+            => benchmarker.BenchSqlServer("ParameterizedSql-TVP",
+                (ctx, rows) =>
+                    SQL($"select QueryTableValue from ({Enumerable.Range(0, rows).ToArray()}) x")
+                        .ReadPlain<int>(ctx)
+                        .Length);
 
         public static void RunWideQuery([NotNull] Benchmarker benchmarker)
-        {
-            benchmarker.BenchSqlServer("ParameterizedSql (26-col)",
-                    (ctx, rows) => WideExampleObject.ParameterizedSqlForRows(rows)
-                        .ReadMetaObjects<WideExampleObject>(ctx)
-                        .Length)
-                ;
-        }
+            => benchmarker.BenchSqlServer("ParameterizedSql (26-col)",
+                (ctx, rows) => WideExampleObject.ParameterizedSqlForRows(rows)
+                    .ReadMetaObjects<WideExampleObject>(ctx)
+                    .Length);
 
         public static void ConstructWithoutExecuting([NotNull] Benchmarker benchmarker)
-        {
-            benchmarker.BenchSqlServer("ParameterizedSql noexec",
+            => benchmarker.BenchSqlServer("ParameterizedSql noexec",
                 (ctx, rows) => {
                     ExampleObject.ParameterizedSqlForRows(rows).CreateSqlCommand(ctx).Dispose();
                     return 0;
                 });
-        }
     }
 }
