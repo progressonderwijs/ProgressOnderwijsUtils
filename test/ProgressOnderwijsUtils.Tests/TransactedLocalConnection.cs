1e47bfff4410737363c0fd7b671bd1b6b1680028
Eamon Nerbonne <eamon@nerbonne.org>
2019-06-12 09:35:33 +0200

    Use the new connection context

--- test/ProgressOnderwijsUtils.Tests/TransactedLocalConnection.cs
+++ test/ProgressOnderwijsUtils.Tests/TransactedLocalConnection.cs
@@ -10,7 +10,11 @@ namespace ProgressOnderwijsUtils.Tests
 
         public TransactedLocalConnection()
         {
-            Context = new SqlCommandCreationContext(new SqlConnection(Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? @"Server = (localdb)\MSSQLLocalDB; Integrated Security = true"), 60, SqlCommandTracer.CreateAlwaysOffTracer(SqlTracerAgumentInclusion.IncludingArgumentValues));
+            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? @"Server = (localdb)\MSSQLLocalDB; Integrated Security = true";
+
+            var sqlCommandTracer = SqlCommandTracer.CreateAlwaysOffTracer(SqlTracerAgumentInclusion.IncludingArgumentValues);
+            var sqlConnection = new SqlConnection(connectionString) { Site = new SqlConnectionContext(60, sqlCommandTracer) };
+            Context = new SqlCommandCreationContext(sqlConnection, 60, sqlCommandTracer);
             try {
                 Context.Connection.Open();
                 ParameterizedSql.TableValuedTypeDefinitionScripts.ExecuteNonQuery(Context);
