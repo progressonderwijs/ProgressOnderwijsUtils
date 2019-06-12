92b0e6ebe4a7ace9d68e849548a4922678593acd
Eamon Nerbonne <eamon@nerbonne.org>
2019-06-12 14:44:55 +0200

    Cleanup

--- test/ProgressOnderwijsUtils.Tests/UtilsTest.cs
+++ test/ProgressOnderwijsUtils.Tests/UtilsTest.cs
@@ -137,7 +137,7 @@ namespace ProgressOnderwijsUtils.Tests
         [Fact]
         public void TimeoutDetectionAbortsWithInconclusiveAfterTimeout()
         {
-            var slowQueryWithTimeout = ParameterizedSqlObjectMapper.OfNonQuery(SQL($"WAITFOR DELAY '00:00:02'")).WithTimeout(BatchTimeout.AbsoluteSeconds(1));
+            var slowQueryWithTimeout = SQL($"WAITFOR DELAY '00:00:02'").OfNonQuery().WithTimeout(BatchTimeout.AbsoluteSeconds(1));
             using (var localdb = new TransactedLocalConnection()) {
                 var ex = Assert.ThrowsAny<Exception>(() => slowQueryWithTimeout.Execute(localdb.Connection));
                 PAssert.That(() => ex.IsRetriableConnectionFailure());
