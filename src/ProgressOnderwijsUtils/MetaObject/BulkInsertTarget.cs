66c05a4ef53ead89067d1b410e176382445aa656
Eamon Nerbonne <eamon@nerbonne.org>
2019-06-14 20:21:17 +0200

    Use optional args, despite reservations

--- src/ProgressOnderwijsUtils/MetaObject/BulkInsertTarget.cs
+++ src/ProgressOnderwijsUtils/MetaObject/BulkInsertTarget.cs
@@ -62,7 +62,7 @@ namespace ProgressOnderwijsUtils
             }
         }
 
-        public void BulkInsert(SqlConnection sqlConn, DbDataReader dbDataReader, string sourceNameForTracing, CommandTimeout timeout)
+        public void BulkInsert(SqlConnection sqlConn, DbDataReader dbDataReader, string sourceNameForTracing, CommandTimeout timeout = default)
             => BulkInsertImplementation.Execute(sqlConn, TableName, Columns, Mode, Options, timeout, dbDataReader, sourceNameForTracing);
     }
 }
