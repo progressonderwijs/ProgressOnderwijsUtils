2ecad27447347421da0291b3ebbcdc6bd7364bcb
Robin <robin.scheper@gmail.com>
2019-07-03 09:58:35 +0200

    fix derp stukje code

--- src/ProgressOnderwijsUtils/Html/HtmlFragment.cs
+++ src/ProgressOnderwijsUtils/Html/HtmlFragment.cs
@@ -51,7 +51,7 @@ namespace ProgressOnderwijsUtils.Html
         }
 
         public bool IsEmpty
-            => Implementation == null || Implementation is string implementation && !string.IsNullOrEmpty(implementation);
+            => Implementation == null || Implementation is string implementation && string.IsNullOrEmpty(implementation);
 
         HtmlFragment(object content)
             => Implementation = content;
