4f9e3961cc12e4208cb6b4936af37ab27abc8cfa
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2019-07-30 16:58:45 +0200

    Nullability annotations TypeExtension

--- src/ProgressOnderwijsUtils/Extensions/TypeExtension.cs
+++ src/ProgressOnderwijsUtils/Extensions/TypeExtension.cs
@@ -1,6 +1,6 @@
-#nullable disable
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using ExpressionToCodeLib;
 using JetBrains.Annotations;
@@ -13,16 +13,14 @@ namespace ProgressOnderwijsUtils
         /// If type is Nullable&lt;T&gt;, returns typeof(T).  For non-Nullable&lt;&gt; types, returns null;
         /// </summary>
         [Pure]
-        [CanBeNull]
-        public static Type IfNullableGetNonNullableType([NotNull] this Type type)
+        public static Type? IfNullableGetNonNullableType(this Type type)
             => type.IsNullableValueType() ? type.GetGenericArguments()[0] : null;
 
         /// <summary>
         /// If type is Nullable&lt;T&gt;, returns typeof(T).  For non-Nullable&lt;&gt; types, returns the type itself - this might also be a reference type, so the resulting type may still permit the value null.
         /// </summary>
         [Pure]
-        [NotNull]
-        public static Type GetNonNullableType([NotNull] this Type type)
+        public static Type GetNonNullableType(this Type type)
             => type.IsNullableValueType() ? type.GetGenericArguments()[0] : type;
 
         /// <summary>
@@ -32,9 +30,8 @@ namespace ProgressOnderwijsUtils
         /// e.g. typeof(string) => typeof(string)
         /// 
         /// </summary>
-        [NotNull]
         [Pure]
-        public static Type GetNonNullableUnderlyingType([NotNull] this Type type)
+        public static Type GetNonNullableUnderlyingType(this Type type)
         {
             var nonNullableType = type.GetNonNullableType();
             if (nonNullableType.IsEnum) {
@@ -48,7 +45,7 @@ namespace ProgressOnderwijsUtils
         /// Nullability is unaltered; Non-enum types are unaltered.
         /// </summary>
         [Pure]
-        public static Type GetUnderlyingType([NotNull] this Type type)
+        public static Type GetUnderlyingType(this Type type)
         {
             var maybeNonNullable = type.IfNullableGetNonNullableType();
             if (!(maybeNonNullable ?? type).IsEnum) {
@@ -56,24 +53,23 @@ namespace ProgressOnderwijsUtils
             } else if (maybeNonNullable == null) {
                 return type.GetEnumUnderlyingType();
             } else {
-                return maybeNonNullable.GetEnumUnderlyingType().MakeNullableType();
+                return maybeNonNullable.GetEnumUnderlyingType().MakeNullableType()! /*not null, since previous cases handle reference types and nullable value types*/;
             }
         }
 
         [Pure]
-        public static bool CanBeNull([NotNull] this Type type)
+        public static bool CanBeNull(this Type type)
             => !type.IsValueType || IsNullableValueType(type);
 
         [Pure]
-        public static bool IsNullableValueType([NotNull] this Type type)
+        public static bool IsNullableValueType(this Type type)
             => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
 
         /// <summary>
         /// If type is non-Nullable value type T, returns typeof(Nullable&lt;T&gt;).  For Nullable&lt;&gt; or reference types, returns null;
         /// </summary>
-        [CanBeNull]
         [Pure]
-        public static Type MakeNullableType([NotNull] this Type type)
+        public static Type? MakeNullableType(this Type type)
             => type.CanBeNull() ? null : typeof(Nullable<>).MakeGenericType(type);
 
         [ItemNotNull]
@@ -90,14 +86,11 @@ namespace ProgressOnderwijsUtils
             }
         }
 
-        [NotNull]
         [Pure]
-        public static string GetNonGenericName([NotNull] this Type type)
+        public static string GetNonGenericName(this Type type)
         {
-            var typename = type.FullName;
-            // ReSharper disable PossibleNullReferenceException
+            var typename = type.FullName ?? throw new ArgumentException("Only types with full names allowed.");
             var backtickIdx = typename.IndexOf('`');
-            // ReSharper restore PossibleNullReferenceException
             return backtickIdx == -1 ? typename : typename.Substring(0, backtickIdx);
         }
 
@@ -106,12 +99,13 @@ namespace ProgressOnderwijsUtils
             => type.ToCSharpFriendlyTypeName();
 
         [Pure]
-        public static T Attr<T>([NotNull] this MemberInfo mi)
+        [return: MaybeNull]
+        public static T Attr<T>(this MemberInfo mi)
             where T : Attribute
         {
             var customAttributes = mi.GetCustomAttributes(typeof(T), true);
             if (customAttributes.Length == 0) {
-                return null;
+                return null! /*return: MaybeNull*/;
             } else if (customAttributes.Length > 1) {
                 throw new InvalidOperationException("Expected zero or one " + typeof(T) + ", found " + customAttributes.Length);
             } else {
