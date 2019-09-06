63c5b32a22b1a064bf3790c43da8eb6a94a5d8fe
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2019-09-06 22:55:44 +0200

    preview9

--- install.ps1
+++ install.ps1
@@ -1,2 +1,2 @@
 ﻿Invoke-WebRequest -Uri 'https://dot.net/v1/dotnet-install.ps1' -UseBasicParsing -OutFile "$env:temp\dotnet-install.ps1"
-& $env:temp\dotnet-install.ps1 -Architecture x64 -Version '3.0.100-preview8-013656' -InstallDir "$env:ProgramFiles\dotnet"
+& $env:temp\dotnet-install.ps1 -Architecture x64 -Version '3.0.100-preview9-014004' -InstallDir "$env:ProgramFiles\dotnet"
