1d3cf23e32539020526e7780fea0fcdc03d97796
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2019-10-17 15:36:46 +0200

    Add some helpers to deal with nullable reference types

---
+++ test/ProgressOnderwijsUtils.Tests/NullableReferenceTypesHelpersTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace ProgressOnderwijsUtils.Tests
+{
+    public sealed class NullableReferenceTypesHelpersTest
+    {
+        [Fact]
+        public void AssertNotNull_throws_when_argument_is_null()
+        {
+            Assert.ThrowsAny<Exception>(() => default(object).AssertNotNull());
+        }
+
+        [Fact]
+        public void AssertNotNull_doesnt_throw_when_argument_is_not_null()
+        {
+            var unused = new object().AssertNotNull();
+        }
+    }
+}
