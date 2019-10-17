e71b127759b5a0f2a9af03e12b741d8d7f02fcfb
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2019-10-17 15:44:46 +0200

    Add test for PretendNullable

--- test/ProgressOnderwijsUtils.Tests/NullableReferenceTypesHelpersTest.cs
+++ test/ProgressOnderwijsUtils.Tests/NullableReferenceTypesHelpersTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ExpressionToCodeLib;
 using Xunit;
 
 namespace ProgressOnderwijsUtils.Tests
@@ -15,6 +16,13 @@ namespace ProgressOnderwijsUtils.Tests
         public void AssertNotNull_doesnt_throw_when_argument_is_not_null()
         {
             var unused = new object().AssertNotNull();
+        }
+
+        [Fact]
+        public void PretendNullable_does_nothing()
+        {
+            var obj = new object();
+            PAssert.That(() => obj.PretendNullable() == obj);
         }
     }
 }
