7814a3224c263f7aceae43591413bd100169d5cd
Eamon Nerbonne <eamon@nerbonne.org>
2019-10-25 15:44:00 +0200

    Add test that number of calls is not dependant on filter-function output.

--- test/ProgressOnderwijsUtils.Tests/TreeWhereTest.cs
+++ test/ProgressOnderwijsUtils.Tests/TreeWhereTest.cs
@@ -25,5 +25,29 @@ namespace ProgressOnderwijsUtils.Tests
             var expected = Tree.Node("1", Tree.Node("2"), Tree.Node("3"));
             PAssert.That(() => expected.Equals(whereNumeric));
         }
+
+        [Fact]
+        public void NastySideEffectsHappenInConsistentOrder()
+        {
+            var tree = Tree.Node("1", Tree.Node("2", Tree.Node("x")), Tree.Node("3"), Tree.Node("y", Tree.Node("4")));
+            var preorder = tree.PreorderTraversal().Select(n => n.NodeValue).ToArray();
+            var expectedOrder = "4x23y1".Select(c => c.ToString());
+            var orderOfWhereTrueCalls = new List<string>();
+            var orderOfWhereFalseCalls = new List<string>();
+
+            var unused = tree.Where(n => {
+                orderOfWhereTrueCalls.Add(n.NodeValue);
+                return true;
+            });
+            unused = tree.Where(n => {
+                orderOfWhereFalseCalls.Add(n.NodeValue);
+                return false;
+            });
+
+            PAssert.That(() => preorder.SetEqual(orderOfWhereFalseCalls));
+            PAssert.That(() => preorder.SetEqual(orderOfWhereTrueCalls));
+            PAssert.That(() => expectedOrder.SequenceEqual(orderOfWhereFalseCalls));
+            PAssert.That(() => expectedOrder.SequenceEqual(orderOfWhereTrueCalls));
+        }
     }
 }
