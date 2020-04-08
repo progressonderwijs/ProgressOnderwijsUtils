20790061653f7eebb43afd892ca2a644437a7b30
Eamon Nerbonne <eamon@nerbonne.org>
2020-04-08 20:57:23 +0200

    Workaround craptaculous annotation of IEqualityComparer<>.GetHashCode

--- src/ProgressOnderwijsUtils/Collections/Tree.cs
+++ src/ProgressOnderwijsUtils/Collections/Tree.cs
@@ -205,10 +205,10 @@ namespace ProgressOnderwijsUtils.Collections
                 }
                 // ReSharper restore HeuristicUnreachableCode
                 // ReSharper restore ConditionIsAlwaysTrueOrFalse
-                ulong hash = (uint)ValueComparer.GetHashCode(obj.NodeValue);
+                ulong hash = obj.NodeValue is null ? 0 : (uint)ValueComparer.GetHashCode(obj.NodeValue);
                 ulong offset = 1; //keep offset odd to ensure no bits are lost in scaling.
                 foreach (var node in obj.PreorderTraversal()) {
-                    hash += offset * ((uint)ValueComparer.GetHashCode(node.NodeValue) + ((ulong)node.Children.Count << 32));
+                    hash += offset * ((uint)(node.NodeValue is null ? 0 : ValueComparer.GetHashCode(node.NodeValue!)) + ((ulong)node.Children.Count << 32));
                     offset += 2;
                 }
                 return (int)((uint)(hash >> 32) + (uint)hash);
