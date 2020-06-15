c004ace2e9af97bfd1e12d4085155900480b0fc6
Fons Dijkstra <f.j.dijkstra@progressonderwijs.nl>
2020-06-15 15:04:55 +0200

    Create the POU2001 analyzer to detect Maybe<Maybe<TOk, TError, TError>> types.

---
+++ src/ProgressOnderwijsUtils.Analyzers/MaybeStateAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace ProgressOnderwijsUtils.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class MaybeStateAnalyzer : DiagnosticAnalyzer
+    {
+        public static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            "POU2001",
+            "Maybe may not contain Maybe state",
+            "The maybe-error state may not be ignored.",
+            "Functional",
+            DiagnosticSeverity.Error,
+            true
+        );
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+            => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(
+                Analyze,
+                SyntaxKind.InvocationExpression
+            );
+        }
+
+        static void Analyze(SyntaxNodeAnalysisContext context)
+        {
+            var type = context.SemanticModel.GetTypeInfo(context.Node).Type;
+            if (type is INamedTypeSymbol named
+                && named.IsSymbolOfType(TypeKind.Struct, "Maybe", "Collections", "ProgressOnderwijsUtils")
+                && named.TypeArguments[0].IsSymbolOfType(TypeKind.Struct, "Maybe", "Collections", "ProgressOnderwijsUtils")) {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation(), type));
+            }
+        }
+    }
+}
---
+++ test/ProgressOnderwijsUtils.Analyzers.Tests/MaybeStateAanalyzerTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using ExpressionToCodeLib;
+using Xunit;
+
+namespace ProgressOnderwijsUtils.Analyzers.Tests
+{
+    public sealed class MaybeStateAanalyzerTest
+    {
+        [Fact]
+        public void Maybe_state_should_not_be_another_maybe()
+        {
+            var source = @"
+                using ProgressOnderwijsUtils.Collections;
+                
+                static class C
+                {
+                    public static void Test()
+                    {
+                        Maybe.Ok().AsMaybeWithoutError<string>()
+                            .WhenOk(m => Maybe.Error(""err"").AsMaybeWithoutValue<Unit>())
+                                .AssertError();
+                    }
+                }
+            ";
+
+            var diagnostics = DiagnosticHelper.GetDiagnostics(new MaybeStateAnalyzer(), source);
+            PAssert.That(() => diagnostics.All(diagnostic => diagnostic.Id == MaybeStateAnalyzer.Rule.Id));
+            PAssert.That(() => diagnostics.Single().Location.GetLineSpan().StartLinePosition.Line == 7);
+        }
+    }
+}
