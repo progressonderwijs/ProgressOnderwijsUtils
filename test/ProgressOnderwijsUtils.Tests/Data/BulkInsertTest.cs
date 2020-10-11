24c7dbfe1147141d8fe0f21ce6b25295b9268be6
Eamon Nerbonne <eamon@nerbonne.org>
2020-10-11 14:49:07 +0200

    Add a small-SmallBatchInsertBench benchmark

--- test/ProgressOnderwijsUtils.Tests/Data/BulkInsertTest.cs
+++ test/ProgressOnderwijsUtils.Tests/Data/BulkInsertTest.cs
@@ -8,11 +8,20 @@ using static ProgressOnderwijsUtils.SafeSql;
 
 namespace ProgressOnderwijsUtils.Tests.Data
 {
-    public sealed class BulkInsertTest : TransactedLocalConnection
+    public sealed class BulkInsertTestSampleRow : ValueBase<BulkInsertTestSampleRow>, IWrittenImplicitly, IReadImplicitly
     {
-        BulkInsertTarget CreateTable(ParameterizedSql tempTableName)
+        public DayOfWeek AnEnum { get; set; }
+        public DateTime? ADateTime { get; set; }
+        public string? SomeString { get; set; }
+        public decimal? LotsOfMoney { get; set; }
+        public double VagueNumber { get; set; }
+        public TrivialValue<string> CustomBla { get; set; }
+        public TrivialValue<string>? CustomBlaThanCanBeNull { get; set; }
+
+        public static BulkInsertTarget CreateTable(SqlConnection sqlConnection, ParameterizedSql tempTableName)
         {
-            SQL($@"
+            SQL(
+                $@"
                 create table {tempTableName} (
                     AnEnum int not null
                     , ADateTime datetime2
@@ -22,24 +31,14 @@ namespace ProgressOnderwijsUtils.Tests.Data
                     , CustomBla nvarchar(max) not null
                     , CustomBlaThanCanBeNull nvarchar(max) null
                 )
-            ").ExecuteNonQuery(Connection);
+            "
+            ).ExecuteNonQuery(sqlConnection);
 
-            return BulkInsertTarget.LoadFromTable(Connection, tempTableName.CommandText());
+            return BulkInsertTarget.LoadFromTable(sqlConnection, tempTableName.CommandText());
         }
 
-        sealed class SampleRow : ValueBase<SampleRow>, IWrittenImplicitly, IReadImplicitly
-        {
-            public DayOfWeek AnEnum { get; set; }
-            public DateTime? ADateTime { get; set; }
-            public string? SomeString { get; set; }
-            public decimal? LotsOfMoney { get; set; }
-            public double VagueNumber { get; set; }
-            public TrivialValue<string> CustomBla { get; set; }
-            public TrivialValue<string>? CustomBlaThanCanBeNull { get; set; }
-        }
-
-        static readonly SampleRow[] SampleData = {
-            new SampleRow {
+        public static readonly BulkInsertTestSampleRow[] SampleData = {
+            new BulkInsertTestSampleRow {
                 ADateTime = new DateTime(2003, 4, 5).AddHours(17.345),
                 AnEnum = DayOfWeek.Saturday,
                 LotsOfMoney = -12.34m,
@@ -47,7 +46,7 @@ namespace ProgressOnderwijsUtils.Tests.Data
                 SomeString = "sdf",
                 CustomBla = TrivialConvertibleValue.Create("aap"),
             },
-            new SampleRow {
+            new BulkInsertTestSampleRow {
                 ADateTime = new DateTime(2013, 8, 7),
                 AnEnum = DayOfWeek.Monday,
                 LotsOfMoney = null,
@@ -55,7 +54,7 @@ namespace ProgressOnderwijsUtils.Tests.Data
                 SomeString = null,
                 CustomBla = TrivialConvertibleValue.Create("aap"),
             },
-            new SampleRow {
+            new BulkInsertTestSampleRow {
                 ADateTime = null,
                 AnEnum = (DayOfWeek)12345,
                 LotsOfMoney = 6543,
@@ -63,7 +62,7 @@ namespace ProgressOnderwijsUtils.Tests.Data
                 SomeString = "Hello world!",
                 CustomBla = TrivialConvertibleValue.Create("aap"),
             },
-            new SampleRow {
+            new BulkInsertTestSampleRow {
                 ADateTime = DateTime.MaxValue,
                 AnEnum = DayOfWeek.Friday,
                 LotsOfMoney = 1000_000_000.00m,
@@ -73,36 +72,39 @@ namespace ProgressOnderwijsUtils.Tests.Data
                 CustomBlaThanCanBeNull = TrivialConvertibleValue.Create("noot"),
             }
         };
+    }
 
+    public sealed class BulkInsertTest : TransactedLocalConnection
+    {
         [Fact]
         public void BulkCopysWithConcurrentQueriesCrash()
         {
-            var target = CreateTable(SQL($"#test"));
-            var evilEnumerable = SampleData.Where(o => SQL($"select 1").ReadScalar<int>(Connection) == 1);
+            var target = BulkInsertTestSampleRow.CreateTable(Connection, SQL($"#test"));
+            var evilEnumerable = BulkInsertTestSampleRow.SampleData.Where(o => SQL($"select 1").ReadScalar<int>(Connection) == 1);
             Assert.ThrowsAny<Exception>(() => evilEnumerable.BulkCopyToSqlServer(Connection, target));
         }
 
         [Fact]
         public void BulkInsertAndReadRoundTrips()
         {
-            var target = CreateTable(SQL($"#test"));
-            SampleData.BulkCopyToSqlServer(Connection, target);
-            var fromDb = SQL($"select * from #test").ReadPocos<SampleRow>(Connection);
-            AssertCollectionsEquivalent(SampleData, fromDb);
+            var target = BulkInsertTestSampleRow.CreateTable(Connection, SQL($"#test"));
+            BulkInsertTestSampleRow.SampleData.BulkCopyToSqlServer(Connection, target);
+            var fromDb = SQL($"select * from #test").ReadPocos<BulkInsertTestSampleRow>(Connection);
+            AssertCollectionsEquivalent(BulkInsertTestSampleRow.SampleData, fromDb);
         }
 
         [Fact]
         public void CanInsertDatatable()
         {
-            var target = CreateTable(SQL($"#test"));
-            var target2 = CreateTable(SQL($"#test2"));
-            target.BulkInsert(Connection, SampleData);
+            var target = BulkInsertTestSampleRow.CreateTable(Connection, SQL($"#test"));
+            var target2 = BulkInsertTestSampleRow.CreateTable(Connection, SQL($"#test2"));
+            target.BulkInsert(Connection, BulkInsertTestSampleRow.SampleData);
 
             var dataTable = SQL($"select * from #test").OfDataTable().Execute(Connection);
             target2.BulkInsert(Connection, dataTable);
 
-            var fromDb = SQL($"select * from #test2").ReadPocos<SampleRow>(Connection);
-            AssertCollectionsEquivalent(SampleData, fromDb);
+            var fromDb = SQL($"select * from #test2").ReadPocos<BulkInsertTestSampleRow>(Connection);
+            AssertCollectionsEquivalent(BulkInsertTestSampleRow.SampleData, fromDb);
         }
 
         sealed class SampleRow2 : ValueBase<SampleRow2>, IWrittenImplicitly, IReadImplicitly
@@ -120,27 +122,31 @@ namespace ProgressOnderwijsUtils.Tests.Data
         {
             using var conn2 = new SqlConnection(ConnectionString);
             conn2.Open();
-            var query = SQL($@"
+            var query = SQL(
+                $@"
                     select *
                     from (
                         values (1, null, 'test', 'test2')
                         , (2, 1, null, 'test3')
                     ) x(intNonNull, intNull, stringNull, stringNonNull)
-                ").OfPocos<SampleRow2>();
+                "
+            ).OfPocos<SampleRow2>();
             var expectedData = new[] {
-                    new SampleRow2 { intNonNull = 1, intNull = null, stringNull = "test", stringNonNull = "test2" },
-                    new SampleRow2 { intNonNull = 2, intNull = 1, stringNull = null, stringNonNull = "test3" },
-                };
+                new SampleRow2 { intNonNull = 1, intNull = null, stringNull = "test", stringNonNull = "test2" },
+                new SampleRow2 { intNonNull = 2, intNull = 1, stringNull = null, stringNonNull = "test3" },
+            };
             AssertCollectionsEquivalent(expectedData, query.Execute(conn2)); //sanity check that we're testing consistent data
 
-            SQL($@"
+            SQL(
+                $@"
                     create table #tmp (
                         intNonNull int not null
                         , intNull int null
                         , stringNull nvarchar(max) null
                         , stringNonNull nvarchar(max) not null
                     )
-                ").ExecuteNonQuery(Connection);
+                "
+            ).ExecuteNonQuery(Connection);
             var target = BulkInsertTarget.LoadFromTable(Connection, "#tmp");
 
             using (var cmd = query.Sql.CreateSqlCommand(conn2, default))
@@ -164,21 +170,24 @@ namespace ProgressOnderwijsUtils.Tests.Data
         [Fact]
         public void EmptyBulkInsertAndReadRoundTrips()
         {
-            var target = CreateTable(SQL($"#test"));
-            SampleData.Take(0).BulkCopyToSqlServer(Connection, target);
-            var fromDb = SQL($"select * from #test").ReadPocos<SampleRow>(Connection);
+            var target = BulkInsertTestSampleRow.CreateTable(Connection, SQL($"#test"));
+            BulkInsertTestSampleRow.SampleData.Take(0).BulkCopyToSqlServer(Connection, target);
+            var fromDb = SQL($"select * from #test").ReadPocos<BulkInsertTestSampleRow>(Connection);
             PAssert.That(() => fromDb.None());
         }
 
         [Fact]
         public void CanCreateDbColumnMetaData()
         {
-            var pocoProperties = PocoProperties<SampleRow>.Instance;
-            var dbProps = pocoProperties.Select(property => DbColumnMetaData.Create(
-                property.Name,
-                property.DataType,
-                property.IsKey,
-                null));
+            var pocoProperties = PocoProperties<BulkInsertTestSampleRow>.Instance;
+            var dbProps = pocoProperties.Select(
+                property => DbColumnMetaData.Create(
+                    property.Name,
+                    property.DataType,
+                    property.IsKey,
+                    null
+                )
+            );
             PAssert.That(() => pocoProperties.Count == dbProps.Count());
         }
     }
---
+++ tools/ProgressOnderwijsUtilsBenchmarks/SmallBatchInsertBench.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+using Microsoft.Data.SqlClient;
+using Perfolizer.Mathematics.OutlierDetection;
+using ProgressOnderwijsUtils;
+using ProgressOnderwijsUtils.Tests.Data;
+using static ProgressOnderwijsUtils.SafeSql;
+
+namespace ProgressOnderwijsUtilsBenchmarks
+{
+    // ReSharper disable once ClassCanBeSealed.Global - BenchmarkDotNet complexity
+    [Config(typeof(Config))]
+    [MemoryDiagnoser]
+    public class SmallBatchInsertBench : IDisposable
+    {
+        sealed class Config : ManualConfig
+        {
+            public Config()
+            {
+                AddJob(
+                    Job.MediumRun
+                        .WithLaunchCount(1)
+                        .WithWarmupCount(3)
+                        .WithOutlierMode(OutlierMode.DontRemove)
+                        .WithMaxIterationCount(5000)
+                        .WithMaxRelativeError(0.002)
+                        .WithToolchain(InProcessNoEmitToolchain.Instance)
+                        .WithId("InProcess")
+                );
+            }
+        }
+
+        readonly SqlConnection sqlConn;
+        readonly BulkInsertTarget cachedTarget;
+        static readonly ParameterizedSql tableName = SQL($"#test");
+
+        public void Dispose()
+            => sqlConn.Dispose();
+
+        static BulkInsertTestSampleRow[] SampleData(int n)
+            => Enumerable.Range(0, (n + 3) / 4).SelectMany(i => BulkInsertTestSampleRow.SampleData).ToArray();
+
+        public SmallBatchInsertBench()
+        {
+            sqlConn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB");
+            sqlConn.Open();
+            ParameterizedSql.TableValuedTypeDefinitionScripts.ExecuteNonQuery(sqlConn);
+            cachedTarget = BulkInsertTestSampleRow.CreateTable(sqlConn, tableName);
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+            => SQL($"truncate table {tableName}").ExecuteNonQuery(sqlConn);
+
+        [Benchmark]
+        public void BulkInsertTarget_LoadFromTable()
+            => BulkInsertTarget.LoadFromTable(sqlConn, tableName.CommandText());
+
+        [Benchmark]
+        public void BulkCopyToSqlServer_cachedTarget_0()
+            => SampleData(0).BulkCopyToSqlServer(sqlConn, cachedTarget);
+
+        [Benchmark]
+        public void BulkCopyToSqlServer_cachedTarget_1()
+            => SampleData(1).BulkCopyToSqlServer(sqlConn, cachedTarget);
+
+        [Benchmark]
+        public void BulkCopyToSqlServer_cachedTarget_9()
+            => SampleData(9).BulkCopyToSqlServer(sqlConn, cachedTarget);
+
+        [Benchmark]
+        public void BulkCopyToSqlServer_cachedTarget_81()
+            => SampleData(81).BulkCopyToSqlServer(sqlConn, cachedTarget);
+
+        [Benchmark]
+        public void BulkCopyToSqlServer_cachedTarget_729()
+            => SampleData(729).BulkCopyToSqlServer(sqlConn, cachedTarget);
+
+        [Benchmark]
+        public void BulkCopyToSqlServer_uncachedTarget_0()
+            => SampleData(0).BulkCopyToSqlServer(sqlConn, BulkInsertTarget.LoadFromTable(sqlConn, tableName.CommandText()));
+
+        [Benchmark]
+        public void BulkCopyToSqlServer_uncachedTarget_1()
+            => SampleData(1).BulkCopyToSqlServer(sqlConn, BulkInsertTarget.LoadFromTable(sqlConn, tableName.CommandText()));
+
+        [Benchmark]
+        public void BulkCopyToSqlServer_uncachedTarget_9()
+            => SampleData(9).BulkCopyToSqlServer(sqlConn, BulkInsertTarget.LoadFromTable(sqlConn, tableName.CommandText()));
+        [Benchmark]
+        public void BulkCopyToSqlServer_uncachedTarget_81()
+            => SampleData(81).BulkCopyToSqlServer(sqlConn, BulkInsertTarget.LoadFromTable(sqlConn, tableName.CommandText()));
+
+        [Benchmark]
+        public void BulkCopyToSqlServer_uncachedTarget_729()
+            => SampleData(729).BulkCopyToSqlServer(sqlConn, BulkInsertTarget.LoadFromTable(sqlConn, tableName.CommandText()));
+    }
+}
