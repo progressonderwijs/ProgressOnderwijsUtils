6b04cd7769a789cc45b800a7182b752c832a6bd8
Leo Leinenga <leo@leinenga.nl>
2021-03-01 19:36:57 +0100

    voeg bitlockercheck toe

---
+++ src/ProgressOnderwijsUtils/Win32/Bitlocker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Microsoft.Windows.Sdk;
+
+namespace ProgressOnderwijsUtils.Win32
+{
+    public static class BitLocker
+    {
+        static void Check(HRESULT result)
+        {
+            if (result.Value != 0) {
+                throw new Exception("HRESULT!=0");
+            }
+        }
+
+        public static unsafe int BitlockerStatusOfDriveLetter(char driveLetter)
+        {
+            Check(PInvoke.SHCreateItemFromParsingName(
+                $"{driveLetter}:",
+                null,
+                Guid.Parse(typeof(IShellItem2).GetCustomAttribute<GuidAttribute>().AssertNotNull().Value),
+                out var ppv
+            ));
+
+            Check(PInvoke.PSGetPropertyKeyFromName("System.Volume.BitLockerProtection", out var key));
+
+            Check(((IShellItem2*)ppv)->GetProperty(key, out var val));
+
+            Check(PInvoke.PropVariantToInt32(val, out var bitLockerStatus));
+
+            return bitLockerStatus;
+        }
+    }
+}
