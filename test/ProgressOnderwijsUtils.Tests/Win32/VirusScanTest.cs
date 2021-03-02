658263e2415456db9df96709863b0501be109b7a
Leo Leinenga <leo@leinenga.nl>
2021-03-02 08:18:06 +0100

    zet test over

---
+++ test/ProgressOnderwijsUtils.Tests/Win32/VirusScanTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using ExpressionToCodeLib;
+using ProgressOnderwijsUtils.Win32;
+using Xunit;
+
+namespace ProgressOnderwijsUtils.Tests.Win32
+{
+    public sealed class VirusScanTest
+    {
+        const string virusString = @"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*";
+        const string schoneString = "Dit is geen virus";
+
+        [Fact]
+        public void VirusAlsString_wordt_herkend()
+            => PAssert.That(() => VirusScan.IsMalware(virusString, "EICAR"));
+
+        [Fact]
+        public void VirusAlsByte_wordt_herkend()
+            => PAssert.That(() => VirusScan.IsMalware(Encoding.ASCII.GetBytes(virusString), "EICAR"));
+
+        [Fact]
+        public void SchoneString_wordt_niet_als_virus_herkend()
+            => PAssert.That(() => !VirusScan.IsMalware(schoneString, "Progress.Net"));
+
+        [Fact]
+        public void SchoneString_als_byte_wordt_niet_als_virus_herkend()
+            => PAssert.That(() => !VirusScan.IsMalware(Encoding.ASCII.GetBytes(schoneString), "Progress.Net"));
+    }
+}
