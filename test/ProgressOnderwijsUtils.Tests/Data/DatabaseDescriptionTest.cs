aa72a78f6d80643cbb8ff922c00d9b294da41b38
Eamon Nerbonne <eamon@nerbonne.org>
2021-03-17 21:04:01 +0100

    Add test

--- test/ProgressOnderwijsUtils.Tests/Data/DatabaseDescriptionTest.cs
+++ test/ProgressOnderwijsUtils.Tests/Data/DatabaseDescriptionTest.cs
@@ -11,24 +11,7 @@ namespace ProgressOnderwijsUtils.Tests.Data
         [Fact]
         public void AllDependantTables_works_recursively()
         {
-            SQL($@"
-                create table dbo.ForeignKeyLookupRoot (
-                    IdRoot int not null primary key
-                );
-
-                create table dbo.ForeignKeyLookupLevel (
-                    IdLevel int not null primary key
-                    , IdRoot int not null foreign key references dbo.ForeignKeyLookupRoot(IdRoot)
-                    , SomeDataWithDefault nvarchar(max) not null default('test')
-                    , SomeDataWithoutDefault nvarchar(max) not null
-                );
-
-                create table dbo.ForeignKeyLookupLeaf (
-                    IdLeaf int not null primary key
-                    , IdLevel int not null foreign key references dbo.ForeignKeyLookupLevel(IdLevel)
-                );
-            ").ExecuteNonQuery(Connection);
-            var db = DatabaseDescription.LoadFromSchemaTables(Connection);
+            var db = CreateSampleData();
 
             var SomeDataWithDefault_metadata = db.GetTableByName("dbo.ForeignKeyLookupLevel").Columns.Single(c => c.ColumnName == "SomeDataWithDefault").ColumnMetaData;
             var SomeDataWithoutDefault_metadata = db.GetTableByName("dbo.ForeignKeyLookupLevel").Columns.Single(c => c.ColumnName == "SomeDataWithoutDefault").ColumnMetaData;
@@ -36,6 +19,46 @@ namespace ProgressOnderwijsUtils.Tests.Data
             PAssert.That(() => SomeDataWithDefault_metadata.HasDefaultValue);
             PAssert.That(() => !SomeDataWithoutDefault_metadata.HasDefaultValue);
         }
+
+        DatabaseDescription CreateSampleData()
+        {
+            SQL(
+                $@"
+                create table dbo.ForeignKeyLookupRoot (
+                    IdRoot int not null primary key
+                );
+
+                create table dbo.ForeignKeyLookupLevel (
+                    IdLevel int not null primary key
+                    , IdRoot int not null constraint LevelToRoot foreign key references dbo.ForeignKeyLookupRoot(IdRoot)
+                    , SomeDataWithDefault nvarchar(max) not null default('test')
+                    , SomeDataWithoutDefault nvarchar(max) not null
+                );
+
+                create table dbo.ForeignKeyLookupLeaf (
+                    IdLeaf int not null primary key
+                    , IdLevel int not null constraint LeafToLevel foreign key references dbo.ForeignKeyLookupLevel(IdLevel) on delete cascade
+                );
+            "
+            ).ExecuteNonQuery(Connection);
+            var db = DatabaseDescription.LoadFromSchemaTables(Connection);
+            return db;
+        }
+
+        [Fact]
+        public void CanFindForeignKeysByUnqualifiedName()
+        {
+            var db = CreateSampleData();
+            var fk1 = db.ForeignKeyConstraintsByUnqualifiedName["LevelToRoot"].Single();
+            PAssert.That(() => fk1.ReferencedParentTable.QualifiedName == "dbo.ForeignKeyLookupRoot");
+            PAssert.That(() => fk1.Columns.Single().ReferencedParentColumn.ColumnName == "IdRoot");
+            PAssert.That(() => fk1.Columns.Single().ReferencingChildColumn.ColumnName == "IdRoot");
+            var fk2 = fk1.ReferencingChildTable.KeysFromReferencingChildren.Single();
+            PAssert.That(() => fk2.ReferencedParentTable.QualifiedName == "dbo.ForeignKeyLookupLevel");
+            PAssert.That(() => fk2.Columns.Single().ReferencedParentColumn.ColumnName == "IdLevel");
+            PAssert.That(() => fk2.Columns.Single().ReferencingChildColumn.ColumnName == "IdLevel");
+        }
+
 
         [Fact]
         public void CheckConstraintWithTable_works()
