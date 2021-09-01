e26cca442a2b0b795e8292aeda57bca19d174670
Eamon Nerbonne <eamon@nerbonne.org>
2021-09-01 15:33:25 +0200

    DateTimeOffset and Time work equivalently.

--- src/ProgressOnderwijsUtils/SchemaReflection/SqlUnderlyingTypeInfo.cs
+++ src/ProgressOnderwijsUtils/SchemaReflection/SqlUnderlyingTypeInfo.cs
@@ -23,7 +23,7 @@ namespace ProgressOnderwijsUtils.SchemaReflection
                 SqlXType.Decimal or SqlXType.Numeric => $"({Precision},{Scale})",
                 SqlXType.NVarChar or SqlXType.NChar => MaxLength == VARCHARMAX_MAXLENGTH_FOR_SQLSERVER ? "(max)" : $"({MaxLength / 2})",
                 SqlXType.VarChar or SqlXType.Char or SqlXType.VarBinary or SqlXType.Binary => MaxLength == VARCHARMAX_MAXLENGTH_FOR_SQLSERVER ? "(max)" : $"({MaxLength})",
-                SqlXType.DateTime2 when Scale != 7 => $"({Scale})",
+                SqlXType.DateTime2 or SqlXType.DateTimeOffset or SqlXType.Time when Scale != 7 => $"({Scale})",
                 _ => ""
             };
 
