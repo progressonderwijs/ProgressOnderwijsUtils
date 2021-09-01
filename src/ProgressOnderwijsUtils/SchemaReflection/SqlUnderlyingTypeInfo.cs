c2b53a19e8d5ee2483adb658ce64f9c9d7c434b6
Eamon Nerbonne <eamon@nerbonne.org>
2021-09-01 15:28:06 +0200

    Convert to switch

--- src/ProgressOnderwijsUtils/SchemaReflection/SqlUnderlyingTypeInfo.cs
+++ src/ProgressOnderwijsUtils/SchemaReflection/SqlUnderlyingTypeInfo.cs
@@ -19,17 +19,12 @@ namespace ProgressOnderwijsUtils.SchemaReflection
         }
 
         string ColumnPrecisionSpecifier()
-        {
-            if (XType == SqlXType.Decimal) {
-                return "(" + Precision + "," + Scale + ")";
-            } else if (XType == SqlXType.NVarChar || XType == SqlXType.NChar) {
-                return MaxLength == VARCHARMAX_MAXLENGTH_FOR_SQLSERVER ? "(max)" : "(" + MaxLength / 2 + ")";
-            } else if (XType == SqlXType.VarChar || XType == SqlXType.Char || XType == SqlXType.VarBinary || XType == SqlXType.Binary) {
-                return MaxLength == VARCHARMAX_MAXLENGTH_FOR_SQLSERVER ? "(max)" : "(" + MaxLength + ")";
-            } else {
-                return "";
-            }
-        }
+            => XType switch {
+                SqlXType.Decimal => "(" + Precision + "," + Scale + ")",
+                SqlXType.NVarChar or SqlXType.NChar => MaxLength == VARCHARMAX_MAXLENGTH_FOR_SQLSERVER ? "(max)" : "(" + MaxLength / 2 + ")",
+                SqlXType.VarChar or SqlXType.Char or SqlXType.VarBinary or SqlXType.Binary => MaxLength == VARCHARMAX_MAXLENGTH_FOR_SQLSERVER ? "(max)" : "(" + MaxLength + ")",
+                _ => ""
+            };
 
         public string ToSqlTypeName()
             => ToSqlTypeNameWithoutNullability() + NullabilityAnnotation();
