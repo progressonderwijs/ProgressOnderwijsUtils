7aeb5d76b8fca26b4fd04f5d77480b3b61e384d7
Eamon Nerbonne <eamon@nerbonne.org>
2021-09-02 11:27:37 +0200

    rename to more conventional name

--- src/ProgressOnderwijsUtils/SchemaReflection/SqlUnderlyingTypeInfo.cs
+++ src/ProgressOnderwijsUtils/SchemaReflection/SqlUnderlyingTypeInfo.cs
@@ -2,16 +2,16 @@ namespace ProgressOnderwijsUtils.SchemaReflection
 {
     public readonly struct SqlTypeInfo
     {
-        public readonly SqlXType XType;
+        public readonly SqlXType UserTypeId;
         public readonly short MaxLength;
         public readonly byte Precision;
         public readonly byte Scale;
         public readonly bool IsNullable;
         public const short VARCHARMAX_MAXLENGTH_FOR_SQLSERVER = -1;
 
-        public SqlTypeInfo(SqlXType xType, short maxLength, byte precision, byte scale, bool isNullable)
+        public SqlTypeInfo(SqlXType userTypeId, short maxLength, byte precision, byte scale, bool isNullable)
         {
-            XType = xType;
+            UserTypeId = userTypeId;
             MaxLength = maxLength;
             Precision = precision;
             Scale = scale;
@@ -19,7 +19,7 @@ namespace ProgressOnderwijsUtils.SchemaReflection
         }
 
         string ColumnPrecisionSpecifier()
-            => XType switch {
+            => UserTypeId switch {
                 SqlXType.Decimal or SqlXType.Numeric => $"({Precision},{Scale})",
                 SqlXType.NVarChar or SqlXType.NChar => MaxLength == VARCHARMAX_MAXLENGTH_FOR_SQLSERVER ? "(max)" : $"({MaxLength / 2})",
                 SqlXType.VarChar or SqlXType.Char or SqlXType.VarBinary or SqlXType.Binary => MaxLength == VARCHARMAX_MAXLENGTH_FOR_SQLSERVER ? "(max)" : $"({MaxLength})",
@@ -31,7 +31,7 @@ namespace ProgressOnderwijsUtils.SchemaReflection
             => ToSqlTypeNameWithoutNullability() + NullabilityAnnotation();
 
         public string ToSqlTypeNameWithoutNullability()
-            => XType.SqlUnderlyingTypeInfo().SqlTypeName + ColumnPrecisionSpecifier();
+            => UserTypeId.SqlUnderlyingTypeInfo().SqlTypeName + ColumnPrecisionSpecifier();
 
         string NullabilityAnnotation()
             => IsNullable ? " null" : " not null";
