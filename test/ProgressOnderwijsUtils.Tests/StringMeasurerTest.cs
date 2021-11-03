9e4242a818dde4718a387405772b79b54256d272
Eamon Nerbonne <eamon@nerbonne.org>
2021-11-03 13:34:08 +0100

    Oh ja, in business scalen we altijd met 0.638, dus dat moet hier ook

--- test/ProgressOnderwijsUtils.Tests/StringMeasurerTest.cs
+++ test/ProgressOnderwijsUtils.Tests/StringMeasurerTest.cs
@@ -8,20 +8,21 @@ namespace ProgressOnderwijsUtils.Tests
         [Fact]
         public void SimpleSanityChecks()
         {
+            var progressScaling = 0.638;
             PAssert.That(
                 () =>
-                    StringMeasurer.Instance.ElideIfNecessary("Dit is een wat langer verhaal, en zal afgebroken worden: en dat gebeurt hier!", 60).result
+                    StringMeasurer.Instance.ElideIfNecessary("Dit is een wat langer verhaal, en zal afgebroken worden: en dat gebeurt hier!", 60 * progressScaling).result
                     == "Dit is een wat langer verhaal, en zal afgebroken worden: en dat gebeurt …"
             );
 
-            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("123456789", 5).result == "123…");
-            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("0,123456789", 5).result == "0,12…");
-            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("1.024", 5).result == "1.024");
-            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("testjes", 5).result == "testj…");
-            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("empty.pdf", 5).result == "emp…");
-            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("wheee!", 5).result == "whe…");
-            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("wheee!\ntwolines!", 5).result == "whe…");
-            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("wheee!\ntwolines!", 10).result == "wheee!\ntw…");
+            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("123456789", 5 * progressScaling).result == "123…");
+            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("0,123456789", 5 * progressScaling).result == "0,12…");
+            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("1.024", 5 * progressScaling).result == "1.024");
+            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("testjes", 5 * progressScaling).result == "testj…");
+            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("empty.pdf", 5 * progressScaling).result == "emp…");
+            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("wheee!", 5 * progressScaling).result == "whe…");
+            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("wheee!\ntwolines!", 5 * progressScaling).result == "whe…");
+            PAssert.That(() => StringMeasurer.Instance.ElideIfNecessary("wheee!\ntwolines!", 10 * progressScaling).result == "wheee!\ntw…");
         }
     }
 }
