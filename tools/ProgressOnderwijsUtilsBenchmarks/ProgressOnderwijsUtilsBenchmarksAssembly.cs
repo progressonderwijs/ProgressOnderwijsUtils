5ef796a419e9ad05671161b5bcd4d48bc82d4851
Eamon Nerbonne <eamon@nerbonne.org>
2021-12-07 13:47:02 +0100

    Pick the most common namespaces as global usings

---
+++ tools/ProgressOnderwijsUtilsBenchmarks/ProgressOnderwijsUtilsBenchmarksAssembly.cs
@@ -0,0 +1,22 @@
+// ReSharper disable RedundantNameQualifier
+
+global using global::JetBrains.Annotations;
+global using global::Microsoft.Data.SqlClient;
+global using global::System;
+global using global::System.Collections.Concurrent;
+global using global::System.Collections.Generic;
+global using global::System.Data;
+global using global::System.Diagnostics;
+global using global::System.Linq;
+global using global::System.Text;
+global using global::System.Threading;
+global using global::System.IO;
+global using global::ProgressOnderwijsUtils.Collections;
+global using static global::ProgressOnderwijsUtils.SafeSql;
+global using global::ProgressOnderwijsUtils.Html;
+global using global::System.Runtime.CompilerServices;
+global using global::System.Threading.Tasks;
+global using global::BenchmarkDotNet.Attributes;
+global using global::BenchmarkDotNet.Configs;
+global using global::BenchmarkDotNet.Jobs;
+global using global::ProgressOnderwijsUtils;
