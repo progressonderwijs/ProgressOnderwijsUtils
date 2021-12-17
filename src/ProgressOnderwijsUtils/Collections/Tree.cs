b49142fca84dc4a1682786c01413241cc1cd4abc
Eamon Nerbonne <eamon@nerbonne.org>
2021-12-17 10:36:26 +0100

    Remove redundant parens

--- src/ProgressOnderwijsUtils/Collections/Tree.cs
+++ src/ProgressOnderwijsUtils/Collections/Tree.cs
@@ -166,7 +166,7 @@ public sealed class Tree<T> : IEquatable<Tree<T>>, IRecursiveStructure<Tree<T>, 
             return "<<TOO DEEP>>";
         }
         var nodeString = (nodeValue?.ToString() ?? "<NULL>").Replace("\n", $"\n{indent}");
-        var childrenString = (Children.Count == 0 ? "." : $":\n{Children.Select(t => t.ToString($"{indent}    ")).JoinStrings("\n")}");
+        var childrenString = Children.Count == 0 ? "." : $":\n{Children.Select(t => t.ToString($"{indent}    ")).JoinStrings("\n")}";
         return $"{indent}{nodeString} {childrenString}";
     }
 
