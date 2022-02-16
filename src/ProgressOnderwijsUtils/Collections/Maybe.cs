cccea4519db54b5085e6530f16cb57003afa068a
Eamon Nerbonne <eamon@nerbonne.org>
2022-02-16 15:21:41 +0100

    Forbid implicit discarding of maybe OK values, but provide a DiscardValue helper instead

--- src/ProgressOnderwijsUtils/Collections/Maybe.cs
+++ src/ProgressOnderwijsUtils/Collections/Maybe.cs
@@ -107,8 +107,8 @@ public readonly struct Maybe<TOk, TError>
     /// Allow discarding the return value. A lot of methods only need to return whether they succeeded, but a lot of sources of maybe return a result
     /// </summary>
     [Pure]
-    public static implicit operator Maybe<Unit, TError>(Maybe<TOk, TError> maybe)
-        => maybe.WhenOk(_ => { });
+    public Maybe<Unit, TError> DiscardValue()
+        => TryGet(out _, out var error) ? Maybe.Ok() : Maybe.Error(error);
 
     public bool TryGet([MaybeNullWhen(false)] out TOk okValueIfOk, [MaybeNullWhen(true)] out TError errorValueIfError)
     {
