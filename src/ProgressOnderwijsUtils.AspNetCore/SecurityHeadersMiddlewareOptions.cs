1f9620e2620695dcafdc959dedad2f21c36403d0
Eamon Nerbonne <eamon@nerbonne.org>
2022-02-18 10:37:51 +0100

    Use a record, so that with syntax becomes available, and make the class instances immutable.

--- src/ProgressOnderwijsUtils.AspNetCore/SecurityHeadersMiddlewareOptions.cs
+++ src/ProgressOnderwijsUtils.AspNetCore/SecurityHeadersMiddlewareOptions.cs
@@ -1,8 +1,8 @@
 namespace ProgressOnderwijsUtils.AspNetCore;
 
-public sealed class SecurityHeadersMiddlewareOptions
+public sealed record SecurityHeadersMiddlewareOptions
 {
-    public string? ContentSecurityPolicy { get; set; } = "object-src 'self'; script-src 'self';";
-    public string? ReferrerPolicy { get; set; } = "same-origin";
-    public string? XContentTypeOptions { get; set; } = "nosniff";
+    public string? ContentSecurityPolicy { get; init; } = "object-src 'self'; script-src 'self';";
+    public string? ReferrerPolicy { get; init; } = "same-origin";
+    public string? XContentTypeOptions { get; init; } = "nosniff";
 }
