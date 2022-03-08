fe4437e5c43fac2764602a54a9ab208931802a4b
Eamon Nerbonne <eamon@nerbonne.org>
2022-03-08 13:26:20 +0100

    Fix warning suppression
    (changed in some R# version; I didn't bother to check which one)

--- test/ProgressOnderwijsUtils.Tests/EnumerableExtensionsTests.cs
+++ test/ProgressOnderwijsUtils.Tests/EnumerableExtensionsTests.cs
@@ -76,7 +76,7 @@ public sealed class EnumerableExtensionsTests
         PAssert.That(() => new[] { 0, 1, 2, }.EmptyIfNull().SequenceEqual(new[] { 0, 1, 2, }));
         PAssert.That(() => default(int[]).EmptyIfNull().SequenceEqual(new int[] { }));
         PAssert.That(() => default(int[]) == null);
-        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
         PAssert.That(() => default(int[]) != default(int[]).EmptyIfNull());
         var arr = new[] { 0, 1, 2, };
         PAssert.That(() => arr.EmptyIfNull() == arr);
