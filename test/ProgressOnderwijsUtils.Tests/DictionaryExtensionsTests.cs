cb80568e3caa97156bb2a3953463aa518df38c98
Eamon Nerbonne <eamon@nerbonne.org>
2022-05-19 16:38:31 +0200

    Test methods actually worth keeping
    without BCL equivalent

--- test/ProgressOnderwijsUtils.Tests/DictionaryExtensionsTests.cs
+++ test/ProgressOnderwijsUtils.Tests/DictionaryExtensionsTests.cs
@@ -3,18 +3,18 @@ namespace ProgressOnderwijsUtils.Tests;
 public sealed class DictionaryExtensionsTests
 {
     [Fact]
-    public void GetDefault()
+    public void GetOrLazyDefault()
     {
         var sut = new Dictionary<int, int> { { 0, 0 }, };
-        PAssert.That(() => sut.GetOrDefault(0, 1) == 0);
-        PAssert.That(() => sut.GetOrDefault(1, 2) == 2);
+        PAssert.That(() => sut.GetOrLazyDefault(0, () => 1) == 0);
+        PAssert.That(() => sut.GetOrLazyDefault(1, () => 2) == 2);
         PAssert.That(() => !sut.ContainsKey(1));
     }
 
     [Fact]
-    public void SetDefault()
+    public void GetOrAdd()
     {
-        IDictionary<int, int> sut = new Dictionary<int, int> { { 0, 0 }, };
+        var sut = new Dictionary<int, int> { { 0, 0 }, };
         PAssert.That(() => sut.GetOrAdd(0, 1) == 0);
         PAssert.That(() => sut.GetOrAdd(1, 2) == 2);
         PAssert.That(() => sut.ContainsKey(1));
