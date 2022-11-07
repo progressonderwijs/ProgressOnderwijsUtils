dca25fd4d8a264431d1002c58207287d37f4a767
MaartjeArnoldus <112384925+MaartjeArnoldus@users.noreply.github.com>
2022-11-07 16:28:43 +0100

    added switch case

--- tools/ProgressOnderwijsUtilsBenchmarks/NullabilityBenchmark.cs
+++ tools/ProgressOnderwijsUtilsBenchmarks/NullabilityBenchmark.cs
@@ -31,6 +31,7 @@ public sealed class NullabilityBenchmark
                     .WithId("InProcess")
             );
     }
+
     readonly SomeClass someClass = new SomeClass();
 
     static string WarnAbout(string field)
@@ -48,9 +49,8 @@ public sealed class NullabilityBenchmark
                         ? WarnAbout(f.Name)
                         : null
                 ).WhereNotNull().JoinStrings();
-        for (var i = 0; i < 300; i++) {
-            _ = CheckValidNonNullablitiy(typeof(SomeClass));
-        }
+
+        _ = CheckValidNonNullablitiy(typeof(SomeClass));
     }
 
     [Benchmark]
@@ -64,27 +64,54 @@ public sealed class NullabilityBenchmark
             ;
     }
 
-    public static Delegate getDelegateOneClass()
-    {
-        var Object = Expression.Parameter(typeof(SomeClass), "Object");
+    static readonly Func<object, string> del = loopAllTypesForDelegate();
 
-        var expBlock = GetExpr(typeof(SomeClass), Object);
-        var ToLambda = Expression.Lambda(expBlock, new[] {Object});
+    [Benchmark]
+    public void Compiled()
+    {
+        var bla = del(someClass);
+        Console.WriteLine(bla);
+    }
+
+    public static Func<object, string> loopAllTypesForDelegate()
+    {
+        var statements = new List<Expression>();
+        var switchstatements = new List<SwitchCase>();
+        var switchValue = Expression.Variable(typeof(Type), "switchValue");
+        var objectParam = Expression.Parameter(typeof(object), "Object");
+        var exceptionVar = Expression.Variable(typeof(string), "exceptionVar");
+        var obj = Expression.Convert(objectParam, typeof(object));
+        statements.Add(Expression.Assign(exceptionVar, Expression.Constant("")));
+        statements.Add(Expression.Assign(switchValue, Expression.Constant(obj.GetType())));
+
+        var types = new[] { typeof(SomeClass) };
+        switchstatements.AddRange(
+            types.Select(
+                t =>
+                    Expression.SwitchCase(Expression.Assign(exceptionVar, Expression.Invoke(Expression.Constant(GetExprBlock(t)), obj)), Expression.Constant(t))
+            )
+        );
+        var switchS = Expression.Switch(switchValue, Expression.Constant(""), switchstatements.ToArray());
+        statements.Add(switchS);
+        statements.Add(exceptionVar);
+        var ToLambda = Expression.Lambda<Func<object, string>>(Expression.Block(new[] { exceptionVar, switchValue }, statements), objectParam);
         return ToLambda.Compile();
     }
 
-    public static BlockExpression GetExpr(Type ObjectType, ParameterExpression ObjectParam)
+    public static Delegate GetExprBlock(Type type)
     {
+        var ObjectParam = Expression.Parameter(typeof(object), "Object");
         NullabilityInfoContext context = new();
         var statements = new List<Expression>();
         var varExcep = Expression.Variable(typeof(string), "exceptionVar");
         statements.Add(Expression.Assign(varExcep, Expression.Constant("")));
+        var obj = Expression.Convert(ObjectParam, type);
 
-        var fields = ObjectType.GetFields();
+        var fields = type.GetFields();
 
         foreach (var field in fields) {
             if (context.Create(field).WriteState == NullabilityState.NotNull) {
-                var memberExpression = Expression.Field(ObjectParam, field.Name);
+                var memberExpression = Expression.Field(obj, field.Name);
                 var fieldValue = Expression.Convert(memberExpression, typeof(object));
                 statements.Add(
                     Expression.IfThen(
@@ -97,7 +124,7 @@ public sealed class NullabilityBenchmark
                                 Expression.Call(
                                     typeof(string).GetMethod(nameof(string.Concat), new[] { typeof(string), typeof(string), typeof(string), typeof(string) }),
                                     Expression.Constant("Found non nullable field in "),
-                                    Expression.Constant(ObjectType.Name),
+                                    Expression.Constant(nameof(obj)),
                                     Expression.Constant("."),
                                     Expression.Constant(field.Name)
                                 )
@@ -108,9 +135,10 @@ public sealed class NullabilityBenchmark
             }
         }
         statements.Add(varExcep);
-        return Expression.Block(new[] { varExcep }, statements);
+        var expBlock = Expression.Block(new[] { varExcep }, statements);
+        var ToLambda = Expression.Lambda(expBlock, ObjectParam);
+        return ToLambda.Compile();
     }
-
 
     class SomeClass
     {
