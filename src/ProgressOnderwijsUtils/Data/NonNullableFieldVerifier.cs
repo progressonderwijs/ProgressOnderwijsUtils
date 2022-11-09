ddd722ea036a876ff106457b0d2a5ab06c8b36be
MaartjeArnoldus <112384925+MaartjeArnoldus@users.noreply.github.com>
2022-11-09 15:23:10 +0100

    use generic type

--- src/ProgressOnderwijsUtils/Data/NonNullableFieldVerifier.cs
+++ src/ProgressOnderwijsUtils/Data/NonNullableFieldVerifier.cs
@@ -6,52 +6,31 @@ using System.Threading.Tasks;
 
 namespace ProgressOnderwijsUtils.Data;
 
-public sealed class NonNullableFieldVerifier
+public static class NonNullableFieldVerifier
 {
-    static readonly Func<object, string> Verifier = InitNonNullableVerifier();
-
-    public static string Verify(object obj)
-        => Verifier(obj);
-
-    public static Func<object, string> InitNonNullableVerifier()
+    public static Func<T, string> MissingRequiredProperties_FuncFactory<T>()
     {
         var statements = new List<Expression>();
-        var switchstatements = new List<SwitchCase>();
-        var switchValue = Expression.Variable(typeof(Type), "switchValue");
-        var objectParam = Expression.Parameter(typeof(object), "obj");
+        var objectParam = Expression.Parameter(typeof(T), "obj");
         var exceptionVar = Expression.Variable(typeof(string), "exceptionVar");
         var obj = Expression.Convert(objectParam, typeof(object));
         statements.Add(Expression.Assign(exceptionVar, Expression.Constant("")));
-        var type = Expression.Call(obj, typeof(object).GetMethod(nameof(GetType)));
-        statements.Add(Expression.Assign(switchValue, type));
-
-        var types = new[] { typeof(NullablityTestClass), typeof(OtherNullablityTestClass) };
-        switchstatements.AddRange(
-            types.Select(
-                t => {
-                    var invokeCall = Expression.Invoke(Expression.Constant(GetNonNullableVerifierForType(t)), Expression.Convert(obj, t));
-                    return Expression.SwitchCase(Expression.Assign(exceptionVar, invokeCall), Expression.Constant(t));
-                }
-            )
-        );
-        var switchS = Expression.Switch(switchValue, Expression.Assign(exceptionVar, Expression.Constant("")), switchstatements.ToArray());
-        statements.Add(switchS);
+        var invokeCall = Expression.Invoke(Expression.Constant(GetNonNullableVerifierForType<T>()), Expression.Convert(obj, typeof(T)));
+        statements.Add(Expression.Assign(exceptionVar, invokeCall));
         statements.Add(exceptionVar);
-        var ToLambda = Expression.Lambda<Func<object, string>>(Expression.Block(new[] { exceptionVar, switchValue }, statements), objectParam);
+        var ToLambda = Expression.Lambda<Func<T, string>>(Expression.Block(new[] { exceptionVar }, statements), objectParam);
         return ToLambda.Compile();
     }
 
-    public static Delegate GetNonNullableVerifierForType(Type type)
+    public static Delegate GetNonNullableVerifierForType<T>()
     {
-        var ObjectParam = Expression.Parameter(typeof(object), "Obj");
+        var ObjectParam = Expression.Parameter(typeof(T), "Obj");
         NullabilityInfoContext context = new();
         var statements = new List<Expression>();
         var varExcep = Expression.Variable(typeof(string), "exceptionVar");
         statements.Add(Expression.Assign(varExcep, Expression.Constant("")));
-        var obj = Expression.Convert(ObjectParam, type);
-
-        var fields = type.GetFields();
-
+        var obj = Expression.Convert(ObjectParam, typeof(T));
+        var fields = typeof(T).GetFields();
         statements.AddRange(
             fields.Where(f => context.Create(f).WriteState == NullabilityState.NotNull).Select(
                 f => {
@@ -64,7 +43,7 @@ public sealed class NonNullableFieldVerifier
                             Expression.Call(
                                 typeof(string).GetMethod(nameof(string.Concat), new[] { typeof(string), typeof(string) }),
                                 varExcep,
-                                Expression.Constant("Found null value in non nullable field in " + type.Name + "." + f.Name + Environment.NewLine)
+                                Expression.Constant("Found null value in non nullable field in " + typeof(T) + "." + f.Name + Environment.NewLine)
                             )
                         )
                     );
@@ -74,7 +53,7 @@ public sealed class NonNullableFieldVerifier
 
         statements.Add(varExcep);
         var expBlock = Expression.Block(new[] { varExcep }, statements);
-        var ToLambda = Expression.Lambda<Func<object, string>>(expBlock, ObjectParam);
+        var ToLambda = Expression.Lambda<Func<T, string>>(expBlock, ObjectParam);
         return ToLambda.Compile();
     }
 }
@@ -87,14 +66,4 @@ public sealed class NullablityTestClass
     public object? SomeNullableObject = null;
     public object[] SomeObjectArray = null;
     public object[] SomeFilledObjectArray = { null };
-}
-
-public sealed class OtherNullablityTestClass
-{
-    public string SomeNullString1 = null;
-    public string? SomeNullableField1 = null;
-    public object SomeObject1 = null;
-    public object? SomeNullableObject1 = null;
-    public object[] SomeObjectArray1 = null;
-    public object[] SomeFilledObjectArray1 = { null };
 }
