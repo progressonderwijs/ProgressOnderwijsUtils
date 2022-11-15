54bbd1e822f055fff6d226042273d8dbe43bd6f4
Eamon Nerbonne <eamon@nerbonne.org>
2022-11-15 16:41:28 +0100

    Remove from this version of the file too

--- test/ProgressOnderwijsUtils.Tests/NonNullableFieldVerifier.cs
+++ test/ProgressOnderwijsUtils.Tests/NonNullableFieldVerifier.cs
@@ -39,13 +39,3 @@ public static class NonNullableFieldVerifier
         return ToLambda.Compile();
     }
 }
-
-public sealed class NullablityTestClass
-{
-    public string SomeNullString = null;
-    public string? SomeNullableField = null;
-    public object SomeObject = null;
-    public object? SomeNullableObject = null;
-    public object[] SomeObjectArray = null;
-    public object[] SomeFilledObjectArray = { null };
-}
