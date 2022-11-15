33c59a807eef967770362099395bdcbc089cad72
Eamon Nerbonne <eamon@nerbonne.org>
2022-11-15 16:37:59 +0100

    Annotate nullability

--- test/ProgressOnderwijsUtils.Tests/NullablityTestClass.cs
+++ test/ProgressOnderwijsUtils.Tests/NullablityTestClass.cs
@@ -2,10 +2,11 @@ namespace ProgressOnderwijsUtils.Tests;
 
 public sealed class NullablityTestClass
 {
-    public string SomeNullString = null;
+    //Intentionally violate nullability assumptions so we can test this:
+    public string SomeNullString = null!;
     public string? SomeNullableField = null;
-    public object SomeObject = null;
+    public object SomeObject = null!;
     public object? SomeNullableObject = null;
-    public object[] SomeObjectArray = null;
-    public object[] SomeFilledObjectArray = { null };
+    public object[] SomeObjectArray = null!;
+    public object[] SomeFilledObjectArray = { null!, };
 }
