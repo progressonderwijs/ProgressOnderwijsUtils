dad7887ff6614afd297d49ddccd26f0c88231d78
Eamon Nerbonne <eamon@nerbonne.org>
2022-11-23 13:13:10 +0100

    We moeten iets met base/subclasses testen

--- test/ProgressOnderwijsUtils.Tests/NullablityTestClass.cs
+++ test/ProgressOnderwijsUtils.Tests/NullablityTestClass.cs
@@ -10,3 +10,16 @@ public sealed class NullablityTestClass
     public object[] SomeObjectArray = null!;
     public object[] SomeFilledObjectArray = { null!, };
 }
+
+public abstract class NullablityTestBaseClass
+{
+    //Intentionally violate nullability assumptions so we can test this:
+    public string SomeNullString = null!;
+    public string? SomeNullableField = null;
+    public object SomeObject = null!;
+    public object? SomeNullableObject = null;
+    public object[] SomeObjectArray = null!;
+    public object[] SomeFilledObjectArray = { null!, };
+}
+
+public sealed class NullabilityTestSubClass : NullablityTestBaseClass { }
