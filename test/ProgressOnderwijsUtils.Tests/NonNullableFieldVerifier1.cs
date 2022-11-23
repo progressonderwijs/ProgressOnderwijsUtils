8b25b5239b5783554548ce0f7a8bacf4d1110a5e
MaartjeArnoldus <112384925+MaartjeArnoldus@users.noreply.github.com>
2022-11-23 13:45:05 +0100

    \n

--- test/ProgressOnderwijsUtils.Tests/NonNullableFieldVerifier1.cs
+++ test/ProgressOnderwijsUtils.Tests/NonNullableFieldVerifier1.cs
@@ -17,10 +17,9 @@ public static class NonNullableFieldVerifier1
         NullabilityInfoContext context = new();
         var concatCall = ((Func<string, string, string>)string.Concat).Method;
 
-        var fields = typeof(T).GetFields();
+        var fields = typeof(T).GetFields().Where(f => context.Create(f).WriteState == NullabilityState.NotNull);
         statements.AddRange(
-            fields.Where(f => context.Create(f).WriteState == NullabilityState.NotNull)
-                .Select(
+            fields.Select(
                     f => {
                         var memberExpression = Expression.Field(objectParam, f);
                         var fieldValue = Expression.Convert(memberExpression, typeof(object));
@@ -31,18 +30,18 @@ public static class NonNullableFieldVerifier1
                                 Expression.Call(
                                     concatCall,
                                     exception,
-                                    Expression.Constant("Found null value in non nullable field in " + typeof(T) + "." + f.Name + Environment.NewLine)
+                                    Expression.Constant("Found null value in non nullable field in " + typeof(T) + "." + f.Name + "\n")
                                 )
                             )
                         );
                     }
                 )
         );
-        
+
         var splitStringCall = x.Method;
 
         var exceptionList = Expression.Variable(typeof(string[]), "exceptionList");
-        var result = Expression.Call(exception, splitStringCall, Expression.Constant(Environment.NewLine), Expression.Constant(StringSplitOptions.RemoveEmptyEntries));
+        var result = Expression.Call(exception, splitStringCall, Expression.Constant("\n"), Expression.Constant(StringSplitOptions.RemoveEmptyEntries));
         statements.Add(Expression.Assign(exceptionList,result));
         statements.Add(Expression.Condition(
             Expression.GreaterThan(
@@ -54,7 +53,7 @@ public static class NonNullableFieldVerifier1
             )
         );
 
-        var ToLambda = Expression.Lambda<Func<T, string[]?>>(Expression.Block(new[] {exception, exceptionList }, statements), objectParam);
+        var ToLambda = Expression.Lambda<Func<T, string[]?>>(Expression.Block(new[] {exception, exceptionList, }, statements), objectParam);
         return ToLambda.Compile();
     }
 }
