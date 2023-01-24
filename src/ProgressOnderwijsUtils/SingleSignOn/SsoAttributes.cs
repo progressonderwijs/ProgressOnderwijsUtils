07922c0ad23ff1e2a5e04e1cc394b3729dbddaa2
Eamon Nerbonne <eamon@nerbonne.org>
2023-01-24 14:21:54 +0100

    Use correct casing

--- src/ProgressOnderwijsUtils/SingleSignOn/SsoAttributes.cs
+++ src/ProgressOnderwijsUtils/SingleSignOn/SsoAttributes.cs
@@ -2,14 +2,14 @@ namespace ProgressOnderwijsUtils.SingleSignOn;
 
 public readonly record struct SsoAttributes()
 {
-    public required string uid { get; init; }
-    public required string? domain { get; init; }
-    public string[] email { get; init; } = Array.Empty<string>();
-    public string[] roles { get; init; } = Array.Empty<string>();
+    public required string Uid { get; init; }
+    public required string? Domain { get; init; }
+    public string[] Email { get; init; } = Array.Empty<string>();
+    public string[] Roles { get; init; } = Array.Empty<string>();
     public string? InResponseTo { get; init; }
     public string? AuthnContextClassRef { get; init; }
     public XElement? RawAssertion { get; init; }
 
     public override string ToString()
-        => $"uid='{uid}'; domain='{domain}'; emails='{StringUtils.ToFlatDebugString(email)}'; roles='{StringUtils.ToFlatDebugString(roles)}'";
+        => $"uid='{Uid}'; domain='{Domain}'; emails='{StringUtils.ToFlatDebugString(Email)}'; roles='{StringUtils.ToFlatDebugString(Roles)}'";
 }
