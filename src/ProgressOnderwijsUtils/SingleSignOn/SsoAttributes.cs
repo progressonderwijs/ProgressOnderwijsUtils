4c2dada1fed536e633c8200463ed490f39b7fa94
Eamon Nerbonne <eamon@nerbonne.org>
2023-01-24 11:57:41 +0100

    Add default values to make nullability accurate

--- src/ProgressOnderwijsUtils/SingleSignOn/SsoAttributes.cs
+++ src/ProgressOnderwijsUtils/SingleSignOn/SsoAttributes.cs
@@ -4,8 +4,8 @@ public readonly record struct SsoAttributes()
 {
     public string uid { get; init; }
     public string? domain { get; init; }
-    public string[] email { get; init; }
-    public string[] roles { get; init; }
+    public string[] email { get; init; } = Array.Empty<string>();
+    public string[] roles { get; init; } = Array.Empty<string>();
     public string? InResponseTo { get; init; }
     public string? AuthnContextClassRef { get; init; }
     public XElement? RawAssertion { get; init; }
