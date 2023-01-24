1752e5d8c0765caae8122788c7391b98da17fe15
Eamon Nerbonne <eamon@nerbonne.org>
2023-01-24 11:35:42 +0100

    To record struct

--- src/ProgressOnderwijsUtils/SingleSignOn/SsoAttributes.cs
+++ src/ProgressOnderwijsUtils/SingleSignOn/SsoAttributes.cs
@@ -1,14 +1,14 @@
 namespace ProgressOnderwijsUtils.SingleSignOn;
 
-public struct SsoAttributes
+public readonly record struct SsoAttributes
 {
-    public string uid;
-    public string? domain;
-    public string[] email;
-    public string[] roles;
-    public string? InResponseTo;
-    public string? AuthnContextClassRef;
-    public XElement RawAssertion;
+    public string uid { get; init; }
+    public string? domain { get; init; }
+    public string[] email { get; init; }
+    public string[] roles { get; init; }
+    public string? InResponseTo { get; init; }
+    public string? AuthnContextClassRef { get; init; }
+    public XElement? RawAssertion { get; init; }
 
     public override string ToString()
         => $"uid='{uid}'; domain='{domain}'; emails='{StringUtils.ToFlatDebugString(email)}'; roles='{StringUtils.ToFlatDebugString(roles)}'";
