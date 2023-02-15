f6c4ea4fc79adea48c55124dcfb8b0b0573ea0c5
MaartjeArnoldus <112384925+MaartjeArnoldus@users.noreply.github.com>
2023-02-15 11:10:41 +0100

    Add recursive method to count all Invalid null occurrences
    including potential nested objects
    
    note: maybe theres another solution for the StartsWith("System.") check

--- src/ProgressOnderwijsUtils/RequiredFields/NonNullableFieldVerifier5.cs
+++ src/ProgressOnderwijsUtils/RequiredFields/NonNullableFieldVerifier5.cs
@@ -15,20 +15,10 @@ public static class NonNullableFieldVerifier5
 
         NullabilityInfoContext context = new();
 
+        statements.AddRange(CountInvalidNullOccurrences(typeof(T),objectParam,ErrorCounter));
+
         var fields = typeof(T).GetFields(BindingFlags.NonPublic|BindingFlags.Public |BindingFlags.Instance).Where(f => context.Create(f).WriteState == NullabilityState.NotNull);
 
-        var variables = new List<ParameterExpression>();
-        foreach (var f in fields) {
-            var memberExpression = Expression.Field(objectParam, f);
-            var fieldValue = Expression.Convert(memberExpression, typeof(object));
-
-            statements.Add(
-                Expression.IfThen(
-                        Expression.Equal(fieldValue, Expression.Constant(null, typeof(object))),
-                        Expression.AddAssign(ErrorCounter, Expression.Constant(1, typeof(int)))
-                )
-            );
-        }
         var setArray = fields.Select(
             f => Expression.IfThen(
                 Expression.Equal(Expression.Convert(Expression.Field(objectParam, f), typeof(object)), Expression.Constant(null, typeof(object))),
@@ -54,9 +44,35 @@ public static class NonNullableFieldVerifier5
         );
         statements.Add(exception);
 
+        var variables = new List<ParameterExpression>();
         variables.AddRange(new[] { exception, ErrorCounter, });
 
         var ToLambda = Expression.Lambda<Func<T, string[]?>>(Expression.Block(variables, statements), objectParam);
         return ToLambda.Compile();
+    }
+
+    static List<Expression> CountInvalidNullOccurrences(Type type, Expression obj, Expression nullCounter)
+    {
+        NullabilityInfoContext context = new();
+
+        var statements = new List<Expression>();
+        var fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance).Where(f => context.Create(f).WriteState == NullabilityState.NotNull);
+
+        foreach (var f in fields) {
+            var memberExpression = Expression.Field(obj, f);
+            var fieldValue = Expression.Convert(memberExpression, typeof(object));
+
+            statements.Add(
+                Expression.IfThen(
+                    Expression.Equal(fieldValue, Expression.Constant(null, typeof(object))),
+                    Expression.AddAssign(nullCounter, Expression.Constant(1, typeof(int)))
+                )
+            );
+            if (f.FieldType.IsClass && !f.FieldType.FullName.AssertNotNull().StartsWith("System.", StringComparison.Ordinal)) {
+                statements.AddRange(CountInvalidNullOccurrences(f.FieldType, memberExpression, nullCounter));
+            }
+        }
+
+        return statements;
     }
 }
