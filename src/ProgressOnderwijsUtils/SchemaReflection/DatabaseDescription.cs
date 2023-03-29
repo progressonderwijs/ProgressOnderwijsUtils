b6ca601a9ad85dbc3de999f38619deb32c8d3643
Eamon Nerbonne <eamon@nerbonne.org>
2023-03-29 09:05:31 +0200

    use property, not var

--- src/ProgressOnderwijsUtils/SchemaReflection/DatabaseDescription.cs
+++ src/ProgressOnderwijsUtils/SchemaReflection/DatabaseDescription.cs
@@ -13,7 +13,6 @@ public sealed class DatabaseDescription
     public DatabaseDescription(RawDatabaseDescription rawDescription)
     {
         var referencedIdByReferencingId = rawDescription.Dependencies.ToLookup(dep => dep.referencing_id, dep => dep.referenced_id);
-        var columnsInOrderByObjectId = rawDescription.Columns.ToGroupedDictionary(col => col.DbObjectId, (_, cols) => cols.Order().ToArray());
 
         Sequences = rawDescription.Sequences.ToDictionary(s => s.QualifiedName, StringComparer.OrdinalIgnoreCase);
 
@@ -22,11 +21,11 @@ public sealed class DatabaseDescription
             ComputedColumns = rawDescription.ComputedColumnDefinitions.ToDictionary(o => (o.ObjectId, o.ColumnId)),
             CheckConstraints = rawDescription.CheckConstraints.ToGroupedDictionary(o => o.TableObjectId, (_, g) => g.ToArray()),
             Triggers = rawDescription.DmlTableTriggers.ToGroupedDictionary(o => o.TableObjectId, (_, g) => g.ToArray()),
-            Columns = columnsInOrderByObjectId,
+            Columns = rawDescription.Columns.ToGroupedDictionary(col => col.DbObjectId, (_, cols) => cols.Order().ToArray()),
         };
 
-        tableById = rawDescription.Tables.ToDictionary(o => o.ObjectId, o => new Table(this, o, columnsInOrderByObjectId.GetValueOrDefault(o.ObjectId).EmptyIfNull(), dataByTableId));
-        viewById = rawDescription.Views.ToDictionary(o => o.ObjectId, o => new View(o, columnsInOrderByObjectId.GetValueOrDefault(o.ObjectId).EmptyIfNull(), referencedIdByReferencingId[o.ObjectId].Select(dep => tableById.GetValueOrDefault(dep)).WhereNotNull().ToArray()));
+        tableById = rawDescription.Tables.ToDictionary(o => o.ObjectId, o => new Table(this, o, dataByTableId.Columns.GetValueOrDefault(o.ObjectId).EmptyIfNull(), dataByTableId));
+        viewById = rawDescription.Views.ToDictionary(o => o.ObjectId, o => new View(o, dataByTableId.Columns.GetValueOrDefault(o.ObjectId).EmptyIfNull(), referencedIdByReferencingId[o.ObjectId].Select(dep => tableById.GetValueOrDefault(dep)).WhereNotNull().ToArray()));
         var fkObjects = rawDescription.ForeignKeys.ArraySelect(o => new ForeignKey(o, tableById));
         fksByReferencedParentObjectId = fkObjects.ToLookup(fk => fk.ReferencedParentTable.ObjectId);
         fksByReferencingChildObjectId = fkObjects.ToLookup(fk => fk.ReferencingChildTable.ObjectId);
