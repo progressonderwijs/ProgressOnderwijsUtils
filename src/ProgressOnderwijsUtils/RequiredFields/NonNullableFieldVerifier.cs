c83cb4291ecdc7fe3c513d9121bee4d15b34e093
Eamon Nerbonne <eamon@nerbonne.org>
2023-03-31 10:58:23 +0200

    inline lambda; rename

--- src/ProgressOnderwijsUtils/RequiredFields/NonNullableFieldVerifier.cs
+++ src/ProgressOnderwijsUtils/RequiredFields/NonNullableFieldVerifier.cs
@@ -50,17 +50,15 @@ public static class NonNullableFieldVerifier
                     }
                 )
             );
-            Func<FieldInfo, BlockExpression> hmm = field => {
-                var propName = AutoPropertyOfFieldOrNull(field) is { } prop ? prop.Name : field.Name;
-                var exceptionMessage = typeof(T).ToCSharpFriendlyTypeName() + "." + propName + " contains NULL despite being non-nullable";
-                var onNullDetected = Expression.Block(
-                    Expression.Assign(Expression.ArrayAccess(exceptionVar, errorCounterVar), Expression.Constant(exceptionMessage)),
+
+            var setArray = ForEachInvalidNull((Func<FieldInfo, BlockExpression>)(field => {
+                var memberName = AutoPropertyOfFieldOrNull(field) is { } autoProp ? autoProp.Name : field.Name;
+                var exceptionMessage1 = typeof(T).ToCSharpFriendlyTypeName() + "." + memberName + " contains NULL despite being non-nullable";
+                return Expression.Block(
+                    Expression.Assign(Expression.ArrayAccess(exceptionVar, errorCounterVar), Expression.Constant(exceptionMessage1)),
                     incrementErrorCounter
                 );
-                return onNullDetected;
-            };
-
-            var setArray = ForEachInvalidNull(hmm);
+            }));
             var falseState = Expression.Block(
                 typeof(string[]),
                 new[] { exceptionVar, },
