996b63c1a16748b934b410e0ab858d2807f3595a
Fons Dijkstra <f.j.dijkstra@progressonderwijs.nl>
2023-06-05 13:10:12 +0200

    Validate that an aborted JSON-writer writes invalid JSON. Thus that is will not try to finish the JSON written as good as possible, and a client will notice the abortion.

--- test/ProgressOnderwijsUtils.Tests/Data/ReadJsonTest.cs
+++ test/ProgressOnderwijsUtils.Tests/Data/ReadJsonTest.cs
@@ -1,9 +1,43 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
 namespace ProgressOnderwijsUtils.Tests.Data;
 
 public sealed class ReadJsonTest : TransactedLocalConnection
 {
+    [Fact]
+    public void Utf8JosonWriter_writes_invalid_json_when_aborted()
+    {
+        using var stream = new MemoryStream();
+        {
+            using var writer = new Utf8JsonWriter(stream);
+            writer.WriteStartArray();
+            writer.WriteStartObject();
+            writer.WriteString("property", "testje");
+        }
+
+        var json = Maybe.Try(() => JsonNode.Parse(Encoding.UTF8.GetString(stream.ToArray()))).Catch<Exception>();
+        PAssert.That(() => json.AssertError().Message.Contains("json", StringComparison.OrdinalIgnoreCase));
+    }
+
+    [Fact]
+    public void Utf8JosonWriter_writes_invalid_json_upon_exception()
+    {
+        using var stream = new MemoryStream();
+        try
+        {
+            using var writer = new Utf8JsonWriter(stream);
+            writer.WriteStartArray();
+            writer.WriteStartObject();
+            writer.WriteString("property", "testje");
+            throw new NotSupportedException();
+        } catch (NotSupportedException) {}
+
+        var json = Maybe.Try(() => JsonNode.Parse(Encoding.UTF8.GetString(stream.ToArray()))).Catch<Exception>();
+        PAssert.That(() => json.AssertError().Message.Contains("json", StringComparison.OrdinalIgnoreCase));
+    }
+
     [Fact]
     public async Task ToDo()
     {
