fd7b0f837a035365ea7f7c9de9befe77af516ec5
PC2022B\degroot <h.j.de.groot@progressonderwijs.nl>
2023-06-13 12:03:17 +0200

    with default collation, before nullability

--- src/ProgressOnderwijsUtils/SchemaReflection/IDbColumn.cs
+++ src/ProgressOnderwijsUtils/SchemaReflection/IDbColumn.cs
@@ -33,7 +33,10 @@ public static class DbColumnExtensions
         => ParameterizedSql.CreateDynamic($"{column.ColumnName} {column.ToSqlTypeName()}");
 
     public static string ToSqlTypeName(this IDbColumn column)
-        => column.ToSqlTypeNameWithoutNullability() + column.NullabilityAnnotation() + (column.ColumnMetaData.CollationName == null ? "" : $" collate {column.ColumnMetaData.CollationName}");
+        => column.ToSqlTypeNameWithoutNullability() + CollationForStringColumn(column) + column.NullabilityAnnotation();
+
+    static string CollationForStringColumn(IDbColumn column)
+        => column.IsString ? $" collate {column.ColumnMetaData.CollationName ?? "SQL_Latin1_General_CP1_CI_AS"}" : "";
 
     public static string ToSqlTypeNameWithoutNullability(this IDbColumn column)
         => column.UserTypeId.SqlUnderlyingTypeInfo().SqlTypeName + column.ColumnPrecisionSpecifier();
