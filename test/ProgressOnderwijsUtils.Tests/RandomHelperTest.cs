dcbd5e8e6a0ba17f1b4061768ece0d35a819b41a
Eamon Nerbonne <eamon@nerbonne.org>
2023-08-29 16:01:24 +0200

    rename variables

--- test/ProgressOnderwijsUtils.Tests/RandomHelperTest.cs
+++ test/ProgressOnderwijsUtils.Tests/RandomHelperTest.cs
@@ -24,9 +24,9 @@ public sealed class RandomHelperTest
     [Fact]
     public void CheckBasic_UInt8()
     {
-        var uint64s = Iter10K().Select(i => RandomHelper.Secure.GetByte());
-        PAssert.That(() => uint64s.Any(num => num > byte.MaxValue / 4 * 3));
-        PAssert.That(() => uint64s.Any(num => num < byte.MaxValue / 4));
+        var bytes = Iter10K().Select(i => RandomHelper.Secure.GetByte());
+        PAssert.That(() => bytes.Any(num => num > byte.MaxValue / 4 * 3));
+        PAssert.That(() => bytes.Any(num => num < byte.MaxValue / 4));
     }
 
     [Fact]
@@ -40,10 +40,10 @@ public sealed class RandomHelperTest
     [Fact]
     public void CheckBasic_NonNegativeInt32()
     {
-        var uint32s = Iter10K().Select(i => RandomHelper.Secure.GetNonNegativeInt32());
-        PAssert.That(() => uint32s.Any(num => num > int.MaxValue / 4 * 3));
-        PAssert.That(() => uint32s.Any(num => num < int.MaxValue / 4));
-        PAssert.That(() => uint32s.None(num => num < 0));
+        var int32s = Iter10K().Select(i => RandomHelper.Secure.GetNonNegativeInt32());
+        PAssert.That(() => int32s.Any(num => num > int.MaxValue / 4 * 3));
+        PAssert.That(() => int32s.Any(num => num < int.MaxValue / 4));
+        PAssert.That(() => int32s.None(num => num < 0));
     }
 
     [Fact]
@@ -65,9 +65,9 @@ public sealed class RandomHelperTest
     [Fact]
     public void CheckBasic_Int32()
     {
-        var int64s = Iter10K().Select(i => RandomHelper.Secure.GetInt32());
-        PAssert.That(() => int64s.Any(num => num > int.MaxValue / 4 * 3));
-        PAssert.That(() => int64s.Any(num => num < int.MinValue / 4 * 3));
+        var int32s = Iter10K().Select(i => RandomHelper.Secure.GetInt32());
+        PAssert.That(() => int32s.Any(num => num > int.MaxValue / 4 * 3));
+        PAssert.That(() => int32s.Any(num => num < int.MinValue / 4 * 3));
     }
 
     [Fact]
