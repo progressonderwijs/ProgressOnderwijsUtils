8cd662412afe0154e33219869e8f2032a995c618
Eamon Nerbonne <eamon@nerbonne.org>
2023-11-21 09:05:06 +0100

    Also, report these error codes more like most windows messages do: in hexadecimal.  Easier to google.

--- src/ProgressOnderwijsUtils/Win32/Win32Extentions.cs
+++ src/ProgressOnderwijsUtils/Win32/Win32Extentions.cs
@@ -9,7 +9,7 @@ public static class Win32Extentions
     internal static void AssertResultOk(this HRESULT result, string attemptingAction, string context)
     {
         if (result.Value is not (0 or INPLACE_S_TRUNCATED)) {
-            throw new($"HRESULT failure ({result.Value}) while attempting {attemptingAction}\nContext:{context}");
+            throw new($"HRESULT failure (0x{result.Value:x}) while attempting {attemptingAction}\nContext:{context}");
         }
     }
 }
