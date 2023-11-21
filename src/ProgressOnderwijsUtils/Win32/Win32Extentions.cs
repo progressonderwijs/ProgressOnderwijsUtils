7d7679831f4ba60830027621ae638a4d9be3ab0f
Eamon Nerbonne <eamon@nerbonne.org>
2023-11-21 09:02:32 +0100

    Fix bitlocker detection issue in which INPLACE_S_TRUNCATED was incorrectly interpreted as an error.

--- src/ProgressOnderwijsUtils/Win32/Win32Extentions.cs
+++ src/ProgressOnderwijsUtils/Win32/Win32Extentions.cs
@@ -4,9 +4,11 @@ namespace ProgressOnderwijsUtils.Win32;
 
 public static class Win32Extentions
 {
+    const int INPLACE_S_TRUNCATED = 0x401a0;
+
     internal static void AssertResultOk(this HRESULT result, string attemptingAction, string context)
     {
-        if (result.Value != 0) {
+        if (result.Value is not (0 or INPLACE_S_TRUNCATED)) {
             throw new($"HRESULT failure ({result.Value}) while attempting {attemptingAction}\nContext:{context}");
         }
     }
