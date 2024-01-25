a37c1eb29c30b3728d3857a1f0ea8b1026046023
Eamon Nerbonne <eamon@nerbonne.org>
2024-01-25 16:52:27 +0100

    Use discard

--- test/ProgressOnderwijsUtils.Tests/Data/TableValuedParameterTest.cs
+++ test/ProgressOnderwijsUtils.Tests/Data/TableValuedParameterTest.cs
@@ -34,7 +34,7 @@ public sealed class TableValuedParameterTest : TransactedLocalConnection
     {
         var nullStringReturningQuery = SQL($@"select cast(null as nvarchar(max))");
 
-        var unused = nullStringReturningQuery.ReadScalar<string>(Connection); //assert query OK.
+        _ = nullStringReturningQuery.ReadScalar<string>(Connection);
 
         _ = Assert.ThrowsAny<Exception>(() => nullStringReturningQuery.ReadScalar<TrivialValue<string>>(Connection));
     }
