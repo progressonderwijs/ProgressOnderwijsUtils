9f19bb81e1f9e5068f3e09ffdf884530f706ee75
Eamon Nerbonne <eamon@nerbonne.org>
2024-11-01 16:10:42 +0100

    Add a bunch of overloads

--- src/ProgressOnderwijsUtils/Collections/UnitExtensions.cs
+++ src/ProgressOnderwijsUtils/Collections/UnitExtensions.cs
@@ -8,9 +8,33 @@ public static class UnitExtensions
             return Unit.Value;
         };
 
-    public static Func<T, Unit> ToUnitReturningFunc<T>(this Action<T> action)
-        => x => {
-            action(x);
+    public static Func<TA, Unit> ToUnitReturningFunc<TA>(this Action<TA> action)
+        => a => {
+            action(a);
+            return Unit.Value;
+        };
+
+    public static Func<TA, TB, Unit> ToUnitReturningFunc<TA, TB>(this Action<TA, TB> action)
+        => (a, b) => {
+            action(a, b);
+            return Unit.Value;
+        };
+
+    public static Func<TA, TB, TC, Unit> ToUnitReturningFunc<TA, TB, TC>(this Action<TA, TB, TC> action)
+        => (a, b, c) => {
+            action(a, b, c);
+            return Unit.Value;
+        };
+
+    public static Func<TA, TB, TC, TD, Unit> ToUnitReturningFunc<TA, TB, TC, TD>(this Action<TA, TB, TC, TD> action)
+        => (a, b, c, d) => {
+            action(a, b, c, d);
+            return Unit.Value;
+        };
+
+    public static Func<TA, TB, TC, TD, TE, Unit> ToUnitReturningFunc<TA, TB, TC, TD, TE>(this Action<TA, TB, TC, TD, TE> action)
+        => (a, b, c, d, e) => {
+            action(a, b, c, d, e);
             return Unit.Value;
         };
 }
