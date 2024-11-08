5a330861ca63e56082907ebe1990edc34e0f1fc0
Eamon Nerbonne <eamon@nerbonne.org>
2024-11-08 16:08:44 +0100

    Update test/ProgressOnderwijsUtils.Tests/Data/PocoObjectMapperTest.cs

--- test/ProgressOnderwijsUtils.Tests/Data/PocoObjectMapperTest.cs
+++ test/ProgressOnderwijsUtils.Tests/Data/PocoObjectMapperTest.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Common;
-using static ProgressOnderwijsUtils.Html.HtmlTagKinds;
 
 namespace ProgressOnderwijsUtils.Tests.Data;
 
