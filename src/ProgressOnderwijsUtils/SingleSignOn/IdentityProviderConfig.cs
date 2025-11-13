adf883f88921caa4ec35b919006e5912c37854b3
Patrick Visser <p.visser@progressonderwijs.nl>
2025-11-13 14:11:44 +0100

    Add CheckSignature func

--- src/ProgressOnderwijsUtils/SingleSignOn/IdentityProviderConfig.cs
+++ src/ProgressOnderwijsUtils/SingleSignOn/IdentityProviderConfig.cs
@@ -8,4 +8,5 @@ public struct IdentityProviderConfig
     public string MetaDataQueryParameter;
     public string identity;
     public X509Certificate2 certificate;
+    public required Func<bool> checkSignature;
 }
